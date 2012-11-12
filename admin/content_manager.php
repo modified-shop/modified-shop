651abea3fced0c446e998309183f8057ee95e1fb
DokuMan <DokuMan@modified-shop.org>
2012-11-12 12:24:44 +0000

    remove subheader "xtc tools"

--- admin/content_manager.php
+++ admin/content_manager.php
@@ -273,9 +273,6 @@ require (DIR_WS_INCLUDES.'head.php');
                     <td width="80" rowspan="2"><?php echo xtc_image(DIR_WS_ICONS.'heading_content.gif'); ?></td>
                     <td class="pageHeading"><?php echo HEADING_TITLE;?></td>
                   </tr>
-                  <tr>
-                    <td class="main" valign="top">XTC Tools</td>
-                  </tr>
                 </table>
               </td>
             </tr>
