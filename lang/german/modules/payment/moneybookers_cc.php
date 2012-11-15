7d7f68fa67ac756d48fea813b2075c3b109fdbc8
Tomcraft1980 <hallo@modified-shop.org>
2012-11-15 17:35:23 +0000

    Fixed german Umlauts in /lang/german/modules/payment/moneybookers_cc.php in trunk and SP1e
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@3961 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/payment/moneybookers_cc.php
+++ lang/german/modules/payment/moneybookers_cc.php
@@ -19,7 +19,7 @@ if (_PAYMENT_MONEYBOOKERS_EMAILID=='') {
   $_var.='<br /><br /><b><font color="red">Bitte nehmen Sie zuerst die Einstellungen unter<br /> Erw. Konfiguration -> Partner -> Moneybookers.com vor!</font></b>';
 }
 define('MODULE_PAYMENT_MONEYBOOKERS_CC_TEXT_DESCRIPTION', $_var);
-define('MODULE_PAYMENT_MONEYBOOKERS_CC_NOCURRENCY_ERROR', 'Es ist keine von Moneybookers akzeptierte Whrung installiert!');
+define('MODULE_PAYMENT_MONEYBOOKERS_CC_NOCURRENCY_ERROR', 'Es ist keine von Moneybookers akzeptierte W&auml;hrung installiert!');
 define('MODULE_PAYMENT_MONEYBOOKERS_CC_ERRORTEXT1', 'payment_error=');
 define('MODULE_PAYMENT_MONEYBOOKERS_CC_TEXT_INFO', '');
 define('MODULE_PAYMENT_MONEYBOOKERS_CC_ERRORTEXT2', '&error=Fehler w&auml;hrend Ihrer Bezahlung bei Moneybookers!');
