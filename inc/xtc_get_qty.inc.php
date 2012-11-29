f9500cfc8b74811d612ea737946e4c8b1b04647f
DokuMan <DokuMan@modified-shop.org>
2012-11-29 08:43:19 +0000

    Partly revoke r3825

--- inc/xtc_get_qty.inc.php
+++ inc/xtc_get_qty.inc.php
@@ -10,27 +10,25 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-function xtc_get_qty($products_id)  {
-/*
-    if (strpos($products_id,'{'))  {
-      $act_id=substr($products_id,0,strpos($products_id,'{'));
-    } else {
-      $act_id=$products_id;
-    }
+function xtc_get_qty($products_id) {
 
-    //BOF - Dokuman - 2010-02-26 - set Undefined index
-    //return $_SESSION['actual_content'][$act_id]['qty'];
-    if (isset($_SESSION['actual_content'][$act_id]['qty']))
-      return $_SESSION['actual_content'][$act_id]['qty'];
-    return 0;
-    //EOF - Dokuman - 2010-02-26 - set Undefined index
-*/
   $result = NULL;
   $act_id = strtok($products_id, '{');
 
-  if (array_key_exists('actual_content', $_SESSION) && array_key_exists($act_id, $_SESSION['actual_content'])) {
-    $result = $_SESSION['actual_content'][$act_id]['qty'];
+/*
+  if (strpos($products_id,'{'))  {
+    $act_id=substr($products_id,0,strpos($products_id,'{'));
+  } else {
+    $act_id=$products_id;
   }
+*/
+
+  //BOF - Dokuman - 2010-02-26 - set Undefined index
+  //return $_SESSION['actual_content'][$act_id]['qty'];
+  if (isset($_SESSION['actual_content'][$act_id]['qty'])) {
+    return $_SESSION['actual_content'][$act_id]['qty'];
+  }
+  //EOF - Dokuman - 2010-02-26 - set Undefined index
 
   return $result;
 }
