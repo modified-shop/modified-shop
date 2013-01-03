a52a4443faae36e53d7416c6492f4f172c50f692
Tomcraft1980 <hallo@modified-shop.org>
2013-01-03 14:36:56 +0000

    Added missing files to r3356
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/update_xtcmodified-1.05-SP1d_to_1.05-SP1e@4169 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ admin/shopgate.php
@@ -0,0 +1,3 @@
+<?php
+require_once 'includes/application_top.php';
+include_once DIR_FS_CATALOG.DIR_WS_INCLUDES.'/shopgate/base/admin/shopgate.php';
---
+++ includes/modules/payment/shopgate.php
@@ -0,0 +1,2 @@
+<?php
+include_once 'includes/shopgate/base/includes/modules/payment/shopgate.php';
