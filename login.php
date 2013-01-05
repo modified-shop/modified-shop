0cd53ed329f92fdc79314b59dc1474e955f560d2
Tomcraft1980 <hallo@modified-shop.org>
2013-01-05 16:58:54 +0000

    Added r4184 from SP1e to trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4186 79ac0487-9261-4f26-a34d-858dee0dcce0

--- login.php
+++ login.php
@@ -94,7 +94,9 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
       $_SESSION['cart']->restore_contents();
 
       if (isset($econda) && is_object($econda)) $econda->_loginUser();
-      
+
+      // BOC added request for set review_prod_id and for order_id, noRiddle
+      /*
       // BOF - GTB - 2011-03-21 redirect to last visited Page after login
       //if ($_SESSION['cart']->count_contents() > 0) {
       if (isset($_SESSION['REFERER']) && !empty($_SESSION['REFERER'])) {
@@ -111,6 +113,22 @@ if (isset ($_GET['action']) && ($_GET['action'] == 'process')) {
         xtc_redirect(xtc_href_link(FILENAME_DEFAULT),'NONSSL');
         //EOF - web28 - 2010-08-11 redirect NONSSL
       }
+      */
+      // BOF - GTB - 2011-03-21 redirect to last visited Page after login
+      //if ($_SESSION['cart']->count_contents() > 0  && !isset($_GET['review_prod_id'])  && !isset($_GET['order_id'])) {
+      if (isset($_SESSION['REFERER']) && !empty($_SESSION['REFERER'])) {
+        xtc_redirect(xtc_href_link($_SESSION['REFERER']));
+      }elseif ($_SESSION['cart']->count_contents() > 0  && !isset($_GET['review_prod_id'])  && !isset($_GET['order_id'])) {
+      // EOF - GTB - 2011-03-21 redirect to last visited Page after login
+        xtc_redirect(xtc_href_link(FILENAME_SHOPPING_CART),'NONSSL');
+      } elseif (isset($_GET['review_prod_id'])) {
+        xtc_redirect(xtc_href_link(FILENAME_PRODUCT_REVIEWS_WRITE, xtc_product_link((int)$_GET['review_prod_id'],''), 'NONSSL'));
+      } elseif (isset($_GET['order_id'])) {
+        xtc_redirect(xtc_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' .(int)$_GET['order_id'], 'NONSSL'));
+      } else {         
+        xtc_redirect(xtc_href_link(FILENAME_DEFAULT),'NONSSL');          
+      }
+      // EOC added request for set review_prod_id and for order_id, noRiddle
 
     }
   }
@@ -125,7 +143,18 @@ $smarty->assign('account_option', ACCOUNT_OPTIONS);
 $smarty->assign('BUTTON_NEW_ACCOUNT', '<a href="'.xtc_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL').'">'.xtc_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE).'</a>');
 $smarty->assign('BUTTON_LOGIN', xtc_image_submit('button_login.gif', IMAGE_BUTTON_LOGIN));
 $smarty->assign('BUTTON_GUEST', '<a href="'.xtc_href_link(FILENAME_CREATE_GUEST_ACCOUNT, '', 'SSL').'">'.xtc_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE).'</a>');
-$smarty->assign('FORM_ACTION', xtc_draw_form('login', xtc_href_link(FILENAME_LOGIN, 'action=process', 'SSL')));
+
+// BOC added review_prod_id to be able to redirect to product_reviews_write when coming from reviews button, and order_id to redirect to account_history_info when coming from Link in change_order_mail, noRiddle
+//$smarty->assign('FORM_ACTION', xtc_draw_form('login', xtc_href_link(FILENAME_LOGIN, 'action=process', 'SSL')));
+if(isset($_GET['review_prod_id'])) {
+  $smarty->assign('FORM_ACTION', xtc_draw_form('login', xtc_href_link(FILENAME_LOGIN, 'action=process&review_prod_id='.(int)$_GET['review_prod_id'], 'SSL')));
+} elseif (isset($_GET['order_id'])) {
+  $smarty->assign('FORM_ACTION', xtc_draw_form('login', xtc_href_link(FILENAME_LOGIN, 'action=process&order_id='.(int)$_GET['order_id'], 'SSL')));
+} else {
+  $smarty->assign('FORM_ACTION', xtc_draw_form('login', xtc_href_link(FILENAME_LOGIN, 'action=process', 'SSL')));
+}
+// EOC added review_prod_id and order_id, noRiddle
+
 $smarty->assign('INPUT_MAIL', xtc_draw_input_field('email_address'));
 $smarty->assign('INPUT_PASSWORD', xtc_draw_password_field('password'));
 $smarty->assign('LINK_LOST_PASSWORD', xtc_href_link(FILENAME_PASSWORD_DOUBLE_OPT, '', 'SSL'));
