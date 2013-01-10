230ad932a911fb34fc94194a56ce21b5e5853962
Tomcraft1980 <hallo@modified-shop.org>
2013-01-10 17:21:26 +0000

    Minor update on configure.php files
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4198 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/configure.php
+++ includes/configure.php
@@ -22,9 +22,9 @@
   define('HTTPS_SERVER', 'https://localhost'); // eg, https://localhost - should not be empty for productive servers
   define('ENABLE_SSL', false); // secure webserver for checkout procedure?
   define('USE_SSL_PROXY', false); // using SSL proxy?
-  define('DIR_WS_CATALOG', '/xtc-modified/');
-  define('DIR_FS_DOCUMENT_ROOT', '/web1/xtc-modified/'); // absolute path required
-  define('DIR_FS_CATALOG', '/web1/xtc-modified/'); // absolute path required
+  define('DIR_WS_CATALOG', '/modified-shop/');
+  define('DIR_FS_DOCUMENT_ROOT', '/web1/modified-shop/'); // absolute path required
+  define('DIR_FS_CATALOG', '/web1/modified-shop/'); // absolute path required
   define('DIR_WS_ADMIN', 'admin/');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ORIGINAL_IMAGES', DIR_WS_IMAGES .'product_images/original_images/');
