c54458afa8c24b84cdca1039686ef30d0545c880
modGTB <gerhard@modified-shop.org>
2013-01-11 09:38:42 +0000

    fix undefined index
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/update_xtcmodified-1.05-SP1d_to_1.05-SP1e@4217 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ inc/xtc_get_products_mo_images.inc.php
@@ -0,0 +1,27 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2004 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+   
+	function xtc_get_products_mo_images($products_id = '') {
+   	$mo_query = "select * from " . TABLE_PRODUCTS_IMAGES . " where products_id = '" . $products_id ."' ORDER BY image_nr";
+   	$products_mo_images_query = xtDBquery($mo_query);
+   
+   	$results = array();
+		while ($row = xtc_db_fetch_array($products_mo_images_query,true)) {
+			$results[($row['image_nr']-1)] = $row;
+		}
+		if (sizeof($results)>0) {
+			 return $results;
+		} else {
+			 return false;
+		}
+	}
+?>
