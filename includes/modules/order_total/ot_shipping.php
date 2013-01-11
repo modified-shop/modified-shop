7e5a0f956db047e58ada76d0c482bb4afd108828
modGTB <gerhard@modified-shop.org>
2013-01-11 21:41:00 +0000

    fix undefined index
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/update_xtcmodified-1.05-SP1d_to_1.05-SP1e@4267 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/order_total/ot_shipping.php
+++ includes/modules/order_total/ot_shipping.php
@@ -56,9 +56,11 @@
 
       if (xtc_not_null($order->info['shipping_method'])) {
         if ($_SESSION['customers_status']['customers_status_show_price_tax'] == 1) {
-        // price with tax
-
-          $shipping_tax = xtc_get_tax_rate($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+          // price with tax
+          $shipping_tax = 0;
+          if (array_key_exists($module, $GLOBALS) && is_object($module)) {
+            $shipping_tax = xtc_get_tax_rate($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
+          }
           $shipping_tax_description = xtc_get_tax_description($GLOBALS[$module]->tax_class, $order->delivery['country']['id'], $order->delivery['zone_id']);
           $tax = $xtPrice->xtcFormat(xtc_add_tax($order->info['shipping_cost'], $shipping_tax),false,0,false)-$order->info['shipping_cost'];
           $tax = $xtPrice->xtcFormat($tax,false,0,true);
