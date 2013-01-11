723c3c031c2c000509c21e31bdb144d89b0fb140
web28 <web28@modified-shop.org>
2013-01-11 15:39:16 +0000

    fix smarty 2_6.27 html_encoding to be PHP5.4 ready
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/update_xtcmodified-1.05-SP1d_to_1.05-SP1e@4254 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/classes/Smarty_2.6.27/Smarty_Compiler.class.php
+++ includes/classes/Smarty_2.6.27/Smarty_Compiler.class.php
@@ -292,7 +292,8 @@ class Smarty_Compiler extends Smarty {
                         $text_blocks[$curr_tb] = str_replace('%%%SMARTYSP'.$curr_sp.'%%%', '<?php echo \''.str_replace("'", "\'", $sp_match[1][$curr_sp]).'\'; ?>'."\n", $text_blocks[$curr_tb]);
                     } else if ($this->php_handling == SMARTY_PHP_QUOTE) {
                         /* quote php tags */
-                        $text_blocks[$curr_tb] = str_replace('%%%SMARTYSP'.$curr_sp.'%%%', htmlspecialchars($sp_match[1][$curr_sp]), $text_blocks[$curr_tb]);
+                        //$text_blocks[$curr_tb] = str_replace('%%%SMARTYSP'.$curr_sp.'%%%', htmlspecialchars($sp_match[1][$curr_sp]), $text_blocks[$curr_tb]);
+                        $text_blocks[$curr_tb] = str_replace('%%%SMARTYSP'.$curr_sp.'%%%', encode_htmlspecialchars($sp_match[1][$curr_sp]), $text_blocks[$curr_tb]); // web28 2013-01-11 - use encode_htmlentities (PHP5.4 ready)
                     } else if ($this->php_handling == SMARTY_PHP_REMOVE) {
                         /* remove php tags */
                         $text_blocks[$curr_tb] = str_replace('%%%SMARTYSP'.$curr_sp.'%%%', '', $text_blocks[$curr_tb]);
