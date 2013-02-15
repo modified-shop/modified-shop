ae35ab97641a63309cde6faac2bb234daeff014a
Tomcraft <Tomcraft@modified-shop.org>
2013-02-15 11:02:49 +0000

    Added missing english language definition for BOX_CONFIGURATION_40

--- lang/english/admin/english.php
+++ lang/english/admin/english.php
@@ -104,6 +104,7 @@ define('BOX_CONFIGURATION_18', 'VAT Reg No');
 define('BOX_CONFIGURATION_19', 'Partner');
 define('BOX_CONFIGURATION_22', 'Search-Options');
 define('BOX_CONFIGURATION_24', 'PIWIK &amp; Google Analytics');
+define('BOX_CONFIGURATION_40', 'Popup Window Options');
 define('BOX_CONFIGURATION_1000', 'My Admin');
 
 define('BOX_MODULES', 'Payment-/Shipping-/Billing-Modules');
