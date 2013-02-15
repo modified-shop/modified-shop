a5b3d5ebf2fee338b79965176cbabcca9ffe1572
Tomcraft <Tomcraft@modified-shop.org>
2013-02-15 21:52:53 +0000

    Added info text for "cod" and "moneyorder" payment modules in /tags/modified-shop-1.06 and trunk

--- lang/english/modules/payment/cod.php
+++ lang/english/modules/payment/cod.php
@@ -18,7 +18,7 @@
 define('MODULE_PAYMENT_TYPE_PERMISSION', 'cod');
 define('MODULE_PAYMENT_COD_TEXT_TITLE', 'Cash on Delivery');
 define('MODULE_PAYMENT_COD_TEXT_DESCRIPTION', 'Cash on Delivery');
-define('MODULE_PAYMENT_COD_TEXT_INFO','');
+define('MODULE_PAYMENT_COD_TEXT_INFO','We ship your order after receipt of payment.');
 define('MODULE_PAYMENT_COD_ZONE_TITLE' , 'Payment Zone');
 define('MODULE_PAYMENT_COD_ZONE_DESC' , 'If a zone is selected, only enable this payment method for that zone.');
 define('MODULE_PAYMENT_COD_ALLOWED_TITLE' , 'Allowed Zones');
