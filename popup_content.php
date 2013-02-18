4d274be22f2ac87b8c723974ebbf1653fef32f2e
Tomcraft1980 <hallo@modified-shop.org>
2013-02-18 13:43:00 +0000

    Fixed popup_content.php
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4483 79ac0487-9261-4f26-a34d-858dee0dcce0

--- popup_content.php
+++ popup_content.php
@@ -23,7 +23,7 @@ $content_data = $main->getContentData($_GET['coID']);
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>" />
   <meta http-equiv="Content-Style-Type" content="text/css" />
-  <meta name="robots" content="noindex, nofollow, noodp" />'
+  <meta name="robots" content="noindex, nofollow, noodp" />
   <title><?php echo htmlspecialchars($content_data['content_heading'], ENT_QUOTES, strtoupper($_SESSION['language_charset']); ?></title>
   <?php /*
   //BOF - GTB - 2010-08-03 - Security Fix - Base
