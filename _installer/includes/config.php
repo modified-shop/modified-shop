d4828fd6c25f6a3fbf8a92099fb303c88a0db634
web28 <web28@modified-shop.org>
2013-02-20 15:26:15 +0000

    update installer to rev11
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4493 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ _installer/includes/config.php
@@ -0,0 +1,18 @@
+<?php
+  /* --------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org] 
+   --------------------------------------------------------------*/
+   
+  /*######################################*/
+  
+  define('INSTALL_CHARSET', 'latin1'); //latin1 oder utf8
+  define('DIR_MODIFIED_INSTALLER', '_installer');
+  define('MODIFIED_SQL', 'modified.sql');
+  define('DISABLE_PATH_CHECK', false); //default false
+  
+  /*######################################*/  
