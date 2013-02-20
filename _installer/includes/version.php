9d4d4125ddffebb3be1cb06172cd34754444c1ba
web28 <web28@modified-shop.org>
2013-02-20 15:26:15 +0000

    update installer to rev11

---
+++ _installer/includes/version.php
@@ -0,0 +1,12 @@
+<?php
+  /* --------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org] 
+   --------------------------------------------------------------*/
+
+
+  define('INSTALLER_VERSION', 'Installer 106 rev11'); 
