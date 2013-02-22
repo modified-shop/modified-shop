386fb6c63fa0ff84c00676ddcec6904e7b1b0fd0
modGTB <gerhard@modified-shop.org>
2013-02-22 08:27:12 +0000

    show categories when no active product
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4502 79ac0487-9261-4f26-a34d-858dee0dcce0

--- index.php
+++ index.php
@@ -34,21 +34,25 @@ require (DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/source/boxes.php');
 // the following cPath references come from application_top.php
 $category_depth = 'top';
 if (isset ($cPath) && xtc_not_null($cPath)) {
-	$categories_products_query = "select count(*) as total from ".TABLE_PRODUCTS_TO_CATEGORIES." where categories_id = '".$current_category_id."'";
-	$categories_products_query = xtDBquery($categories_products_query);
-	$cateqories_products = xtc_db_fetch_array($categories_products_query, true);
-	if ($cateqories_products['total'] > 0) {
-		$category_depth = 'products'; // display products
-	} else {
-		$category_parent_query = "select count(*) as total from ".TABLE_CATEGORIES." where parent_id = '".$current_category_id."'";
-		$category_parent_query = xtDBquery($category_parent_query);
-		$category_parent = xtc_db_fetch_array($category_parent_query, true);
-		if ($category_parent['total'] > 0) {
-			$category_depth = 'nested'; // navigate through the categories
-		} else {
-			$category_depth = 'products'; // category has no products, but display the 'no products' message
-		}
-	}
+  $categories_products_query = "select p2c.products_id
+                                  from ".TABLE_PRODUCTS_TO_CATEGORIES." p2c
+                                  left join ".TABLE_PRODUCTS." p
+                                   on p2c.products_id = p.products_id
+                                  where p2c.categories_id = ".(int)$current_category_id."
+                                  and p.products_status = 1";
+  $categories_products_result = xtDBquery($categories_products_query);
+  if (xtc_db_num_rows($categories_products_result, true) > 0) {
+    $category_depth = 'products'; // display products
+  } else {
+    $category_parent_query = "select parent_id from ".TABLE_CATEGORIES." where parent_id = ".(int)$current_category_id." AND categories_status = 1";
+    $category_parent_result = xtDBquery($category_parent_query);
+    $category_parent = xtc_db_fetch_array($category_parent_result, true);
+    if (xtc_db_num_rows($category_parent_result, true) > 0) {
+      $category_depth = 'nested'; // navigate through the categories
+    } else {
+      $category_depth = 'products'; // category has no products, but display the 'no products' message
+    }
+  }
 }
 
 include (DIR_WS_MODULES.'default.php');
