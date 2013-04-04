ffea2323d358bdd82952bfeac5f3fd86b0704717
Tomcraft1980 <hallo@modified-shop.org>
2013-04-04 17:56:13 +0000

    Relocated shopgate module in /tags/modified-shop-1.06 to conform the shopgate wiki updating procedure and updated the module to version 2.1.16
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4569 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/payment/shopgate.php
+++ includes/modules/payment/shopgate.php
@@ -1,2 +1,2 @@
 <?php
-include_once DIR_FS_CATALOG.'includes/shopgate/base/includes/modules/payment/shopgate.php';
+include_once DIR_FS_CATALOG.'includes/external/shopgate/base/includes/modules/payment/shopgate.php';
