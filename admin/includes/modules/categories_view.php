63285c12f01652243f7c55f7552f5958be1bc2ea
web28 <web28@modified-shop.org>
2013-04-15 14:46:43 +0000

    clean up code
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4626 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/categories_view.php
+++ admin/includes/modules/categories_view.php
@@ -341,8 +341,9 @@
                } else {
                    echo '<tr class="dataTableRow" onmouseover="this.className=\'dataTableRowOver\';this.style.cursor=\'pointer\'" onmouseout="this.className=\'dataTableRow\'">' . "\n";
                }
-                 ?>
-                 <td class="categories_view_data"><input type="checkbox" name="multi_categories[]" value="<?php echo $categories['categories_id'] . '" '; if (isset($_POST['multi_categories']) && is_array($_POST['multi_categories'])) { if (in_array($categories['categories_id'], $_POST['multi_categories'])) { echo 'checked="checked"'; } } ?>></td>
+               $checked = isset($_POST['multi_categories']) && is_array($_POST['multi_categories']) && in_array($categories['categories_id'], $_POST['multi_categories']) ? 'checked="checked"' : ''; 
+               ?>
+                 <td class="categories_view_data"><input type="checkbox" name="multi_categories[]" value="<?php echo $categories['categories_id'];?>"<?php echo $checked;?>></td>
                  <td class="categories_view_data">--</td>
                  <td class="categories_view_data"><?php echo $categories['sort_order']; ?></td>
                  <?php
