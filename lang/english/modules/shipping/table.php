433a12947bf0fa1a67bc51ced98fe421bcbd70c6
Tomcraft <Tomcraft@modified-shop.org>
2013-06-11 22:27:09 +0000

    Added display of shipping weight to shipping module "table"

--- lang/english/modules/shipping/table.php
+++ lang/english/modules/shipping/table.php
@@ -17,7 +17,7 @@
 
 define('MODULE_SHIPPING_TABLE_TEXT_TITLE', 'Table Rate');
 define('MODULE_SHIPPING_TABLE_TEXT_DESCRIPTION', 'Table Rate');
-define('MODULE_SHIPPING_TABLE_TEXT_WAY', 'Best Way');
+define('MODULE_SHIPPING_TABLE_TEXT_WAY', 'Best Way (%01.2f kg)');
 define('MODULE_SHIPPING_TABLE_TEXT_WEIGHT', 'Weight');
 define('MODULE_SHIPPING_TABLE_TEXT_AMOUNT', 'Amount');
 define('MODULE_SHIPPING_TABLE_UNDEFINED_RATE', 'The shipping rate cannot be determined at this time.');
