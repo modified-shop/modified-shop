b1ff648bd05300e3917eb7f23384658d993aa80e
Tomcraft1980 <hallo@modified-shop.org>
2013-07-07 10:08:54 +0000

    Added r5035, r5036 & r5037 from /tags/modified-shop-1.06 to trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5038 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/shipping/ap.php
+++ lang/german/modules/shipping/ap.php
@@ -31,8 +31,6 @@ define('MODULE_SHIPPING_AP_UNDEFINED_RATE', 'Die Versandkosten k&ouml;nnen im Mo
 
 define('MODULE_SHIPPING_AP_STATUS_TITLE' , '&Ouml;sterreichische Post AG');
 define('MODULE_SHIPPING_AP_STATUS_DESC' , 'Wollen Sie den Versand &uuml;ber die &Ouml;sterreichische Post AG anbieten?');
-define('MODULE_SHIPPING_AP_HANDLING_TITLE' , 'Handling Fee');
-define('MODULE_SHIPPING_AP_HANDLING_DESC' , 'Bearbeitungsgeb&uuml;hr f&uuml;r diese Versandart in Euro');
 define('MODULE_SHIPPING_AP_TAX_CLASS_TITLE' , 'Steuersatz');
 define('MODULE_SHIPPING_AP_TAX_CLASS_DESC' , 'W&auml;hlen Sie den MwSt.-Satz f&uuml;r diese Versandart aus.');
 define('MODULE_SHIPPING_AP_ZONE_TITLE' , 'Versand Zone');
@@ -41,36 +39,17 @@ define('MODULE_SHIPPING_AP_SORT_ORDER_TITLE' , 'Reihenfolge der Anzeige');
 define('MODULE_SHIPPING_AP_SORT_ORDER_DESC' , 'Niedrigste wird zuerst angezeigt.');
 define('MODULE_SHIPPING_AP_ALLOWED_TITLE' , 'Einzelne Versandzonen');
 define('MODULE_SHIPPING_AP_ALLOWED_DESC' , 'Geben Sie <b>einzeln</b> die Zonen an, in welche ein Versand m&ouml;glich sein soll. zb AT,DE');
-define('MODULE_SHIPPING_AP_COUNTRIES_1_TITLE' , 'Zone 1a L&auml;nder');
-define('MODULE_SHIPPING_AP_COUNTRIES_1_DESC' , 'Durch Komma getrennt Liste der L&auml;nder als zwei Zeichen ISO-Code Landeskennzahlen, die Teil der Zone 1a sind.');
-define('MODULE_SHIPPING_AP_COST_1_TITLE' , 'Zone 1a Tarif Tabelle bis 20 kg');
-define('MODULE_SHIPPING_AP_COST_1_DESC' , 'Tarif Tabelle f&uuml;r die Zone 1a, basiered auf <b>\'Schnelles Paket\'</b> bis 20 kg Versandgewicht.');
-define('MODULE_SHIPPING_AP_COUNTRIES_2_TITLE' , 'Zone 1b L&auml;nder');
-define('MODULE_SHIPPING_AP_COUNTRIES_2_DESC' , 'Durch Komma getrennt Liste der L&auml;nder als zwei Zeichen ISO-Code Landeskennzahlen, die Teil der Zone 1b sind.');
-define('MODULE_SHIPPING_AP_COST_2_TITLE' , 'Zone 1b Tarif Tabelle bis 20 kg');
-define('MODULE_SHIPPING_AP_COST_2_DESC' , 'Tarif Tabelle f&uuml;r die Zone 1b, basiered auf <b>\'Schnelles Paket\'</b> bis 20 kg Versandgewicht.');
-define('MODULE_SHIPPING_AP_COUNTRIES_3_TITLE' , 'Zone 2 L&auml;nder');
-define('MODULE_SHIPPING_AP_COUNTRIES_3_DESC' , 'Durch Komma getrennt Liste der L&auml;nder als zwei Zeichen ISO-Code Landeskennzahlen, die Teil der Zone 2 sind.');
-define('MODULE_SHIPPING_AP_COST_3_TITLE' , 'Zone 2 Tarif Tabelle bis 20 kg');
-define('MODULE_SHIPPING_AP_COST_3_DESC' , 'Tarif Tabelle f&uuml;r die Zone 2, basiered auf <b>\'Schnelles Paket\'</b> bis 20 kg Versandgewicht.');
-define('MODULE_SHIPPING_AP_COUNTRIES_4_TITLE' , 'Zone 3 L&auml;nder');
-define('MODULE_SHIPPING_AP_COUNTRIES_4_DESC' , 'Durch Komma getrennt Liste der L&auml;nder als zwei Zeichen ISO-Code Landeskennzahlen, die Teil der Zone 3 sind.');
-define('MODULE_SHIPPING_AP_COST_4_TITLE' , 'Zone 3 Tarif Tabelle bis 20 kg');
-define('MODULE_SHIPPING_AP_COST_4_DESC' , 'Tarif Tabelle f&uuml;r die Zone 3, basiered auf <b>\'Schnelles Paket\'</b> bis 20 kg Versandgewicht.');
-define('MODULE_SHIPPING_AP_COUNTRIES_5_TITLE' , 'Zone 4 L&auml;nder');
-define('MODULE_SHIPPING_AP_COUNTRIES_5_DESC' , 'Durch Komma getrennt Liste der L&auml;nder als zwei Zeichen ISO-Code Landeskennzahlen, die Teil der Zone 4 sind.');
-define('MODULE_SHIPPING_AP_COST_5_TITLE' , 'Zone 4 Tarif Tabelle bis 20 kg');
-define('MODULE_SHIPPING_AP_COST_5_DESC' , 'Tarif Tabelle f&uuml;r die Zone 4, basiered auf <b>\'Schnelles Paket\'</b> bis 20 kg Versandgewicht.');
-define('MODULE_SHIPPING_AP_COUNTRIES_6_TITLE' , 'Zone 4 L&auml;nder');
-define('MODULE_SHIPPING_AP_COUNTRIES_6_DESC' , 'Durch Komma getrennt Liste der L&auml;nder als zwei Zeichen ISO-Code Landeskennzahlen, die Teil der Zone 4 sind.');
-define('MODULE_SHIPPING_AP_COST_6_TITLE' , 'Zone 4 Tarif Tabelle bis 20 kg');
-define('MODULE_SHIPPING_AP_COST_6_DESC' , 'Tarif Tabelle f&uuml;r die Zone 4, basiered auf <b>\'Schnelles Paket\'</b> bis 20 kg Versandgewicht.');
-define('MODULE_SHIPPING_AP_COUNTRIES_7_TITLE' , 'Zone 5 L&auml;nder');
-define('MODULE_SHIPPING_AP_COUNTRIES_7_DESC' , 'Durch Komma getrennt Liste der L&auml;nder als zwei Zeichen ISO-Code Landeskennzahlen, die Teil der Zone 5 sind.');
-define('MODULE_SHIPPING_AP_COST_7_TITLE' , 'Zone 5 Tarif Tabelle bis 20 kg');
-define('MODULE_SHIPPING_AP_COST_7_DESC' , 'Tarif Tabelle f&uuml;r die Zone 5, basiered auf <b>\'Schnelles Paket\'</b> bis 20 kg Versandgewicht.');
-define('MODULE_SHIPPING_AP_COUNTRIES_8_TITLE' , 'Zone Inland');
-define('MODULE_SHIPPING_AP_COUNTRIES_8_DESC' , 'Inlandszone');
-define('MODULE_SHIPPING_AP_COST_8_TITLE' , 'Zone Tarif Tabelle bis 31.5 kg');
-define('MODULE_SHIPPING_AP_COST_8_DESC' , 'Tarif Tabelle f&uuml;r die Inlandszone, bis 31.5 kg Versandgewicht.');
+define('MODULE_SHIPPING_AP_NUMBER_ZONES_TITLE' , 'Anzahl der Zonen');
+define('MODULE_SHIPPING_AP_NUMBER_ZONES_DESC' , 'Anzahl der bereitgestellten Zonen');
+define('MODULE_SHIPPING_AP_DISPLAY_TITLE' , 'Anzeige aktivieren');
+define('MODULE_SHIPPING_AP_DISPLAY_DESC' , 'M&ouml;chten Sie anzeigen, wenn kein Versand in das Land m&ouml;glich ist bzw. keine Versandkosten berechnet werden konnten?');
+
+for ($module_shipping_ap_i = 1; $module_shipping_ap_i <= MODULE_SHIPPING_AP_NUMBER_ZONES; $module_shipping_ap_i ++) {
+  define('MODULE_SHIPPING_AP_COUNTRIES_'.$module_shipping_ap_i.'_TITLE' , '<hr/>Zone '.$module_shipping_ap_i.' L&auml;nder');
+  define('MODULE_SHIPPING_AP_COUNTRIES_'.$module_shipping_ap_i.'_DESC' , 'Durch Komma getrennte Liste von ISO L&auml;ndercodes (2 Zeichen), welche Teil von Zone '.$module_shipping_ap_i.' sind.');
+  define('MODULE_SHIPPING_AP_COST_'.$module_shipping_ap_i.'_TITLE' , 'Zone '.$module_shipping_ap_i.' Tarif Tabelle bis 20 kg');
+  define('MODULE_SHIPPING_AP_COST_'.$module_shipping_ap_i.'_DESC' , 'Versandkosten nach Zone '.$module_shipping_ap_i.' Bestimmungsorte, basierend auf einer Gruppe von max. Bestellgewichten. Beispiel: 3:8.50,7:10.50,... Gewicht von kleiner oder gleich 3 w&uuml;rde 8.50 f&uuml;r die Zone '.$module_shipping_ap_i.' Bestimmungsl&auml;nder kosten.');
+  define('MODULE_SHIPPING_AP_HANDLING_'.$module_shipping_ap_i.'_TITLE' , 'Zone '.$module_shipping_ap_i.' Handling Geb&uuml;hr');
+  define('MODULE_SHIPPING_AP_HANDLING_'.$module_shipping_ap_i.'_DESC' , 'Handling Geb&uuml;hr f&uuml;r diese Versandzone');
+}
 ?>
