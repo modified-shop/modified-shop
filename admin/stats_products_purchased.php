d9816876e4b7b491fc2a69983c07a7ef44c4bae5
web28 <web28@modified-shop.org>
2013-07-18 09:32:14 +0000

    set new table classes
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5116 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/stats_products_purchased.php
+++ admin/stats_products_purchased.php
@@ -20,101 +20,75 @@
   require (DIR_WS_INCLUDES.'head.php');
 ?>
 </head>
-<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
+<body>
 <!-- header //-->
 <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
 <!-- header_eof //-->
-<!-- body //-->
-<table border="0" width="100%" cellspacing="2" cellpadding="2">
-  <tr>
-    <td class="columnLeft2" width="<?php echo BOX_WIDTH; ?>" valign="top">
-      <table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
-<!-- left_navigation //-->
-<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
-<!-- left_navigation_eof //-->
-      </table>
-    </td>
-<!-- body_text //-->
-    <td class="boxCenter" width="100%" valign="top">
-      <table border="0" width="100%" cellspacing="0" cellpadding="2">
-        <tr>
-          <td>
-            <table border="0" width="100%" cellspacing="0" cellpadding="0">
-              <tr>
-                <td width="80" rowspan="2"><?php echo xtc_image(DIR_WS_ICONS.'heading_statistic.gif'); ?></td>
-                <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
-              </tr>
-              <tr>
-                <td class="main" valign="top">Statistics</td>
-              </tr>
-            </table>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <table border="0" width="100%" cellspacing="0" cellpadding="0">
-              <tr>
-                <td valign="top">
-                  <table border="0" width="100%" cellspacing="0" cellpadding="2">
-                    <tr class="dataTableHeadingRow">
-                      <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_NUMBER; ?></td>
-                      <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_MODEL; ?></td>
-                      <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_PRODUCTS; ?></td>
-                      <td class="dataTableHeadingContent" align="center"><?php echo TABLE_HEADING_PURCHASED; ?>&nbsp;</td>
-                      <td class="dataTableHeadingContent" align="center"><?php echo TABLE_HEADING_QUANTITY; ?>&nbsp;</td>
-                    </tr>
-                    <?php
-                    $rows = (isset($_GET['page']) && $_GET['page'] > 1) ? $_GET['page'] * $maxrows - $maxrows : 0;                    
-                    $products_query_raw = "select p.products_id,
-                                                  p.products_model,  
-                                                  p.products_ordered,
-                                                  p.products_quantity,
-                                                  pd.products_name 
-                                             from " . TABLE_PRODUCTS . " p, 
-                                                  " . TABLE_PRODUCTS_DESCRIPTION . " pd 
-                                            where pd.products_id = p.products_id 
-                                              and pd.language_id = '" . $_SESSION['languages_id'] . "' 
-                                              and p.products_ordered > 0 
-                                         group by pd.products_id 
-                                         order by p.products_ordered DESC, pd.products_name";
-                    $products_split = new splitPageResults($_GET['page'], '20', $products_query_raw, $products_query_numrows, 'p.products_id');
 
-                    $products_query = xtc_db_query($products_query_raw);
-                    while ($products = xtc_db_fetch_array($products_query)) {
-                      $rows++;
-                      if (strlen($rows) < 2) {
-                        $rows = '0' . $rows;
-                      }
-                    ?>
-                    <tr class="dataTableRow" onmouseover="this.className='dataTableRowOver';this.style.cursor='pointer'" onmouseout="this.className='dataTableRow'" onclick="document.location.href='<?php echo xtc_href_link(FILENAME_CATEGORIES, 'action=new_product_preview&read=only&pID=' . $products['products_id'] . '&origin=' . FILENAME_STATS_PRODUCTS_PURCHASED . '?page=' . $_GET['page'], 'NONSSL'); ?>'">
-                      <td class="dataTableContent"><?php echo $rows; ?>.</td>
-                      <td class="dataTableContent"><?php echo $products['products_model']; ?>&nbsp;</td>
-                      <td class="dataTableContent"><?php echo $products['products_name']; ?></td>                      
-                      <td class="dataTableContent" align="center"><?php echo $products['products_ordered']; ?>&nbsp;</td>
-                      <td class="dataTableContent" align="center"><?php echo $products['products_quantity']; ?>&nbsp;</td>
-                    </tr>
-                  <?php
-                    }
-                  ?>
-                  </table>
-                </td>
-              </tr>
-              <tr>
-                <td colspan="3">
-                  <table border="0" width="100%" cellspacing="0" cellpadding="2">
-                    <tr>
-                      <td class="smallText" valign="top"><?php echo $products_split->display_count($products_query_numrows, '20', $_GET['page'], TEXT_DISPLAY_NUMBER_OF_PRODUCTS); ?></td>
-                      <td class="smallText" align="right"><?php echo $products_split->display_links($products_query_numrows, '20', MAX_DISPLAY_PAGE_LINKS, $_GET['page']); ?>&nbsp;</td>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-            </table>
-          </td>
+<!-- body //-->
+<table class="tableBody">
+  <tr>
+    <?php //left_navigation
+    if (USE_ADMIN_TOP_MENU == 'false') {
+      echo '<td class="columnLeft2">'.PHP_EOL;
+      echo '<!-- left_navigation //-->'.PHP_EOL;       
+      require_once(DIR_WS_INCLUDES . 'column_left.php');
+      echo '<!-- left_navigation eof //-->'.PHP_EOL; 
+      echo '</td>'.PHP_EOL;      
+    }
+    ?>
+    <!-- body_text //-->
+    <td class="boxCenter">
+      <div class="pageHeadingImage"><?php echo xtc_image(DIR_WS_ICONS.'heading/icon_statistic.png'); ?></div>
+      <div class="pageHeading pdg2"><?php echo HEADING_TITLE; ?></div>              
+      <div class="main pdg2">Statistics</div>
+      
+      <table class="tableCenter collapse">
+        <tr class="dataTableHeadingRow">
+          <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_NUMBER; ?></td>
+          <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_MODEL; ?></td>
+          <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_PRODUCTS; ?></td>
+          <td class="dataTableHeadingContent" align="center"><?php echo TABLE_HEADING_PURCHASED; ?>&nbsp;</td>
+          <td class="dataTableHeadingContent" align="center"><?php echo TABLE_HEADING_QUANTITY; ?>&nbsp;</td>
         </tr>
-      </table>
+        <?php
+        $rows = (isset($_GET['page']) && $_GET['page'] > 1) ? $_GET['page'] * $maxrows - $maxrows : 0;                    
+        $products_query_raw = "select p.products_id,
+                                      p.products_model,  
+                                      p.products_ordered,
+                                      p.products_quantity,
+                                      pd.products_name 
+                                 from " . TABLE_PRODUCTS . " p, 
+                                      " . TABLE_PRODUCTS_DESCRIPTION . " pd 
+                                where pd.products_id = p.products_id 
+                                  and pd.language_id = '" . $_SESSION['languages_id'] . "' 
+                                  and p.products_ordered > 0 
+                             group by pd.products_id 
+                             order by p.products_ordered DESC, pd.products_name";
+        $products_split = new splitPageResults($_GET['page'], '20', $products_query_raw, $products_query_numrows, 'p.products_id');
+
+        $products_query = xtc_db_query($products_query_raw);
+        while ($products = xtc_db_fetch_array($products_query)) {
+          $rows++;
+          if (strlen($rows) < 2) {
+            $rows = '0' . $rows;
+          }
+        ?>
+        <tr class="dataTableRow" onmouseover="this.className='dataTableRowOver';this.style.cursor='pointer'" onmouseout="this.className='dataTableRow'" onclick="document.location.href='<?php echo xtc_href_link(FILENAME_CATEGORIES, 'action=new_product_preview&read=only&pID=' . $products['products_id'] . '&origin=' . FILENAME_STATS_PRODUCTS_PURCHASED . '?page=' . $_GET['page'], 'NONSSL'); ?>'">
+          <td class="dataTableContent"><?php echo $rows; ?>.</td>
+          <td class="dataTableContent"><?php echo $products['products_model']; ?>&nbsp;</td>
+          <td class="dataTableContent"><?php echo $products['products_name']; ?></td>                      
+          <td class="dataTableContent" align="center"><?php echo $products['products_ordered']; ?>&nbsp;</td>
+          <td class="dataTableContent" align="center"><?php echo $products['products_quantity']; ?>&nbsp;</td>
+        </tr>
+      <?php
+        }
+      ?>
+      </table>             
+      <div class="smallText pdg2 flt-l"><?php echo $products_split->display_count($products_query_numrows, '20', $_GET['page'], TEXT_DISPLAY_NUMBER_OF_PRODUCTS); ?></div>
+      <div class="smallText pdg2 flt-r"><?php echo $products_split->display_links($products_query_numrows, '20', MAX_DISPLAY_PAGE_LINKS, $_GET['page']); ?></div>
     </td>
-<!-- body_text_eof //-->
+    <!-- body_text_eof //-->
   </tr>
 </table>
 <!-- body_eof //-->
