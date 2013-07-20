98766f776e952941a9eab07713d15f203f56457b
modGTB <gerhard@modified-shop.org>
2013-07-20 10:02:22 +0000

    missing language constant
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5181 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/admin/german.php
+++ lang/german/admin/german.php
@@ -168,6 +168,10 @@ define('BOX_REMOVEOLDPICS','Alte Bilder l&ouml;schen'); // Remove old pictures -
 define('BOX_JANOLAW','janolaw AGB Hosting'); // Tomcraft - 2011-06-17 - Added janolaw AGB hosting service
 define('BOX_HAENDLERBUND','H&auml;ndlerbund AGB Service'); // Tomcraft - 2012-12-08 - Added haendlerbund AGB interface
 define('BOX_SAFETERMS','Safeterms - AGB Service'); // Tomcraft - 2013-06-21 - Safeterms AGB interface
+define('BOX_SHOP','Shop');
+define('BOX_LOGOUT','Abmelden');
+define('BOX_CREDITS','Credits');
+define('BOX_UPDATE','Update');
 
 define('TXT_GROUPS','<b>Gruppen</b>:');
 define('TXT_SYSTEM','System');
