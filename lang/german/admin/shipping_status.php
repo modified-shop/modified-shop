8017018c056e582b9de7bfa9476eba4138581f49
modGTB <gerhard@modified-shop.org>
2013-07-21 18:47:58 +0000

    missing language definition to r5194
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5195 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/admin/shipping_status.php
+++ lang/german/admin/shipping_status.php
@@ -28,6 +28,7 @@ define('TEXT_INFO_HEADING_NEW_SHIPPING_STATUS', 'Neuer Lieferstatus');
 define('TEXT_INFO_HEADING_EDIT_SHIPPING_STATUS', 'Lieferstatus bearbeiten');
 define('TEXT_INFO_SHIPPING_STATUS_IMAGE', 'Bild:');
 define('TEXT_INFO_HEADING_DELETE_SHIPPING_STATUS', 'Lieferstatus l&ouml;schen');
+define('TEXT_DELETE_IMAGE', 'Bild l&ouml;schen');
 
 define('ERROR_REMOVE_DEFAULT_SHIPPING_STATUS', 'Fehler: Der Standard-Lieferstatus kann nicht gel&ouml;scht werden. Bitte definieren Sie einen neuen Standard-Lieferstatus und wiederholen Sie den Vorgang.');
 define('ERROR_STATUS_USED_IN_ORDERS', 'Fehler: Dieser Lieferstatus wird zur Zeit noch f&uuml;r Artikel verwendet.');
