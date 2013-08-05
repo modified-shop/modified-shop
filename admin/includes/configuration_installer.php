14054ae9252bcd16bb871d35fbc742e78837ed2b
Tomcraft <Tomcraft@modified-shop.org>
2013-08-05 11:54:22 +0000

    Added option to show page parse time only to admin

--- admin/includes/configuration_installer.php
+++ admin/includes/configuration_installer.php
@@ -49,6 +49,11 @@ $values_group_update = array();
   $values[] = "(NULL, 'STORE_DB_SLOW_QUERY', 'true', '10', '6', NULL, NOW(), NULL, 'xtc_cfg_select_option(array(\'true\', \'false\'),');";
   $values[] = "(NULL, 'STORE_DB_SLOW_QUERY_TIME', '1.0', '10', '7', NULL, NOW(), NULL, NULL);";
 
+  $values_update[] = array (
+                           'values' => "configuration_group_id = '10', set_function = 'xtc_cfg_select_option(array(\'none\', \'admin\', \'all\'),'",
+                           'configuration_key' => 'DISPLAY_PAGE_PARSE_TIME'
+                           );
+
 //configuration_group_id 11 --- "Cache Optionen"
 
 //configuration_group_id 12 --- "Email Optionen"
