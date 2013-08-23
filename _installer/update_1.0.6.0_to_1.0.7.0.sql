777b617b0ba5739d637f7d0ccdf8461bf9556d5a
Tomcraft1980 <hallo@modified-shop.org>
2013-08-23 16:57:23 +0000

    Fixed encoding on r5424 (set to latin1)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5425 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update_1.0.6.0_to_1.0.7.0.sql
+++ _installer/update_1.0.6.0_to_1.0.7.0.sql
@@ -78,23 +78,23 @@ UPDATE admin_access SET stats_stock_warning = 5 WHERE customers_id = 'groups' LI
 INSERT INTO zones VALUES ('',203,'K','Blekinge');
 INSERT INTO zones VALUES ('',203,'W','Dalarna');
 INSERT INTO zones VALUES ('',203,'I','Gotland');
-INSERT INTO zones VALUES ('',203,'X','GÃ¤vleborg');
+INSERT INTO zones VALUES ('',203,'X','Gävleborg');
 INSERT INTO zones VALUES ('',203,'N','Halland');
-INSERT INTO zones VALUES ('',203,'Z','JÃ¤mtland');
-INSERT INTO zones VALUES ('',203,'F','JÃ¶nkÃ¶ping');
+INSERT INTO zones VALUES ('',203,'Z','Jämtland');
+INSERT INTO zones VALUES ('',203,'F','Jönköping');
 INSERT INTO zones VALUES ('',203,'H','Kalmar');
 INSERT INTO zones VALUES ('',203,'G','Kronoberg');
 INSERT INTO zones VALUES ('',203,'BD','Norrbotten');
-INSERT INTO zones VALUES ('',203,'T','Ã–rebro');
-INSERT INTO zones VALUES ('',203,'E','Ã–stergÃ¶tland');
-INSERT INTO zones VALUES ('',203,'M','SkÃ¥ne');
+INSERT INTO zones VALUES ('',203,'T','Örebro');
+INSERT INTO zones VALUES ('',203,'E','Östergötland');
+INSERT INTO zones VALUES ('',203,'M','Skåne');
 INSERT INTO zones VALUES ('',203,'AB','Stockholm');
-INSERT INTO zones VALUES ('',203,'D','SÃ¶dermanland');
+INSERT INTO zones VALUES ('',203,'D','Södermanland');
 INSERT INTO zones VALUES ('',203,'C','Uppsala');
-INSERT INTO zones VALUES ('',203,'S','VÃ¤rmland');
-INSERT INTO zones VALUES ('',203,'AC','VÃ¤sterbotten');
-INSERT INTO zones VALUES ('',203,'Y','VÃ¤sternorrland');
-INSERT INTO zones VALUES ('',203,'U','VÃ¤stmanland');
-INSERT INTO zones VALUES ('',203,'O','VÃ¤stra GÃ¶taland');
+INSERT INTO zones VALUES ('',203,'S','Värmland');
+INSERT INTO zones VALUES ('',203,'AC','Västerbotten');
+INSERT INTO zones VALUES ('',203,'Y','Västernorrland');
+INSERT INTO zones VALUES ('',203,'U','Västmanland');
+INSERT INTO zones VALUES ('',203,'O','Västra Götaland');
 
 # Keep an empty line at the end of this file for the db_updater to work properly
