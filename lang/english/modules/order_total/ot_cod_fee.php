4467db6a12db7d3e4747c8dab8e423720c6ad64f
DokuMan <DokuMan@modified-shop.org>
2013-08-30 09:58:12 +0000

    Fix notices using COD payment module

--- lang/english/modules/order_total/ot_cod_fee.php
+++ lang/english/modules/order_total/ot_cod_fee.php
@@ -15,7 +15,6 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-
   define('MODULE_ORDER_TOTAL_COD_FEE_TITLE', 'COD charge');
   define('MODULE_ORDER_TOTAL_COD_FEE_DESCRIPTION', 'Calculation of the COD charge');
 
@@ -27,32 +26,33 @@
 
   define('MODULE_ORDER_TOTAL_COD_FEE_TAX_CLASS_TITLE','Taxclass');
   define('MODULE_ORDER_TOTAL_COD_FEE_TAX_CLASS_DESC','Choose a taxclass.');
-  
+
   function define_shipping_titles() {
     $module_keys = str_replace('.php','',MODULE_SHIPPING_INSTALLED);
     $installed_shipping_modules = explode(';',$module_keys);
-    
+    $title = '';
+
     if (count($installed_shipping_modules) > 0) {
         foreach($installed_shipping_modules as $shipping_code) {
             $module_type = 'shipping';
-            $file = $shipping_code.'.php';            
+            $file = $shipping_code.'.php';
             $shipping_code = strtoupper($shipping_code);
-            if (file_exists(DIR_FS_LANGUAGES . $_SESSION['language'] . '/modules/' . $module_type . '/' . $file)) {
+            if (defined('DIR_FS_LANGUAGES') && file_exists(DIR_FS_LANGUAGES . $_SESSION['language'] . '/modules/' . $module_type . '/' . $file)) {
                 include_once(DIR_FS_LANGUAGES . $_SESSION['language'] . '/modules/' . $module_type . '/' . $file);
                 $title = constant('MODULE_SHIPPING_'.$shipping_code.'_TEXT_TITLE');
             }
             $shipping_code = ($shipping_code == 'FREEAMOUNT') ? 'FREEAMOUNT_FREE' : 'FEE_' . $shipping_code;
-            
+
             define('MODULE_ORDER_TOTAL_COD_'.$shipping_code.'_TITLE',$title);
             define('MODULE_ORDER_TOTAL_COD_'.$shipping_code.'_DESC','&lt;ISO2-Code&gt;:&lt;Price&gt;, ....<br />
             00 as ISO2-Code allows the COD shipping in all countries. If
             00 is used you have to enter it as last argument. If
             no 00:9.99 is entered the COD shipping into foreign countries will not be calculated
-            (not possible). To exclude only one country, do not enter costs for this country. 
-            Example: DE:4.00,CH:,00:9.99<br />-> Explanation: Shipping to DE: 4&euro; / Shipping to CH: not possible 
+            (not possible). To exclude only one country, do not enter costs for this country.
+            Example: DE:4.00,CH:,00:9.99<br />-> Explanation: Shipping to DE: 4&euro; / Shipping to CH: not possible
             / Shipping to the rest of the world: 9,99&euro;');
-                }          
+                }
     }
   }
-  define_shipping_titles();  
+  define_shipping_titles();
 ?>
