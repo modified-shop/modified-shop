eccc923a595d81b14cb7c207e92f9733eb6f1925
Tomcraft <Tomcraft@modified-shop.org>
2013-09-03 16:37:41 +0000

    Addition on r5466

--- inc/xtc_db_perform.inc.php
+++ inc/xtc_db_perform.inc.php
@@ -17,6 +17,8 @@
    ---------------------------------------------------------------------------------------*/
 
   function xtc_db_perform($table, $data, $action = 'insert', $parameters = '', $link = 'db_link') {
+    global $$link;
+    
     reset($data);
 
     if ($action == 'insert') {
@@ -27,7 +29,7 @@
       $query = substr($query, 0, -2) . ') values (';
       reset($data);
       while (list(, $value) = each($data)) {
-      	 $value = (is_Float($value) && defined('PHP4_3_10') && PHP4_3_10 === true) ? sprintf("%.F",$value) : (string)($value);
+      	 $value = (is_float($value) && defined('PHP4_3_10') && PHP4_3_10 === true) ? sprintf("%.F",$value) : (string)($value);
         switch ($value) {
           case 'now()':
             $query .= 'now(), ';
@@ -44,7 +46,7 @@
     } elseif ($action == 'update') {
       $query = 'update ' . $table . ' set ';
       while (list($columns, $value) = each($data)) {
-         $value = (is_Float($value) && defined('PHP4_3_10') && PHP4_3_10 === true) ? sprintf("%.F",$value) : (string)($value);
+         $value = (is_float($value) && defined('PHP4_3_10') && PHP4_3_10 === true) ? sprintf("%.F",$value) : (string)($value);
       	switch ($value) {
           case 'now()':
             $query .= $columns . ' = now(), ';
