c4356a69a14f9be9fbde9dc6ee93063a9e385ca2
Tomcraft <Tomcraft@modified-shop.org>
2013-09-08 10:23:51 +0000

    Added r5562 from /tags/modified-shop-1.06 to trunk

--- includes/modules/shipping/fedexeu.php
+++ includes/modules/shipping/fedexeu.php
@@ -37,7 +37,7 @@
       $this->tax_class = MODULE_SHIPPING_FEDEXEU_TAX_CLASS;
       $this->enabled = ((MODULE_SHIPPING_FEDEXEU_STATUS == 'True') ? true : false);
 
-      if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_FEDEXEU_ZONE > 0) ) {
+      if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_FEDEXEU_ZONE > 0) && is_object($order)) {
         $check_flag = false;
         $check_query = xtc_db_query("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_SHIPPING_FEDEXEU_ZONE . "' and zone_country_id = '" . $order->delivery['country']['id'] . "' order by zone_id");
         while ($check = xtc_db_fetch_array($check_query)) {
--- includes/modules/shipping/upse.php
+++ includes/modules/shipping/upse.php
@@ -40,7 +40,7 @@
       $this->tax_class = MODULE_SHIPPING_UPSE_TAX_CLASS;
       $this->enabled = ((MODULE_SHIPPING_UPSE_STATUS == 'True') ? true : false);
 
-      if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_UPSE_ZONE > 0) ) {
+      if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_UPSE_ZONE > 0) && is_object($order)) {
         $check_flag = false;
         $check_query = xtc_db_query("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_SHIPPING_UPSE_ZONE . "' and zone_country_id = '" . $order->delivery['country']['id'] . "' order by zone_id");
         while ($check = xtc_db_fetch_array($check_query)) {
