7de8abc87d4d5a1e6446c0d1f803365f4d194e50
web28 <web28@modified-shop.org>
2013-09-08 15:44:03 +0000

    set new table classes

--- admin/server_info.php
+++ admin/server_info.php
@@ -31,78 +31,65 @@ require (DIR_WS_INCLUDES.'head.php');
     <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
     <!-- header_eof //-->
     <!-- body //-->
-    <table border="0" width="100%" cellspacing="2" cellpadding="2">
+    <table class="tableBody">
       <tr>
-        <td class="columnLeft2" width="<?php echo BOX_WIDTH; ?>" valign="top">
-          <table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
-            <!-- left_navigation //-->
-            <?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
-            <!-- left_navigation_eof //-->
+        <?php //left_navigation
+        if (USE_ADMIN_TOP_MENU == 'false') {
+          echo '<td class="columnLeft2">'.PHP_EOL;
+          echo '<!-- left_navigation //-->'.PHP_EOL;       
+
+
+          require_once(DIR_WS_INCLUDES . 'column_left.php');
+          echo '<!-- left_navigation eof //-->'.PHP_EOL; 
+          echo '</td>'.PHP_EOL;      
+        }
+        ?>
+
+
+
+        <!-- body_text //--> 
+        <td class="boxCenter">
+        
+          <div class="pageHeading pgd2 mrg5"><?php echo HEADING_TITLE; ?></div>       
+        
+          <table class="tableCenter mrg5" style="width:900px">          
+
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_HOST; ?></strong></td>
+              <td class="smallText"><?php echo $system['host'] . ' (' . $system['ip'] . ')'; ?></td>
+              <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_HOST; ?></strong></td>
+              <td class="smallText"><?php echo $system['db_server'] . ' (' . $system['db_ip'] . ')'; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_OS; ?></strong></td>
+              <td class="smallText"><?php echo $system['system'] . ' ' . $system['kernel']; ?></td>
+              <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE; ?></strong></td>
+              <td class="smallText"><?php echo $system['db_version']; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_DATE; ?></strong></td>
+              <td class="smallText"><?php echo $system['date']; ?></td>
+              <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_DATE; ?></strong></td>
+              <td class="smallText"><?php echo $system['db_date']; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_UP_TIME; ?></strong></td>
+              <td colspan="3" class="smallText"><?php echo $system['uptime']; ?></td>
+            </tr>
+            <tr>
+              <td colspan="4"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_HTTP_SERVER; ?></strong></td>
+              <td colspan="3" class="smallText"><?php echo $system['http_server']; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_PHP_VERSION; ?></strong></td>
+              <td colspan="3" class="smallText"><?php echo $system['php'] . ' (' . TITLE_ZEND_VERSION . ' ' . $system['zend'] . ')'; ?></td>
+            </tr>
           </table>
-        </td>
-        <!-- body_text //-->
-        <td class="boxCenter" width="100%" valign="top">
-          <table border="0" width="100%" cellspacing="0" cellpadding="2">
-            <tr>
-              <td>
-                <table border="0" width="100%" cellspacing="0" cellpadding="0">
-                  <tr>
-                    <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
-                    <td class="pageHeading" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <table border="0" width="100%" cellspacing="0" cellpadding="2">
-                  <tr>
-                    <td align="left">
-                      <table border="0" cellspacing="0" cellpadding="3">
-                        <tr>
-                          <td class="smallText"><strong><?php echo TITLE_SERVER_HOST; ?></strong></td>
-                          <td class="smallText"><?php echo $system['host'] . ' (' . $system['ip'] . ')'; ?></td>
-                          <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_HOST; ?></strong></td>
-                          <td class="smallText"><?php echo $system['db_server'] . ' (' . $system['db_ip'] . ')'; ?></td>
-                        </tr>
-                        <tr>
-                          <td class="smallText"><strong><?php echo TITLE_SERVER_OS; ?></strong></td>
-                          <td class="smallText"><?php echo $system['system'] . ' ' . $system['kernel']; ?></td>
-                          <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE; ?></strong></td>
-                          <td class="smallText"><?php echo $system['db_version']; ?></td>
-                        </tr>
-                        <tr>
-                          <td class="smallText"><strong><?php echo TITLE_SERVER_DATE; ?></strong></td>
-                          <td class="smallText"><?php echo $system['date']; ?></td>
-                          <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_DATE; ?></strong></td>
-                          <td class="smallText"><?php echo $system['db_date']; ?></td>
-                        </tr>
-                        <tr>
-                          <td class="smallText"><strong><?php echo TITLE_SERVER_UP_TIME; ?></strong></td>
-                          <td colspan="3" class="smallText"><?php echo $system['uptime']; ?></td>
-                        </tr>
-                        <tr>
-                          <td colspan="4"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
-                        </tr>
-                        <tr>
-                          <td class="smallText"><strong><?php echo TITLE_HTTP_SERVER; ?></strong></td>
-                          <td colspan="3" class="smallText"><?php echo $system['http_server']; ?></td>
-                        </tr>
-                        <tr>
-                          <td class="smallText"><strong><?php echo TITLE_PHP_VERSION; ?></strong></td>
-                          <td colspan="3" class="smallText"><?php echo $system['php'] . ' (' . TITLE_ZEND_VERSION . ' ' . $system['zend'] . ')'; ?></td>
-                        </tr>
-                      </table>
-                    </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-            <tr>
-              <td><?php echo xtc_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-            </tr>
-            <tr>
-              <!--td>
+                    
+              <!--
               <iframe src="<?php echo xtc_href_link(basename($PHP_SELF), 'phpInfo', 'NONSSL'); ?>" width="100%" height="700" style="border: solid 1px #a3a3a3;">
               <p>Der verwendete Browser kann leider nicht mit inline Frames (iframe)
               umgehen:
@@ -110,8 +97,8 @@ require (DIR_WS_INCLUDES.'head.php');
               Seite vom System</a>
               </p>
               </iframe>
-              </td-->
-              <td align="center">
+              -->
+             
                 <div style="width:980px; border: solid 1px #a3a3a3; padding:10px; overflow:auto; height:600px;">
                   <?php
                   if (function_exists('ob_start')) {
@@ -154,9 +141,7 @@ require (DIR_WS_INCLUDES.'head.php');
                   }
                   ?>
                 </div>
-              </td>
-            </tr>
-          </table>
+                
         </td>
         <!-- body_text_eof //-->
       </tr>
