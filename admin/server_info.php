6640704011188c91b3f84a026873426071eec0c9
web28 <web28@modified-shop.org>
2013-09-08 15:44:03 +0000

    set new table classes
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5573 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/server_info.php
+++ admin/server_info.php
@@ -1,18 +1,19 @@
 <?php
-/* --------------------------------------------------------------
-   $Id: server_info.php 899 2005-04-29 02:40:57Z hhgag $   
+  /* --------------------------------------------------------------
+   $Id: server_info.php 4981 2013-06-26 02:39:47Z Tomcraft $
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    --------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(server_info.php,v 1.4 2003/03/17); www.oscommerce.com 
-   (c) 2003	 nextcommerce (server_info.php,v 1.7 2003/08/18); www.nextcommerce.org
+   (c) 2002-2003 osCommerce(server_info.php,v 1.4 2003/03/17); www.oscommerce.com
+   (c) 2003 nextcommerce (server_info.php,v 1.7 2003/08/18); www.nextcommerce.org
+   (c) 2006 XT-Commerce (server_info.php 899 2005-04-29)
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    --------------------------------------------------------------*/
 
 require('includes/application_top.php');
@@ -28,94 +29,80 @@ require (DIR_WS_INCLUDES.'head.php');
 ?>
 
 </head>
-<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
-<!-- header //-->
-<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
-<!-- header_eof //-->
-
-<!-- body //-->
-<table border="0" width="100%" cellspacing="2" cellpadding="2">
-  <tr>
-    <td class="columnLeft2" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
-<!-- left_navigation //-->
-<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
-<!-- left_navigation_eof //-->
-    </table></td>
-<!-- body_text //-->
-    <td class="boxCenter" width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+<body>
+    <!-- header //-->
+    <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+    <!-- header_eof //-->
+    <!-- body //-->
+    <table class="tableBody">
       <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
-          <tr>
-            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
-            <td class="pageHeading" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
-          </tr>
-        </table></td>
-      </tr>
-      <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
-          <tr>
-            <td align="left"><table border="0" cellspacing="0" cellpadding="3">
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_HOST; ?></strong></td>
-                <td class="smallText"><?php echo $system['host'] . ' (' . $system['ip'] . ')'; ?></td>
-                <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_HOST; ?></strong></td>
-                <td class="smallText"><?php echo $system['db_server'] . ' (' . $system['db_ip'] . ')'; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_OS; ?></strong></td>
-                <td class="smallText"><?php echo $system['system'] . ' ' . $system['kernel']; ?></td>
-                <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE; ?></strong></td>
-                <td class="smallText"><?php echo $system['db_version']; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_DATE; ?></strong></td>
-                <td class="smallText"><?php echo $system['date']; ?></td>
-                <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_DATE; ?></strong></td>
-                <td class="smallText"><?php echo $system['db_date']; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_SERVER_UP_TIME; ?></strong></td>
-                <td colspan="3" class="smallText"><?php echo $system['uptime']; ?></td>
-              </tr>
-              <tr>
-                <td colspan="4"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_HTTP_SERVER; ?></strong></td>
-                <td colspan="3" class="smallText"><?php echo $system['http_server']; ?></td>
-              </tr>
-              <tr>
-                <td class="smallText"><strong><?php echo TITLE_PHP_VERSION; ?></strong></td>
-                <td colspan="3" class="smallText"><?php echo $system['php'] . ' (' . TITLE_ZEND_VERSION . ' ' . $system['zend'] . ')'; ?></td>
-              </tr>
-            </table></td>
-          </tr>
-        </table></td>
-      </tr>
-      <tr>
-        <td><?php echo xtc_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-      </tr>
-      <tr>
-        <td>
-           <iframe src="<?php echo xtc_href_link(basename($PHP_SELF), 'phpInfo', 'NONSSL'); ?>" width="100%" height="700" style="border: solid 1px #a3a3a3;">
-           <p>Der verwendete Browser kann leider nicht mit inline Frames (iframe)
+        <?php //left_navigation
+        if (USE_ADMIN_TOP_MENU == 'false') {
+          echo '<td class="columnLeft2">'.PHP_EOL;
+          echo '<!-- left_navigation //-->'.PHP_EOL;       
+          require_once(DIR_WS_INCLUDES . 'column_left.php');
+          echo '<!-- left_navigation eof //-->'.PHP_EOL; 
+          echo '</td>'.PHP_EOL;      
+        }
+        ?>
+        <!-- body_text //--> 
+        <td class="boxCenter">
+        
+          <div class="pageHeading pgd2 mrg5"><?php echo HEADING_TITLE; ?></div>       
+        
+          <table class="tableCenter mrg5" style="width:900px">          
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_HOST; ?></strong></td>
+              <td class="smallText"><?php echo $system['host'] . ' (' . $system['ip'] . ')'; ?></td>
+              <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_HOST; ?></strong></td>
+              <td class="smallText"><?php echo $system['db_server'] . ' (' . $system['db_ip'] . ')'; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_OS; ?></strong></td>
+              <td class="smallText"><?php echo $system['system'] . ' ' . $system['kernel']; ?></td>
+              <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE; ?></strong></td>
+              <td class="smallText"><?php echo $system['db_version']; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_DATE; ?></strong></td>
+              <td class="smallText"><?php echo $system['date']; ?></td>
+              <td class="smallText">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><?php echo TITLE_DATABASE_DATE; ?></strong></td>
+              <td class="smallText"><?php echo $system['db_date']; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_SERVER_UP_TIME; ?></strong></td>
+              <td colspan="3" class="smallText"><?php echo $system['uptime']; ?></td>
+            </tr>
+            <tr>
+              <td colspan="4"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_HTTP_SERVER; ?></strong></td>
+              <td colspan="3" class="smallText"><?php echo $system['http_server']; ?></td>
+            </tr>
+            <tr>
+              <td class="smallText"><strong><?php echo TITLE_PHP_VERSION; ?></strong></td>
+              <td colspan="3" class="smallText"><?php echo $system['php'] . ' (' . TITLE_ZEND_VERSION . ' ' . $system['zend'] . ')'; ?></td>
+            </tr>
+          </table>           
+       
+          <iframe src="<?php echo xtc_href_link(basename($PHP_SELF), 'phpInfo', 'NONSSL'); ?>" style="width:100%;height:700px;border:solid 1px #a3a3a3;">
+          <p>Der verwendete Browser kann leider nicht mit inline Frames (iframe)
              umgehen:
              <a href="<?php echo xtc_href_link(basename($PHP_SELF), 'phpInfo', 'NONSSL'); ?>" target="_blank">Hier geht es zur phpinfo()
              Seite vom System</a>
-           </p>
-         </iframe>
+          </p>
+          </iframe>
+        
         </td>
+        <!-- body_text_eof //-->
       </tr>
-    </table></td>
-<!-- body_text_eof //-->
-  </tr>
-</table>
-<!-- body_eof //-->
-
-<!-- footer //-->
-<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
-<!-- footer_eof //-->
-<br />
-</body>
+    </table>
+    <!-- body_eof //-->
+    <!-- footer //-->
+    <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+    <!-- footer_eof //-->
+    <br />
+  </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
