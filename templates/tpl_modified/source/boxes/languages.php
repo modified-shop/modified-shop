19881ae9107633e9e20eade1bef6ca19b7101c3e
web28 <web28@modified-shop.org>
2013-09-10 11:35:07 +0000

    use text link instead image link if no image available

--- templates/tpl_modified/source/boxes/languages.php
+++ templates/tpl_modified/source/boxes/languages.php
@@ -1,11 +1,11 @@
 <?php
   /* -----------------------------------------------------------------------------------------
-   $Id: languages.php 2 2012-11-21 15:30:32Z GTB $
+   $Id: languages.php 5581 2013-09-08 21:26:38Z Tomcraft $
 
-   xtcModified - community made shopping
-   http://www.xtc-modified.org
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2010 xtcModified
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
    based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
@@ -16,13 +16,7 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-  // include needed functions
-  require_once(DIR_FS_INC . 'xtc_get_all_get_params.inc.php');
-
-  //BOF - 2010-02-28 - Fix Undefined variable: lng
-  //if (!isset($lng) && !is_object($lng)) {
   if (!isset($lng) || (isset($lng) && !is_object($lng))) {
-  //EOF - 2010-02-28 - Fix Undefined variable: lng
     include(DIR_WS_CLASSES . 'language.php');
     $lng = new language;
   }
@@ -31,23 +25,24 @@
   $count_lng = 0;
   reset($lng->catalog_languages);
   while (list($key, $value) = each($lng->catalog_languages)) {
-    $count_lng++;    
-    $languages_string .= '<a href="' . xtc_href_link(basename($PHP_SELF), 'language=' . $key.'&'.xtc_get_all_get_params(array('language', 'currency')), $request_type) . '">' . xtc_image('lang/' .  $value['directory'] .'/' . $value['image'], $value['name']) . '</a>';
+    $count_lng++;
+    $lng_link_txt = file_exists('lang/' .  $value['directory'] .'/' . $value['image']) ? xtc_image('lang/' .  $value['directory'] .'/' . $value['image'], $value['name']) : $value['name'];
+    $languages_string .= ' <a href="' . xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . $lng_link_txt . '</a> ';
   }
 
   // dont show box if there's only 1 language
   if ($count_lng > 1 ) {
     $box_smarty = new smarty;
-    //BOF - GTB - 2010-08-03 - Security Fix - Base
     $box_smarty->assign('tpl_path',DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/');
-    //$box_smarty->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/');
-    //EOF - GTB - 2010-08-03 - Security Fix - Base
+
     $box_content='';
     $box_smarty->assign('BOX_CONTENT', $languages_string);
     $box_smarty->assign('language', $_SESSION['language']);
+
     // set cache ID
     $box_smarty->caching = 0;
     $box_languages= $box_smarty->fetch(CURRENT_TEMPLATE.'/boxes/box_languages.html');
+
     $smarty->assign('box_LANGUAGES',$box_languages);
   }
 ?>
