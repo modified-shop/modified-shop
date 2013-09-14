6f54ac75abdf69ee481c7bb04dd5d4636366e0a8
Tomcraft1980 <hallo@modified-shop.org>
2013-09-14 08:44:49 +0000

    Revised english language definitions for order_total modules
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5757 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/modules/order_total/ot_subtotal.php
+++ lang/english/modules/order_total/ot_subtotal.php
@@ -1,26 +1,27 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: ot_subtotal.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id: ot_subtotal.php 4200 2013-01-10 19:47:11Z Tomcraft1980 $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(ot_subtotal.php,v 1.2 2002/04/17); www.oscommerce.com 
    (c) 2003	 nextcommerce (ot_subtotal.php,v 1.5 2003/08/13); www.nextcommerce.org
+   (c) 2006 xt:Commerce; www.xt-commerce.com
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_TITLE', 'Sub-Total');
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_TITLE_NO_TAX', 'Sub-Total, no-tax');
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_DESCRIPTION', 'Order Sub-Total');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_TITLE', 'Subtotal');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_TITLE_NO_TAX', 'Subtotal, no tax');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_DESCRIPTION', 'Order subtotal');
 
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_STATUS_TITLE','Display Sub-Total');
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_STATUS_DESC','Do you want to display the order sub-total cost?');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_STATUS_TITLE','Display subtotal');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_STATUS_DESC','Do you want to display the order subtotal cost?');
 
   define('MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER_TITLE','Sort Order');
   define('MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER_DESC','Sort order of display.');
--- lang/english/modules/order_total/ot_subtotal_no_tax.php
+++ lang/english/modules/order_total/ot_subtotal_no_tax.php
@@ -1,25 +1,26 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: ot_subtotal_no_tax.php 899 2005-04-29 02:40:57Z hhgag $   
+   $Id: ot_subtotal_no_tax.php 4200 2013-01-10 19:47:11Z Tomcraft1980 $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(ot_subtotal.php,v 1.2 2002/04/17); www.oscommerce.com 
    (c) 2003	 nextcommerce (ot_subtotal_no_tax.php,v 1.4 2003/08/13); www.nextcommerce.org
+   (c) 2006 xt:Commerce; www.xt-commerce.com
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_TITLE', '<b>Total, no-tax</b>');
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_DESCRIPTION', 'Order Sub-Total');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_TITLE', '<b>Total, no tax</b>');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_DESCRIPTION', 'Order subtotal');
 
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_TITLE','Display Sub-Total without tax');
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_DESC','Do you want to display the order sub-total cost?');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_TITLE','Display subtotal without tax');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_DESC','Do you want to display the order subtotal cost?');
 
   define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_SORT_ORDER_TITLE','Sort Order');
   define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_SORT_ORDER_DESC', 'Sort order of display.');
