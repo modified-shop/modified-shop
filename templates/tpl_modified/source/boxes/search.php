ca623fb467b1d6e2c651a52c5f3f6ad5dadd6a5a
Markus <Markus@modified-shop.org>
2013-09-29 15:29:13 +0000

    tpl_modified

--- templates/tpl_modified/source/boxes/search.php
+++ templates/tpl_modified/source/boxes/search.php
@@ -31,10 +31,11 @@ $box_smarty->assign('FORM_ACTION', xtc_draw_form('quick_find', xtc_href_link(FIL
 //$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', '', 'size="20" maxlength="30"'));
 //BOF - web28 - 2010-04-11 - change input html size to css width
 //$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'size="20" maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
-$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'style="width:160px;" maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
+$box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'class="search_field" maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
 //EOF - web28 - 2010-04-11 - change input html size to css width
 //EOF - Dokuman - 14.08.2009 - Put dynamic "search"-text into box
-$box_smarty->assign('BUTTON_SUBMIT', xtc_image_submit('button_quick_find.gif', IMAGE_BUTTON_SEARCH));
+$box_smarty->assign('BUTTON_SUBMIT', xtc_image_submit('button_quick_find_head.gif', IMAGE_BUTTON_SEARCH,'class="search_button"'));
+
 $box_smarty->assign('FORM_END', '</form>');
 $box_smarty->assign('LINK_ADVANCED', xtc_href_link(FILENAME_ADVANCED_SEARCH));
 $box_smarty->assign('BOX_CONTENT', $box_content);
