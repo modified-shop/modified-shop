679e7209469931d70c30c36462c63870755d7d78
GTB <GTB@modified-shop.org>
2013-10-09 19:14:36 +0000

    commit r5906 to trunk

--- checkout_payment.php
+++ checkout_payment.php
@@ -52,7 +52,8 @@ require_once (DIR_FS_INC . 'xtc_check_stock.inc.php');
 unset ($_SESSION['tmp_oID']);
 unset ($_SESSION['transaction_id']); ### moneybookers payment module version 2.4
 
-if (isset($_SESSION['credit_covers'])) unset($_SESSION['credit_covers']); //ICW ADDED FOR CREDIT CLASS SYSTEM
+if (isset($_SESSION['credit_covers'])) unset($_SESSION['credit_covers']);
+if (isset($_SESSION['cot_gv'])) unset($_SESSION['cot_gv']); 
 
 require (DIR_WS_INCLUDES.'checkout_requirements.php');
 
