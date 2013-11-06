2b60ec1a2e465fc517a8dd6036b59488dc5d66cd
GTB <GTB@modified-shop.org>
2013-11-06 15:52:21 +0000

    implement conversion tracker for easymarketing api

--- includes/application_bottom.php
+++ includes/application_bottom.php
@@ -53,6 +53,12 @@ if (defined('MODULE_FINDOLOGIC_STATUS') && MODULE_FINDOLOGIC_STATUS == 'True' &&
 }
 /******** Findologic **********/
 
+## easymarketing - conversion tracking
+if (defined('MODULE_EASYMARKETING_STATUS') && MODULE_EASYMARKETING_STATUS == 'True') {
+  include(DIR_FS_CATALOG.'api/easymarketing/conversion_tracker.php');
+}
+## easymarketing - conversion tracking
+
 /**
  * new error handling
  */
