8ded8a6033c7226f98e3a352ab0235c4069b55e1
web28 <web28@modified-shop.org>
2013-11-14 18:47:27 +0000

    set default product class later

--- includes/modules/set_ids_by_url_parameters.php
+++ includes/modules/set_ids_by_url_parameters.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: set_ids_by_url_parameters.php 3851 2012-11-06 10:33:23Z web28 $
+   $Id: set_ids_by_url_parameters.php 6052 2013-11-14 09:06:49Z web28 $
 
    Modified - community made shopping
    http://www.modified-shop.org
@@ -11,7 +11,7 @@
    ---------------------------------------------------------------------------------------*/
 
 // product URLS
-if (isset ($_GET['info'])) {
+if (isset($_GET['info'])) {
   $site = explode('_', $_GET['info']);
   $pID = $site[0];
   $actual_products_id = (int) str_replace('p', '', $pID);
@@ -20,18 +20,16 @@ if (isset ($_GET['info'])) {
   $actual_products_id = (int) $_GET['products_id'];
   $product = new product($actual_products_id);
 }
-if (!isset($product) || !is_object($product)) {
-  $product = new product();
-}
+
 // category URLS
-if (isset ($_GET['cat'])) {
+if (isset($_GET['cat'])) {
   $site = explode('_', $_GET['cat']);
   $cID = $site[0];
   $cID = str_replace('c', '', $cID);
   $_GET['cPath'] = xtc_get_category_path($cID);
 }
 // manufacturer URLS
-if (isset ($_GET['manu'])) {
+if (isset($_GET['manu'])) {
   $site = explode('_', $_GET['manu']);
   $mID = $site[0];
   $mID = (int)str_replace('m', '', $mID);
@@ -39,9 +37,9 @@ if (isset ($_GET['manu'])) {
 }
 
 // calculate category path
-if (isset ($_GET['cPath'])) {
+if (isset($_GET['cPath'])) {
   $cPath = $_GET['cPath'] = xtc_input_validation($_GET['cPath'], 'cPath', '');
-} elseif (is_object($product) && !isset ($_GET['manufacturers_id'])) {
+} elseif (isset($product) && is_object($product) && !isset($_GET['manufacturers_id'])) {
   if ($product->isProduct()) {
     $cPath = xtc_get_product_path($actual_products_id);
   } else {
@@ -50,13 +48,17 @@ if (isset ($_GET['cPath'])) {
 } else {
   $cPath = '';
 }
-
+// set default product class
+if (!isset($product) || !is_object($product)) {
+  $product = new product();
+}
+// content URLS
 if (isset ($_GET['coID'])) {
   $coPath_array = xtc_get_content_path($_GET['coID']);
   $coPath_array[sizeof($coPath_array)] = xtc_get_content_id($_GET['coID']);  
   $coPath = implode('_', $coPath_array);
 }
-
+// set $current_category_id and verify $cPath
 if (xtc_not_null($cPath)) {
   $cPath_array = xtc_parse_category_path($cPath);
   $current_category_id = end($cPath_array);
