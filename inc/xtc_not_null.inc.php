3faa655819a62f2ea39abb79181e1af7411ca655
h-h-h <h-h-h@modified-shop.org>
2013-11-23 16:58:18 +0000

    new xtc_not_null update http://www.hackersolutions.com/modified/xtc_not_null_vs_empty

--- inc/xtc_not_null.inc.php
+++ inc/xtc_not_null.inc.php
@@ -13,22 +13,16 @@
    (c) 2003	 nextcommerce (xtc_not_null.inc.php,v 1.3 2003/08/13 23:38:05); www.nextcommerce.org
    (c) 2006 xt:Commerce; www.xt-commerce.com
 
+   new release:
+   (c) 2013 Hacker Solutions - www.hackersolutions.com/modified/xtc_not_null_vs_empty
+
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
    
   function xtc_not_null($value) {
-    if (is_array($value)) {
-      if (sizeof($value) > 0) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      if (($value != '') && ($value != 'NULL') && (strlen(trim($value)) > 0)) {
-        return true;
-      } else {
-        return false;
-      }
+    if ($value == '' || $value == "NULL" || (is_array($value) ? empty($value) : trim($value) == '')) {
+      return false;
     }
+    return true;
   }
  ?>
