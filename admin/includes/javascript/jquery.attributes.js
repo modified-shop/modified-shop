8d97d82058efccbe49305e3a2e103354e2f7e94c
web28 <web28@modified-shop.org>
2013-12-01 10:53:50 +0000

    new edit attributes
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6130 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/javascript/jquery.attributes.js
+++ admin/includes/javascript/jquery.attributes.js
@@ -1,34 +1,31 @@
 /*-----------------------
-  jquery.attribute.js Vers. 1.00
+  jquery.attribute.js Vers. 1.10
   (c) 2013 by noRiddle - www.revilonetz.de
   (c) 2013 by web28 - www.rpa-com.de
 -------------------------*/
 
 $(document).ready(function($) {
     var table = $("#attributes");
-    table.find('input[type=text],input.cb[type="checkbox"], select').each(function() {
-      $(this).attr("disabled", "disabled");
-    });
+    //find all inputs/selects and set it to disabled
+    table.find('input[type=text],input.cb[type="checkbox"], select').attr("disabled", "disabled");
+    
     $('.button_save').show();
     $('input[name="button_submit"]').hide();
-
-    $('input.cb[type="checkbox"]').parent().parent().hide(); //checkboxes
-    $('select.dl').parent().parent().hide(); //select downloads
+    //$('.attributes-odd,.attributes-even,.downloads').hide(); //change to css
     var flag  = true;
     var $bsave = $('.button_save');
 
-    var $dthr = $('.dataTableHeadingRow');
-    $dthr.css('cursor', 'pointer');
+    var dthr = $('.dataTableHeadingRow');
+    dthr.css('cursor', 'pointer');
     
-    $dthr.click(function() {
+    dthr.click(function() {
         //alert ($(this).attr('id'));
-        var input_n_ch = $('input.cb[type="checkbox"],select.dl');
-        var ch_this = $(this).nextUntil('.dataTableHeadingRow').find(input_n_ch);
-        var input = ch_this.parent().parent();
-        var input_dl = $('.downloads');
-        var $className = $(this).attr('class');
-        var input_fields = $('input,select');
-        ch_this = $(this).nextUntil('.dataTableHeadingRow').find(input_fields);
+        var oid = $(this).attr('id');
+        var className = $(this).attr('class');
+        var rows_oid = $('.'+oid);
+        var input_types = $('input,select');
+        var input_fields = rows_oid.find(input_types);
+       
         //check / uncheck all
         var ckb_all = $(this).find('input.select_all');
         ckb_all.click(function() {
@@ -38,35 +35,37 @@ $(document).ready(function($) {
            var is_check = ckb_all.is(':checked');
            checkboxes.each(function() {
               if (is_check) {
-                $(this).attr("checked", "checked");
+                  $(this).attr("checked", "checked");
               } else {
-                $(this).removeAttr("checked");
+                  $(this).removeAttr("checked");
               }
            });
         });
+        
         if (flag) {
-            input.toggle();
+            rows_oid.toggle();
             $(this).find('input.select_all').click(function(){select_all()});
-            if ($className == 'dataTableHeadingRow att-green' || $className == 'dataTableHeadingRow') {
+            if (className == 'dataTableHeadingRow att-green' || className == 'dataTableHeadingRow') {
                 $(this).removeClass('att-green').addClass('att-red');
                 $(this).find('td').addClass('active');
                 ckb_all.show();
                 $('form[name="SUBMIT_ATTRIBUTES"]').append('<input type="hidden" name="options_id[]" value="' + $(this).attr('id') + '" />');
-                $(ch_this).removeAttr('disabled');
-            } else if ($className == 'dataTableHeadingRow att-red') {
+                $(input_fields).removeAttr('disabled');
+            } else if (className == 'dataTableHeadingRow att-red') {
                 $(this).removeClass('att-red').addClass('att-green');
                 $(this).find('input.select_all').hide();
                 $('input[type="hidden"][value="' + $(this).attr('id') + '"]').remove();
                 $(this).find('td').removeClass('active');
-                $(ch_this).attr('disabled','disabled');
+                $(input_fields).attr('disabled','disabled');
             }
         }
         flag = true;
     });
 
     $('.button_save').click(function() {
-        var input_n_ch = $('input[type="checkbox"]').not(':checked');
-        var input = input_n_ch.parent().parent();
+        //remove all unchecked inputs
+        var input_unchecked = $('input[type="checkbox"]').not(':checked');
+        var input = input_unchecked.parent().parent();
         table.hide();
         input.remove();
         //$(input).find('input,select').attr('disabled','disabled');
