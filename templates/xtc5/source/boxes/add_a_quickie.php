383feff2076fc5165103527955ee96ed25403a73
h-h-h <h-h-h@modified-shop.org>
2013-12-15 20:29:52 +0000

    merge templates - part 1
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6176 79ac0487-9261-4f26-a34d-858dee0dcce0

--- templates/xtc5/source/boxes/add_a_quickie.php
+++ templates/xtc5/source/boxes/add_a_quickie.php
@@ -9,19 +9,19 @@
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(search.php,v 1.22 2003/02/10); www.oscommerce.com 
-   (c) 2003	 nextcommerce (search.php,v 1.9 2003/08/17); www.nextcommerce.org
-   (c) 2003 XT-Commerce
+   (c) 2002-2003 osCommerce (search.php,v 1.22 2003/02/10); www.oscommerce.com 
+   (c) 2003      nextcommerce (search.php,v1.9 2003/08/17); www.nextcommerce.org
+   (c) 2003-2006 XT-Commerce
 
    Third Party contributions:
    Add A Quickie v1.0 Autor  Harald Ponce de Leon
 
-   -----------------------------------------------------------------------------------------
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
   $box_smarty = new smarty;
   $box_content = '';
+
   $box_smarty->assign('tpl_path', 'templates/' . CURRENT_TEMPLATE . '/');
 
   $box_smarty->assign('FORM_ACTION',xtc_draw_form('quick_add', xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array ('action')) . 'action=add_a_quickie', $request_type)));
@@ -33,8 +33,15 @@
   $box_smarty->assign('BOX_CONTENT', $box_content);
   $box_smarty->assign('language', $_SESSION['language']);
 
+if (!CacheCheck()) {
   $box_smarty->caching = 0;
   $box_add_a_quickie = $box_smarty->fetch(CURRENT_TEMPLATE . '/boxes/box_add_a_quickie.html');
-
-  $smarty->assign('box_ADD_QUICKIE', $box_add_a_quickie);
+} else {
+  $box_smarty->caching = 1;
+  $box_smarty->cache_lifetime=CACHE_LIFETIME;
+  $box_smarty->cache_modified_check=CACHE_CHECK;
+  $cache_id = $_SESSION['language'];
+  $box_add_a_quickie= $box_smarty->fetch(CURRENT_TEMPLATE.'/boxes/box_add_a_quickie.html',$cache_id);
+}
+$smarty->assign('box_ADD_QUICKIE', $box_add_a_quickie);
 ?>
