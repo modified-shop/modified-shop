dd6de5247cc542fa20caf462ac0cff13aa3e7e50
GTB <GTB@modified-shop.org>
2014-02-12 12:41:26 +0000

    update installer and admin menu

--- includes/modules/payment/shopgate.php
+++ includes/modules/payment/shopgate.php
@@ -1,2 +1,3 @@
 <?php
+include_once DIR_FS_CATALOG.'includes/external/shopgate/base/lang/'.$_SESSION['language'].'/modules/payment/shopgate.php';
 include_once DIR_FS_CATALOG.'includes/external/shopgate/base/includes/modules/payment/shopgate.php';
