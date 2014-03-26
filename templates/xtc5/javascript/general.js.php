0957d194d5d8689958775a6c6859301a50b9cf16
web28 <web28@modified-shop.org>
2014-03-26 19:23:45 +0000

    add/modified jQuery alerts

--- templates/xtc5/javascript/general.js.php
+++ templates/xtc5/javascript/general.js.php
@@ -23,17 +23,7 @@ define('DIR_TMPL_JS', 'templates/'.CURRENT_TEMPLATE. '/javascript/');
 <script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>thickbox.js" type="text/javascript"></script>
 <script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.alerts.min.js" type="text/javascript"></script>
 
-<?php if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) { // TABS/ACCORDION in product_info - web28 ?>
-<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery-ui.js" type="text/javascript"></script>
 <script type="text/javascript">
-/* <![CDATA[ */
-  $.get("<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>"+"/css/javascript.css", function(css) {
-    $("head").append("<style type='text/css'>"+css+"<\/style>");
-  });
-  $(function() {
-    $("#tabbed_product_info").tabs();
-    $("#accordion_product_info").accordion({ autoHeight: false });
-  });
   /*BOC jQuery Alerts*/
   $.alerts.overlayOpacity = .2;
   $.alerts.overlayColor = '#000';
@@ -46,6 +36,19 @@ define('DIR_TMPL_JS', 'templates/'.CURRENT_TEMPLATE. '/javascript/');
     jConfirm(message, title);
   }
   /*EOC jQuery Alerts*/
+</script>
+
+<?php if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) { // TABS/ACCORDION in product_info - web28 ?>
+<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery-ui.js" type="text/javascript"></script>
+<script type="text/javascript">
+/* <![CDATA[ */
+  $.get("<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>"+"/css/javascript.css", function(css) {
+    $("head").append("<style type='text/css'>"+css+"<\/style>");
+  });
+  $(function() {
+    $("#tabbed_product_info").tabs();
+    $("#accordion_product_info").accordion({ autoHeight: false });
+  });
 /*]]>*/
 </script>
 <?php } // TABS/ACCORDION in product_info - web28 ?>
