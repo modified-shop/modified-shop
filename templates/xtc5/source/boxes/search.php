a1223f5d1c940b0ec4493c093d75f12266bf20fe
modGTB <gerhard@modified-shop.org>
2014-03-31 15:26:16 +0000

    fix findologic autocomplete
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6507 79ac0487-9261-4f26-a34d-858dee0dcce0

--- templates/xtc5/source/boxes/search.php
+++ templates/xtc5/source/boxes/search.php
@@ -23,7 +23,11 @@
 
   require_once (DIR_FS_INC . 'xtc_image_submit.inc.php');
 
-  $box_smarty->assign('FORM_ACTION', xtc_draw_form('quick_find', xtc_href_link(FILENAME_ADVANCED_SEARCH_RESULT, '', $request_type, false), 'get', 'class="box-search"') . xtc_hide_session_id());
+  $filename = FILENAME_ADVANCED_SEARCH_RESULT;
+  if (defined('MODULE_FINDOLOGIC_STATUS') && MODULE_FINDOLOGIC_STATUS == 'True') {
+    $filename = FILENAME_FINDOLOGIC;
+  }
+  $box_smarty->assign('FORM_ACTION', xtc_draw_form('quick_find', xtc_href_link($filename, '', $request_type, false), 'get', 'class="box-search"') . xtc_hide_session_id());
   $box_smarty->assign('INPUT_SEARCH', xtc_draw_input_field('keywords', IMAGE_BUTTON_SEARCH, 'maxlength="30" onfocus="if(this.value==this.defaultValue) this.value=\'\';" onblur="if(this.value==\'\') this.value=this.defaultValue;"'));
   $box_smarty->assign('BUTTON_SUBMIT', xtc_image_submit('button_quick_find.gif', IMAGE_BUTTON_SEARCH));
   $box_smarty->assign('FORM_END', '</form>');
