db749bec0e002c35a2e3db1d7477fcb738d64d16
GTB <GTB@modified-shop.org>
2014-03-31 14:35:06 +0000

    configuration for html5/xhtml

--- lang/english/admin/configuration.php
+++ lang/english/admin/configuration.php
@@ -905,4 +905,7 @@ define('MAX_DISPLAY_ADVANCED_SEARCH_RESULTS_DESC', 'Amount of products in search
 
 define('PRODUCT_IMAGE_SHOW_NO_IMAGE_TITLE', 'Show noimage.gif');
 define('PRODUCT_IMAGE_SHOW_NO_IMAGE_DESC', 'Show noimage.gif if there is no image');
+
+define('TEMPLATE_HTML_ENGINE_TITLE', 'HTML Version');
+define('TEMPLATE_HTML_ENGINE_DESC', 'HTML Version to use<br/><b>ATTENTION</b> this only works with template tpl_modified or compatible HTML5 Templates.');
 ?>
