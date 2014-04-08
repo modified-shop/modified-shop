c0a26ce0eb126e103c4bdeede5c8d37523bb9b08
Tomcraft <Tomcraft@modified-shop.org>
2014-04-08 15:37:49 +0000

    Addition on r6542

--- _installer/update_1.0.6.0_to_2.0.0.0.sql
+++ _installer/update_1.0.6.0_to_2.0.0.0.sql
@@ -168,4 +168,10 @@ INSERT INTO content_manager VALUES (20, 0, 0, '', 2, 'Lieferzeit', 'Lieferzeit',
 #Web28 - 2014-03-20 add content_active
 ALTER TABLE content_manager ADD content_active int(1) NOT NULL DEFAULT '1';
 
+#Tomcraft - 2014-04-08 - Added it_recht_kanzlei
+ALTER TABLE admin_access
+  ADD it_recht_kanzlei INT(1) NOT NULL DEFAULT 0;
+UPDATE admin_access SET it_recht_kanzlei = 1 WHERE customers_id = 1 LIMIT 1;
+UPDATE admin_access SET it_recht_kanzlei = 1 WHERE customers_id = 'groups' LIMIT 1;
+
 # Keep an empty line at the end of this file for the db_updater to work properly
