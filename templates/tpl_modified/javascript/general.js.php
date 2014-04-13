b7f0da67e5a6e17a01f0beef98d47385d4a6bf3e
Markus <Markus@modified-shop.org>
2014-04-13 12:26:17 +0000

    tpl_modified - load javascript at bottom of the page

--- templates/tpl_modified/javascript/general.js.php
+++ templates/tpl_modified/javascript/general.js.php
@@ -13,34 +13,8 @@
    -----------------------------------------------------------
 */
 define('DIR_TMPL_JS', 'templates/'.CURRENT_TEMPLATE. '/javascript/');
-
-// this javascriptfile get includes at the BOTTOM of every template page in shop
+// this javascriptfile get includes at the TOP of every template page in shop
 // you can add your template specific js scripts here
 ?>
 <script type="text/javascript">var DIR_WS_BASE="<?php echo DIR_WS_BASE ?>"</script>
-
-<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery-1.8.3.min.js" type="text/javascript"></script>
-<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.easyTabs.min.js" type="text/javascript"></script>
-<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.colorbox.min.js" type="text/javascript"></script>
-<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.unveil.min.js" type="text/javascript"></script>
-<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.alerts.min.js" type="text/javascript"></script>
-<script type="text/javascript">
-  $(document).ready(function(){
-    $(".cbimages").colorbox({rel:'cbimages', scalePhotos:true, maxWidth: "90%", maxHeight: "90%"});
-    $(".iframe").colorbox({iframe:true, width:"780", height:"560", maxWidth: "90%", maxHeight: "90%"});
-
-    /*BOC jQuery Alerts*/
-    $.alerts.overlayOpacity = .2;
-    $.alerts.overlayColor = '#000';
-    function alert(message, title) {
-      title = title || 'Information';
-      jAlert(message, title);
-    }
-    /*EOC jQuery Alerts*/
-
-    $(".unveil").show();
-    $(".unveil").unveil();
-  });
-</script>
-
 <?php require DIR_FS_CATALOG . DIR_TMPL_JS . 'get_states.js.php'; // Ajax State/District/Bundesland Updater - h-h-h ?>
--- templates/tpl_modified/javascript/general_bottom.js.php
+++ templates/tpl_modified/javascript/general_bottom.js.php
@@ -1,3 +1,45 @@
+<?php
+/*-----------------------------------------------------------
+   $Id:$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+  -----------------------------------------------------------
+   based on: (c) 2003 - 2006 XT-Commerce (general.js.php)
+  -----------------------------------------------------------
+   Released under the GNU General Public License
+   -----------------------------------------------------------
+*/
+// this javascriptfile get includes at the BOTTOM of every template page in shop
+// you can add your template specific js scripts here
+?>
+
+<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery-1.8.3.min.js" type="text/javascript"></script>
+<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.easyTabs.min.js" type="text/javascript"></script>
+<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.colorbox.min.js" type="text/javascript"></script>
+<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.unveil.min.js" type="text/javascript"></script>
+<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.alerts.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $(".cbimages").colorbox({rel:'cbimages', scalePhotos:true, maxWidth: "90%", maxHeight: "90%"});
+    $(".iframe").colorbox({iframe:true, width:"780", height:"560", maxWidth: "90%", maxHeight: "90%"});
+
+    /*BOC jQuery Alerts*/
+    $.alerts.overlayOpacity = .2;
+    $.alerts.overlayColor = '#000';
+    function alert(message, title) {
+      title = title || 'Information';
+      jAlert(message, title);
+    }
+    /*EOC jQuery Alerts*/
+
+    $(".unveil").show();
+    $(".unveil").unveil();
+  });
+</script>
+
 <?php if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) { // TABS/ACCORDION in product_info ?>
 <script type="text/javascript">
     $(document).ready(function () {
