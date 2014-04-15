a9076ad5119af1c7e0af701e9bdc15e2a29f07cd
GTB <GTB@modified-shop.org>
2014-04-15 15:06:57 +0000

    caching for new category function

--- templates/tpl_modified/source/boxes/categories.php
+++ templates/tpl_modified/source/boxes/categories.php
@@ -25,7 +25,7 @@
 include(DIR_FS_BOXES_INC . 'smarty_default.php');
 
 // set cache id
-$cache_id = $_SESSION['language'].$_SESSION['customers_status']['customers_status'].'-'.$cPath;
+$cache_id = $_SESSION['language'].$_SESSION['customers_status']['customers_status'].'-'.$cPath.((SPECIALS_CATEGORIES === true || WHATSNEW_CATEGORIES === true) ? '-'.basename($PHP_SELF) : '');
 
 if (!$box_smarty->is_cached(CURRENT_TEMPLATE.'/boxes/box_categories.html', $cache_id) || !$cache) {
 
