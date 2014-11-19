574a96e3a9a19917c5583a04f3178741571f76c5
GTB <GTB@modified-shop.org>
2014-11-19 15:51:21 +0000

    extend extra menu for magnalister

--- admin/includes/extra_menu.php
+++ admin/includes/extra_menu.php
@@ -15,12 +15,12 @@ defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.'
 if (!function_exists('extraMenue')) {
   function extraMenue() {
     $add_contents = array();
-    if ($extra_files = @scandir(DIR_WS_INCLUDES . 'extra/menu')) {      
+    if ($extra_files = @scandir(DIR_WS_INCLUDES . 'extra/menu')) {
       foreach ($extra_files as $filename) {
-        $filename_array = explode('.',$filename); 
+        $filename_array = explode('.',$filename);
         $file_ext = end($filename_array);
-        if($file_ext == 'php') {          
-          require_once(DIR_WS_INCLUDES . 'extra/menu/' . $filename);
+        if($file_ext == 'php') {
+          require(DIR_WS_INCLUDES . 'extra/menu/' . $filename);
         }
       }
     }
@@ -32,6 +32,17 @@ if (!function_exists('extraMenue')) {
 if (!function_exists('subMenue')){ // zweite ebene
     function subMenue($admin_access_name = '', $filename = '', $linktext= '', $parameters = '', $ssl = 'NONSSL'){
         global $admin_access;
+
+        ## magnalister
+        if (!is_array($admin_access) || count($admin_access) < 1) {
+          $admin_access = array();
+          if (($_SESSION['customers_status']['customers_status_id'] == '0')) {
+            $admin_access_query = xtc_db_query("SELECT * FROM " . TABLE_ADMIN_ACCESS . " WHERE customers_id = ".(int)$_SESSION['customer_id']);
+            $admin_access = xtc_db_fetch_array($admin_access_query);
+          }
+        }
+        ## magnalister
+
         $html = '';
         if (isset($admin_access[$admin_access_name]) && $admin_access[$admin_access_name] == '1') {
 
@@ -47,7 +58,7 @@ if (!function_exists('subMenue')){ // zweite ebene
               $html = '<li><a href="' . xtc_href_link($filename, $parameters, $ssl) . '" class="menuBoxContentLink"> -' . $linktext . '</a></li>';
             } else {
               echo 'ERROR --- '. 'AdminAccess: '. $admin_access_name . '|FileName: NO FILENAME DEFINED<br>';
-            }            
+            }
         }
         return $html;
     }
@@ -57,8 +68,15 @@ if (!function_exists('subMenue')){ // zweite ebene
 if (!function_exists('dynamicsAdds')){ // Menüpunkte dynamisch ergänzen
     function dynamicsAdds($box){
         global $add_contents;
+
+        ## magnalister
+        if (!is_array($add_contents) || count($add_contents) < 1) {
+          $add_contents = extraMenue();
+        }
+        ## magnalister
+
         $html = '';
-        if(isset($add_contents[$box]) && count($add_contents[$box] > 0)) {            
+        if(isset($add_contents[$box]) && count($add_contents[$box] > 0)) {
             foreach ($add_contents[$box] as $key) {
                 $html.= subMenue($key['admin_access_name'],
                                  $key['filename'],
@@ -74,3 +92,4 @@ if (!function_exists('dynamicsAdds')){ // Menüpunkte dynamisch ergänzen
 
 $add_contents = array();
 $add_contents = extraMenue();
+?>
