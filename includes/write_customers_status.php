80ca961b5bb8e2de6adb1405a0947a53f9ba811f
GTB <GTB@modified-shop.org>
2014-12-16 14:55:58 +0000

    better do redirect after deleting account

--- includes/write_customers_status.php
+++ includes/write_customers_status.php
@@ -59,37 +59,7 @@
         'customers_status_read_reviews' => $customers_status_value['customers_status_read_reviews']
       );
     } else {
-      $customers_status_query = xtc_db_query("SELECT
-                                                  *
-                                              FROM
-                                                  " . TABLE_CUSTOMERS_STATUS . "
-                                              WHERE
-                                                  customers_status_id = '" . DEFAULT_CUSTOMERS_STATUS_ID_GUEST . "' AND language_id = '" . $_SESSION['languages_id'] . "'");
-      $customers_status_value = xtc_db_fetch_array($customers_status_query);
-
-      $_SESSION['customers_status'] = array();
-      $_SESSION['customers_status']= array(
-        'customers_status_id' => DEFAULT_CUSTOMERS_STATUS_ID_GUEST,
-        'customers_status_name' => $customers_status_value['customers_status_name'],
-        'customers_status_image' => $customers_status_value['customers_status_image'],
-        'customers_status_discount' => $customers_status_value['customers_status_discount'],
-        'customers_status_public' => $customers_status_value['customers_status_public'],
-        'customers_status_min_order' => $customers_status_value['customers_status_min_order'],
-        'customers_status_max_order' => $customers_status_value['customers_status_max_order'],
-        'customers_status_ot_discount_flag' => $customers_status_value['customers_status_ot_discount_flag'],
-        'customers_status_ot_discount' => $customers_status_value['customers_status_ot_discount'],
-        'customers_status_graduated_prices' => $customers_status_value['customers_status_graduated_prices'],
-        'customers_status_show_price' => $customers_status_value['customers_status_show_price'],
-        'customers_status_show_price_tax' => $customers_status_value['customers_status_show_price_tax'],
-        'customers_status_add_tax_ot' => $customers_status_value['customers_status_add_tax_ot'],
-        'customers_status_payment_unallowed' => $customers_status_value['customers_status_payment_unallowed'],
-        'customers_status_shipping_unallowed' => $customers_status_value['customers_status_shipping_unallowed'],
-        'customers_status_discount_attributes' => $customers_status_value['customers_status_discount_attributes'],
-        'customers_fsk18' => $customers_status_value['customers_fsk18'],
-        'customers_fsk18_display' => $customers_status_value['customers_fsk18_display'],
-        'customers_status_write_reviews' => $customers_status_value['customers_status_write_reviews'],
-        'customers_status_read_reviews' => $customers_status_value['customers_status_read_reviews']
-      );
+      xtc_redirect(xtc_href_link(FILENAME_LOGOFF),'NONSSL');
     }
   } else {
     $customers_status_query = xtc_db_query("SELECT
