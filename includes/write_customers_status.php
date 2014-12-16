8676a8a35da747ca6a010665ecd2197931e09e19
modGTB <gerhard@modified-shop.org>
2014-12-16 13:55:02 +0000

    fix customers group after deleting account
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6929 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/write_customers_status.php
+++ includes/write_customers_status.php
@@ -10,10 +10,10 @@
    based on:
    (c) 2003	 nextcommerce (write_customers_status.php,v 1.8 2003/08/1); www.nextcommerce.org
    (c) 2006 xtCommerce (write_customers_status.php)
-   
+
    Released under the GNU General Public License
-   --------------------------------------------------------------------------------------- 
-   
+   ---------------------------------------------------------------------------------------
+
    based on Third Party contribution:
    Customers Status v3.x  (c) 2002-2003 Copyright Elari elari@free.fr | www.unlockgsm.com/dload-osc/ | CVS : http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/elari/?sortby=date#dirlist
 
@@ -22,30 +22,43 @@
 
   // write customers status in session
   if (isset($_SESSION['customer_id'])) {
-    $customers_status_query_1 = xtc_db_query("SELECT customers_status 
-                                                FROM " . TABLE_CUSTOMERS . " 
+    $customers_status_query_1 = xtc_db_query("SELECT customers_status
+                                                FROM " . TABLE_CUSTOMERS . "
                                                WHERE customers_id = '" . $_SESSION['customer_id'] . "'");
-    $customers_status_value_1 = xtc_db_fetch_array($customers_status_query_1);
 
-    $customers_status_query = xtc_db_query("SELECT *
-                                              FROM " . TABLE_CUSTOMERS_STATUS . "
-                                             WHERE customers_status_id = '" . $customers_status_value_1['customers_status'] . "' 
-                                               AND language_id = '" . $_SESSION['languages_id'] . "'");
+    if (xtc_db_num_rows($customers_status_query_1) == 1) {
+      $customers_status_value_1 = xtc_db_fetch_array($customers_status_query_1);
 
-    $_SESSION['customers_status'] = xtc_db_fetch_array($customers_status_query);
-    
-    if ($customers_status_value_1['customers_status'] == '0' && !defined('RUN_MODE_ADMIN')) {
-      $_SESSION['customers_status']['customers_status_id'] = DEFAULT_CUSTOMERS_STATUS_ID_ADMIN;
-      $_SESSION['customers_status']['customers_status'] = $customers_status_value_1['customers_status'];
+      $customers_status_query = xtc_db_query("SELECT *
+                                                FROM " . TABLE_CUSTOMERS_STATUS . "
+                                               WHERE customers_status_id = '" . $customers_status_value_1['customers_status'] . "'
+                                                 AND language_id = '" . $_SESSION['languages_id'] . "'");
+
+      $_SESSION['customers_status'] = xtc_db_fetch_array($customers_status_query);
+
+      if ($customers_status_value_1['customers_status'] == '0' && !defined('RUN_MODE_ADMIN')) {
+        $_SESSION['customers_status']['customers_status_id'] = DEFAULT_CUSTOMERS_STATUS_ID_ADMIN;
+        $_SESSION['customers_status']['customers_status'] = $customers_status_value_1['customers_status'];
+      } else {
+        $_SESSION['customers_status']['customers_status_id'] = $customers_status_value_1['customers_status'];
+        $_SESSION['customers_status']['customers_status'] = $customers_status_value_1['customers_status'];
+      }
     } else {
-      $_SESSION['customers_status']['customers_status_id'] = $customers_status_value_1['customers_status'];
-      $_SESSION['customers_status']['customers_status'] = $customers_status_value_1['customers_status'];
+      $customers_status_query = xtc_db_query("SELECT *
+                                                FROM " . TABLE_CUSTOMERS_STATUS . "
+                                               WHERE customers_status_id = '" . DEFAULT_CUSTOMERS_STATUS_ID_GUEST . "'
+                                                 AND language_id = '" . $_SESSION['languages_id'] . "'");
+
+      $_SESSION['customers_status'] = xtc_db_fetch_array($customers_status_query);
+      $_SESSION['customers_status']['customers_status_id'] = DEFAULT_CUSTOMERS_STATUS_ID_GUEST;
+      $_SESSION['customers_status']['customers_status'] = DEFAULT_CUSTOMERS_STATUS_ID_GUEST;
     }
   } else {
     $customers_status_query = xtc_db_query("SELECT *
                                               FROM " . TABLE_CUSTOMERS_STATUS . "
-                                             WHERE customers_status_id = '" . DEFAULT_CUSTOMERS_STATUS_ID_GUEST . "' 
+                                             WHERE customers_status_id = '" . DEFAULT_CUSTOMERS_STATUS_ID_GUEST . "'
                                                AND language_id = '" . $_SESSION['languages_id'] . "'");
+
     $_SESSION['customers_status'] = xtc_db_fetch_array($customers_status_query);
     $_SESSION['customers_status']['customers_status_id'] = DEFAULT_CUSTOMERS_STATUS_ID_GUEST;
     $_SESSION['customers_status']['customers_status'] = DEFAULT_CUSTOMERS_STATUS_ID_GUEST;
