0f86bfa715a5279c995d77b2ae654cf697823754
modGTB <gerhard@modified-shop.org>
2015-01-20 14:29:54 +0000

    update gv
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7098 79ac0487-9261-4f26-a34d-858dee0dcce0

--- gv_redeem.php
+++ gv_redeem.php
@@ -1,17 +1,17 @@
 <?php
-
 /* -----------------------------------------------------------------------------------------
-   $Id: gv_redeem.php 4221 2013-01-11 10:18:52Z gtb-modified $
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
    based on:
    (c) 2000-2001 The Exchange Project (earlier name of osCommerce)
    (c) 2002-2003 osCommerce (gv_redeem.php,v 1.3.2.1 2003/04/18); www.oscommerce.com
-
+   (c) 2006 XT-Commerce
+   
    Released under the GNU General Public License
    -----------------------------------------------------------------------------------------
    Third Party contribution:
@@ -23,35 +23,40 @@
    Copyright (c) Andre ambidex@gmx.net
    Copyright (c) 2001,2002 Ian C Wilson http://www.phesis.org
 
-
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
 require ('includes/application_top.php');
 
-if (ACTIVATE_GIFT_SYSTEM != 'true')
-	xtc_redirect(FILENAME_DEFAULT);
-
-// is customer logged on ?
-	if (!isset ($_SESSION['customer_id'])) xtc_redirect(FILENAME_SHOPPING_CART);
-
+// smarty
 $smarty = new Smarty;
 
 // include boxes
 require (DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/source/boxes.php');
 
-require (DIR_WS_INCLUDES.'header.php');
+if (ACTIVATE_GIFT_SYSTEM != 'true') {
+  xtc_redirect(FILENAME_DEFAULT);
+}
+
+// if the customer is not logged on, redirect them to the login page
+if (!isset ($_SESSION['customer_id'])) {
+  xtc_redirect(xtc_href_link(FILENAME_LOGIN, '', 'SSL'));
+}
 
 // check for a voucher number in the url
 if (isset ($_GET['gv_no'])) {
-	
-	
-	
 	$error = true;
-	$gv_query = xtc_db_query("select c.coupon_id, c.coupon_amount from ".TABLE_COUPONS." c, ".TABLE_COUPON_EMAIL_TRACK." et where coupon_code = '".xtc_db_input($_GET['gv_no'])."' and c.coupon_id = et.coupon_id");
+	$gv_query = xtc_db_query("SELECT c.coupon_id, 
+	                                 c.coupon_amount 
+	                            FROM ".TABLE_COUPONS." c
+	                            JOIN ".TABLE_COUPON_EMAIL_TRACK." et 
+	                                 ON c.coupon_id = et.coupon_id
+	                           WHERE coupon_code = '".xtc_db_input($_GET['gv_no'])."'");
 	if (xtc_db_num_rows($gv_query) > 0) {
 		$coupon = xtc_db_fetch_array($gv_query);
-		$redeem_query = xtc_db_query("select coupon_id from ".TABLE_COUPON_REDEEM_TRACK." where coupon_id = '".$coupon['coupon_id']."'");
+		$redeem_query = xtc_db_query("SELECT coupon_id 
+		                                FROM ".TABLE_COUPON_REDEEM_TRACK." 
+		                               WHERE coupon_id = '".$coupon['coupon_id']."'");
 		if (xtc_db_num_rows($redeem_query) == 0) {
 			// check for required session variables
 			$_SESSION['gv_id'] = $coupon['coupon_id'];
@@ -63,15 +68,24 @@ if (isset ($_GET['gv_no'])) {
 } else {
 	xtc_redirect(FILENAME_DEFAULT);
 }
-if ((!$error) && (isset ($_SESSION['customer_id']))) {
+
+if ($error === false && isset($_SESSION['customer_id'])) {
 	// Update redeem status
-	$gv_query = xtc_db_query("insert into  ".TABLE_COUPON_REDEEM_TRACK." (coupon_id, customer_id, redeem_date, redeem_ip) values ('".$coupon['coupon_id']."', '".$_SESSION['customer_id']."', now(),'".$REMOTE_ADDR."')");
-	$gv_update = xtc_db_query("update ".TABLE_COUPONS." set coupon_active = 'N' where coupon_id = '".$coupon['coupon_id']."'");
+	$sql_data_array = array('coupon_id' => $coupon['coupon_id'],
+	                        'customer_id' => $_SESSION['customer_id'],
+	                        'redeem_date' => 'now()',
+	                        'redeem_ip' => );
+	xtc_db_perform(TABLE_COUPON_REDEEM_TRACK, $sql_data_array);                        
+	xtc_db_query("UPDATE ".TABLE_COUPONS." 
+	                 SET coupon_active = 'N' 
+	               WHERE coupon_id = '".$coupon['coupon_id']."'");
 	xtc_gv_account_update($_SESSION['customer_id'], $_SESSION['gv_id']);
-	unset ($_SESSION['gv_id']);
+	unset($_SESSION['gv_id']);
 }
 
 $breadcrumb->add(NAVBAR_GV_REDEEM);
+
+require (DIR_WS_INCLUDES.'header.php');
 
 // if we get here then either the url gv_no was not set or it was invalid
 // so output a message.
@@ -79,6 +93,7 @@ $smarty->assign('coupon_amount', $xtPrice->xtcFormat($coupon['coupon_amount'], t
 $smarty->assign('error', $error);
 $smarty->assign('LINK_DEFAULT', '<a href="'.xtc_href_link(FILENAME_DEFAULT).'">'.xtc_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE).'</a>');
 $smarty->assign('language', $_SESSION['language']);
+
 $smarty->caching = 0;
 $main_content = $smarty->fetch(CURRENT_TEMPLATE.'/module/gv_redeem.html');
 
