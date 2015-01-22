43670f764b80b71912ca0caf3bc11b54d030afa7
modMarkus <markus@modified-shop.org>
2015-01-22 09:38:42 +0000

    tpl_modified - move additional css files to the bottom of the page
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7128 79ac0487-9261-4f26-a34d-858dee0dcce0

--- templates/tpl_modified/css/general.css.php
+++ templates/tpl_modified/css/general.css.php
@@ -16,8 +16,4 @@
    // Put CSS-Definitions here, these CSS-files will be loaded at the TOP of every page
 ?>
 
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/stylesheet.css" type="text/css" />
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery.toggle.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery.colorbox.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery.alerts.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery.bxslider.css" type="text/css" media="screen" />    
+<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/stylesheet.css" type="text/css" />    
