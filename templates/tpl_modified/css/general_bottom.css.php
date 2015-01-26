d7735738e56dcb4199fb72a1bc0fbfb3429cbaf5
modGTB <gerhard@modified-shop.org>
2015-01-26 08:33:47 +0000

    move stylesheet back to header
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7152 79ac0487-9261-4f26-a34d-858dee0dcce0

--- templates/tpl_modified/css/general_bottom.css.php
+++ templates/tpl_modified/css/general_bottom.css.php
@@ -15,30 +15,7 @@
 
    // This CSS file get includes at the BOTTOM of every template page in shop
    // you can add your template specific css scripts here
-
-$css_plain = DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/stylesheet.css';
-$css_min = DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/stylesheet.min.css';
-
-$css_file = '/stylesheet.css';
-if (COMPRESS_STYLESHEET == 'true' && (!is_file($css_min) || filemtime($css_min) != COMPRESS_STYLESHEET_TIME)) {
-  require_once(DIR_FS_EXTERNAL.'compactor/compactor.php');
-  
-  if (($css_content = file_get_contents($css_plain)) !== false) {
-    $compactor = new Compactor();
-    $css_content = $compactor->_simpleCodeCompress($css_content);
-    if (file_put_contents($css_min, $css_content, LOCK_EX) !== false) {
-      $css_file = '/stylesheet.min.css';
-    }
-    xtc_db_query("UPDATE ".TABLE_CONFIGURATION." 
-                     SET configuration_value = '".filemtime($css_min)."' 
-                   WHERE configuration_key = 'COMPRESS_STYLESHEET_TIME'");
-  }
-} elseif (COMPRESS_STYLESHEET == 'true' && is_file($css_min)) {
-  $css_file = '/stylesheet.min.css';
-}
 ?>
-
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.$css_file; ?>" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery.toggle.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery.colorbox.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>/css/jquery.alerts.css" type="text/css" media="screen" />
