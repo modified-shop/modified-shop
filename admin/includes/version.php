4e5e0d91bcd32c804b9d270725347d388dff450d
Tomcraft <Tomcraft@modified-shop.org>
2015-01-26 15:31:58 +0000

    Revised „/admin/includes/version.php“

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -10,5 +10,12 @@
 
 defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
 
+define('PROJECT_MAJOR_VERSION', '2');
+define('PROJECT_MINOR_VERSION', '00');
+define('PROJECT_REVISION', '6996');
+define('PROJECT_SERVICEPACK_VERSION', '');
+define('PROJECT_RELEASE_DATE', 'yyyy-mm-dd');
+define('MINIMUM_DB_VERSION', '200'); // currently not in use
+
 // Define the project version
-define('PROJECT_VERSION', 'modified eCommerce Shopssoftware v2.00');
+define('PROJECT_VERSION', 'modified eCommerce Shopssoftware v' . PROJECT_MAJOR_VERSION . '.' . PROJECT_MINOR_VERSION . ' rev ' . PROJECT_REVISION . ((PROJECT_SERVICEPACK_VERSION != '') ? ' SP' . PROJECT_SERVICEPACK_VERSION : ''). ' dated: ' . PROJECT_RELEASE_DATE);
