62026d4186f7a847abb4314165c3915c432e71d5
modGTB <gerhard@modified-shop.org>
2015-01-28 15:41:26 +0000

    layout specials
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7197 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/admin/specials.php
+++ lang/german/admin/specials.php
@@ -32,7 +32,6 @@ define('TEXT_SPECIALS_SPECIAL_PRICE', 'Angebotspreis:');
 define('TEXT_SPECIALS_SPECIAL_QUANTITY', 'Anzahl:');
 define('TEXT_SPECIALS_START_DATE', 'G&uuml;ltig ab: <small>(JJJJ-MM-TT)</small>');
 define('TEXT_SPECIALS_EXPIRES_DATE', 'G&uuml;ltig bis: <small>(JJJJ-MM-TT)</small>');
-define('TEXT_SPECIALS_PRICE_TIP', '<strong>Bemerkung:</strong><br>Sie k&ouml;nnen im Feld Angebotspreis auch prozentuale Werte angeben, z.B.: <strong>20%</strong><br>Wenn Sie einen neuen Preis eingeben, m&uuml;ssen die Nachkommastellen mit einem \'.\' getrennt werden, z.B.: <strong>49.99</strong><br>Lassen Sie das Feld <strong>\'G&uuml;ltig bis\'</strong> leer, wenn der Angebotspreis zeitlich unbegrenzt gelten soll.<br>Im Feld <strong>Anzahl</strong> k&ouml;nnen Sie die St&uuml;ckzahl eingeben, f&uuml;r die das Angebot gelten soll. Lassen Sie das Feld leer, wenn Sie die Anzahl nicht begrenzen wollen.');
 
 define('TEXT_INFO_DATE_ADDED', 'hinzugef&uuml;gt am:');
 define('TEXT_INFO_LAST_MODIFIED', 'letzte &Auml;nderung:');
@@ -47,4 +46,10 @@ define('TEXT_INFO_HEADING_DELETE_SPECIALS', 'Sonderangebot l&ouml;schen');
 define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass Sie das Sonderangebot l&ouml;schen m&ouml;chten?');
 
 define('TEXT_IMAGE_NONEXISTENT','Kein Bild verf&uuml;gbar!');
+
+define('TEXT_SPECIALS_PRICE_TIP', 'Sie k&ouml;nnen im Feld Angebotspreis auch prozentuale Werte angeben, z.B.: <strong>20%</strong><br>Wenn Sie einen neuen Preis eingeben, m&uuml;ssen die Nachkommastellen mit einem \'.\' getrennt werden, z.B.: <strong>49.99</strong>');
+define('TEXT_SPECIALS_QUANTITY_TIP', 'Im Feld <strong>Anzahl</strong> k&ouml;nnen Sie die St&uuml;ckzahl eingeben, f&uuml;r die das Angebot gelten soll.<br>Lassen Sie das Feld <strong>Anzahl</strong> leer, wenn Sie die Anzahl nicht begrenzen wollen.');
+define('TEXT_SPECIALS_DATE_FORMAT', 'Format: <strong>(JJJJ-MM-TT)</strong>');
+define('TEXT_SPECIALS_START_DATE_TIP', '<br>Geben Sie das Datum an, ab wann der Angebotspreis gelten soll.<br>');
+define('TEXT_SPECIALS_EXPIRES_DATE_TIP', '<br>Lassen Sie das Feld <strong>G&uuml;ltig bis</strong> leer, wenn der Angebotspreis zeitlich unbegrenzt gelten soll.<br>');
 ?>
