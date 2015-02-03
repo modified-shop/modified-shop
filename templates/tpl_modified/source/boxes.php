7d90fc0f8af30d3be3543e2b91a2fd5181a38d1d
GTB <GTB@modified-shop.org>
2015-02-03 13:34:01 +0000

    load not all boxes

--- templates/tpl_modified/source/boxes.php
+++ templates/tpl_modified/source/boxes.php
@@ -21,13 +21,46 @@ if (file_exists(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/source/inc/css_bu
   require_once ('templates/'.CURRENT_TEMPLATE.'/source/inc/css_button.inc.php');
 }
 
-//BOC require boxes
+// define full content sites
+$fullcontent = array(FILENAME_CHECKOUT_SHIPPING,
+                     FILENAME_CHECKOUT_PAYMENT,
+                     FILENAME_CHECKOUT_CONFIRMATION,
+                     FILENAME_CHECKOUT_SUCCESS,
+                     FILENAME_ACCOUNT,
+                     FILENAME_ACCOUNT_EDIT,
+                     FILENAME_ACCOUNT_HISTORY,
+                     FILENAME_ACCOUNT_HISTORY_INFO,
+                     FILENAME_ACCOUNT_PASSWORD,
+                     FILENAME_ACCOUNT_DELETE,
+                     FILENAME_CREATE_ACCOUNT,
+                     FILENAME_CREATE_GUEST_ACCOUNT,
+                     FILENAME_ADDRESS_BOOK,
+                     FILENAME_ADDRESS_BOOK_PROCESS,
+                     FILENAME_PASSWORD_DOUBLE_OPT,
+                     FILENAME_ADVANCED_SEARCH_RESULT,
+                     FILENAME_SHOPPING_CART,
+                     FILENAME_GV_SEND,
+                     FILENAME_NEWSLETTER,
+                     FILENAME_LOGIN,
+                     FILENAME_CONTENT,
+                     FILENAME_REVIEWS,
+                     );
+
 // -----------------------------------------------------------------------------------------
-//	Immer sichtbar
+//	full content
 // -----------------------------------------------------------------------------------------
-  require_once(DIR_FS_BOXES . 'categories.php');
-  require_once(DIR_FS_BOXES . 'manufacturers.php');
-  require_once(DIR_FS_BOXES . 'last_viewed.php');
+  if (!in_array(basename($PHP_SELF), $fullcontent)) {
+    require_once(DIR_FS_BOXES . 'categories.php');
+    require_once(DIR_FS_BOXES . 'manufacturers.php');
+    require_once(DIR_FS_BOXES . 'last_viewed.php');
+  } else {
+    // smarty full content
+    $smarty->assign('fullcontent', true);  
+  }
+
+// -----------------------------------------------------------------------------------------
+//	always visible
+// -----------------------------------------------------------------------------------------
   require_once(DIR_FS_BOXES . 'search.php');
   require_once(DIR_FS_BOXES . 'content.php');
   require_once(DIR_FS_BOXES . 'information.php');
@@ -37,81 +70,63 @@ if (file_exists(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/source/inc/css_bu
   require_once(DIR_FS_BOXES . 'loginbox.php');
   require_once(DIR_FS_BOXES . 'newsletter.php');
 // -----------------------------------------------------------------------------------------
-//	Nur, wenn Preise sichtbar
+//	onyl if show price
 // -----------------------------------------------------------------------------------------
   if ($_SESSION['customers_status']['customers_status_show_price'] == '1') {
     require_once(DIR_FS_BOXES . 'add_a_quickie.php');
     require_once(DIR_FS_BOXES . 'shopping_cart.php');
   }
 // -----------------------------------------------------------------------------------------
-//	In der Suche verborgen
+//	hide in search
 // -----------------------------------------------------------------------------------------
   if (substr(basename($PHP_SELF), 0,8) != 'advanced' && WHATSNEW_CATEGORIES === false) {
     require_once(DIR_FS_BOXES . 'whats_new.php'); 
   }
 // -----------------------------------------------------------------------------------------
-//	Nur fuer Admins
+//	admins only
 // -----------------------------------------------------------------------------------------
   if ($_SESSION['customers_status']['customers_status'] == '0') {
     require_once(DIR_FS_BOXES . 'admin.php');
     $smarty->assign('is_admin', true);
   }
 // -----------------------------------------------------------------------------------------
-//	Produkt-Detailseiten
+//	product details
 // -----------------------------------------------------------------------------------------
   if ($product->isProduct()) {
-    //Aktuelle Seite ist Produkt-Detailseite
     require_once(DIR_FS_BOXES . 'manufacturer_info.php');
   } else {
-    //Aktuelle Seite ist keine  Produkt-Detailseite
     require_once(DIR_FS_BOXES . 'best_sellers.php');
     if (SPECIALS_CATEGORIES === false) {
       require_once(DIR_FS_BOXES . 'specials.php');
     }
   }
 // -----------------------------------------------------------------------------------------
-//	Nur fuer eingeloggte Besucher
+//	only logged id users
 // -----------------------------------------------------------------------------------------
   if (isset($_SESSION['customer_id'])) {
     require_once(DIR_FS_BOXES . 'order_history.php');
   }
 // -----------------------------------------------------------------------------------------
-//	Nur, wenn Bewertungen erlaubt
+//	only if reviews allowed
 // -----------------------------------------------------------------------------------------
   if ($_SESSION['customers_status']['customers_status_read_reviews'] == '1') {
     require_once(DIR_FS_BOXES . 'reviews.php');
   }
 // -----------------------------------------------------------------------------------------
-//	Waehrend des Kauf-Abschlusses verborgen 
+//	hide during checkout
 // -----------------------------------------------------------------------------------------
   if (substr(basename($PHP_SELF), 0, 8) != 'checkout') {
     require_once(DIR_FS_BOXES . 'currencies.php');
   }
 // -----------------------------------------------------------------------------------------
-//EOC require boxes
 
 // -----------------------------------------------------------------------------------------
-// Smarty Zuweisung Startseite
+// Smarty home
 // -----------------------------------------------------------------------------------------
 $smarty->assign('home', ((basename($PHP_SELF) == FILENAME_DEFAULT && !isset($_GET['cPath']) && !isset($_GET['manufacturers_id'])) ? 1 : 0));
 
 // -----------------------------------------------------------------------------------------
-// Smarty Zuweisung Full content
-// -----------------------------------------------------------------------------------------
-$smarty->assign('fullcontent', strpos($PHP_SELF, 'checkout') 
-                            || strpos($PHP_SELF, 'account') 
-                            || strpos($PHP_SELF, 'address') 
-                            || strpos($PHP_SELF, 'password') 
-                            || strpos($PHP_SELF, FILENAME_ADVANCED_SEARCH_RESULT) 
-                            || strpos($PHP_SELF, FILENAME_SHOPPING_CART) 
-                            || strpos($PHP_SELF, FILENAME_GV_SEND) 
-                            || strpos($PHP_SELF, FILENAME_NEWSLETTER) 
-                            || strpos($PHP_SELF, FILENAME_LOGIN) 
-                            || strpos($PHP_SELF, FILENAME_CONTENT) 
-                            || strpos($PHP_SELF, FILENAME_REVIEWS)); 
-
-// -----------------------------------------------------------------------------------------
-// Smarty Zuweisung bestseller
+// Smarty bestseller
 // -----------------------------------------------------------------------------------------
 $smarty->assign('bestseller', strpos($PHP_SELF, FILENAME_LOGOFF) 
                            || strpos($PHP_SELF, FILENAME_CHECKOUT_SUCCESS) 
