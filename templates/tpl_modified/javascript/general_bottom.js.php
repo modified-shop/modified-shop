a4a0d4f76f5016060ea0f0c2b35035afc2e6b860
GTB <GTB@modified-shop.org>
2015-02-04 07:36:34 +0000

    popcart for new template

--- templates/tpl_modified/javascript/general_bottom.js.php
+++ templates/tpl_modified/javascript/general_bottom.js.php
@@ -15,25 +15,20 @@
 // this javascriptfile get includes at the BOTTOM of every template page in shop
 // you can add your template specific js scripts here
 ?>
-
 <script type="text/javascript">var DIR_WS_BASE="<?php echo DIR_WS_BASE ?>"</script>
-
 <script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery-1.8.3.min.js" type="text/javascript"></script>
 <script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.colorbox.min.js" type="text/javascript"></script>
 <script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.unveil.min.js" type="text/javascript"></script>
 <script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.alerts.min.js" type="text/javascript"></script>
 <script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.bxslider.min.js" type="text/javascript"></script>
-
-<?php require DIR_FS_CATALOG . DIR_TMPL_JS . 'get_states.js.php'; // Ajax State/District/Bundesland Updater - h-h-h ?><script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.easyTabs.min.js" type="text/javascript"></script>
-
+<?php require DIR_FS_CATALOG . DIR_TMPL_JS . 'get_states.js.php'; ?>
+<script src="<?php echo DIR_WS_BASE.DIR_TMPL_JS; ?>jquery.easyTabs.min.js" type="text/javascript"></script>
 <script type="text/javascript">
   $(document).ready(function(){
     $(".cbimages").colorbox({rel:'cbimages', scalePhotos:true, maxWidth: "90%", maxHeight: "90%", fixed: true});
     $(".iframe").colorbox({iframe:true, width:"780", height:"560", maxWidth: "90%", maxHeight: "90%", fixed: true});
-
     $(".unveil").show();
     $(".unveil").unveil(200);
-
     $('.bxcarousel_bestseller').bxSlider({
       minSlides: 6,
       maxSlides: 8,
@@ -41,45 +36,38 @@
       slideWidth: 109,
       slideMargin: 18
     });
-
     $(document).bind('cbox_complete', function(){
       if($('#cboxTitle').height() > 20){
         $("#cboxTitle").hide();
         $("<div>"+$("#cboxTitle").html()+"</div>").css({color: $("#cboxTitle").css('color')}).insertAfter("#cboxPhoto");
         $.fn.colorbox.resize();
       }
-    });
-      
+    });   
   });
 </script>
-
-
-
 <script type="text/javascript">
-  /*BOC jQuery Alerts*/
   $.alerts.overlayOpacity = .2;
   $.alerts.overlayColor = '#000';
   function alert(message, title) {
     title = title || 'Information';
     jAlert(message, title);
   }
-  /*EOC jQuery Alerts*/
-	
-	/* BOC jQuery Shopping Cart */
   $(function() {
     $('#toggle_cart').click(function() {
       $('.toggle_cart').slideToggle('slow');
       return false;
     });
-
     $("html").not('.toggle_cart').bind('click',function(e) {
       $('.toggle_cart').slideUp('slow');
     });
-  });     
-	/* EOC jQuery Shopping Cart */
-
+    <?php if (isset($_SESSION['new_products_id_in_cart'])) {
+      unset($_SESSION['new_products_id_in_cart']); ?>
+      $('.toggle_cart').slideToggle('slow');
+      timer = setTimeout(function(){$('.toggle_cart').slideUp('slow');}, 3000);
+      $('.toggle_cart').mouseover(function() {clearTimeout(timer);});
+    <?php } ?>
+ });     
 </script>
-
 <?php if (strstr($PHP_SELF, FILENAME_PRODUCT_INFO )) { ?>
 <script type="text/javascript">
     $.get("<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>"+"/css/jquery.easyTabs.css", function(css) {
@@ -95,7 +83,6 @@
     });
 </script>
 <?php } ?>
-
 <?php if (strstr($PHP_SELF, 'checkout')) { ?>
 <script type="text/javascript">
     $.get("<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE; ?>"+"/css/jquery.easyTabs.css", function(css) {
