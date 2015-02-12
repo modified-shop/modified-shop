e7e0db870f8720220bda716561602edf9661d8b9
GTB <GTB@modified-shop.org>
2015-02-12 17:08:09 +0000

    remove lang definition

--- admin/includes/modules/system/easybillcsv.php
+++ admin/includes/modules/system/easybillcsv.php
@@ -15,24 +15,6 @@
 
 defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
 
-define('MODULE_EASYBILL_CSV_TEXT_DESCRIPTION', 'Export - easyBill.de<br />Online Rechnungen einfach erstellen');
-define('MODULE_EASYBILL_CSV_TEXT_TITLE', 'easybill - CSV Export');
-define('MODULE_EASYBILL_CSV_ORDER_STATUS_DESC', 'mit welchem Bestell-Status sollen Bestellungen exportiert werden? <br/>(mehrfach Auswahl mit gedr&uuml;ckter SHIFT Taste m&ouml;glich)');
-define('MODULE_EASYBILL_CSV_ORDER_STATUS_TITLE','<hr /><strong>Bestellstatus</strong>');
-define('MODULE_EASYBILL_CSV_CUSTOMER_STATUS_DESC','mit welchem Kunden-Status sollen Bestellungen exportiert werden? <br/>(mehrfach Auswahl mit gedr&uuml;ckter SHIFT Taste m&ouml;glich)');
-define('MODULE_EASYBILL_CSV_CUSTOMER_STATUS_TITLE','<hr /><strong>Kundenstatus</strong>');
-define('MODULE_EASYBILL_CSV_ORDER_DATE_DESC','ab welchem Bestell-Datum sollen Bestellungen exportiert werden?');
-define('MODULE_EASYBILL_CSV_ORDER_DATE_TITLE','<strong>Bestelldatum</strong>');
-define('MODULE_EASYBILL_CSV_EXPORT_YES','Nur Herunterladen');
-define('MODULE_EASYBILL_CSV_EXPORT_NO','Am Server Speichern');
-define('MODULE_EASYBILL_CSV_EXPORT_CRON', 'Cronjob URL erzeugen');
-define('MODULE_EASYBILL_CSV_CRON_URL_TITLE', '<hr /><strong>URL f&uuml;r Cronjob</strong>');
-define('MODULE_EASYBILL_CSV_CRON_URL_DESC','Diese URL muss in easyBill eingetragen werden, damit die Bestellungen automatisch abgeholt werden.');
-define('MODULE_EASYBILL_CSV_EXPORT','Bitte den Export AUF KEINEN FALL unterbrechen. Dieser kann einige Minuten in Anspruch nehmen.');
-define('MODULE_EASYBILL_CSV_EXPORT_TYPE','<hr /><strong>Speicherart:</strong>');
-define('MODULE_EASYBILL_CSV_ERROR', 'Es wurde keine Bestellung f&uuml;r die ausgew&auml;hlten Kriterien gefunden.');
-define('MODULE_EASYBILL_CSV_STANDARD_TAX_CLASS', '1');
-
 // include needed classes
 require_once (DIR_WS_CLASSES . 'order.php');
 require_once (DIR_FS_CATALOG . 'includes/classes/xtcPrice.php');
