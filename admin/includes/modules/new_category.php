056dba52af9459668219c606bef0275fef6d29e6
web28 <web28@modified-shop.org>
2015-02-26 10:44:54 +0000

    change inline css
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7624 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_category.php
+++ admin/includes/modules/new_category.php
@@ -95,8 +95,8 @@
         </tr>
         <tr>
           <td class="main"><?php echo TEXT_EDIT_PRODUCT_SORT_ORDER; ?>:</td>
-          <td class="main"><?php echo xtc_draw_pull_down_menu('products_sorting',$order_array,((xtc_not_null($cInfo->products_sorting))?$cInfo->products_sorting:$default_value), 'style="width: 155px"'); ?><br/>
-                           <?php echo xtc_draw_pull_down_menu('products_sorting2',$order_array_desc,$cInfo->products_sorting2, 'style="width: 155px; margin-top: 8px;"'); ?></td>
+          <td class="main"><?php echo xtc_draw_pull_down_menu('products_sorting',$order_array,((xtc_not_null($cInfo->products_sorting))?$cInfo->products_sorting:$default_value), 'style="width: 155px"'); ?>
+                           <?php echo xtc_draw_pull_down_menu('products_sorting2',$order_array_desc,$cInfo->products_sorting2, 'style="width: 155px; margin-left: 5px;"'); ?></td>
         </tr>
         <tr>
           <td class="main"><?php echo TEXT_EDIT_SORT_ORDER; ?></td>
