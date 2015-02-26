1f7439cc5f7180b83441f1a2cf9e6efcd87698e3
Tomcraft <Tomcraft@modified-shop.org>
2015-02-26 15:32:52 +0000

    Add english language definitions for newsfeed.php on r7584

--- lang/english/admin/english.php
+++ lang/english/admin/english.php
@@ -538,6 +538,12 @@ define('SPECIALS_DATE_END_TT', 'Offers end at midnight (23.59.59)');
 define('BOX_PARCEL_CARRIERS', 'Parcel carriers');
 define('TEXT_DISPLAY_NUMBER_OF_CARRIERS', 'Displaying <b>%d</b> to <b>%d</b> (of <b>%d</b> carriers)');
 
+define('RSS_FEED_TITLE', 'Latest informations from modified eCommerce Shopsoftware Blog');
+define('RSS_FEED_DESCRIPTION', 'Latest informations from modified eCommerce Shopsoftware Support Forum');
+define('RSS_FEED_LINK', 'http://www.modified-shop.org/blog');
+define('RSS_FEED_ALTERNATIVE', 'Unfortunately the latest news cannot be displayed in the RSS feed. Please visit our Blog at <a href="'.RSS_FEED_LINK.'">www.modified-shop.org/blog</a> to get important informations for shop maintainers concerning the following topics: <ul><li>Important Updates and Fixes</li><li>Function extensions</li><li>jurisdictions</li><li>News</li><li>Gossip</li></ul>');
+define('TEXT_DISPLAY_NUMBER_OF_NEWSFEED', 'Displaying <b>%d</b> to <b>%d</b> (of <b>%d</b> News)');
+
 define('CFG_TXT_YES', 'Yes');
 define('CFG_TXT_NO', 'No');
 define('CFG_TXT_OR', 'or');
