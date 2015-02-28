cb540c37fffd975b75e8138eb343b3b0d0e3fdde
h-h-h <h-h-h@modified-shop.org>
2015-02-28 00:16:17 +0000

    load new janolaw content only as admin, fix velocity problems
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7672 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/extra/application_top_end/30_janolaw.php
+++ includes/extra/application_top_end/30_janolaw.php
@@ -1,6 +1,8 @@
 <?php
 if (defined('MODULE_JANOLAW_STATUS') && MODULE_JANOLAW_STATUS == 'True') {
-  require_once(DIR_FS_EXTERNAL.'janolaw/janolaw.php');
-  $janolaw = new janolaw_content();
+  if ($_SESSION['customers_status']['customers_status_id'] == 0) {
+    require_once(DIR_FS_EXTERNAL.'janolaw/janolaw.php');
+    $janolaw = new janolaw_content();
+  }
 }
 ?>
