b39952c033845552df07b57ca45c90e41f1d47f7
modGTB <gerhard@modified-shop.org>
2015-03-03 11:47:40 +0000

    update orders heading
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7695 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/orders_info_blocks.php
+++ admin/includes/modules/orders_info_blocks.php
@@ -13,8 +13,8 @@
 ?>
  
       <div class="pageHeadingImage"><?php echo xtc_image(DIR_WS_ICONS.'heading/icon_orders.png'); ?></div>
-      <div class="pageHeading pdg2"><?php echo HEADING_TITLE . '<span style="font-size:13px"> - '.TABLE_HEADING_ORDERS_ID.': ' . $oID . ' - ' . $order->info['date_purchased'] ; ?></span></div>
-      <div class="main pdg2"><?php echo TABLE_HEADING_CUSTOMERS ?></div>
+      <div class="pageHeading pdg2"><?php echo HEADING_TITLE;</div>
+      <div class="main pdg2"><?php echo TABLE_HEADING_ORDERS_ID.': ' . $oID . ' - ' . xtc_datetime_short($order->info['date_purchased']); ?></div>
       <div class="clear" style="margin-left: 5px;">     
         <a class="button" href="<?php echo xtc_href_link(FILENAME_ORDERS, xtc_get_all_get_params(array('action')));?>"><?php echo BUTTON_BACK; ?></a>
         <a class="button" href="<?php echo xtc_href_link(FILENAME_ORDERS_EDIT, 'oID='.$oID.'&cID=' . $order->customer['ID']);?>"><?php echo BUTTON_EDIT ?></a>        
