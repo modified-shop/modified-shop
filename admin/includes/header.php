13d82afe1644efcb0a3216b63eff800e0aae45a7
modGTB <gerhard@modified-shop.org>
2015-03-04 10:43:32 +0000

    show always lang icons in admin
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7714 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -30,10 +30,13 @@
   
   // Admin Language Switch
   $languages_string = '';
-  if (!isset($_GET['action']) || $_GET['action'] == 'edit') {
-    $ls_languages = xtc_get_languages();
-    if (count($ls_languages) > 1) {
-      while (list($key, $value) = each($ls_languages)) {
+  
+  $ls_languages = xtc_get_languages();
+  if (count($ls_languages) > 1) {
+    while (list($key, $value) = each($ls_languages)) {
+      if (!isset($_GET['action']) || $_GET['action'] == 'edit') {
+        $languages_string .= '&nbsp;' . xtc_image('../lang/' .  $value['directory'] .'/admin/images/' . $value['image'], $value['name']);
+      } else {
         $languages_string .= '&nbsp;<a href="' . xtc_href_link($current_page, xtc_get_all_get_params(array('language', 'currency')).'language=' . $value['code'], 'NONSSL') . '">' . xtc_image('../lang/' .  $value['directory'] .'/admin/images/' . $value['image'], $value['name']) . '</a>';
       }
     }
