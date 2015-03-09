d38d06cc742b1ae79e31995d4f3063c1822090e1
Tomcraft <Tomcraft@modified-shop.org>
2015-03-09 14:16:50 +0000

    Revised installer (remove all obsolete files & folders since shopversion 1.00 during update)

--- _installer/includes/delete_dirs.php
+++ _installer/includes/delete_dirs.php
@@ -1,13 +1,19 @@
 <?php
 // set all directories to be deleted                     
-$unlink_dir = array('admin/includes/modules/carp',
+$unlink_dir = array('admin/includes/local', // neu
+                    'admin/includes/modules/carp',
                     'admin/includes/modules/fckeditor',
                     'admin/includes/modules/magpierss',
                     'admin/includes/modules/magpierss/extlib',
                     'admin/rss',
+                    'images/infobox', // neu
                     'includes/classes/Smarty_2.6.22',
                     'includes/classes/Smarty_2.6.26',
+                    'includes/econda', // neu
                     'includes/iclear',
                     'includes/janolaw',
-                    'shopstat');
+                    'includes/nusoap', // neu
+                    'shopstat',
+                    'xtc_installer', // neu
+                    );
 ?>
