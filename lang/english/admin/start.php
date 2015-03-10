dcfe16efb7818bbe021789a0337778aa6730fb92
GTB <GTB@modified-shop.org>
2015-03-10 09:27:23 +0000

    update admin start page

--- lang/english/admin/start.php
+++ lang/english/admin/start.php
@@ -109,7 +109,7 @@ define('TOTAL_TURNOVER', 'Total turnover');
 // main heading
 define('HEADING_TITLE', 'Welcome to the Admin Area');
 // users online
-define('TABLE_CAPTION_USERS_ONLINE', 'Users Online (last %s Customers)');
+define('TABLE_CAPTION_USERS_ONLINE', 'Online');
 define('TABLE_CAPTION_USERS_ONLINE_HINT', '***Please click user name for details***');
 define('TABLE_HEADING_USERS_ONLINE_SINCE', 'Online Since');
 define('TABLE_HEADING_USERS_ONLINE_NAME', 'Name');
@@ -117,7 +117,7 @@ define('TABLE_HEADING_USERS_ONLINE_LAST_CLICK', 'Last Click');
 define('TABLE_HEADING_USERS_ONLINE_INFO', 'Info');
 define('TABLE_CELL_USERS_ONLINE_INFO', 'More...');
 // new customers
-define('TABLE_CAPTION_NEW_CUSTOMERS', 'New Customers');
+define('TABLE_CAPTION_NEW_CUSTOMERS', 'Customers');
 define('TABLE_CAPTION_NEW_CUSTOMERS_COMMENT', '(Last 15)');
 define('TABLE_HEADING_NEW_CUSTOMERS_LASTNAME', 'Last Name');
 define('TABLE_HEADING_NEW_CUSTOMERS_FIRSTNAME', 'First Name');
@@ -128,7 +128,7 @@ define('TABLE_CELL_NEW_CUSTOMERS_EDIT', 'Edit...');
 define('TABLE_CELL_NEW_CUSTOMERS_DELETE', 'Delete...');
 define('TABLE_CELL_NEW_CUSTOMERS_ORDERS', 'Show...');
 // new orders
-define('TABLE_CAPTION_NEW_ORDERS', 'New Orders');
+define('TABLE_CAPTION_NEW_ORDERS', 'Orders');
 define('TABLE_CAPTION_NEW_ORDERS_COMMENT', '(Last 20)');
 define('TABLE_HEADING_NEW_ORDERS_ORDER_NUMBER', 'Order #');
 define('TABLE_HEADING_NEW_ORDERS_ORDER_DATE', 'Order Date');
