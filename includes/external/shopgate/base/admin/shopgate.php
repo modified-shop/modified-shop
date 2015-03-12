bd006724f2679bbc183d469ebad377d9ef55c293
Tomcraft1980 <hallo@modified-shop.org>
2015-03-12 16:37:27 +0000

    Revised shopgate module
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7896 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/shopgate/base/admin/shopgate.php
+++ includes/external/shopgate/base/admin/shopgate.php
@@ -414,26 +414,30 @@ echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.
 	$tableClass = 'dataTableContent';
 ##### XTC3 | XTCM EOF #####
 ?>
-<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
+<body>
 
 	<!-- header //-->
 	<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
 	<!-- header_eof //-->
 
 	<!-- body //-->
-	<table border="0" width="100%" cellspacing="2" cellpadding="2">
-		<tr>
-			<td class="columnLeft2" width="<?php echo BOX_WIDTH; ?>" valign="top">
-				<table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1"
-					cellpadding="1" class="columnLeft">
-					<!-- left_navigation //-->
-					<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
-					<!-- left_navigation_eof //-->
-				</table>
-			</td>
-			<!-- body_text //-->
-			<td class="boxCenter" width="100%" valign="top" style="height: 100%;">
-				<table border="0" width="100%" cellspacing="0" cellpadding="2" style="height:100%;">
+  <table class="tableBody">
+    <tr>
+      <?php //left_navigation
+      if (USE_ADMIN_TOP_MENU == 'false') {
+        echo '<td class="columnLeft2">'.PHP_EOL;
+        echo '<!-- left_navigation //-->'.PHP_EOL;       
+        require_once(DIR_WS_INCLUDES . 'column_left.php');
+        echo '<!-- left_navigation eof //-->'.PHP_EOL; 
+        echo '</td>'.PHP_EOL;      
+      }
+      ?>
+      <!-- body_text //-->
+			<td class="boxCenter">
+        <div class="pageHeadingImage"><?php echo xtc_image(DIR_WS_ICONS.'heading/icon_modules.png'); ?></div>
+        <div class="pageHeading pdg2">Shopgate</div>
+        <div class="main">Modules</div>   
+				<table class="tableCenter">
           <tr>
             <td valign="middle" class="dataTableHeadingContent" style="width:250px;">
               <?php echo SHOPGATE_CONFIG_TITLE; ?>
