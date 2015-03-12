4549f78a20f711d204fb2609c271a14493fafd80
GTB <GTB@modified-shop.org>
2015-03-12 10:57:01 +0000

    fix width select

--- admin/stats_sales_report.php
+++ admin/stats_sales_report.php
@@ -290,7 +290,7 @@
                                     $year = date("Y", $startDate);                                  
                                   }
                                   echo REPORT_START_DATE.'<br/>';
-                                  echo '<span class="select_w80">'.xtc_draw_pull_down_menu('startD', $day_array, $day).'</span>';
+                                  echo '<span class="select_w60">'.xtc_draw_pull_down_menu('startD', $day_array, $day).'</span>';
                                   echo '<span class="select_w120">'.xtc_draw_pull_down_menu('startM', $month_array, $month).'</span>';
                                   echo '<span class="select_w80">'.xtc_draw_pull_down_menu('startY', $year_array, $year).'</span>';
                                 ?>
@@ -335,7 +335,7 @@
                                     $year = date("Y", $endDate - (60* 60 * 24));
                                   }
                                   echo REPORT_END_DATE.'<br/>';
-                                  echo '<span class="select_w80">'.xtc_draw_pull_down_menu('endD', $day_array, $day).'</span>';
+                                  echo '<span class="select_w60">'.xtc_draw_pull_down_menu('endD', $day_array, $day).'</span>';
                                   echo '<span class="select_w120">'.xtc_draw_pull_down_menu('endM', $month_array, $month).'</span>';
                                   echo '<span class="select_w80">'.xtc_draw_pull_down_menu('endY', $year_array, $year).'</span>';
                                 ?>
