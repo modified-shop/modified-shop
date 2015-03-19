2bd53fc1f92551d09c6fcc4714609dc411fae773
GTB <GTB@modified-shop.org>
2015-03-19 14:00:18 +0000

    prepare for DIR_WS_BASE

--- templates/xtc5/source/boxes/manufacturer_info.php
+++ templates/xtc5/source/boxes/manufacturer_info.php
@@ -59,7 +59,7 @@
         if (!file_exists($image)) $image = DIR_WS_IMAGES . 'manufacturers/noimage.gif';
         // EOF - Tomcraft - 2009-10-30 - noimage.gif is displayed, when no image is defined
       }
-      $box_smarty->assign('IMAGE', DIR_WS_BASE.$image);
+      $box_smarty->assign('IMAGE', (($image != '') ? DIR_WS_BASE . $image : ''));
       $box_smarty->assign('NAME',$manufacturer['manufacturers_name']);
       if ($manufacturer['manufacturers_url']!='')
         $box_smarty->assign('URL','<a href="' . xtc_href_link(FILENAME_REDIRECT, 'action=manufacturer&'.xtc_manufacturer_link($manufacturer['manufacturers_id'],$manufacturer['manufacturers_name'])) . '" onclick="window.open(this.href); return false;">' . sprintf(BOX_MANUFACTURER_INFO_HOMEPAGE, $manufacturer['manufacturers_name']) . '</a>');
