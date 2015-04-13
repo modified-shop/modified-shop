ce35ad30df9b31cde3299e351060bd45640e6715
GTB <GTB@modified-shop.org>
2015-04-13 12:04:26 +0000

    update sql caching

--- inc/db_functions.inc.php
+++ inc/db_functions.inc.php
@@ -95,4 +95,56 @@
     }
     return $result;
   }
+
+
+  function xtc_db_queryCached($query, $link='db_link') {
+    global $$link;
+
+    if (defined('STORE_DB_TRANSACTIONS') && STORE_DB_TRANSACTIONS == 'true') {    
+      $queryStartTime = array_sum(explode(" ",microtime()));
+    }
+
+    // get HASH ID for filename
+    $id = md5($query);
+
+    // cache File Name
+    $file = SQL_CACHEDIR . $id . '.mod.cache';
+
+    // file life time
+    $expire = DB_CACHE_EXPIRE;
+
+    if (file_exists($file) && filemtime($file) > (time() - $expire)) {
+
+      // get cached resulst
+      $result = unserialize(base64_decode(file_get_contents($file)));
+
+      if (defined('STORE_DB_TRANSACTIONS') && STORE_DB_TRANSACTIONS == 'true') {
+        $queryEndTime = array_sum(explode(" ",microtime())); 
+        $processTime = number_format(round($queryEndTime - $queryStartTime, 3), 3, '.', '');
+        if (defined('STORE_DB_SLOW_QUERY') && ((STORE_DB_SLOW_QUERY == 'true' && $processTime >= STORE_DB_SLOW_QUERY_TIME) || STORE_DB_SLOW_QUERY == 'false')) {
+          error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' [' . $processTime . 's] ' . 'QUERY CACHED ' . $query . "\n", 3, DIR_FS_LOG.'mod_sql_' .date('Y-m-d') .'.log');
+        }
+      }
+
+    } else {
+
+      if (file_exists($file)) @unlink($file);
+
+      // get result from DB and create new file
+      $result = xtc_db_query($query, $link);
+
+      // fetch data into array
+      $records = array();
+      while ($record = xtc_db_fetch_array($result)) {
+        $records[] = $record;
+      }
+      
+      // safe result into file.
+      file_put_contents($file, base64_encode(serialize($records)));
+      $result = $records;
+    }
+
+    return $result;
+  }
+
 ?>
