234e41e7d8e124a85dbab06b69e1841e72822289
modGTB <gerhard@modified-shop.org>
2015-05-15 09:29:11 +0000

    fix add select
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8209 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/define_add_select.php
+++ includes/define_add_select.php
@@ -18,14 +18,14 @@ $add_select_default[] = 'p.products_extra_field';
 
 */
   // used in /includes/modules/default.php
-  define('ADD_SELECT_DEFAULT', 'p.products_manufacturers_model, '.(isset($add_select_default)?implode(', ', $add_select_default):''));
+  define('ADD_SELECT_DEFAULT', 'p.products_manufacturers_model, '.(isset($add_select_default) ? trim(implode(', ', $add_select_default), ',').', ' : ''));
   
   // used in /advanced_search_result.php
-  define('ADD_SELECT_SEARCH', 'p.products_manufacturers_model, '.(isset($add_select_search)?implode(', ', $add_select_search):''));
+  define('ADD_SELECT_SEARCH', 'p.products_manufacturers_model, '.(isset($add_select_search) ? trim(implode(', ', $add_select_search), ',').', ' : ''));
   
   // used in /includes/classes/product.php
-  define('ADD_SELECT_PRODUCT', (isset($add_select_product)?implode(', ', $add_select_product):''));
-
+  define('ADD_SELECT_PRODUCT', (isset($add_select_product) ? trim(implode(', ', $add_select_product), ',').', ' : ''));
+  
   // used in /includes/classes/shopping_cart.php
-  define('ADD_SELECT_CART', (isset($add_select_cart)?implode(', ', $add_select_cart):''));
+  define('ADD_SELECT_CART', (isset($add_select_cart) ? trim(implode(', ', $add_select_cart), ',').', ' : ''));
 ?>
