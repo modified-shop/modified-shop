d8b607253e4e827373a362c01f77d28def90fd4d
modGTB <gerhard@modified-shop.org>
2015-06-02 11:24:25 +0000

    update xtc_redirect for installer
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8261 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/xtc_redirect.inc.php
+++ inc/xtc_redirect.inc.php
@@ -36,7 +36,10 @@
 
     // save SESSION before redirect
     session_write_close();
-    xtc_db_close();
+
+    if (function_exists('xtc_db_close')) {
+      xtc_db_close();
+    }
     
     header('Location: ' . preg_replace("/[\r\n]+(.*)$/i", "", html_entity_decode($url)));
     exit();
