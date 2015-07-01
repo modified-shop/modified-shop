ab5b87c32f469cb3a5d03269ca4d03244866b579
Tomcraft1980 <hallo@modified-shop.org>
2015-07-01 14:12:33 +0000

    Revoked changes from r8348 (was already fixed in r8347)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8358 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_product.php
+++ admin/includes/modules/new_product.php
@@ -304,7 +304,7 @@
     }
     ?>
 
-    <div>
+    <div style="padding:5px;">
        <!-- BOF Product images //-->
       <div class="main div_header"><?php echo HEADING_PRODUCT_IMAGES; ?></div>
         <?php
