fc811c93a1ba5036ebd826d4c8420dfa86fb18d1
Tomcraft1980 <hallo@modified-shop.org>
2015-07-30 09:42:49 +0000

    Fix #591
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8504 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/products_media.php
+++ includes/modules/products_media.php
@@ -59,7 +59,10 @@ if (xtc_db_num_rows($content_query, true) > 0) {
                   '</a>';
         $filesize = xtc_filesize($content_data['content_file']);
       } elseif ($content_data['content_file'] != '') {
-        $button = '<a href="'.xtc_href_link('media/products/'.$content_data['content_file']).'">'.xtc_image_button('button_download.gif', TEXT_DOWNLOAD).'</a>';
+        $button = '<a target="_blank"'.
+                  'href="'.xtc_href_link('media/products/'.$content_data['content_file']).'">'.
+                  xtc_image_button('button_download.gif', TEXT_DOWNLOAD).
+                  '</a>';
         $filesize = xtc_filesize($content_data['content_file']);
       }
     }
