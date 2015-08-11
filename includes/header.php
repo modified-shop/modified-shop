b6fcd9b0e1267ca3ddaefda2a1f9d4e97e9edeb6
GTB <GTB@modified-shop.org>
2015-08-11 16:00:41 +0000

    new favicon function (fix #608)

--- includes/header.php
+++ includes/header.php
@@ -53,7 +53,7 @@ defined('TEMPLATE_HTML_ENGINE') or define('TEMPLATE_HTML_ENGINE', 'xhtml');
 <html<?php echo ((TEMPLATE_HTML_ENGINE == 'xhtml') ? ' '.HTML_PARAMS : ' lang="'.$_SESSION['language_code'].'"'); ?>>
 <head>
 <?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
-<link rel="shortcut icon" href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER).DIR_WS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/favicon.ico';?>" type="image/x-icon" />
+<?php include(DIR_WS_MODULES.'favicon.php'); ?>
 <?php
 /*
   The following copyright announcement is in compliance
