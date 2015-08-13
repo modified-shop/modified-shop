932ac5af9b073015377953186e421575fe4b91e2
Tomcraft <Tomcraft@modified-shop.org>
2015-08-13 16:29:25 +0000

    Fix installer for Strato

--- _installer/includes/application.php
+++ _installer/includes/application.php
@@ -21,6 +21,7 @@
   include('includes/version.php');
   
   // Set the level of error reporting
+  @ini_set('display_errors', true);
   error_reporting(E_ALL & ~E_NOTICE);
   
   if (INSTALL_CHARSET == 'utf8') {
@@ -152,15 +153,18 @@
       $domain = str_replace($tmp[0].'.','',$domain);
     }
     $document_root = str_replace($_SERVER["PHP_SELF"],'',$_SERVER["SCRIPT_FILENAME"]);
+    //Unterverzeichnis ermitteln
+    $tmp = explode(DIR_MODIFIED_INSTALLER, $_SERVER["PHP_SELF"]);
+    $subdir = $tmp[0];
     //Prüfen ob Domain im Pfad enthalten ist, wenn nein Pfad Stratopfad erzeugen: /home/strato/www/ersten zwei_buchstaben/www.wunschname.de/htdocs/
     if(stristr($document_root, $domain) === FALSE) {
-      //Erste 2 Buchstaben der Domain ermittlen
-      $domain2 = substr($tmp[count($tmp)-2], 0, 2);
-      //Korrektur Unterverzeichnis
+      //Korrektur Unterverzeichnis      
       $htdocs = str_replace($_SERVER["SCRIPT_NAME"],'',$_SERVER["SCRIPT_FILENAME"]);
       $htdocs = '/htdocs' . str_replace($_SERVER["DOCUMENT_ROOT"],'',$htdocs);
       //MUSTER: /home/strato/www/wu/www.wunschname.de/htdocs/
-      $document_root = '/home/strato/www/'.$domain2. '/www.'.$domain.$htdocs;
+      $document_root = '/home/strato/www/'.substr($domain, 0, 2). '/www.'.$domain.$htdocs.$subdir;
+    } else {
+      $document_root .= $subdir;
     }
     return $document_root;
   }
