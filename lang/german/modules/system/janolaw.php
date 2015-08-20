f757c5a510da0779612169bdafef293ee069c531
modGTB <gerhard@modified-shop.org>
2015-08-20 21:27:50 +0000

    update janolaw
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8689 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/system/janolaw.php
+++ lang/german/modules/system/janolaw.php
@@ -35,37 +35,39 @@ define('MODULE_JANOLAW_ERROR', 'Bitte pr&uuml;fen sie die Zuordnung der Dokument
 
 define('MODULE_JANOLAW_TYPE_DATASECURITY_TITLE', '<hr noshade>Rechtstext Datenschutz');
 define('MODULE_JANOLAW_TYPE_DATASECURITY_DESC', 'Bitte geben Sie an, in welcher Seite dieser Rechtstext automatisch eingef&uuml;gt werden soll.');
-define('MODULE_JANOLAW_PDF_DATASECURITY_TITLE', 'Zus&auml;tzlich als PDF speichern');
-define('MODULE_JANOLAW_PDF_DATASECURITY_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert werden?');
+define('MODULE_JANOLAW_PDF_DATASECURITY_TITLE', 'PDF als Download');
+define('MODULE_JANOLAW_PDF_DATASECURITY_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert und ein Link eingef&uuml;gt werden?<br/><b>Wichtig:</b> Das funktioniert nur in der HTML Version!');
 define('MODULE_JANOLAW_MAIL_DATASECURITY_TITLE', 'PDF als E-Mail Anhang');
-define('MODULE_JANOLAW_MAIL_DATASECURITY_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?<br/><b>Wichtig:</b> Das Dokument muss als PDF gespeichert werden!');
+define('MODULE_JANOLAW_MAIL_DATASECURITY_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?');
 
 define('MODULE_JANOLAW_TYPE_TERMS_TITLE', '<hr noshade>Rechtstext AGB');
 define('MODULE_JANOLAW_TYPE_TERMS_DESC', 'Bitte geben Sie an, in welcher Seite dieser Rechtstext automatisch eingef&uuml;gt werden soll.');
-define('MODULE_JANOLAW_PDF_TERMS_TITLE', 'Zus&auml;tzlich als PDF speichern');
-define('MODULE_JANOLAW_PDF_TERMS_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert werden?');
+define('MODULE_JANOLAW_PDF_TERMS_TITLE', 'PDF als Download');
+define('MODULE_JANOLAW_PDF_TERMS_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert und ein Link eingef&uuml;gt werden?<br/><b>Wichtig:</b> Das funktioniert nur in der HTML Version!');
 define('MODULE_JANOLAW_MAIL_TERMS_TITLE', 'PDF als E-Mail Anhang');
-define('MODULE_JANOLAW_MAIL_TERMS_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?<br/><b>Wichtig:</b> Das Dokument muss als PDF gespeichert werden!');
+define('MODULE_JANOLAW_MAIL_TERMS_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?');
 
 define('MODULE_JANOLAW_TYPE_LEGALDETAILS_TITLE', '<hr noshade>Rechtstext Impressum');
 define('MODULE_JANOLAW_TYPE_LEGALDETAILS_DESC', 'Bitte geben Sie an, in welcher Seite dieser Rechtstext automatisch eingef&uuml;gt werden soll.');
-define('MODULE_JANOLAW_PDF_LEGALDETAILS_TITLE', 'Zus&auml;tzlich als PDF speichern');
-define('MODULE_JANOLAW_PDF_LEGALDETAILS_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert werden?');
+define('MODULE_JANOLAW_PDF_LEGALDETAILS_TITLE', 'PDF als Download');
+define('MODULE_JANOLAW_PDF_LEGALDETAILS_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert und ein Link eingef&uuml;gt werden?<br/><b>Wichtig:</b> Das funktioniert nur in der HTML Version!');
 define('MODULE_JANOLAW_MAIL_LEGALDETAILS_TITLE', 'PDF als E-Mail Anhang');
-define('MODULE_JANOLAW_MAIL_LEGALDETAILS_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?<br/><b>Wichtig:</b> Das Dokument muss als PDF gespeichert werden!');
+define('MODULE_JANOLAW_MAIL_LEGALDETAILS_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?');
 
 define('MODULE_JANOLAW_TYPE_REVOCATION_TITLE', '<hr noshade>Rechtstext Widerruf');
 define('MODULE_JANOLAW_TYPE_REVOCATION_DESC', 'Bitte geben Sie an, in welcher Seite dieser Rechtstext automatisch eingef&uuml;gt werden soll.');
-define('MODULE_JANOLAW_PDF_REVOCATION_TITLE', 'Zus&auml;tzlich als PDF speichern');
-define('MODULE_JANOLAW_PDF_REVOCATION_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert werden?');
+define('MODULE_JANOLAW_PDF_REVOCATION_TITLE', 'PDF als Download');
+define('MODULE_JANOLAW_PDF_REVOCATION_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert und ein Link eingef&uuml;gt werden?<br/><b>Wichtig:</b> Das funktioniert nur in der HTML Version!');
 define('MODULE_JANOLAW_MAIL_REVOCATION_TITLE', 'PDF als E-Mail Anhang');
-define('MODULE_JANOLAW_MAIL_REVOCATION_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?<br/><b>Wichtig:</b> Das Dokument muss als PDF gespeichert werden!');
+define('MODULE_JANOLAW_MAIL_REVOCATION_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?');
 
 define('MODULE_JANOLAW_TYPE_WITHDRAWAL_TITLE', '<hr noshade>Rechtstext Widerrufsformular');
 define('MODULE_JANOLAW_TYPE_WITHDRAWAL_DESC', 'Bitte geben Sie an, in welcher Seite dieser Rechtstext automatisch eingef&uuml;gt werden soll.<br/><br/><b>Wichtig:</b> das funktioniert erst ab Version 3. Die Umstellung kann bei Janolaw veranlasst werden.');
-define('MODULE_JANOLAW_PDF_WITHDRAWAL_TITLE', 'Zus&auml;tzlich als PDF speichern');
-define('MODULE_JANOLAW_PDF_WITHDRAWAL_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert werden?');
+define('MODULE_JANOLAW_PDF_WITHDRAWAL_TITLE', 'PDF als Download');
+define('MODULE_JANOLAW_PDF_WITHDRAWAL_DESC', 'Sollen die Daten zus&auml;tzlich als PDF gespeichert und ein Link eingef&uuml;gt werden?<br/><b>Wichtig:</b> Das funktioniert nur in der HTML Version!');
 define('MODULE_JANOLAW_MAIL_WITHDRAWAL_TITLE', 'PDF als E-Mail Anhang');
-define('MODULE_JANOLAW_MAIL_WITHDRAWAL_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?<br/><b>Wichtig:</b> Das Dokument muss als PDF gespeichert werden!');
+define('MODULE_JANOLAW_MAIL_WITHDRAWAL_DESC', 'Soll das PDF als Anhang zur Auftragsbest&auml;tigung mitgeschickt werden?');
+define('MODULE_JANOLAW_WITHDRAWAL_COMBINE_TITLE', 'Kombinierte Widerrufsbelehrung/Widerrufsformular');
+define('MODULE_JANOLAW_WITHDRAWAL_COMBINE_DESC', 'Soll eine kombinierte Widerrufsbelehrung mit Widerrufsformular erstellt werden?');
 
 ?>
