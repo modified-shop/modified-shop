890e371c9d7860288d4f040d7a69d3a6500f81cc
web28 <web28@modified-shop.org>
2015-08-31 14:24:46 +0000

    fix #635, fix wrong constant names
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8726 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/payment/amoneybookers.php
+++ includes/modules/payment/amoneybookers.php
@@ -109,7 +109,7 @@ class amoneybookers {
 
 		if (($this->enabled == true) && ((int) MODULE_PAYMENT_AMONEYBOOKERS_ZONE > 0)) {
 			$check_flag = false;
-			$check_query = xtc_db_query("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_PAYMENT_MONEYBOOKERS_ZONE . "' and zone_country_id = '" . $order->billing['country']['id'] . "' order by zone_id");
+			$check_query = xtc_db_query("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_PAYMENT_AMONEYBOOKERS_ZONE . "' and zone_country_id = '" . $order->billing['country']['id'] . "' order by zone_id");
 			while ($check = xtc_db_fetch_array($check_query)) {
 				if ($check['zone_id'] < 1) {
 					$check_flag = true;
