4762ef12d6fd7ea0df49f0dc93b919df947795c7
modGTB <gerhard@modified-shop.org>
2015-09-15 15:36:52 +0000

    use absolute path
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8824 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/application_top_export.php
+++ includes/application_top_export.php
@@ -69,8 +69,8 @@ define('PROJECT_VERSION', 'modified eCommerce Shopsoftware');
 define('TAX_DECIMAL_PLACES', 0);
 
 // set the type of request (secure or not)
-if (file_exists('includes/request_type.php')) {
-  include ('includes/request_type.php');
+if (file_exists(DIR_WS_INCLUDES.'request_type.php')) {
+  include (DIR_WS_INCLUDES.'request_type.php');
 } else {
   $request_type = 'NONSSL';
 }
