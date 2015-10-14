13982609f891ac7c99be60522c96654e419aa30c
Tomcraft <Tomcraft@modified-shop.org>
2015-10-14 10:30:05 +0000

    Minor changes on r8916

--- lang/english/modules/system/admin_log.php
+++ lang/english/modules/system/admin_log.php
@@ -18,15 +18,15 @@ define('MODULE_ADMIN_LOG_TEXT_DESCRIPTION', 'Changes done by an Admin will be lo
   <li>Products</li>
   <li>Content Manager</li>
   <li>Shipping Methods</li>
-  <li>Payment Systems</li>
+  <li>Payment Methods</li>
   <li>Order Total</li>
   <li>System Modules</li>
   <li>Export Modules</li>
   <li>Configuration</li>
 </ul>
   ');
-define('MODULE_ADMIN_LOG_STATUS_TITLE', 'Module activate?');
-define('MODULE_ADMIN_LOG_STATUS_DESC', 'Administrator Log activate');
+define('MODULE_ADMIN_LOG_STATUS_TITLE', 'Activate module?');
+define('MODULE_ADMIN_LOG_STATUS_DESC', 'Activate Administrator Log');
 
 define('MODULE_ADMIN_LOG_DISPLAY_TITLE', 'Display Log?');
 define('MODULE_ADMIN_LOG_DISPLAY_DESC', 'Display administrator Log in Footer');
