012aff434d48a8eddfcf2c6b1cfd69c8d0ed2645
Tomcraft1980 <hallo@modified-shop.org>
2015-10-17 14:08:33 +0000

    Security fix for /branches/modified-shop-1.06-SP3 (props Hetfield)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/modified-shop-1.06-SP3@8930 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/xtc_get_products_stock.inc.php
+++ inc/xtc_get_products_stock.inc.php
@@ -17,7 +17,7 @@
    
   function xtc_get_products_stock($products_id) {
     $products_id = xtc_get_prid($products_id);
-    $stock_query = xtc_db_query("select products_quantity from " . TABLE_PRODUCTS . " where products_id = '" . $products_id . "'");
+    $stock_query = xtc_db_query("select products_quantity from " . TABLE_PRODUCTS . " where products_id = '" . xtc_db_input((int)$products_id) . "'");
     $stock_values = xtc_db_fetch_array($stock_query);
 
     return $stock_values['products_quantity'];
