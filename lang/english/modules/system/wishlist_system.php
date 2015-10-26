c10eaca74bd9ecbba3c4c0bed0d17a442dbaad76
Tomcraft <Tomcraft@modified-shop.org>
2015-10-26 14:21:54 +0000

    Minor addition on r8962 (fix #688 - system module for wishlist)

--- lang/english/modules/system/wishlist_system.php
+++ lang/english/modules/system/wishlist_system.php
@@ -12,6 +12,6 @@
 
 define('MODULE_WISHLIST_SYSTEM_TEXT_TITLE', 'Wishlist');
 define('MODULE_WISHLIST_SYSTEM_TEXT_DESCRIPTION', 'With the wishlist customers can save products');
-define('MODULE_WISHLIST_SYSTEM_STATUS_TITLE', 'activate Module?');
-define('MODULE_WISHLIST_SYSTEM_STATUS_DESC', 'activate Wishlist');
+define('MODULE_WISHLIST_SYSTEM_STATUS_TITLE', 'Activate Module?');
+define('MODULE_WISHLIST_SYSTEM_STATUS_DESC', 'Activate Wishlist');
 ?>
