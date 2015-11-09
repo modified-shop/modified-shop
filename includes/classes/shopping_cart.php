44aa91b3bb3041a7c88f5f472e731e2f1578cd74
web28 <web28@modified-shop.org>
2015-11-09 17:38:49 +0000

    don't reset $this->contents[$products_id] by update_quantity
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8996 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/classes/shopping_cart.php
+++ includes/classes/shopping_cart.php
@@ -264,7 +264,9 @@ class shoppingCart {
     }
     // xs:booster end
 
-    $this->contents[$products_id] = array ('qty' => (int)$quantity);
+    //$this->contents[$products_id] = array ('qty' => (int)$quantity);
+    $this->contents[$products_id]['qty'] = (int)$quantity; //don't reset $this->contents[$products_id] by update_quantity
+    
     // update database
     if (isset($_SESSION['customer_id'])){
       xtc_db_query("UPDATE ".$this->table_basket."
