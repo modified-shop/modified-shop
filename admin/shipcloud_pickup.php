18c8d5ce79a3c3a80054c040c7bf4cdd1702f576
Tomcraft1980 <hallo@modified-shop.org>
2016-01-14 17:11:14 +0000

    Addition on r9180
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/modified-shop-1.06-SP3@9183 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/shipcloud_pickup.php
+++ admin/shipcloud_pickup.php
@@ -40,12 +40,12 @@ require (DIR_WS_INCLUDES.'head.php');
 		});
 	</script>
 </head>
-<body>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
   <!-- header //-->
   <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
   <!-- header_eof //-->
   <!-- body //-->
-  <table class="tableBody">
+  <table width="100%" cellspacing="2" cellpadding="2" border="0">
     <tr>
       <?php //left_navigation
       if (USE_ADMIN_TOP_MENU == 'false') {
@@ -57,60 +57,76 @@ require (DIR_WS_INCLUDES.'head.php');
       }
       ?>
       <!-- body_text //-->
-      <td class="boxCenter">
-        <div class="pageHeadingImage"><?php echo xtc_image(DIR_WS_ICONS.'heading/icon_modules.png'); ?></div>
-        <div class="pageHeading pdg2"><?php echo HEADING_SHIPCLOUD_PICKUP; ?></div>
-        <div class="main">Modules</div>      
-        <?php
-        	$carriers_query = xtc_db_query("SELECT *
-        																		FROM ".TABLE_CARRIERS."
-        																	 WHERE (LOWER(carrier_name) = 'dpd'
-        																	        OR LOWER(carrier_name) = 'fedex'
-        																	        OR LOWER(carrier_name) = 'hermes'
-        																	        OR LOWER(carrier_name) = 'ups')");
-        	while ($carriers = xtc_db_fetch_array($carriers_query)) {
-						$shipcloud_query = xtc_db_query("SELECT * 
-																							FROM ".TABLE_ORDERS_TRACKING." 
-																						 WHERE carrier_id = '".$carriers['carrier_id']."'
-																						   AND TIMESTAMP(sc_date_pickup) = 0");
-						if (xtc_db_num_rows($shipcloud_query) > 0) {
-		          echo xtc_draw_form('pickup', basename($PHP_SELF), 'action=pickup');
-		          echo xtc_draw_hidden_field('carrier', strtolower($carriers['carrier_name']));
-							?>
-							<table class="tableCenter">
-								<tr class="dataTableHeadingRow">
-									<td class="dataTableHeadingContent txta-c" colspan="4"><?php echo $carriers['carrier_name']; ?></td>
-								</tr>
-								<tr class="dataTableHeadingRow">
- 									<td class="dataTableHeadingContent"><?php echo xtc_draw_checkbox_field(strtolower($carriers['carrier_name']), '', '', '', 'onClick="selectAll(document.getElementsByName(\'sc_'.strtolower($carriers['carrier_name']).'[]\'), \''.strtolower($carriers['carrier_name']).'\');"').' '.TABLE_HEADING_EDIT; ?></td>
-									<td class="dataTableHeadingContent"><?php echo TABLE_HEADING_ORDERS_ID; ?></td>
-									<td class="dataTableHeadingContent"><?php echo TABLE_HEADING_TRACKING_ID; ?></td>
-									<td class="dataTableHeadingContent"><?php echo TABLE_HEADING_DATE_ADDED; ?></td>
-								</tr>
-								<?php
-								while ($shipcloud = xtc_db_fetch_array($shipcloud_query)) {
-									?>
-									<tr class="dataTableRow">
-										<td class="dataTableContent"><?php echo xtc_draw_checkbox_field('sc_'.strtolower($carriers['carrier_name']).'[]', $shipcloud['sc_id']); ?></td>
-										<td class="dataTableContent"><?php echo $shipcloud['orders_id']; ?></td>
-										<td class="dataTableContent"><?php echo $shipcloud['parcel_id']; ?></td>
-										<td class="dataTableContent"><?php echo xtc_date_short($shipcloud['sc_date_added']); ?></td>
-									</tr>
-									<?php
-								}
-								?>
-							</table>
-							<div style="clear:both;"></div>
-							<div class="txta-r pdg2" style="margin-bottom:10px;">
-								<?php echo xtc_draw_input_field('earliest', '', 'class="earliest" style="width: 155px" placeholder="'.TEXT_SC_EARLIEST.'"'); ?>
-								<?php echo xtc_draw_input_field('latest', '', 'class="latest" style="width: 155px" placeholder="'.TEXT_SC_LATEST.'"'); ?>
-								<input type="submit" class="button" style="margin-top: -4px;" name="update" value="<?php echo BUTTON_PICKUP; ?>">
-							</div>
-							</form>
-						<?php
-						}
-      		}
-      	?>
+      <td class="boxCenter" width="100%" valign="top">
+        <table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td width="100%">
+              <table border="0" width="100%" cellspacing="0" cellpadding="0">
+                <tr>
+                  <td width="80" rowspan="2"><?php echo xtc_image(DIR_WS_ICONS.'heading_modules.gif'); ?></td>
+                  <td class="pageHeading"><?php echo HEADING_SHIPCLOUD_PICKUP; ?></td>
+                </tr>
+                <tr>
+                  <td class="main" valign="top">Modules</td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <?php
+                $carriers_query = xtc_db_query("SELECT *
+                                                  FROM ".TABLE_CARRIERS."
+                                                 WHERE (LOWER(carrier_name) = 'dpd'
+                                                        OR LOWER(carrier_name) = 'fedex'
+                                                        OR LOWER(carrier_name) = 'hermes'
+                                                        OR LOWER(carrier_name) = 'ups')");
+                while ($carriers = xtc_db_fetch_array($carriers_query)) {
+                  $shipcloud_query = xtc_db_query("SELECT * 
+                                                    FROM ".TABLE_ORDERS_TRACKING." 
+                                                   WHERE carrier_id = '".$carriers['carrier_id']."'
+                                                     AND TIMESTAMP(sc_date_pickup) = 0");
+                  if (xtc_db_num_rows($shipcloud_query) > 0) {
+                    echo xtc_draw_form('pickup', basename($PHP_SELF), 'action=pickup');
+                    echo xtc_draw_hidden_field('carrier', strtolower($carriers['carrier_name']));
+                    ?>
+                    <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                      <tr class="dataTableHeadingRow">
+                        <td class="dataTableHeadingContent txta-c" colspan="4"><?php echo $carriers['carrier_name']; ?></td>
+                      </tr>
+                      <tr class="dataTableHeadingRow">
+                        <td class="dataTableHeadingContent"><?php echo '<input type="checkbox" name="'.strtolower($carriers['carrier_name']).'" onClick="selectAll(document.getElementsByName(\'sc_'.strtolower($carriers['carrier_name']).'[]\'), \''.strtolower($carriers['carrier_name']).'\');" /> '.TABLE_HEADING_EDIT; ?></td>
+                        <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_ORDERS_ID; ?></td>
+                        <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_TRACKING_ID; ?></td>
+                        <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_DATE_ADDED; ?></td>
+                      </tr>
+                      <?php
+                      while ($shipcloud = xtc_db_fetch_array($shipcloud_query)) {
+                        ?>
+                        <tr class="dataTableRow">
+                          <td class="dataTableContent"><?php echo xtc_draw_checkbox_field('sc_'.strtolower($carriers['carrier_name']).'[]', $shipcloud['sc_id']); ?></td>
+                          <td class="dataTableContent"><?php echo $shipcloud['orders_id']; ?></td>
+                          <td class="dataTableContent"><?php echo $shipcloud['parcel_id']; ?></td>
+                          <td class="dataTableContent"><?php echo xtc_date_short($shipcloud['sc_date_added']); ?></td>
+                        </tr>
+                        <?php
+                      }
+                      ?>
+                    </table>
+                    <div style="clear:both;"></div>
+                    <div style="text-align: right; padding:5px 2px; margin-bottom:10px;">
+                      <?php echo xtc_draw_input_field('earliest', '', 'class="earliest" style="width: 155px" placeholder="'.TEXT_SC_EARLIEST.'"'); ?>
+                      <?php echo xtc_draw_input_field('latest', '', 'class="latest" style="width: 155px" placeholder="'.TEXT_SC_LATEST.'"'); ?>
+                      <input type="submit" class="button" style="margin-top: -4px;" name="update" value="<?php echo BUTTON_PICKUP; ?>">
+                    </div>
+                    </form>
+                  <?php
+                  }
+                }
+              ?>
+            </td>
+          </tr>
+        </table>
       </td>
       <!-- body_text_eof //-->
     </tr>
