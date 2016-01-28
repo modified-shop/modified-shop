df0065227f6ca7f5564ca5db550efdc383d929e8
GTB <GTB@modified-shop.org>
2016-01-28 08:40:37 +0000

    update paypal lang

--- lang/english/admin/paypal_webhook.php
+++ lang/english/admin/paypal_webhook.php
@@ -15,7 +15,8 @@ $lang_array = array(
   'TEXT_PAYPAL_WEBHOOK_HEADING_TITLE' => 'PayPal Webhooks',
 
   'TEXT_PAYPAL_WEBHOOK_STATUS_NOT_DEFINED' => 'not configured',
-  'TEXT_PAYPAL_WEBHOOK_INFO' => 'To use webhook, it is imperative that you have a valid SSL certificate.',
+  'TEXT_PAYPAL_WEBHOOK_INFO' => '<ul><li>No Webhooks available.</li><li>With Webhhoks you receive updates for Payments and orders status</li><li>To use webhook, it is imperative that you have a valid SSL certificate.</li></ul>',
+  'TEXT_PAYPAL_WEBHOOK_CREDENTIAL_INFO' => 'No Webhooks available. Please check your credentials.',
   
   'TABLE_HEADING_URL' => 'URL',
   'TABLE_HEADING_WEBHOOK' => 'Webhook',
