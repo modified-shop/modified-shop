dcad04b9fcb70a54c6ff24851e5b731235d0a8ef
Tomcraft1980 <hallo@modified-shop.org>
2016-02-05 00:47:52 +0000

    Update delete_files.php in installer
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@9329 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/includes/delete_files.php
+++ _installer/includes/delete_files.php
@@ -183,6 +183,8 @@ $unlink_file = array('_installer/images/icons/error.gif', // neu
                      'includes/classes/sessions.php', // neu
                      'includes/classes/cc_validation.php',
                      'includes/configure.org.php',
+                     'includes/external/janolaw/send_order.php', // from r9293
+                     'includes/external/payone/modules/send_order.php', // from r9293
                      'includes/external/shopgate/helper/ShopgateHelperLoader.php', // neu
                      'includes/external/shopgate/Model/ShopgateModelLoader.php', // neu
                      'includes/external/shopgate/shopgate_library/vendors/mobile_redirect.class.php', // neu
