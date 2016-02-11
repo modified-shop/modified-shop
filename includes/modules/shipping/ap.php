41c71457fa846ee8b7de74e3bbe5aa77cd596a7f
GTB <GTB@modified-shop.org>
2016-02-11 15:29:22 +0000

    fix shipping modules

--- includes/modules/shipping/ap.php
+++ includes/modules/shipping/ap.php
@@ -61,7 +61,7 @@
         //update compatibility
         if (!defined('MODULE_SHIPPING_AP_NUMBER_ZONES')) {
           $this->num_zones = $check_zones_rows;
-          xtc_db_query("insert into " . TABLE_CONFIGURATION . " ( configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_SHIPPING_AP_NUMBER_ZONES', '". (int)$this->num_dp ."', '6', '0', now())");
+          xtc_db_query("insert into " . TABLE_CONFIGURATION . " ( configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_SHIPPING_AP_NUMBER_ZONES', '". (int)$this->num_zones ."', '6', '0', now())");
         }
 
         if ($check_zones_rows != $this->num_dp) {
--- includes/modules/shipping/zones.php
+++ includes/modules/shipping/zones.php
@@ -57,7 +57,7 @@
         //update compatibility
         if (!defined('MODULE_SHIPPING_ZONES_NUMBER_ZONES')) {
           $this->num_zones = $check_zones_rows;
-          xtc_db_query("insert into " . TABLE_CONFIGURATION . " ( configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_SHIPPING_ZONES_NUMBER_ZONES', '". (int)$this->num_dp ."', '6', '0', now())");
+          xtc_db_query("insert into " . TABLE_CONFIGURATION . " ( configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_SHIPPING_ZONES_NUMBER_ZONES', '". (int)$this->num_zones ."', '6', '0', now())");
         }
 
         if ($check_zones_rows != $this->num_zones) {
