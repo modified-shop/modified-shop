405028c00e4f0d331f3bf5c9234e80988f28e3b1
GTB <GTB@modified-shop.org>
2016-02-18 07:20:36 +0000

    new auto include for orders

--- admin/includes/modules/orders_info_blocks.php
+++ admin/includes/modules/orders_info_blocks.php
@@ -121,110 +121,10 @@
                   <td class="main"><?php echo get_payment_name($order->info['payment_method']) . ' ('.$order->info['payment_method'].')'; ?></td>
                 </tr>
                 <?php
-
-                ## invoice number and date
-                echo add_table_infos_ibillnr($order);
+                  ## invoice number and date
+                  echo add_table_infos_ibillnr($order);
                 
-                ## easyBill
-                include (DIR_FS_EXTERNAL.'easybill/admin/easybill.info.php');
-
-                ## Paypal
-                include (DIR_FS_EXTERNAL.'paypal/modules/orders_paypal.php');
-
-                ## Banktransfer
-                $banktransfer_query = xtc_db_query("SELECT banktransfer_owner,
-                                                           banktransfer_number,
-                                                           banktransfer_bankname,
-                                                           banktransfer_blz,
-                                                           banktransfer_iban,
-                                                           banktransfer_bic,
-                                                           banktransfer_status,
-                                                           banktransfer_prz,
-                                                           banktransfer_fax,
-                                                           banktransfer_owner_email
-                                                      FROM ".TABLE_BANKTRANSFER."
-                                                     WHERE orders_id = ".$oID);
-                $banktransfer = xtc_db_fetch_array($banktransfer_query);
-                if ($banktransfer['banktransfer_bankname'] || $banktransfer['banktransfer_blz'] || $banktransfer['banktransfer_number'] || $banktransfer['banktransfer_iban'] ) {
-                  ?>
-                  <tr>
-                    <td colspan="2"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_OWNER; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_owner']; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_NUMBER; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_number']; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_BLZ; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_blz']; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_IBAN; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_iban']; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_BIC; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_bic']; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_NAME; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_bankname']; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_OWNER_EMAIL; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_owner_email']; ?></td>
-                  </tr>
-                  <?php  if ($banktransfer['banktransfer_status'] == 0) { ?>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_STATUS; ?></td>
-                    <td class="main"><?php echo "OK"; ?></td>
-                  </tr>
-                  <?php } else { ?>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_STATUS; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_status']; ?></td>
-                  </tr>
-                  <?php
-                  $bt_status = (int) $banktransfer['banktransfer_status'];
-                  $error_val = defined('TEXT_BANK_ERROR_'.$bt_status) ? constant('TEXT_BANK_ERROR_'.$bt_status) : '';
-                  ?>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_ERRORCODE; ?></td>
-                    <td class="main"><?php echo $error_val; ?></td>
-                  </tr>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_PRZ; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_prz']; ?></td>
-                  </tr>
-                  <?php }
-                }
-                if ($banktransfer['banktransfer_fax']) {
-                ?>
-                  <tr>
-                    <td class="main"><?php echo TEXT_BANK_FAX; ?></td>
-                    <td class="main"><?php echo $banktransfer['banktransfer_fax']; ?></td>
-                  </tr>
-                <?php
-                }
-
-                ## Moneybookers
-                if ($order->info['payment_method'] == 'amoneybookers') {
-                  if (file_exists(DIR_FS_CATALOG.DIR_WS_MODULES.'payment/'.$order->info['payment_method'].'.php')) {
-                    include(DIR_FS_CATALOG.DIR_WS_MODULES.'payment/'.$order->info['payment_method'].'.php');
-                    include(DIR_FS_CATALOG.'lang/'.$order->info['language'].'/modules/payment/'.$order->info['payment_method'].'.php');
-                    $class = $order->info['payment_method'];
-                    $payment = new $class();
-                    $payment->admin_order($oID);
-                  }
-                }
-
-                ## Payone
-                include (DIR_FS_EXTERNAL.'payone/modules/orders_payone.php'); 
-                
+                  foreach(auto_include(DIR_FS_ADMIN.'includes/extra/module/orders/orders_info_payment/','php') as $file) require ($file);
                 ?>
               </table>
             </td>
@@ -346,9 +246,7 @@
         <!-- EOC DOWNLOAD BLOCK -->
 
         <?php
-          /* magnalister v1.0.0 */
-          if (function_exists('magnaExecute')) echo magnaExecute('magnaRenderOrderDetails', array('oID' => $oID), array('order_details.php'));
-          /* END magnalister */
+          foreach(auto_include(DIR_FS_ADMIN.'includes/extra/module/orders/orders_info_blocks/','php') as $file) require ($file);
         ?>
 
         <!-- BOC ORDER TRACK & TRACE BLOCK -->
