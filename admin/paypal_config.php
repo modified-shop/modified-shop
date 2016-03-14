09d929c373e17855107dbbd9a83a6b1071453f49
modGTB <gerhard@modified-shop.org>
2016-03-14 09:27:13 +0000

    update paypal for sp4
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/modified-shop-1.06-SP4@9552 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/paypal_config.php
+++ admin/paypal_config.php
@@ -133,9 +133,9 @@ require (DIR_WS_INCLUDES.'head.php');
                 <td class="dataTableConfig col-right"><?php echo TEXT_PAYPAL_CONFIG_MODE_INFO; ?></td>
               </tr>
               <tr>
-                <td class="dataTableConfig col-left"><?php echo TEXT_PAYPAL_CONFIG_TRANSFER_ORDER_ID; ?></td>
-                <td class="dataTableConfig col-middle"><?php echo draw_on_off_selection('config[PAYPAL_CONFIG_TRANSFER_ORDER_ID]', $status_array, $paypal->get_config('PAYPAL_CONFIG_TRANSFER_ORDER_ID')); ?></td>
-                <td class="dataTableConfig col-right"><?php echo TEXT_PAYPAL_CONFIG_TRANSFER_ORDER_ID_INFO; ?></td>
+                <td class="dataTableConfig col-left"><?php echo TEXT_PAYPAL_CONFIG_INVOICE_PREFIX; ?></td>
+                <td class="dataTableConfig col-middle"><?php echo xtc_draw_input_field('config[PAYPAL_CONFIG_INVOICE_PREFIX]', $paypal->get_config('PAYPAL_CONFIG_INVOICE_PREFIX'), 'style="width: 300px;"'); ?></td>
+                <td class="dataTableConfig col-right"><?php echo TEXT_PAYPAL_CONFIG_INVOICE_PREFIX_INFO; ?></td>
               </tr>
               <tr>
                 <td class="dataTableConfig col-left"><?php echo TEXT_PAYPAL_CONFIG_TRANSACTION; ?></td>
