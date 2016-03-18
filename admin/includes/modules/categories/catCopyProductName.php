37b9ef5a11bc7cc34e0a9f24063b636776b27063
modGTB <gerhard@modified-shop.org>
2016-03-18 15:31:06 +0000

    insert missing file header
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@9596 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/categories/catCopyProductName.php
+++ admin/includes/modules/categories/catCopyProductName.php
@@ -1,4 +1,14 @@
 <?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
 
 class catCopyProductName {  //Important same name as filename
   
