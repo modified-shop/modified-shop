fac94d51d34c28ad0860d2940f0e110bea527091
Tomcraft1980 <hallo@modified-shop.org>
2016-03-18 12:43:06 +0000

    Update shopgate integration in /branches/modified-shop-1.06-SP4
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/branches/modified-shop-1.06-SP4@9591 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/header.php
+++ includes/header.php
@@ -42,7 +42,7 @@ elseif (isset($error) && ($error == CATEGORIE_NOT_FOUND || $error == TEXT_PRODUC
 }
 
 /******** SHOPGATE **********/
-if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
+if(defined('MODULE_PAYMENT_SHOPGATE_STATUS') && MODULE_PAYMENT_SHOPGATE_STATUS=='True' && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
   include_once (DIR_FS_CATALOG.'includes/external/shopgate/base/includes/header.php');
 }
 /******** SHOPGATE **********/
@@ -55,7 +55,7 @@ if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSIO
 <meta http-equiv="Content-Style-Type" content="text/css" />
 <?php
 /******** SHOPGATE **********/
-if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
+if(defined('MODULE_PAYMENT_SHOPGATE_STATUS') && MODULE_PAYMENT_SHOPGATE_STATUS=='True' && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
   echo $shopgateJsHeader;
 }
 /******** SHOPGATE **********/
