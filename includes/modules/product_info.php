4922c95255a13f1b98449b77edead496f2b75341
Tomcraft <Tomcraft@modified-shop.org>
2016-03-18 12:43:06 +0000

    Update shopgate integration in /branches/modified-shop-1.06-SP4

--- includes/modules/product_info.php
+++ includes/modules/product_info.php
@@ -23,7 +23,7 @@
    ---------------------------------------------------------------------------------------*/
 
 /******* SHOPGATE **********/
-if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
+if(defined('MODULE_PAYMENT_SHOPGATE_STATUS') && MODULE_PAYMENT_SHOPGATE_STATUS=='True' && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
   include_once DIR_FS_CATALOG.'includes/external/shopgate/base/includes/modules/product_info.php';
 }
 /******* SHOPGATE **********/
