f5b2910639c921e559f263b3274bfcf3cfea0d80
web28 <web28@modified-shop.org>
2016-04-25 09:39:38 +0000

    fix #854, r9718

--- inc/check_country_required_zones.inc.php
+++ inc/check_country_required_zones.inc.php
@@ -16,9 +16,9 @@ function check_country_required_zones($country_id)
     $query = xtc_db_query("
       SELECT required_zones
         FROM ".TABLE_COUNTRIES."
-       WHERE zone_country_id = '".(int)$country_id."'
+       WHERE countries_id  = '".(int)$country_id."'
       ");
-      
+    
     if (xtc_db_num_rows($query)) {
         $dbData = xtc_db_fetch_array($query);
         return $dbData['required_zones'];
