f2c3d56324b47a51e449783785d4973a452d9903
Tomcraft1980 <hallo@modified-shop.org>
2016-05-20 15:52:23 +0000

    Partly fix #746 - add combine_files.inc.php for template xtc5
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@9843 79ac0487-9261-4f26-a34d-858dee0dcce0

--- templates/xtc5/css/general.css.php
+++ templates/xtc5/css/general.css.php
@@ -13,27 +13,26 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-  $css_plain = DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/stylesheet.css';
-  $css_min = DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/stylesheet.min.css';
+  define('DIR_TMPL', 'templates/'.CURRENT_TEMPLATE.'/');
+  define('DIR_TMPL_CSS', DIR_TMPL.'css/');
 
-  $css_file = '/stylesheet.css';
+  if ($_SESSION['customers_status']['customers_status'] == '0') {
+    echo '<link rel="stylesheet" property="stylesheet" href="'.DIR_WS_BASE.DIR_TMPL_CSS.'adminbar.css" type="text/css" media="screen" />';
+  }
+
+  $css_array = array(
+    DIR_TMPL.'stylesheet.css',
+  );
+  $css_min = DIR_TMPL.'stylesheet.min.css';
+
   if (COMPRESS_STYLESHEET == 'true') {
-    $css_plain_ts = filemtime($css_plain);
-    $css_min_ts = is_writeable($css_min) ? filemtime($css_min) : false;
-    if ($css_min_ts && ($css_plain_ts > $css_min_ts || filesize($css_min) == 0)) {
-      require_once(DIR_FS_EXTERNAL.'compactor/compactor.php');
-      if (($css_content = file_get_contents($css_plain)) !== false) {
-        $compactor = new Compactor(array('strip_php_comments' => true));
-        $css_content = $compactor->squeeze($css_content);
-        if (file_put_contents($css_min, $css_content, LOCK_EX) !== false) {
-          $css_file = '/stylesheet.min.css?v='.$css_min_ts;
-        }
-      }
-    } elseif ($css_min_ts) {
-      $css_file = '/stylesheet.min.css?v='.$css_min_ts;
-    }
+    require_once(DIR_FS_BOXES_INC.'combine_files.inc.php');
+    $css_array = combine_files($css_array,$css_min,true);
   }
 
   // Put CSS-Inline-Definitions here, these CSS-files will be loaded at the TOP of every page
+  
+  foreach ($css_array as $css) {
+    echo '<link rel="stylesheet" property="stylesheet" href="'.DIR_WS_BASE.$css.'" type="text/css" media="screen" />'.PHP_EOL;
+  }
 ?>
-<link rel="stylesheet" href="<?php echo DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.$css_file; ?>" type="text/css" media="screen" />
--- templates/xtc5/source/boxes.php
+++ templates/xtc5/source/boxes.php
@@ -17,44 +17,44 @@
 // -----------------------------------------------------------------------------------------
 //	Immer sichtbar
 // -----------------------------------------------------------------------------------------
-  require_once(DIR_WS_BOXES . 'categories.php');
-  require_once(DIR_WS_BOXES . 'manufacturers.php');
-  require_once(DIR_WS_BOXES . 'last_viewed.php');
-  require_once(DIR_WS_BOXES . 'search.php');
-  require_once(DIR_WS_BOXES . 'content.php');
-  require_once(DIR_WS_BOXES . 'information.php');
-  require_once(DIR_WS_BOXES . 'languages.php'); 
-  require_once(DIR_WS_BOXES . 'infobox.php');
-  require_once(DIR_WS_BOXES . 'loginbox.php');
-  require_once(DIR_WS_BOXES . 'newsletter.php');
+  require_once(DIR_FS_BOXES . 'categories.php');
+  require_once(DIR_FS_BOXES . 'manufacturers.php');
+  require_once(DIR_FS_BOXES . 'last_viewed.php');
+  require_once(DIR_FS_BOXES . 'search.php');
+  require_once(DIR_FS_BOXES . 'content.php');
+  require_once(DIR_FS_BOXES . 'information.php');
+  require_once(DIR_FS_BOXES . 'languages.php'); 
+  require_once(DIR_FS_BOXES . 'infobox.php');
+  require_once(DIR_FS_BOXES . 'loginbox.php');
+  require_once(DIR_FS_BOXES . 'newsletter.php');
   if (defined('MODULE_TS_TRUSTEDSHOPS_ID') 
       && (MODULE_TS_WIDGET == '1'
           || (MODULE_TS_REVIEW_STICKER != '' && MODULE_TS_REVIEW_STICKER_STATUS == '1'))
       ) 
   {
-    require_once(DIR_WS_BOXES . 'trustedshops.php');
+    require_once(DIR_FS_BOXES . 'trustedshops.php');
   }
 // -----------------------------------------------------------------------------------------
 //	Nur, wenn Preise sichtbar
 // -----------------------------------------------------------------------------------------
   if ($_SESSION['customers_status']['customers_status_show_price'] == '1') {
-    require_once(DIR_WS_BOXES . 'add_a_quickie.php');
-    require_once(DIR_WS_BOXES . 'shopping_cart.php');
+    require_once(DIR_FS_BOXES . 'add_a_quickie.php');
+    require_once(DIR_FS_BOXES . 'shopping_cart.php');
     if (defined('MODULE_WISHLIST_SYSTEM_STATUS') && MODULE_WISHLIST_SYSTEM_STATUS == 'true') {
-      require_once(DIR_WS_BOXES . 'wishlist.php');
+      require_once(DIR_FS_BOXES . 'wishlist.php');
     }
   }
 // -----------------------------------------------------------------------------------------
 //	In der Suche verborgen
 // -----------------------------------------------------------------------------------------
   if (substr(basename($PHP_SELF), 0,8) != 'advanced') {
-    require_once(DIR_WS_BOXES . 'whats_new.php'); 
+    require_once(DIR_FS_BOXES . 'whats_new.php'); 
   }
 // -----------------------------------------------------------------------------------------
 //	Nur fuer Admins
 // -----------------------------------------------------------------------------------------
   if ($_SESSION['customers_status']['customers_status'] == '0') {
-    require_once(DIR_WS_BOXES . 'admin.php');
+    require_once(DIR_FS_BOXES . 'admin.php');
     $smarty->assign('is_admin', true);
   }
 // -----------------------------------------------------------------------------------------
@@ -62,31 +62,31 @@
 // -----------------------------------------------------------------------------------------
   if ($product->isProduct() === true) {
     //Aktuelle Seite ist Produkt-Detailseite
-    require_once(DIR_WS_BOXES . 'manufacturer_info.php');
+    require_once(DIR_FS_BOXES . 'manufacturer_info.php');
   } else {
     //Aktuelle Seite ist keine  Produkt-Detailseite
-    require_once(DIR_WS_BOXES . 'best_sellers.php');
+    require_once(DIR_FS_BOXES . 'best_sellers.php');
     if ($_SESSION['customers_status']['customers_status_specials'] == '1') {
-      require_once(DIR_WS_BOXES . 'specials.php');
+      require_once(DIR_FS_BOXES . 'specials.php');
     }
   }
 // -----------------------------------------------------------------------------------------
 //	Nur fuer eingeloggte Besucher
 // -----------------------------------------------------------------------------------------
   if (isset($_SESSION['customer_id'])) {
-    require_once(DIR_WS_BOXES . 'order_history.php');
+    require_once(DIR_FS_BOXES . 'order_history.php');
   }
 // -----------------------------------------------------------------------------------------
 //	Nur, wenn Rezensionen erlaubt
 // -----------------------------------------------------------------------------------------
   if ($_SESSION['customers_status']['customers_status_read_reviews'] == '1') {
-    require_once(DIR_WS_BOXES . 'reviews.php');
+    require_once(DIR_FS_BOXES . 'reviews.php');
   }
 // -----------------------------------------------------------------------------------------
 //	Waehrend des Kauf-Abschlusses verborgen 
 // -----------------------------------------------------------------------------------------
   if (substr(basename($PHP_SELF), 0, 8) != 'checkout') {
-    require_once(DIR_WS_BOXES . 'currencies.php');
+    require_once(DIR_FS_BOXES . 'currencies.php');
   }
 // -----------------------------------------------------------------------------------------
 //EOC require boxes
