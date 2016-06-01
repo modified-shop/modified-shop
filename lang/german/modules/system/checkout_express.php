b01ff03277a0f5ae7bc66c674eaa941ab5090bee
modGTB <gerhard@modified-shop.org>
2016-06-01 08:57:22 +0000

    fix #906
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@9910 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/system/checkout_express.php
+++ lang/german/modules/system/checkout_express.php
@@ -14,8 +14,9 @@ define('MODULE_CHECKOUT_EXPRESS_STATUS_TITLE','Status');
 define('MODULE_CHECKOUT_EXPRESS_STATUS_DESC','Modulstatus');
 define('MODULE_CHECKOUT_EXPRESS_STATUS_INFO','Aktiv');
 define('MODULE_CHECKOUT_EXPRESS_TEXT_TITLE', 'Express Checkout');
-define('MODULE_CHECKOUT_EXPRESS_TEXT_DESCRIPTION', 'Express Checkout erm&ouml;glicht das Speichern von Versand und Zahlinformation.<br/><b>Achtung:</b> Das funktioniert nur mit einem f&uuml;r Shopversion 2.00 kompatiblem Template!');    
+define('MODULE_CHECKOUT_EXPRESS_TEXT_DESCRIPTION', 'Express Checkout erm&ouml;glicht das Speichern von Versand und Zahlinformation.<br/><br/><b>Achtung:</b> Das funktioniert nur mit einem f&uuml;r Shopversion 2.00 kompatiblem Template!');    
 define('MODULE_CHECKOUT_EXPRESS_CONTENT_TITLE','Content');
 define('MODULE_CHECKOUT_EXPRESS_CONTENT_DESC','Sie k&ouml;nnen noch eine Contentseite anlegen und den Expresscheckout erkl&auml;ren.');
 
+define('MODULE_CHECKOUT_EXPRESS_DESCRIPTION_INSTALL','<br/><br/>Zus&auml;tzlich k&ouml;nnen sie eine Informationsseite f&uuml;r den Express Checkout installieren.');
 ?>
