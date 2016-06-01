ab67d9da02913293ff0dacfcc728b2cbff8ee8a2
GTB <GTB@modified-shop.org>
2016-06-01 08:57:22 +0000

    fix #906

--- lang/english/modules/system/checkout_express.php
+++ lang/english/modules/system/checkout_express.php
@@ -14,8 +14,9 @@ define('MODULE_CHECKOUT_EXPRESS_STATUS_TITLE','Status');
 define('MODULE_CHECKOUT_EXPRESS_STATUS_DESC','Module status');
 define('MODULE_CHECKOUT_EXPRESS_STATUS_INFO','Enabled');
 define('MODULE_CHECKOUT_EXPRESS_TEXT_TITLE', 'Express Checkout');
-define('MODULE_CHECKOUT_EXPRESS_TEXT_DESCRIPTION', 'With Express Checkout it is possible to save shipping and payment informations.<br/><b>Attention:</b> This only works with a shop version 2.00 compatible template!');    
+define('MODULE_CHECKOUT_EXPRESS_TEXT_DESCRIPTION', 'With Express Checkout it is possible to save shipping and payment informations.<br/><br/><b>Attention:</b> This only works with a shop version 2.00 compatible template!');    
 define('MODULE_CHECKOUT_EXPRESS_CONTENT_TITLE','Content');
 define('MODULE_CHECKOUT_EXPRESS_CONTENT_DESC','Choose a content page to describe the express checkout');
 
+define('MODULE_CHECKOUT_EXPRESS_DESCRIPTION_INSTALL','<br/><br/>Additional you can install an informationpage for Express Checkout.');
 ?>
