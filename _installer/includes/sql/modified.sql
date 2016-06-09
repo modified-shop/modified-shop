54f055bf6eba815f37bb9703e32ab22b5cbc349d
modGTB <gerhard@modified-shop.org>
2016-06-09 09:16:40 +0000

    fix #920
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@9953 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/includes/sql/modified.sql
+++ _installer/includes/sql/modified.sql
@@ -1066,6 +1066,8 @@ CREATE TABLE products_tags (
   products_options_values_id int(11) NOT NULL DEFAULT '0',
   KEY idx_products_options_values (products_id,options_id,values_id),
   KEY idx_products_options_id (products_options_id)
+  KEY idx_options_id (options_id),
+  KEY idx_values_id (values_id),
 ) ENGINE=MyISAM;
 
 DROP TABLE IF EXISTS products_tags_options;
