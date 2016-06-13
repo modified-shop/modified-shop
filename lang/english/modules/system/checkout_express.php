aca3dd4f6de43bfe1665a77b1a96f519643bbe08
Tomcraft1980 <hallo@modified-shop.org>
2016-06-13 19:12:42 +0000

    Minor changes on language definitions
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@9975 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/modules/system/checkout_express.php
+++ lang/english/modules/system/checkout_express.php
@@ -14,7 +14,7 @@ define('MODULE_CHECKOUT_EXPRESS_STATUS_TITLE','Status');
 define('MODULE_CHECKOUT_EXPRESS_STATUS_DESC','Module status');
 define('MODULE_CHECKOUT_EXPRESS_STATUS_INFO','Enabled');
 define('MODULE_CHECKOUT_EXPRESS_TEXT_TITLE', 'Express Checkout');
-define('MODULE_CHECKOUT_EXPRESS_TEXT_DESCRIPTION', 'With Express Checkout it is possible to save shipping and payment informations.<br/><br/><b>Attention:</b> This only works with a shop version 2.00 compatible template!');    
+define('MODULE_CHECKOUT_EXPRESS_TEXT_DESCRIPTION', 'With Express Checkout it is possible to save shipping and payment informations.<br/><br/><b>Attention:</b> This only works with a shop version 2.0.0.0 compatible template!');    
 define('MODULE_CHECKOUT_EXPRESS_CONTENT_TITLE','Content');
 define('MODULE_CHECKOUT_EXPRESS_CONTENT_DESC','Choose a content page to describe the express checkout');
 
