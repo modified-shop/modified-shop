97fe876a757a2fca0b30a6d78122947e67dbda90
Tomcraft1980 <hallo@modified-shop.org>
2016-06-22 09:21:41 +0000

    Fix typo (props astaller)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10004 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/shipping/freeamount.php
+++ lang/german/modules/shipping/freeamount.php
@@ -46,6 +46,6 @@ for ($module_shipping_freeamount_i = 1; $module_shipping_freeamount_i <= MODULE_
   define('MODULE_SHIPPING_FREEAMOUNT_COUNTRIES_'.$module_shipping_freeamount_i.'_TITLE' , '<hr/>Zone '.$module_shipping_freeamount_i.' L&auml;nder');
   define('MODULE_SHIPPING_FREEAMOUNT_COUNTRIES_'.$module_shipping_freeamount_i.'_DESC' , 'Durch Komma getrennte Liste von ISO L&auml;ndercodes (2 Zeichen), welche Teil von Zone '.$module_shipping_freeamount_i.' sind (WORLD eintragen f&uuml;r den Rest der Welt.).');
   define('MODULE_SHIPPING_FREEAMOUNT_AMOUNT_'.$module_shipping_freeamount_i.'_TITLE' , 'Zone '.$module_shipping_freeamount_i.' Mindestbetrag');
-  define('MODULE_SHIPPING_FREEAMOUNT_AMOUNT_'.$module_shipping_freeamount_i.'_DESC' , 'Midestbestellwert f&uuml;r Zone '.$module_shipping_freeamount_i.' damit der Versand kostenlos ist.');
+  define('MODULE_SHIPPING_FREEAMOUNT_AMOUNT_'.$module_shipping_freeamount_i.'_DESC' , 'Mindestbestellwert f&uuml;r Zone '.$module_shipping_freeamount_i.' damit der Versand kostenlos ist.');
 }
 ?>
