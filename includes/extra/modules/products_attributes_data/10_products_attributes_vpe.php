84ac3fe778937b30ea487d0762fa95c1c379303a
GTB <GTB@modified-shop.org>
2016-07-28 10:36:32 +0000

    fix vpe round

--- includes/extra/modules/products_attributes_data/10_products_attributes_vpe.php
+++ includes/extra/modules/products_attributes_data/10_products_attributes_vpe.php
@@ -17,8 +17,8 @@ switch ($products_options['weight_prefix']) {
 if ($products_options['attributes_vpe_value'] != 0.0 && $vpe_price > 0) {
   $attr_vpe_name = xtc_get_vpe_name($products_options['attributes_vpe_id']);
   $attr_vpe_value = $vpe_value;
-  $attr_vpe_price = $xtPrice->xtcFormat($vpe_price * (1 / $vpe_value), true);
   $attr_vpe_price_plain = $xtPrice->xtcFormat($vpe_price * (1 / $vpe_value), false);
+  $attr_vpe_price = $xtPrice->xtcFormatCurrency($attr_vpe_price_plain, 0, false);
 }
 
 $products_options_data[$row]['DATA'][$col]['VPE_NAME'] = $attr_vpe_name;
