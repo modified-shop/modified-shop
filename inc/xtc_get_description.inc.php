a23689d0a74316e286ce3f2b5e2c912e16936308
GTB <GTB@modified-shop.org>
2016-08-08 10:37:41 +0000

    update function get description

--- inc/xtc_get_description.inc.php
+++ inc/xtc_get_description.inc.php
@@ -26,7 +26,11 @@
     $product_query  = xtDBquery($product_query);
     $product = xtc_db_fetch_array($product_query,true);
     
-    if ($shorten === true) {
+    if ($shorten === true 
+        && defined('CHECKOUT_USE_PRODUCTS_DESCRIPTION_FALLBACK_LENGTH') 
+        && CHECKOUT_USE_PRODUCTS_DESCRIPTION_FALLBACK_LENGTH > 0
+        ) 
+    {
       return preg_replace("/[^ ]*$/", '', substr(strip_tags($product['products_description']), 0, CHECKOUT_USE_PRODUCTS_DESCRIPTION_FALLBACK_LENGTH)) . ' [...]';
     }
     
