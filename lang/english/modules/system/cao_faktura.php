2c434673767fbd0539e3f53ef27816db01dd770c
Tomcraft <Tomcraft@modified-shop.org>
2016-09-24 07:25:17 +0000

    Revised r10297 (fix #59, partly fix #853 - update CAO faktura)

--- lang/english/modules/system/cao_faktura.php
+++ lang/english/modules/system/cao_faktura.php
@@ -12,13 +12,13 @@
 
 define('MODULE_CAO_FAKTURA_TEXT_TITLE', 'CAO Faktura');
 define('MODULE_CAO_FAKTURA_TEXT_DESCRIPTION', 'CAO Faktura Connector');
-define('MODULE_CAO_FAKTURA_STATUS_TITLE', 'Module activate?');
-define('MODULE_CAO_FAKTURA_STATUS_DESC', 'CAO Faktura activate');
+define('MODULE_CAO_FAKTURA_STATUS_TITLE', 'Activate module?');
+define('MODULE_CAO_FAKTURA_STATUS_DESC', 'Activate CAO Faktura');
 
-define('MODULE_CAO_FAKTURA_EMAIL_TITLE', 'E-Mail Address');
-define('MODULE_CAO_FAKTURA_EMAIL_DESC', 'Enter your E-Mail Address');
+define('MODULE_CAO_FAKTURA_EMAIL_TITLE', 'E-Mail address');
+define('MODULE_CAO_FAKTURA_EMAIL_DESC', 'Enter an E-Mail address. This has to be equal to the entererd E-Mail address in CAO Faktura.');
 
 define('MODULE_CAO_FAKTURA_PASSWORD_TITLE', 'Password');
-define('MODULE_CAO_FAKTURA_PASSWORD_DESC', 'Enter your Password');
+define('MODULE_CAO_FAKTURA_PASSWORD_DESC', 'Enter a password. This has to be equal to the entererd password in CAO Faktura.');
 
 ?>
