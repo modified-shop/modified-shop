e7026941857993c1e8a0337117b95ed19d4e0fbc
modGTB <gerhard@modified-shop.org>
2016-09-27 06:52:03 +0000

    fix duplicate form id
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10300 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/listing_filter.php
+++ includes/modules/listing_filter.php
@@ -294,7 +294,7 @@ if (PRODUCT_LIST_FILTER == 'true') {
       unset($values['NAME']);
       $options_array = array_merge($options_array, $values);
             
-      $filter_dropdown[$options_id] = xtc_draw_form('filter', xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('page', 'show', 'cat'))), 'get');
+      $filter_dropdown[$options_id] = xtc_draw_form('filter_'.$options_id, xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('page', 'show', 'cat'))), 'get');
       if (isset($_GET['manufacturers_id']) && $_GET['manufacturers_id'] > 0) {
         if (basename($PHP_SELF) == FILENAME_ADVANCED_SEARCH_RESULT || SEARCH_ENGINE_FRIENDLY_URLS != 'true') {
           $filter_dropdown[$options_id] .= xtc_draw_hidden_field('manufacturers_id', (int)$_GET['manufacturers_id']).PHP_EOL;
