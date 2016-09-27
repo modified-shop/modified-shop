30250945af0ffe4efb4945dc43334eac8d6a01a1
modGTB <gerhard@modified-shop.org>
2016-09-27 07:37:30 +0000

    fix duplicate form id
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10301 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/listing_filter.php
+++ includes/modules/listing_filter.php
@@ -329,7 +329,7 @@ if (PRODUCT_LIST_FILTER == 'true') {
         }
       }
       $filter_dropdown[$options_id] .= xtc_draw_pull_down_menu('filter['.$options_id.']', $options_array, isset($_GET['filter'][$options_id]) ? (int)$_GET['filter'][$options_id] : '', 'onchange="this.form.submit()"').PHP_EOL;
-      $filter_dropdown[$options_id] .= '<noscript><input type="submit" value="'.SMALL_IMAGE_BUTTON_VIEW.'" id="filter_submit" /></noscript>'.PHP_EOL;
+      $filter_dropdown[$options_id] .= '<noscript><input type="submit" value="'.SMALL_IMAGE_BUTTON_VIEW.'" id="filter_'.$options_id.'_submit" /></noscript>'.PHP_EOL;
       $filter_dropdown[$options_id] .= xtc_hide_session_id() .PHP_EOL;
       $filter_dropdown[$options_id] .= '</form>'.PHP_EOL;
     }
