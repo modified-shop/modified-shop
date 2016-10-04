5c0f0e4c3a78757f4b19016220e5981a02f905c2
web28 <web28@modified-shop.org>
2016-10-04 11:31:00 +0000

    add version control, add support for display, display_end methods
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10310 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/modules.php
+++ admin/modules.php
@@ -1,6 +1,6 @@
 <?php
   /* --------------------------------------------------------------
-   $Id: modules.php 4200 2013-01-10 19:47:11Z Tomcraft1980 $
+   $Id$
 
    modified eCommerce Shopsoftware
    http://www.modified-shop.org
@@ -514,7 +514,7 @@ if (xtc_not_null($action) && !$box) {
                 while (list($key, $value) = each($mInfo->keys)) {
                   $keys .= '<b>' . $value['title'] . '</b><br />' .  $value['description'].'<br />';
                   if ($value['set_function']) {
-                    if (preg_match('/->/', $value['set_function'])) { // Hetfield - 2009-08-19 - replaced deprecated function ereg with preg_match to be ready for PHP >= 5.3
+                    if (strpos($value['set_function'], '->') !== false) {
                       $class_method = explode('->', $value['set_function']);
                       if (!isset(${$class_method[0]}) || !is_object(${$class_method[0]})) { // DokuMan - 2011-05-10 - check if object is first set
                         include(DIR_WS_CLASSES . $class_method[0] . '.php');
@@ -533,7 +533,9 @@ if (xtc_not_null($action) && !$box) {
                 $heading[] = array('text' => '<b>' . $mInfo->title . '</b>');
                 $contents = array('form' => (isset($mInfo->properties['form_edit']) ? $mInfo->properties['form_edit'] : xtc_draw_form('modules', FILENAME_MODULES, 'set=' . $set . '&module=' . $module_class . '&action=save')));
                 $contents[] = array('text' => $keys);
+                $contents[] = method_exists($module,'display') ? $module->display() : '';
                 $contents[] = array('align' => 'center', 'text' => '<br /><input type="submit" class="button" onclick="this.blur();" value="' . BUTTON_UPDATE . '"/> <a class="button" onclick="this.blur();" href="' . xtc_href_link(FILENAME_MODULES, 'set=' . $set . '&module=' . $module_class) . '">' . BUTTON_CANCEL . '</a>');
+                $contents[] = method_exists($module,'display_end') ? $module->display_end() : '';
                 break;
 
               default:
@@ -546,7 +548,7 @@ if (xtc_not_null($action) && !$box) {
                       $keys .= '<b>' . (isset($value['title'])?$value['title']:'') . '</b><br />';
                       if ($value['use_function']) {
                         $use_function = $value['use_function'];
-                        if (strpos($use_function, '->') !== false) { // Hetfield - 2009-08-19 - replaced deprecated function ereg with preg_match to be ready for PHP >= 5.3
+                        if (strpos($use_function, '->') !== false) {
                           $class_method = explode('->', $use_function);
                           if (!isset(${$class_method[0]}) || !is_object(${$class_method[0]})) { // DokuMan - 2011-05-10 - check if object is first set
                             include(DIR_WS_CLASSES . $class_method[0] . '.php');
