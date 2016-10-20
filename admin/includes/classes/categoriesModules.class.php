18596de8eb75d0887fe74d485a397a35ba559ce4
web28 <web28@modified-shop.org>
2016-10-20 10:03:08 +0000

    partly fix #1055
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10330 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/classes/categoriesModules.class.php
+++ admin/includes/classes/categoriesModules.class.php
@@ -40,6 +40,20 @@ class categoriesModules {
         return $arg_list[0];  //Returns only first parameter
     }
 
+    function secure_call_module_method() //change no parameter
+    {
+        $arg_list = func_get_args();
+        $function_call = $this->function_call;
+        if (is_array($this->modules)) {
+            reset($this->modules);
+            foreach($this->modules as $class) {
+                if (is_callable(array($GLOBALS[$class], $function_call))) {
+                    call_user_func(array($GLOBALS[$class], $function_call), $arg_list); //Call the $GLOBALS[$class]->$function_call() method with $arg_list
+                }
+            }
+        }
+    }
+
     //----- CATEGORIES FUNCTIONS -----//
     function insert_category_before($sql_data_array,$categories_data)
     {
@@ -50,7 +64,7 @@ class categoriesModules {
     function insert_category_after($categories_data,$categories_id)
     {
         $this->function_call = 'insert_category_after';
-        $this->call_module_method($categories_data,$categories_id);
+        $this->secure_call_module_method($categories_data,$categories_id);
     }
 
     function insert_category_desc($sql_data_array,$categories_data,$categories_id,$language_id)
@@ -74,25 +88,25 @@ class categoriesModules {
     function remove_category($category_id)
     {
         $this->function_call = 'remove_category';
-        $this->call_module_method($category_id);
+        $this->secure_call_module_method($category_id);
     }
     
     function delete_category_image($category_image)
     {
         $this->function_call = 'delete_category_image';
-        $this->call_module_method($category_image);
+        $this->secure_call_module_method($category_image);
     }
     
     function copy_category_image($src_pic, $dest_pic)
     {
         $this->function_call = 'copy_category_image';
-        $this->call_module_method($src_pic, $dest_pic);
+        $this->secure_call_module_method($src_pic, $dest_pic);
     }
     
     function categories_image_process($categories_image_name, $categories_image_name_process)
     {
         $this->function_call = 'categories_image_process';
-        $this->call_module_method($categories_image_name, $categories_image_name_process);
+        $this->secure_call_module_method($categories_image_name, $categories_image_name_process);
     }
 
     //----- PRODUCTS FUNCTIONS -----//
@@ -105,7 +119,7 @@ class categoriesModules {
     function insert_product_after($products_data,$products_id)
     {
         $this->function_call = 'insert_product_after';
-        $this->call_module_method($products_data,$products_id);
+        $this->secure_call_module_method($products_data,$products_id);
     }
 
     function insert_product_desc($sql_data_array,$products_data,$products_id,$language_id)
@@ -117,13 +131,13 @@ class categoriesModules {
     function remove_product($products_id)
     {
         $this->function_call = 'remove_product';
-        $this->call_module_method($products_id);
+        $this->secure_call_module_method($products_id);
     }
 
     function delete_product($product_id,$product_categories)
     {
         $this->function_call = 'delete_product';
-        $this->call_module_method($product_id,$product_categories);
+        $this->secure_call_module_method($product_id,$product_categories);
     }
 
     function duplicate_product_before($sql_data_array,$src_products_id,$dest_categories_id)
@@ -159,6 +173,6 @@ class categoriesModules {
     function insert_mo_images_after($products_data,$iImg,$products_id)
     {
         $this->function_call = 'insert_mo_images_after';
-        $this->call_module_method($products_data,$iImg,$products_id);  //Return parameter must be in first place
+        $this->secure_call_module_method($products_data,$iImg,$products_id);  //Return parameter must be in first place
     }   
 }
