88895f9ef39911a19b01bbfc87ca97f53c729350
Tomcraft1980 <hallo@modified-shop.org>
2017-05-04 10:23:50 +0000

    Fix #1191
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10712 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/contact_us.php
+++ lang/german/contact_us.php
@@ -7,7 +7,7 @@
  define('EMAIL_PHONE', 'Telefon: ');
  define('EMAIL_FAX', 'Fax: ');
  define('EMAIL_SENT_BY', '&Uuml;bermittelt von %s %s am %s um %s Uhr');
- define('EMAIL_NOTIFY', 'ACHTUNG, diese E-Mail kann NICHT mit -ABSENDER ANTWORTEN- beantwortet werden!');
+ //define('EMAIL_NOTIFY', 'ACHTUNG, diese E-Mail kann NICHT mit -ABSENDER ANTWORTEN- beantwortet werden!'); // Not used yet
  
  //default fields
  define('EMAIL_NAME', 'Name: ');
@@ -15,8 +15,7 @@
  define('EMAIL_MESSAGE', 'Nachricht: ');
  
  //contact-form error messages
- define('ERROR_EMAIL','<p><b>Ihre E-Mail-Adresse:</b> Keine oder ung&uuml;ltige Eingabe!</p>');
- define('ERROR_VVCODE','<p><b>Sicherheitscode:</b> Keine &Uuml;bereinstimmung, bitte geben Sie den Sicherheitscode erneut ein!</p>');
- define('ERROR_MSG_BODY','<p><b>Ihre Nachricht:</b> Keine Eingabe!</p>');	
-   
+ //define('ERROR_EMAIL','<p><b>Ihre E-Mail-Adresse:</b> Keine oder ung&uuml;ltige Eingabe!</p>'); // already defined in german.php
+ //define('ERROR_VVCODE','<p><b>Sicherheitscode:</b> Keine &Uuml;bereinstimmung, bitte geben Sie den Sicherheitscode erneut ein!</p>'); // already defined in german.php
+ //define('ERROR_MSG_BODY','<p><b>Ihre Nachricht:</b> Keine Eingabe!</p>');	 // already defined in german.php
 ?>
