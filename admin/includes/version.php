72f150f135bc6bdbea9fd87bf81ead665fc6ae54
web28 <web28@modified-shop.org>
2017-06-22 17:20:28 +0000

    fix #1224

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -8,6 +8,9 @@
    Copyright (c) 2009 - 2013 [www.modified-shop.org] 
    --------------------------------------------------------------*/
 
+// DB version, used for updates (_installer)
+define('DB_VERSION', 'MOD_2.0.2.2'); // ToDo before release!
+
 define('PROJECT_MAJOR_VERSION', '2');
 define('PROJECT_MINOR_VERSION', '0.2.2');
 define('PROJECT_REVISION', '10690'); // ToDo before release!
@@ -18,3 +21,5 @@ define('MINIMUM_DB_VERSION', '200'); // currently not in use
 // Define the project version
 $version = 'modified eCommerce Shopssoftware v' . PROJECT_MAJOR_VERSION . '.' . PROJECT_MINOR_VERSION . ' rev ' . PROJECT_REVISION . ((PROJECT_SERVICEPACK_VERSION != '') ? ' SP' . PROJECT_SERVICEPACK_VERSION : ''). ' dated: ' . PROJECT_RELEASE_DATE;
 define('PROJECT_VERSION', $version);
+
+define('PROJECT_VERSION_NO', PROJECT_MAJOR_VERSION . '.' . PROJECT_MINOR_VERSION);
