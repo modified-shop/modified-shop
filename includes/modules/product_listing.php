6b011aa363f1c9342a956b8824daa72599ad977a
Tomcraft <Tomcraft@modified-shop.org>
2017-07-04 10:23:35 +0000

    Fix #1235

--- includes/modules/product_listing.php
+++ includes/modules/product_listing.php
@@ -44,7 +44,7 @@ if ($listing_split->number_of_rows > 0) {
   if (USE_PAGINATION_LIST == 'false') {
     $module_smarty->assign('NAVIGATION', '<div class="smallText" style="clear:both;">
                                             <div style="float:left;">'.$listing_split->display_count(TEXT_DISPLAY_NUMBER_OF_PRODUCTS).'</div> 
-                                            <div align="right">'.TEXT_RESULT_PAGE.' '.$listing_split->display_links(MAX_DISPLAY_PAGE_LINKS, xtc_get_all_get_params(array ('page', 'info', 'x', 'y', 'keywords')).(isset($_GET['keywords'])?'keywords='. urlencode($_GET['keywords']):'')).'</div> 
+                                            <div style="text-align:right;">'.TEXT_RESULT_PAGE.' '.$listing_split->display_links(MAX_DISPLAY_PAGE_LINKS, xtc_get_all_get_params(array ('page', 'info', 'x', 'y', 'keywords')).(isset($_GET['keywords'])?'keywords='. urlencode($_GET['keywords']):'')).'</div> 
                                           </div>');
   } else {   
     $module_smarty->assign('DISPLAY_COUNT', $listing_split->display_count(TEXT_DISPLAY_NUMBER_OF_PRODUCTS));
