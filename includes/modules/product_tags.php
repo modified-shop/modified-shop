29bde24154c9de1629cf645a13f58ff7da37dd15
web28 <web28@modified-shop.org>
2017-07-10 08:05:09 +0000

    add hook points to products_tags.php
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10835 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/product_tags.php
+++ includes/modules/product_tags.php
@@ -10,18 +10,28 @@
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
+$add_tags_select = array();
+
+require_once(DIR_FS_INC.'auto_include.inc.php');
+foreach(auto_include(DIR_FS_CATALOG.'includes/extra/modules/products_tags_top/','php') as $file) require ($file);
+
+define('ADD_TAGS_SELECT', (count($add_tags_select) ? rtrim(implode(', ', $add_tags_select), ',').', ' : ''));
+
 $module_smarty = new Smarty;
 $module_smarty->assign('tpl_path', DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/');
 
 $module_content = array ();
                           
-$tags_query = xtDBquery("SELECT pto.options_id,
+$tags_query = xtDBquery("SELECT ".ADD_TAGS_SELECT."
+                                pto.options_id,
                                 pto.options_name,
                                 pto.options_description,
+                                pto.sort_order AS options_sort_order,
                                 pto.options_content_group,
                                 ptv.values_id,
                                 ptv.values_name,
                                 ptv.values_description,
+                                ptv.sort_order AS values_sort_order,
                                 ptv.values_image,
                                 ptv.values_content_group
                            FROM ".TABLE_PRODUCTS_TAGS." pt
@@ -40,15 +50,21 @@ if (xtc_db_num_rows($tags_query, true) > 0) {
   while ($tags = xtc_db_fetch_array($tags_query, true)) {
     if (!isset($module_content[$tags['options_id']])) {
       $module_content[$tags['options_id']] = array('OPTIONS_NAME' => $tags['options_name'],
+                                                   'OPTIONS_ID' => $tags['options_id'],
+                                                   'OPTIONS_SORT_ORDER' => $tags['options_sort_order'],
                                                    'OPTIONS_DESCRIPTION' => $tags['options_description'],
                                                    'OPTIONS_CONTENT_LINK' => (($tags['options_content_group'] != '') ? xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.$tags['options_content_group'], 'NONSSL') : ''),
                                                    'DATA' => array());
     }
     $module_content[$tags['options_id']]['DATA'][] = array('VALUES_NAME' => $tags['values_name'],
+                                                           'VALUES_ID' => $tags['values_id'],
+                                                           'VALUES_SORT_ORDER' => $tags['values_sort_order'],
                                                            'VALUES_DESCRIPTION' => $tags['values_description'],
                                                            'VALUES_IMAGE' => (($tags['values_image'] != '' && is_file(DIR_FS_CATALOG.DIR_WS_IMAGES.$tags['values_image'])) ? DIR_WS_BASE.DIR_WS_IMAGES.$tags['values_image'] : ''),
                                                            'VALUES_CONTENT_LINK' => (($tags['values_content_group'] != '') ? xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.$tags['values_content_group'], 'NONSSL') : ''),
                                                            );
+                                                           
+    foreach(auto_include(DIR_FS_CATALOG.'includes/extra/modules/products_tags_data/','php') as $file) require ($file);
   } 
 }
                      
@@ -56,6 +72,8 @@ if (xtc_db_num_rows($tags_query, true) > 0) {
 if (sizeof($module_content) >= 1) {
   $module_smarty->assign('language', $_SESSION['language']);
   $module_smarty->assign('module_content', $module_content);
+  
+  foreach(auto_include(DIR_FS_CATALOG.'includes/extra/modules/products_tags_end/','php') as $file) require ($file);
 
   // set cache ID
    if (!CacheCheck()) {
@@ -70,4 +88,5 @@ if (sizeof($module_content) >= 1) {
   }
   $info_smarty->assign('MODULE_product_tags', $module);
 }
+foreach(auto_include(DIR_FS_CATALOG.'includes/extra/modules/products_tags_bottom/','php') as $file) require ($file);
 ?>
