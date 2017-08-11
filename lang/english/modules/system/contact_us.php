9ef2aa1543f7567e5df53ebc635c002b50722d77
Tomcraft <Tomcraft@modified-shop.org>
2017-08-11 16:04:02 +0000

    Fixed typo

--- lang/english/modules/system/contact_us.php
+++ lang/english/modules/system/contact_us.php
@@ -12,7 +12,7 @@
 
 define('MODULE_CONTACT_US_STATUS_TITLE','Status');
 define('MODULE_CONTACT_US_STATUS_DESC','Module status');
-define('MODULE_CONTACT_US_STATUS_INFO','Activ');
+define('MODULE_CONTACT_US_STATUS_INFO','Active');
 define('MODULE_CONTACT_US_TEXT_TITLE', 'Contact Log');
 define('MODULE_CONTACT_US_TEXT_DESCRIPTION', 'A log will be written to database for each contact inquiry.');    
 ?>
