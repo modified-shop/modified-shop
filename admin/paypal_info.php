f6de52108f4ce4bffb177e45f7564102f39c1ee8
GTB <GTB@modified-shop.org>
2017-12-06 14:59:11 +0000

    update paypal

---
+++ admin/paypal_info.php
@@ -0,0 +1,70 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+
+require('includes/application_top.php');
+
+// include needed classes
+require_once(DIR_FS_EXTERNAL.'paypal/classes/PayPalAdmin.php');
+$paypal = new PayPalAdmin();
+
+require (DIR_WS_INCLUDES.'head.php');
+?>
+<link rel="stylesheet" type="text/css" href="../includes/external/paypal/css/stylesheet.css">  
+<style type="text/css">
+  .check { width: 40px; float: left; padding-top: 3px; }
+  .drop { width: 230px; float: left; }
+</style>
+</head>
+<body>
+    <!-- header //-->
+    <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+    <!-- header_eof //-->
+
+    <!-- body //-->
+    <table class="tableBody">
+      <tr>
+        <?php //left_navigation
+        if (USE_ADMIN_TOP_MENU == 'false') {
+          echo '<td class="columnLeft2">'.PHP_EOL;
+          echo '<!-- left_navigation //-->'.PHP_EOL;       
+          require_once(DIR_WS_INCLUDES . 'column_left.php');
+          echo '<!-- left_navigation eof //-->'.PHP_EOL; 
+          echo '</td>'.PHP_EOL;      
+        }
+        ?>
+        <!-- body_text //-->
+        <td class="boxCenter">         
+          <div class="pageHeadingImage"><?php echo xtc_image(DIR_WS_ICONS.'heading/icon_configuration.png'); ?></div>
+          <div class="flt-l">
+            <div class="pageHeading pdg2"><?php echo TEXT_PAYPAL_INFO_HEADING_TITLE; ?></div>
+            <div class="main">v<?php echo $paypal->paypal_version; ?></div>
+          </div>
+          <?php
+            include_once(DIR_FS_EXTERNAL.'paypal/modules/admin_menu.php');
+          ?>
+          <div class="clear div_box mrg5" style="margin-top:-1px;">
+            <table class="clear tableConfig">
+
+            </table>
+          </div>
+        </td>
+        <!-- body_text_eof //-->
+      </tr>
+    </table>
+    <!-- body_eof //-->
+    <!-- footer //-->
+    <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+    <!-- footer_eof //-->
+  </body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
