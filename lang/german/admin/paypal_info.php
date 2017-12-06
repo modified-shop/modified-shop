b2e8ce26fbc0e38c50fb722f80c6d826c4bf43ff
modGTB <gerhard@modified-shop.org>
2017-12-06 14:59:11 +0000

    update paypal
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11020 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/german/admin/paypal_info.php
@@ -0,0 +1,22 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+
+$lang_array = array(
+  'TEXT_PAYPAL_INFO_HEADING_TITLE' => 'PayPal Informationen',
+);
+
+
+foreach ($lang_array as $key => $val) {
+  defined($key) or define($key, $val);
+}
+?>
