2308dc70d479fdefdbb815ff17fb2e16de0bcf95
Tomcraft1980 <hallo@modified-shop.org>
2017-12-22 21:14:14 +0000

    fix #1307
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11043 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/payment/invoice.php
+++ lang/german/modules/payment/invoice.php
@@ -30,7 +30,7 @@ define('MODULE_PAYMENT_INVOICE_ALLOWED_TITLE' , 'Erlaubte Zonen');
 define('MODULE_PAYMENT_INVOICE_ALLOWED_DESC' , 'Geben Sie <b>einzeln</b> die Zonen an, welche f&uuml;r dieses Modul erlaubt sein sollen. (z.B. AT,DE (wenn leer, werden alle Zonen erlaubt))');
 define('MODULE_PAYMENT_INVOICE_MIN_ORDER_TITLE' , 'Notwendige Bestellungen');
 define('MODULE_PAYMENT_INVOICE_MIN_ORDER_DESC' , 'Die Mindestanzahl an Bestellungen, die ein Kunden haben muss, damit die Zahlungsmethode zur Verf&uuml;gung steht.');
-define('MODULE_PAYMENT_INVOICE_MIN_ORDER_STATUS_ID_TITLE' , 'Bestellstatus Anzahl Bestellungen');
+define('MODULE_PAYMENT_INVOICE_MIN_ORDER_STATUS_ID_TITLE' , 'Bestellstatus notwendiger Bestellungen');
 define('MODULE_PAYMENT_INVOICE_MIN_ORDER_STATUS_ID_DESC' , 'Wählen Sie hier den Status der Bestellungen aus, der für die unter "Notwendige Bestellungen" eingetragene Anzahl berücksichtigt werden soll.');
 define('MODULE_PAYMENT_INVOICE_MAX_AMOUNT_TITLE' , 'H&ouml;chstbetrag');
 define('MODULE_PAYMENT_INVOICE_MAX_AMOUNT_DESC' , 'Geben Sie hier den H&ouml;chstbetrag f&uuml;r die Bezahlung auf Rechnung an.');
