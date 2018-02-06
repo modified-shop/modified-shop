85f5bc2699f0ea5d6ac7b4491cd2a6dea201f0b4
Tomcraft1980 <hallo@modified-shop.org>
2018-02-06 18:39:07 +0000

    change Online Dispute Resolution (odr) links from http to https
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11060 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ _installer/update/update_2.0.3.0_to_2.0.4.0.sql
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#Tomcraft - 2018-02-06 - changed database_version
+INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.4.0');
+
+#Tomcraft - 2018-02-06 - change Online Dispute Resolution (odr) links from http to https
+UPDATE content_manager SET content_text = REPLACE(content_text, 'http://ec.europa.eu/consumers/odr', 'https://ec.europa.eu/consumers/odr');
+
+# Keep an empty line at the end of this file for the db_updater to work properly
