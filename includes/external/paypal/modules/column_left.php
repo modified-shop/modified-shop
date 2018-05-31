f39b1fdb5dcbb74775e693136f6b81a4495deaa4
modGTB <gerhard@modified-shop.org>
2018-05-31 09:36:25 +0000

    update paypal
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11180 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/modules/column_left.php
+++ includes/external/paypal/modules/column_left.php
@@ -20,10 +20,10 @@
   {      
     echo '<li><a href="#" class="menuBoxContentLinkSub"> -PayPal</a><ul>';
     if (isset($admin_access['paypal_info']) && $admin_access['paypal_info'] == '1') echo '<li><a href="' . xtc_href_link('paypal_info.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_INFO . '</a></li>';
+    if (isset($admin_access['paypal_module']) && $admin_access['paypal_module'] == '1') echo '<li><a href="' . xtc_href_link('paypal_module.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_MODULE . '</a></li>';
     if (isset($admin_access['paypal_config']) && $admin_access['paypal_config'] == '1') echo '<li><a href="' . xtc_href_link('paypal_config.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_CONFIG . '</a></li>';
     if (isset($admin_access['paypal_profile']) && $admin_access['paypal_profile'] == '1') echo '<li><a href="' . xtc_href_link('paypal_profile.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_PROFILE . '</a></li>';
     if (isset($admin_access['paypal_webhook']) && $admin_access['paypal_webhook'] == '1') echo '<li><a href="' . xtc_href_link('paypal_webhook.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_WEBHOOK . '</a></li>';
-    if (isset($admin_access['paypal_module']) && $admin_access['paypal_module'] == '1') echo '<li><a href="' . xtc_href_link('paypal_module.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_MODULE . '</a></li>';
     echo '  </ul></li>';
   } elseif (isset($admin_access['paypal_info']) && $admin_access['paypal_info'] == '1') {
     echo '<li><a href="' . xtc_href_link('paypal_info.php', '') . '" class="menuBoxContentLink"> -PayPal</a></li>';
