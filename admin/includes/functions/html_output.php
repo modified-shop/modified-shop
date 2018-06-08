98aae23be863547120de172dd9ca6f1aa9f2af17
GTB <GTB@modified-shop.org>
2018-06-08 11:46:15 +0000

    fix #1465

--- admin/includes/functions/html_output.php
+++ admin/includes/functions/html_output.php
@@ -294,9 +294,9 @@
    */
   function xtc_sorting($page,$sort) {
     $nav= '<br /><a href="'.xtc_href_link($page, xtc_get_all_get_params(array('action','sorting')).'sorting='.$sort).'" title="'.TEXT_SORT_ASC.'">';
-    $nav.= xtc_image(DIR_WS_ICONS . 'sort_down.gif', TEXT_SORT_ASC, '20' ,'20').'</a>';
+    $nav.= xtc_image(DIR_WS_ICONS . 'sort_up.gif', TEXT_SORT_ASC, '20' ,'20').'</a>';
     $nav.= '<a href="'.xtc_href_link($page, xtc_get_all_get_params(array('action','sorting')).'sorting='.$sort.'-desc').'" title="'.TEXT_SORT_DESC.'">';
-    $nav.= xtc_image(DIR_WS_ICONS . 'sort_up.gif', TEXT_SORT_DESC, '20' ,'20').'</a>';    
+    $nav.= xtc_image(DIR_WS_ICONS . 'sort_down.gif', TEXT_SORT_DESC, '20' ,'20').'</a>';    
     return $nav;
   }
   
