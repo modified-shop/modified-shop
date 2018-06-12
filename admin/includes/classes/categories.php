47d7172bbf58b94899be083bf0d9e48c02914a5c
GTB <GTB@modified-shop.org>
2018-06-12 09:37:42 +0000

    fix #1451

--- admin/includes/classes/categories.php
+++ admin/includes/classes/categories.php
@@ -1328,6 +1328,13 @@ class categories {
         $tax_rate = xtc_get_tax_rate($products_data['products_tax_class_id']);
         $products_data['specials_price'] = $this->priceCheck($products_data['specials_price'], $tax_rate);
       } else {
+        if (!isset($products_data['products_price'])) {
+          $price_query = xtc_cb_query("SELECT products_price
+                                         FROM ".TABLE_PRODUCTS."
+                                        WHERE products_id = '".(int)$products_data['products_id']."'");
+          $price = xtc_db_fetch_array($price_query);
+          $products_data['products_price'] = $price['products_price'];
+        }
         $products_data['specials_price'] = ($products_data['products_price'] - (($products_data['specials_price'] / 100) * $products_data['products_price']));
       }
 
