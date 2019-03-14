047bb4a761842db4dcc00e8d1099380770609407
Tomcraft <Tomcraft@modified-shop.org>
2019-03-14 11:50:24 +0000

    fix warnings

--- includes/extra/modules/product_info_end/easycredit.php
+++ includes/extra/modules/product_info_end/easycredit.php
@@ -11,19 +11,21 @@
    ---------------------------------------------------------------------------------------*/
   
   
-  require_once(DIR_WS_LANGUAGES.$_SESSION['language'].'/modules/payment/easycredit.php');
-  require_once(DIR_WS_MODULES.'payment/easycredit.php');
-  $easycredit = new easycredit();
+  if (defined('MODULE_PAYMENT_EASYCREDIT_STATUS') && MODULE_PAYMENT_EASYCREDIT_STATUS == 'True') {
+    require_once(DIR_WS_LANGUAGES.$_SESSION['language'].'/modules/payment/easycredit.php');
+    require_once(DIR_WS_MODULES.'payment/easycredit.php');
+    $easycredit = new easycredit();
 
-  require_once (DIR_FS_INC.'xtc_get_countries.inc.php');
-  $country = xtc_get_countriesList(((isset($_SESSION['country'])) ? $_SESSION['country'] : ((isset($_SESSION['customer_country_id'])) ? $_SESSION['customer_country_id'] : STORE_COUNTRY)), true);
+    require_once (DIR_FS_INC.'xtc_get_countries.inc.php');
+    $country = xtc_get_countriesList(((isset($_SESSION['country'])) ? $_SESSION['country'] : ((isset($_SESSION['customer_country_id'])) ? $_SESSION['customer_country_id'] : STORE_COUNTRY)), true);
   
-  if ($easycredit->enabled === true
-      && $country['countries_iso_code_2'] == 'DE'
-      ) 
-  {  
-    $amount = $xtPrice->xtcGetPrice($product->data['products_id'], false, 1, $product->data['products_tax_class_id'], $product->data['products_price']); 
-    $presentment = $easycredit->get_presentment_product($amount);
-    $info_smarty->assign('EASYCREDIT', $presentment);
+    if ($easycredit->enabled === true
+        && $country['countries_iso_code_2'] == 'DE'
+        ) 
+    {  
+      $amount = $xtPrice->xtcGetPrice($product->data['products_id'], false, 1, $product->data['products_tax_class_id'], $product->data['products_price']); 
+      $presentment = $easycredit->get_presentment_product($amount);
+      $info_smarty->assign('EASYCREDIT', $presentment);
+    }
   }
 ?>
