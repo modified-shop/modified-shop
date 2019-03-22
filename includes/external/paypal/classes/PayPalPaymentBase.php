9dee30be585cb9537167917c11b2168f5a35698c
Tomcraft1980 <hallo@modified-shop.org>
2019-03-22 11:33:40 +0000

    update paypal - raise module version from 1.11 to 1.12 due to r11583, r11594 & r11597 (update paypal for php 7.3)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11619 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.11';
+    $this->paypal_version = '1.12';
 
     $this->admin_access_array = array(
       'paypal_info',
