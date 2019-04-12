e2b48badb6546fd7ce99dd96e315f6ff03760c53
GTB <GTB@modified-shop.org>
2019-04-12 15:40:24 +0000

    fix #1552

--- address_book_process.php
+++ address_book_process.php
@@ -233,7 +233,7 @@ if (isset ($_POST['action']) && (($_POST['action'] == 'process') || ($_POST['act
       if (isset ($_POST['primary']) && ($_POST['primary'] == 'on')) {
         $_SESSION['customer_first_name'] = $firstname;
         $_SESSION['customer_country_id'] = (int)$country;
-        $_SESSION['customer_zone_id'] = (($zone_id > 0) ? (int) $zone_id : '0');
+        $_SESSION['customer_zone_id'] = ((isset($zone_id) && $zone_id > 0) ? (int)$zone_id : 0);
         if (isset ($_POST['primary']) && ($_POST['primary'] == 'on')) {
           $_SESSION['customer_default_address_id'] = $new_address_book_id;
         }
