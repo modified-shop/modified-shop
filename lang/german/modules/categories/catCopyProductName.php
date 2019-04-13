f4b18ff6a5c5d0cd86ea7cb8bc9af90a772e799f
modGTB <gerhard@modified-shop.org>
2019-04-13 05:36:11 +0000

    fix #831, #1571 & #1612
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11774 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/german/modules/categories/catCopyProductName.php
@@ -0,0 +1,15 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+
+  define('MODULE_CATEGORIES_CATCOPYPRODUCTNAME_TITLE', 'Kopierte Artikel Suffix');
+  define('MODULE_CATEGORIES_CATCOPYPRODUCTNAME_DESCRIPTION', 'Beim kopierten Artikel wird der Artikelname mit (copy) erg&auml;nzt');
