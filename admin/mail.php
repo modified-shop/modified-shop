afb9d9ee0fe2484b92919a605be5d44a20c21df1
GTB <GTB@modified-shop.org>
2019-04-18 12:56:50 +0000

    fix sending duplicate mails/voucher

--- admin/mail.php
+++ admin/mail.php
@@ -22,93 +22,92 @@
 
   require('includes/application_top.php');
 
-  require_once (DIR_FS_INC.'xtc_php_mail.inc.php');
-  require_once (DIR_FS_INC . 'xtc_wysiwyg.inc.php'); 
+  // include needed functions
+  require_once(DIR_FS_INC . 'xtc_wysiwyg.inc.php');
+  require_once(DIR_FS_INC . 'xtc_php_mail.inc.php');
 
-  if ( ($_GET['action'] == 'send_email_to_user') && ($_POST['customers_email_address']) && (!$_POST['back']) ) {
-    switch ($_POST['customers_email_address']) {
-      case '***':
-        $mail_query = xtc_db_query("select customers_firstname, customers_lastname, customers_email_address from " . TABLE_CUSTOMERS);
-        $mail_sent_to = TEXT_ALL_CUSTOMERS;
-        break;
-
-      case '**D':
-        $mail_query = xtc_db_query("select customers_firstname, customers_lastname, customers_email_address from " . TABLE_CUSTOMERS . " where customers_newsletter = '1'");
-        $mail_sent_to = TEXT_NEWSLETTER_CUSTOMERS;
-        break;
-
-      default:
-        if (is_numeric($_POST['customers_email_address'])) {
-          $mail_query = xtc_db_query("select customers_firstname, customers_lastname, customers_email_address from " . TABLE_CUSTOMERS . " where customers_status = " . $_POST['customers_email_address']);
-          $sent_to_query = xtc_db_query("select customers_status_name from " . TABLE_CUSTOMERS_STATUS . " WHERE customers_status_id = '" . $_POST['customers_email_address'] . "' AND language_id='" . (int)$_SESSION['languages_id'] . "'");
-          $sent_to = xtc_db_fetch_array($sent_to_query);
-          $mail_sent_to = $sent_to['customers_status_name'];
-        } else {
-          $customers_email_address = xtc_db_prepare_input($_POST['customers_email_address']);
-          $mail_query = xtc_db_query("select customers_firstname, customers_lastname, customers_email_address from " . TABLE_CUSTOMERS . " where customers_email_address = '" . xtc_db_input($customers_email_address) . "'");
-          $mail_sent_to = $_POST['customers_email_address'];
-        }
-        break;
-    }
-
-    $from = xtc_db_prepare_input($_POST['from']);
-    $subject = xtc_db_prepare_input($_POST['subject']);
-    $message = xtc_db_prepare_input($_POST['message']);
-
-
-    while ($mail = xtc_db_fetch_array($mail_query)) {
-
-      xtc_php_mail(EMAIL_SUPPORT_ADDRESS,
-                   EMAIL_SUPPORT_NAME,
-                   $mail['customers_email_address'] ,
-                   $mail['customers_firstname'] . ' ' . $mail['customers_lastname'] ,
-                   '',
-                   EMAIL_SUPPORT_REPLY_ADDRESS,
-                   EMAIL_SUPPORT_REPLY_ADDRESS_NAME,
-                   '',
-                   '',
-                   $subject,
-                   $message,
-                   $message);
-    }
-
-
-
-    xtc_redirect(xtc_href_link(FILENAME_MAIL, 'mail_sent_to=' . urlencode($mail_sent_to)));
-  }
   $error = false;
-  if ( ($_GET['action'] == 'preview') && (!$_POST['customers_email_address']) ) {
-    $messageStack->add(ERROR_NO_CUSTOMER_SELECTED, 'error');
-    $error = true;
-  }
+  if (isset($_GET['action']) && $_GET['action'] != '') {
+    switch ($_GET['action']) {
+      case 'send_email_to_user':
+        if (isset($_POST['customers_email_address']) 
+            && $_POST['customers_email_address'] != ''
+            && !isset($_POST['back'])
+            )
+        {
+          switch ($_POST['customers_email_address']) {
+            case '***':
+              $mail_query = xtc_db_query("SELECT *
+                                            FROM " . TABLE_CUSTOMERS . " 
+                                        GROUP BY customers_email_address");
+              $mail_sent_to = TEXT_ALL_CUSTOMERS;
+              break;
+            case '**D':
+              $mail_query = xtc_db_query("SELECT *
+                                            FROM " . TABLE_NEWSLETTER_RECIPIENTS . " 
+                                           WHERE mail_status = '1'");
+              $mail_sent_to = TEXT_NEWSLETTER_CUSTOMERS;
+              break;
+            default:
+              $mail_sent_to = xtc_db_prepare_input($_POST['customers_email_address']);
+              
+              $where = "WHERE customers_email_address = '" . xtc_db_input($mail_sent_to) . "'";
+              if (is_numeric($mail_sent_to)) {
+                $where = "WHERE customers_status = '" . (int)$mail_sent_to . "'";
+              }
+              $mail_query = xtc_db_query("SELECT *
+                                            FROM " . TABLE_CUSTOMERS . " 
+                                                 " . $where . "
+                                        GROUP BY customers_email_address");
+              if (isset($_POST['email_to']) && $_POST['email_to'] != '') {
+                $mail_sent_to = $_POST['email_to'];
+              }
+              break;
+          }
 
-  if ($_GET['mail_sent_to']) {
-    $messageStack->add(sprintf(NOTICE_EMAIL_SENT_TO, $_GET['mail_sent_to']), 'success');
-  }
+          $subject = xtc_db_prepare_input($_POST['subject']);
+          $message = xtc_db_prepare_input($_POST['message']);
   
-  if (isset($_POST['customers_email_address']) && !empty($_POST['customers_email_address'])) {
-    switch ($_POST['customers_email_address']) {
-      case '***':
-        $mail_sent_to = TEXT_ALL_CUSTOMERS;
-        break;
-      case '**D':
-        $mail_sent_to = TEXT_NEWSLETTER_CUSTOMERS;
-        break;
-      default:
-        $mail_sent_to = $_POST['customers_email_address'];
-        if ($_POST['email_to']) {
-          $mail_sent_to = $_POST['email_to'];
+          if (xtc_db_num_rows($mail_query) > 0) {
+            while ($mail = xtc_db_fetch_array($mail_query)) {
+              $mail['subject'] = $subject; 
+              $mail['message'] = $message; 
+      
+              send_gv_mail($data);
+            }
+          }
+  
+          if (isset($_POST['email_to']) && $_POST['email_to'] != '') {
+            $mail['subject'] = $subject; 
+            $mail['message'] = $message; 
+            $mail['customers_email_address'] = $_POST['email_to'];
+  
+            send_gv_mail($data);
+          }
+  
+          $messageStack->add_session(sprintf(NOTICE_EMAIL_SENT_TO, $mail_sent_to), 'success');
+          if (isset($_GET['cID']) && $_GET['cID'] != '') {
+            xtc_redirect(xtc_href_link(FILENAME_CUSTOMERS, xtc_get_all_get_params(array('action'))));
+          }
+          xtc_redirect(xtc_href_link(FILENAME_MAIL, xtc_get_all_get_params(array('action', 'cID'))));
         }
         break;
-    }        
+      
+      case 'preview':
+        if (!isset($_POST['customers_email_address']) || $_POST['customers_email_address'] == '') {
+          $messageStack->add(ERROR_NO_CUSTOMER_SELECTED, 'error');
+          $error = true;
+        }
+        break;
+    }
   }
 
   require (DIR_WS_INCLUDES.'head.php');
 
-  if (USE_WYSIWYG=='true' && ($_GET['action'] != 'preview' || $error== true)) {
+  if (USE_WYSIWYG == 'true' && ($_GET['action'] != 'preview' || $error == true)) {
     $query=xtc_db_query("SELECT code FROM ". TABLE_LANGUAGES ." WHERE languages_id='".(int)$_SESSION['languages_id']."'");
     $data=xtc_db_fetch_array($query);
-    echo xtc_wysiwyg('mail',$data['code']);
+    echo xtc_wysiwyg('gv_mail', $data['code']);
   } 
 ?>
 </head>
@@ -130,14 +129,14 @@
       ?>
       <!-- body_text //-->
       <td class="boxCenter"> 
-      <div class="mrg5" style="width:1000px;">
         <div class="pageHeadingImage"><?php echo xtc_image(DIR_WS_ICONS.'heading/icon_news.png'); ?></div>
         <div class="flt-l">
-          <div class="pageHeading pdg2"><?php echo HEADING_TITLE; ?></div>              
+          <div class="pageHeading"><?php echo HEADING_TITLE; ?></div>              
         </div>
         <div class="clear"></div>
-        <?php
-          if ( ($_GET['action'] == 'preview') && ($_POST['customers_email_address']) ) {
+        <div class="div_box brd-none pdg2">
+          <?php
+          if ($_GET['action'] == 'preview' && $error === false) {
             switch ($_POST['customers_email_address']) {
               case '***':
                 $mail_sent_to = TEXT_ALL_CUSTOMERS;
@@ -146,25 +145,20 @@
                 $mail_sent_to = TEXT_NEWSLETTER_CUSTOMERS;
                 break;
               default:
-                if (is_numeric($_POST['customers_email_address'])) {
-                  $sent_to_query = xtc_db_query("select customers_status_name from " . TABLE_CUSTOMERS_STATUS . " WHERE customers_status_id = '" . $_POST['customers_email_address'] . "' AND language_id='" . (int)$_SESSION['languages_id'] . "'");
-                  $sent_to = xtc_db_fetch_array($sent_to_query);
-                  $mail_sent_to = $sent_to['customers_status_name'];
-                } else {
-                  $mail_sent_to = $_POST['customers_email_address'];
+                $mail_sent_to = $_POST['customers_email_address'];
+                if (is_numeric($mail_sent_to)) {
+                  $status = xtc_get_customers_statuses(true);
+                  $mail_sent_to = $status[$mail_sent_to]['text'];
                 }
                 break;
-            }
-        ?>
-        <?php echo xtc_draw_form('mail', FILENAME_MAIL, 'action=send_email_to_user'); ?>
+            }  
+
+            echo xtc_draw_form('mail', FILENAME_MAIL, 'action=send_email_to_user');
+              ?>
               <table class="tableConfig borderall">
                 <tr>
                   <td class="dataTableConfig col-left"><?php echo TEXT_CUSTOMER; ?></td>
                   <td class="dataTableConfig col-single-right"><?php echo $mail_sent_to; ?></td>
-                </tr>
-                <tr>
-                  <td class="dataTableConfig col-left"><?php echo TEXT_FROM; ?></td>
-                  <td class="dataTableConfig col-single-right"><?php echo encode_htmlspecialchars(stripslashes($_POST['from'])); ?></td>
                 </tr>
                 <tr>
                   <td class="dataTableConfig col-left"><?php echo TEXT_SUBJECT; ?></td>
@@ -176,84 +170,72 @@
                 </tr>                
               </table> 
               <?php
-              /* Re-Post all POST'ed variables */
-              reset($_POST);
-              foreach ($_POST as $key => $value) {
-                if (!is_array($_POST[$key])) {
-                  echo xtc_draw_hidden_field($key, encode_htmlspecialchars(stripslashes($value)));
+                reset($_POST);
+                foreach ($_POST as $key => $value) {
+                  if (!is_array($_POST[$key])) {
+                    echo xtc_draw_hidden_field($key, encode_htmlspecialchars(stripslashes($value)));
+                  }
                 }
-              }
               ?>
-              
               <div class="smallText flt-l mrg5"><?php echo '<input type="submit" class="button" name="back" onclick="this.blur();" value="' . BUTTON_BACK . '"/>'; ?></div>
-              <div class="smallText flt-r mrg5"><?php echo '<a class="button" onclick="this.blur();" href="' . xtc_href_link(FILENAME_MAIL) . '">' . BUTTON_CANCEL . '</a> <input type="submit" class="button" onclick="this.blur();" value="' . BUTTON_SEND_EMAIL . '"/>'; ?></div>
-
-          </form>
- 
-        <?php
+              <div class="smallText flt-r mrg5"><?php echo '<a class="button" onclick="this.blur();" href="' . xtc_href_link(FILENAME_MAIL, xtc_get_all_get_params(array('action'))) . '">' . BUTTON_CANCEL . '</a> <input type="submit" class="button" onclick="this.blur();" value="' . BUTTON_SEND_EMAIL . '"/>'; ?></div>
+            </form>
+            <?php
           } else {
+
             $customers = array();
             $customers[] = array('id' => '', 'text' => TEXT_SELECT_CUSTOMER);
             $customers[] = array('id' => '***', 'text' => TEXT_ALL_CUSTOMERS);
             $customers[] = array('id' => '**D', 'text' => TEXT_NEWSLETTER_CUSTOMERS);
-            // Customers Status 1.x
-            //    $customers_statuses_array = xtc_get_customers_statuses();
-            $customers_statuses_array = xtc_db_query("select customers_status_id , customers_status_name from " . TABLE_CUSTOMERS_STATUS . " WHERE language_id='" . (int)$_SESSION['languages_id'] . "' order by customers_status_name");
-            while ($customers_statuses_value = xtc_db_fetch_array($customers_statuses_array)) {
-              $customers[] = array('id' => $customers_statuses_value['customers_status_id'],
-                                   'text' => $customers_statuses_value['customers_status_name']);
-            }
-            // End customers Status 1.x
-            $selected_customer = isset($_GET['customer']) ? $_GET['customer'] : $_POST['customers_email_address'];
-            if (!empty($selected_customer)) {
-              $mail_query = xtc_db_query("SELECT customers_email_address, 
-                                                 customers_firstname, 
-                                                 customers_lastname 
+            $customers = array_merge($customers, xtc_get_customers_statuses());
+
+            $selected_customer = urldecode((isset($_GET['customer'])) ? $_GET['customer'] : ((isset($_POST['customers_email_address'])) ? $_POST['customers_email_address'] : ''));
+            
+            if ($selected_customer != '') {
+              $mail_query = xtc_db_query("SELECT *
                                             FROM " . TABLE_CUSTOMERS . " 
-                                           WHERE customers_email_address = '".$selected_customer."'
+                                           WHERE customers_email_address = '".xtc_db_input($selected_customer)."'
+                                        GROUP BY customers_email_address
                                         ORDER BY customers_lastname");
               while($customers_values = xtc_db_fetch_array($mail_query)) {
                 $customers[] = array(
                   'id' => $customers_values['customers_email_address'],
-                  'text' => $customers_values['customers_lastname'] . ', ' . $customers_values['customers_firstname'] . ' (' . $customers_values['customers_email_address'] . ')');
+                  'text' => $customers_values['customers_lastname'] . ', ' . $customers_values['customers_firstname'] . ' (' . $customers_values['customers_email_address'] . ')'
+                );
               }
-            }  
-          ?>
-          <?php echo xtc_draw_form('mail', FILENAME_MAIL, 'action=preview'); ?>
+            }
+            
+            echo xtc_draw_form('mail', FILENAME_MAIL, xtc_get_all_get_params(array('action')).'action=preview');
+              ?>
               <table class="tableConfig borderall">
                 <tr>
                   <td class="dataTableConfig col-left"><?php echo TEXT_CUSTOMER; ?></td>
-                  <td class="dataTableConfig col-single-right"><?php echo xtc_draw_pull_down_menu('customers_email_address', $customers, ((isset($_GET['customer'])) ? $_GET['customer'] : $_POST['customers_email_address']));?></td>
-                </tr>
-                <tr>
-                  <td class="dataTableConfig col-left"><?php echo TEXT_FROM; ?></td>
-                  <td class="dataTableConfig col-single-right"><?php echo xtc_draw_input_field('from', EMAIL_FROM); ?></td>
+                  <td class="dataTableConfig col-single-right"><?php echo xtc_draw_pull_down_menu('customers_email_address', $customers, $selected_customer);?></td>
                 </tr>
                 <tr>
                   <td class="dataTableConfig col-left"><?php echo TEXT_SUBJECT; ?></td>
-                  <td class="dataTableConfig col-single-right"><?php echo xtc_draw_input_field('subject', $_POST['subject']); ?></td>
+                  <td class="dataTableConfig col-single-right"><?php echo xtc_draw_input_field('subject', ((isset($_POST['subject'])) ? $_POST['subject'] : ''), 'style="width: 100%;"'); ?></td>
                 </tr>
                 <tr>
                   <td class="dataTableConfig col-left"><?php echo TEXT_MESSAGE; ?></td>
-                  <td class="dataTableConfig col-single-right"><?php echo xtc_draw_textarea_field('message', 'soft', '100%', '55', $_POST['message']); ?></td>
+                  <td class="dataTableConfig col-single-right"><?php echo xtc_draw_textarea_field('message', 'soft', '100%', '55', ((isset($_POST['message'])) ? $_POST['message'] : '')); ?></td>
                 </tr>                
-              </table>
-              
-            <div class="smallText flt-r mrg5"><?php echo '<input type="submit" class="button" onclick="this.blur();" value="' . BUTTON_SEND_EMAIL . '"/>'; ?></div>
-          </form>
-      <?php
-        }
-      ?>
-      </div>
-    </td>
-<!-- body_text_eof //-->
-  </tr>
-</table>
-<!-- body_eof //-->
-<!-- footer //-->
-<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
-<!-- footer_eof //-->
-<br />
+              </table> 
+
+              <div class="smallText mrg5 txta-r"><?php echo '<input type="submit" class="button" onclick="this.blur();" value="' . BUTTON_SEND_EMAIL . '"/>'; ?></div>
+            </form>
+          <?php
+          }
+        ?>
+        </div>
+      </td>
+      <!-- body_text_eof //-->
+    </tr>
+  </table>
+  <!-- body_eof //-->
+  <!-- footer //-->
+  <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+  <!-- footer_eof //-->
 </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
