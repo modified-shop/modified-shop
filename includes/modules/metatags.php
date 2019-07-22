7080b25908b1b9c54c9c0a68910cc5083ce0e627
modGTB <gerhard@modified-shop.org>
2019-07-22 08:59:11 +0000

    fix #704, #1366, #1438 & #1529
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11958 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/metatags.php
+++ includes/modules/metatags.php
@@ -531,10 +531,17 @@
     require_once(DIR_WS_CLASSES . 'language.php');
     $lng = new language;
   }
-  if (SEARCH_ENGINE_FRIENDLY_URLS == 'true' && $set_hreflang && count($lng->catalog_languages) > 1 && (!isset($_GET['page']) || $_GET['page'] == 1)) {
+  if (SEARCH_ENGINE_FRIENDLY_URLS == 'true' 
+      && defined('MODULE_MULTILANG_STATUS')
+      && MODULE_MULTILANG_STATUS == 'true'
+      && $set_hreflang 
+      && count($lng->catalog_languages) > 1 
+      && (!isset($_GET['page']) || $_GET['page'] == 1)
+      ) 
+  {
     $canonical_flag = true;
     $x_default_flag = true;
-    $x_default_lng = ((defined('META_X_DEFAULT')) ? META_X_DEFAULT : 'en');
+    $x_default_lng = ((defined('MODULE_MULTILANG_X_DEFAULT')) ? MODULE_MULTILANG_X_DEFAULT : 'en');
     $x_default_link = xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params_include(array('products_id', 'cPath', 'manufacturers_id', 'coID')).'language='.$x_default_lng, 'NONSSL', false);
     if ($x_default_link != '#') {
       $meta_alternate['x-default'] = '<link rel="alternate" href="'.$x_default_link.'" hreflang="x-default" />';
