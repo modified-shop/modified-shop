ad75231b47e98db5ff45f3f68b433dac46581d4f
modGTB <gerhard@modified-shop.org>
2019-07-25 11:05:11 +0000

    fix #1492 - fix notice
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12007 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/classes/order.php
+++ includes/classes/order.php
@@ -553,16 +553,18 @@
         $this->billing = $customers_standard_arr;
       }
       
-      require_once (DIR_WS_CLASSES . 'shipping.php');
-      $shipping_modules = new shipping($_SESSION['shipping']);
-      $shipping_class = substr($this->info['shipping_class'], 0, strpos($this->info['shipping_class'], '_'));
+      if (isset($_SESSION['shipping']) && $_SESSION['shipping'] != false) {
+        require_once (DIR_WS_CLASSES . 'shipping.php');
+        $shipping_modules = new shipping($_SESSION['shipping']);
+        $shipping_class = substr($this->info['shipping_class'], 0, strpos($this->info['shipping_class'], '_'));
       
-      if (isset($GLOBALS[$shipping_class])
-          && is_object($GLOBALS[$shipping_class])
-          && method_exists($GLOBALS[$shipping_class], 'address')
-          )
-      {
-        $this->delivery = $GLOBALS[$shipping_class]->address();
+        if (isset($GLOBALS[$shipping_class])
+            && is_object($GLOBALS[$shipping_class])
+            && method_exists($GLOBALS[$shipping_class], 'address')
+            )
+        {
+          $this->delivery = $GLOBALS[$shipping_class]->address();
+        }
       }
       
       $index = 0;
