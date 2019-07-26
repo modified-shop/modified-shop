9c72731af32edad6ddb3b154c4c6d1a6c468a588
GTB <GTB@modified-shop.org>
2019-07-26 16:19:21 +0000

    fix #1457

---
+++ lang/english/modules/system/dsgvo_export.php
@@ -0,0 +1,17 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_DSGVO_EXPORT_TEXT_TITLE', 'DSGVO Export');
+  define('MODULE_DSGVO_EXPORT_TEXT_DESCRIPTION', 'XML export of all saved data for a customer.');
+  define('MODULE_DSGVO_EXPORT_SEARCH_TITLE', '<b>Customers search</b><br/>');
+  define('MODULE_DSGVO_EXPORT_SEARCH_DESC', 'Enter the E-Mail address of the customer.');
+?>
---
+++ lang/german/modules/system/dsgvo_export.php.php
@@ -0,0 +1,17 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_DSGVO_EXPORT_TEXT_TITLE', 'DSGVO Export');
+  define('MODULE_DSGVO_EXPORT_TEXT_DESCRIPTION', 'XML Export aller gepsicherten Daten für einen Kunden.');
+  define('MODULE_DSGVO_EXPORT_SEARCH_TITLE', '<b>Kundensuche</b><br/>');
+  define('MODULE_DSGVO_EXPORT_SEARCH_DESC', 'Geben sie die E-Mail Adresse des Kunden ein.');
+?>
