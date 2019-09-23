66eebf274d3a96a7e21a31e3a5ce7fe54ce96917
GTB <GTB@modified-shop.org>
2019-09-23 07:42:38 +0000

    fix #1545 - update cookie consent

--- lang/english/extra/cookieconsent.php
+++ lang/english/extra/cookieconsent.php
@@ -1,6 +1,7 @@
 <?php
  //additional fields
- define('TEXT_COOKIECONSENT_MESSAGE', 'Our website uses cookies to make your browsing expirience better. By using our site you agree to our use of cookies.');
+ define('TEXT_COOKIECONSENT_MESSAGE_INFO', 'Our website uses cookies to make your browsing expirience better. By using our site you agree to our use of cookies.');
+ define('TEXT_COOKIECONSENT_MESSAGE_TRACKING', 'This website uses cookies to analyze website traffic and marketing efforts. By using our site, you agree to this use.');
  define('TEXT_COOKIECONSENT_DISSMISS', 'Agree!');
  define('TEXT_COOKIECONSENT_LINK', 'Learn more');
  define('TEXT_COOKIECONSENT_POLICY', 'Cookie Policy');
