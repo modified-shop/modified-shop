30f234bcb04532f8577edf05c325c31aadbcab10
modGTB <gerhard@modified-shop.org>
2019-10-11 07:51:42 +0000

    update piwik
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12272 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/smarty/plugins/function.piwik.php
+++ includes/external/smarty/plugins/function.piwik.php
@@ -55,28 +55,26 @@ function smarty_function_piwik($params, $smarty) {
 
   $beginCode = '
     <script type="text/javascript">
-    function TrackingPiwik () {
       var _paq = _paq || [];
-      (function(){
+      function TrackingPiwik () {
         var u="//'.$url.'/";
         _paq.push([\'setSiteId\', '.$id.']);
         _paq.push([\'setTrackerUrl\', u+\'piwik.php\']);
-  '."\n";
+        _paq.push([\'trackPageView\']);
+        _paq.push([\'enableLinkTracking\']);'."\n";
 
   $endCode = '
-        _paq.push([\'trackPageView\']);
-        _paq.push([\'enableLinkTracking\']);
-
-        var d=document,
-        g=d.createElement(\'script\'),
-        s=d.getElementsByTagName(\'script\')[0];
-        g.type=\'text/javascript\';
-        g.defer=true;
-        g.async=true;
-        g.src=u+\'piwik.js\';
-        s.parentNode.insertBefore(g,s);
-      })();
-    }
+        (function(){
+          var d=document,
+          g=d.createElement(\'script\'),
+          s=d.getElementsByTagName(\'script\')[0];
+          g.type=\'text/javascript\';
+          g.defer=true;
+          g.async=true;
+          g.src=u+\'piwik.js\';
+          s.parentNode.insertBefore(g,s);
+        })();
+      }
     </script>
   ';
   
