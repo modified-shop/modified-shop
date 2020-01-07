a22638f17fcb0d9010d11824e5a860bfecb7173a
modGTB <gerhard@modified-shop.org>
2020-01-07 15:18:41 +0000

    fix html error
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12499 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_category.php
+++ admin/includes/modules/new_category.php
@@ -155,7 +155,7 @@
       <input type="submit" class="button" name="cat_save" value="<?php echo BUTTON_SAVE; ?>" style="cursor:pointer" <?php echo $confirm_save_entry;?>>&nbsp;&nbsp;
       <?php
       if (isset($_GET['cID']) && $_GET['cID'] > 0) {
-        echo '<input type="submit" class="button" name="cat_update" value="'.BUTTON_UPDATE.'" style="cursor:pointer" '.$confirm_save_entry.'/>&nbsp;&nbsp';
+        echo '<input type="submit" class="button" name="cat_update" value="'.BUTTON_UPDATE.'" style="cursor:pointer" '.$confirm_save_entry.'/>&nbsp;&nbsp;';
       }
       ?>
       <a class="button" onclick="this.blur()" href="<?php echo xtc_href_link(FILENAME_CATEGORIES, 'cPath=' . $cPath . ((isset($_GET['action']) && $_GET['action']=='edit_category') ? '&cID=' . (int)$_GET['cID'] : '') . ((isset($_GET['page']) && $_GET['page']>'1') ? '&page=' . (int)$_GET['page'] : '')); ?>"><?php echo BUTTON_CANCEL ; ?></a>
@@ -226,7 +226,7 @@
         <input type="submit" class="button" name="cat_save" value="<?php echo BUTTON_SAVE; ?>" style="cursor:pointer" <?php echo $confirm_save_entry;?>>&nbsp;&nbsp;
         <?php
         if (isset($_GET['cID']) && $_GET['cID'] > 0) {
-          echo '<input type="submit" class="button" name="cat_update" value="'.BUTTON_UPDATE.'" style="cursor:pointer" '.$confirm_save_entry.'/>&nbsp;&nbsp';
+          echo '<input type="submit" class="button" name="cat_update" value="'.BUTTON_UPDATE.'" style="cursor:pointer" '.$confirm_save_entry.'/>&nbsp;&nbsp;';
         }
         ?>
         <a class="button" onclick="this.blur()" href="<?php echo xtc_href_link(FILENAME_CATEGORIES, 'cPath=' . $cPath . ((isset($_GET['action']) && $_GET['action']=='edit_category') ? '&cID=' . (int)$_GET['cID'] : '') . ((isset($_GET['page']) && $_GET['page']>'1') ? '&page=' . (int)$_GET['page'] : '')); ?>"><?php echo BUTTON_CANCEL ; ?></a>
