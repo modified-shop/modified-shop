f91051fbf1a154e020a048164e655fb954b53f25
GTB <GTB@modified-shop.org>
2020-01-10 16:36:19 +0000

    fix caching

--- print_product_info.php
+++ print_product_info.php
@@ -86,7 +86,7 @@ if (!is_object($product) || $product->isProduct() === false || $language_not_fou
                                         ".SPECIALS_CONDITIONS;
   $special_expires_date_query = xtc_db_query($special_expires_date_query);
   if (xtc_db_num_rows($special_expires_date_query) > 0) {
-    $sDate = xtc_db_fetch_array($special_expires_date_query, true);
+    $sDate = xtc_db_fetch_array($special_expires_date_query);
     $info_smarty->assign('PRODUCTS_EXPIRES', $sDate['expires_date'] != '0000-00-00 00:00:00' ? xtc_date_short($sDate['expires_date']) : '');
   }
 
