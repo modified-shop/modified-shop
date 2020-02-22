79ebda193b952bc76c033da5e2fcfc497e82a232
Tomcraft <Tomcraft@modified-shop.org>
2020-02-22 12:05:22 +0000

    update paypal - raise module version from 1.36 to 1.37 due to r12575, r12576, r12577 & r12580

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.36';
+    $this->paypal_version = '1.37';
 
     $this->admin_access_array = array(
       'paypal_info',
