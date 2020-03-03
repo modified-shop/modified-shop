c0d1c026cbb85776800a68e31fae180272a79910
modGTB <gerhard@modified-shop.org>
2020-03-03 07:20:03 +0000

    fix #1432, #1774 - removed masterpayment
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12614 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/includes/delete_dirs.php
+++ _installer/includes/delete_dirs.php
@@ -13,57 +13,59 @@
 
   // set all directories to be deleted                     
   $unlink_dir = array(
-    '_installer/buttons', // neu
-    '_installer/images/buttons', // neu
-    '_installer/inc', // neu
-    '_installer/includes/css', // neu
-    '_installer/includes/javascript', // neu
-    '_installer/includes/templates', // neu
-    '_installer/language', // neu
-    DIR_ADMIN.'includes/local', // neu
+    '_installer/buttons',
+    '_installer/images/buttons',
+    '_installer/inc',
+    '_installer/includes/css',
+    '_installer/includes/javascript',
+    '_installer/includes/templates',
+    '_installer/language',
+    DIR_ADMIN.'includes/local',
     DIR_ADMIN.'includes/modules/carp',
-    DIR_ADMIN.'includes/modules/export/idealo_lib', // neu
+    DIR_ADMIN.'includes/modules/export/idealo_lib',
     DIR_ADMIN.'includes/modules/fckeditor',
     DIR_ADMIN.'includes/modules/filemanager/js/cors',
     DIR_ADMIN.'includes/modules/kcfinder',
     DIR_ADMIN.'includes/modules/magpierss',
     DIR_ADMIN.'includes/safeterms',
-    DIR_ADMIN.'includes/xsbooster', // neu
+    DIR_ADMIN.'includes/xsbooster',
     DIR_ADMIN.'rss',
-    'api/easybill',  // neu
+    'api/easybill', 
     'api/easymarketing',
     'api/findologic',
-    'callback/pn_sofortueberweisung', // neu
-    'callback/xtbooster', // neu
-    'export/easybill', // neu
-    'export/idealo', // neu
-    'export/idealo_realtime', // neu
-    'images/infobox', // neu
-    'includes/classes/nusoap', // neu
+    'callback/pn_sofortueberweisung',
+    'callback/masterpayment',
+    'callback/xtbooster',
+    'export/easybill',
+    'export/idealo',
+    'export/idealo_realtime',
+    'images/infobox',
+    'includes/classes/nusoap',
     'includes/classes/Smarty_2.6.22',
     'includes/classes/Smarty_2.6.26',
-    'includes/classes/Smarty_2.6.27', // neu
-    'includes/external/easybill',  // neu
-    'includes/external/billsafe',  // neu
+    'includes/classes/Smarty_2.6.27',
+    'includes/external/easybill', 
+    'includes/external/billsafe', 
     'includes/external/findologic',
+    'includes/external/masterpayment',
     'includes/external/paypal/lib/Psr',
-    'includes/external/phpfastcache/3.0.0',  // neu
-    'includes/external/phpfastcache/_extensions',  // neu
-    'includes/external/phpmailer/language', // neu
+    'includes/external/phpfastcache/3.0.0', 
+    'includes/external/phpfastcache/_extensions', 
+    'includes/external/phpmailer/language',
     'includes/external/sofort/core',
     'includes/external/sofort/unittests',
-    'includes/econda', // neu
+    'includes/econda',
     'includes/iclear',
     'includes/janolaw',
-    'includes/masterpayment', // neu
-    'includes/nusoap', // neu
-    'includes/shopgate', // neu
+    'includes/masterpayment',
+    'includes/nusoap',
+    'includes/shopgate',
     'shopstat',
-    'sseq-filter', // neu
-    'sseq-lib', // neu
-    'callback/sofort/library', // neu
-    'callback/sofort/ressources', // neu
-    'xtc_installer', // neu
+    'sseq-filter',
+    'sseq-lib',
+    'callback/sofort/library',
+    'callback/sofort/ressources',
+    'xtc_installer',
   );
 
 
