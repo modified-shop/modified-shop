6594c1b9a7ad4875e4422ff1e225bbd39b766e74
modGTB <gerhard@modified-shop.org>
2020-03-05 14:33:45 +0000

    use backticks for sql updates
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12619 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.4.2_to_2.0.5.0.sql
+++ _installer/update/update_2.0.4.2_to_2.0.5.0.sql
@@ -11,21 +11,21 @@
 INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.5.0');
 
 #GTB - 2018-06-11 - fix #1378
-ALTER TABLE orders_products MODIFY products_weight DECIMAL(15,4) NOT NULL;
-ALTER TABLE products MODIFY products_weight DECIMAL(15,4) NOT NULL;
+ALTER TABLE `orders_products` MODIFY `products_weight` DECIMAL(15,4) NOT NULL;
+ALTER TABLE `products` MODIFY `products_weight` DECIMAL(15,4) NOT NULL;
 
 #GTB - 2018-06-19 - add newsletter update
 ALTER TABLE admin_access ADD newsletter_recipients INT(1) NOT NULL DEFAULT '0' AFTER paypal_module;
-UPDATE admin_access SET newsletter_recipients = 1 WHERE customers_id = 1 LIMIT 1;
-UPDATE admin_access SET newsletter_recipients = 5 WHERE customers_id = 'groups' LIMIT 1;
+UPDATE `admin_access` SET `newsletter_recipients` = 1 WHERE `customers_id` = 1 LIMIT 1;
+UPDATE `admin_access` SET `newsletter_recipients` = 5 WHERE `customers_id` = 'groups' LIMIT 1;
 
-DROP TABLE IF EXISTS newsletter_recipients_history;
-CREATE TABLE newsletter_recipients_history (
-  customers_email_address VARCHAR(255) NOT NULL,
-  customers_action VARCHAR(32) NOT NULL,
-  ip_address varchar(50) DEFAULT NULL,
-  date_added datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  KEY idx_customers_email_address (customers_email_address)
+DROP TABLE IF EXISTS `newsletter_recipients_history`;
+CREATE TABLE `newsletter_recipients_history` (
+  `customers_email_address` VARCHAR(255) NOT NULL,
+  `customers_action` VARCHAR(32) NOT NULL,
+  `ip_address` varchar(50) DEFAULT NULL,
+  `date_added datetime` NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `idx_customers_email_address` (`customers_email_address`)
 );
 
 #GTB - 2018-06-27 - add sort order tags
@@ -35,44 +35,44 @@ ALTER TABLE `products_tags` ADD `sort_order` INT(11) NOT NULL DEFAULT '0' AFTER 
 ALTER TABLE `coupon_email_track` DROP INDEX `idx_coupon_id`;
 
 #GTB - 2018-09-04 new captcha handling
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_USE_COLOR';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_USE_SHADOW';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_CODE_LENGTH';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_NUM_LINES';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_MIN_FONT';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_MAX_FONT';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_BACKGROUND_RGB';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_LINES_RGB';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_CHARS_RGB';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_WIDTH';
-DELETE FROM `configuration` WHERE configuration_key = 'MODULE_CAPTCHA_HEIGHT';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_USE_COLOR';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_USE_SHADOW';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_CODE_LENGTH';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_NUM_LINES';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_MIN_FONT';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_MAX_FONT';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_BACKGROUND_RGB';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_LINES_RGB';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_CHARS_RGB';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_WIDTH';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_CAPTCHA_HEIGHT';
 
 #Tomcraft - 2018-10-13 - Delete entries for France, Metropolitan and Yugoslavia
-DELETE FROM `zones_to_geo_zones` WHERE association_id = 74;
-DELETE FROM `zones_to_geo_zones` WHERE association_id = 236;
+DELETE FROM `zones_to_geo_zones` WHERE `association_id` = 74;
+DELETE FROM `zones_to_geo_zones` WHERE `association_id` = 236;
 
 #Tomcraft - 2019-01-09 - Update Tracking Link for HERMES
-UPDATE carriers SET carrier_tracking_link = 'https://tracking.hermesworld.com/?TrackID=$1' WHERE carrier_name = 'HERMES';
+UPDATE `carriers` SET `carrier_tracking_link` = 'https://tracking.hermesworld.com/?TrackID=$1' WHERE `carrier_name` = 'HERMES';
 
 #Tomcraft - 2019-01-18 - Added Monaco to EU Zones
-UPDATE zones_to_geo_zones SET geo_zone_id = 5 WHERE zone_country_id = 141;
+UPDATE `zones_to_geo_zones` SET `geo_zone_id` = 5 WHERE `zone_country_id` = 141;
 
 #GTB - 2019-02-05 - fix #1505
-ALTER TABLE orders MODIFY shipping_class VARCHAR(64) NOT NULL;
+ALTER TABLE `orders` MODIFY `shipping_class` VARCHAR(64) NOT NULL;
 
 #GTB - 2019-02-05 - fix #1510
-UPDATE customers_info set customers_info_date_of_last_logon = customers_info_date_account_created WHERE customers_info_date_of_last_logon = '0000-00-00 00:00:00';
-UPDATE customers_info set customers_info_number_of_logons = 1 WHERE customers_info_number_of_logons = 0;
+UPDATE `customers_info` set `customers_info_date_of_last_logon` = `customers_info_date_account_created` WHERE `customers_info_date_of_last_logon` = '0000-00-00 00:00:00';
+UPDATE `customers_info` set `customers_info_number_of_logons` = 1 WHERE `customers_info_number_of_logons` = 0;
 
 #GTB - 2019-04-01 update contact us
-UPDATE content_manager SET content_file = 'contact_us.php' WHERE content_group = '7';
+UPDATE `content_manager` SET `content_file` = 'contact_us.php' WHERE `content_group` = '7';
 
 #GTB - 2019-04-03 - fix #1517
-ALTER TABLE module_backup MODIFY configuration_key VARCHAR(128) NOT NULL;
-ALTER TABLE configuration MODIFY configuration_key VARCHAR(128) NOT NULL;
+ALTER TABLE `module_backup` MODIFY `configuration_key` VARCHAR(128) NOT NULL;
+ALTER TABLE `configuration` MODIFY `configuration_key` VARCHAR(128) NOT NULL;
 
 #GTB - 2019-04-03 - fix #1541
-ALTER TABLE content_manager MODIFY content_text longtext NOT NULL;
+ALTER TABLE `content_manager` MODIFY `content_text` longtext NOT NULL;
 
 #GTB - 2019-04-26 - primary keys
 ALTER TABLE `address_book` MODIFY `address_book_id` INT(11) NOT NULL AUTO_INCREMENT;
@@ -172,18 +172,18 @@ ALTER TABLE `shipping_status` MODIFY `shipping_status_image` VARCHAR(64) NOT NUL
 DELETE FROM `configuration` WHERE configuration_key = 'GOOGLE_RSS_FEED_REFID';
 
 #Tomcraft - 2019-07-22 - removed safeterms
-ALTER TABLE admin_access DROP safeterms;
-DELETE FROM `configuration` WHERE configuration_key LIKE '%SAFETERMS%';
+ALTER TABLE `admin_access` DROP `safeterms`;
+DELETE FROM `configuration` WHERE `configuration_key` LIKE '%SAFETERMS%';
 
 #GTB - 2019-07-22 - delete obsulete entry (fix #1529 & #1438 & #704)
-DELETE FROM `configuration` WHERE configuration_key = 'META_X_DEFAULT';
+DELETE FROM `configuration` WHERE `configuration_key` = 'META_X_DEFAULT';
 
 #Tomcraft - 2019-07-22 - removed easymarketing
-ALTER TABLE admin_access DROP easymarketing;
-DELETE FROM `configuration` WHERE configuration_key LIKE '%EASYMARKETING%';
+ALTER TABLE `admin_access` DROP easymarketing;
+DELETE FROM `configuration` WHERE `configuration_key` LIKE '%EASYMARKETING%';
 
 #Tomcraft - 2019-07-22 - removed findologic
-DELETE FROM `configuration` WHERE configuration_key LIKE '%FINDOLOGIC%';
+DELETE FROM `configuration` WHERE `configuration_key` LIKE '%FINDOLOGIC%';
 
 #GTB - 2019-07-22 - add index - fix #826
 ALTER TABLE `specials` ADD KEY `idx_start_date` (`start_date`);
@@ -194,42 +194,42 @@ ALTER TABLE `products_tags_options` ADD KEY `idx_filter_multi` (`languages_id`,`
 ALTER TABLE `products_tags_values` ADD KEY `idx_filter_multi` (`languages_id`,`filter`,`options_id`,`sort_order`);
 
 #GTB - 2019-10-06 - update content manager
-CREATE TABLE IF NOT EXISTS content_manager_content (
-  content_id INT(11) NOT NULL AUTO_INCREMENT,
-  content_manager_id INT(11) NOT NULL DEFAULT 0,
-  group_ids TEXT,
-  content_name VARCHAR(255) NOT NULL DEFAULT '',
-  content_file VARCHAR(255) NOT NULL,
-  content_link TEXT NOT NULL,
-  languages_id INT(11) NOT NULL,
-  content_read INT(11) NOT NULL DEFAULT 0,
-  file_comment TEXT NOT NULL,
-  PRIMARY KEY (content_id),
-  KEY idx_content_manager_id (content_manager_id)
+CREATE TABLE IF NOT EXISTS `content_manager_content` (
+  `content_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `content_manager_id` INT(11) NOT NULL DEFAULT 0,
+  `group_ids` TEXT,
+  `content_name` VARCHAR(255) NOT NULL DEFAULT '',
+  `content_file` VARCHAR(255) NOT NULL,
+  `content_link` TEXT NOT NULL,
+  `languages_id` INT(11) NOT NULL,
+  `content_read` INT(11) NOT NULL DEFAULT 0,
+  `file_comment` TEXT NOT NULL,
+  PRIMARY KEY (`content_id`),
+  KEY `idx_content_manager_id` (`content_manager_id`)
 );
 
-CREATE TABLE IF NOT EXISTS email_content (
-  content_id INT(11) NOT NULL AUTO_INCREMENT,
-  email_id VARCHAR(64) NOT NULL DEFAULT 0,
-  group_ids TEXT,
-  content_name VARCHAR(255) NOT NULL DEFAULT '',
-  content_file VARCHAR(255) NOT NULL,
-  content_link TEXT NOT NULL,
-  languages_id INT(11) NOT NULL,
-  content_read INT(11) NOT NULL DEFAULT 0,
-  file_comment TEXT NOT NULL,
-  PRIMARY KEY (content_id),
-  KEY idx_email_id (email_id)
+CREATE TABLE IF NOT EXISTS `email_content` (
+  `content_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `email_id` VARCHAR(64) NOT NULL DEFAULT 0,
+  `group_ids` TEXT,
+  `content_name` VARCHAR(255) NOT NULL DEFAULT '',
+  `content_file` VARCHAR(255) NOT NULL,
+  `content_link` TEXT NOT NULL,
+  `languages_id` INT(11) NOT NULL,
+  `content_read` INT(11) NOT NULL DEFAULT 0,
+  `file_comment TEXT NOT NULL,
+  PRIMARY KEY (`content_id`),
+  KEY `idx_email_id` (`email_id`)
 );
 
 #GTB - 2019-10-23 - add products_heading_title
-ALTER TABLE products_description ADD products_heading_title VARCHAR(255) NOT NULL DEFAULT '' AFTER products_name;
+ALTER TABLE `products_description` ADD `products_heading_title` VARCHAR(255) NOT NULL DEFAULT '' AFTER `products_name`;
 
 #GTB - 2019-10-30 - removed blz update
-ALTER TABLE admin_access DROP blz_update;
+ALTER TABLE `admin_access` DROP `blz_update`;
 
 #GTB - 2019-10-30 - removed start
-ALTER TABLE admin_access DROP `start`;
+ALTER TABLE `admin_access` DROP `start`;
 
 #GTB - 2019-11-08 - add unique index
 ALTER TABLE `carriers` ADD UNIQUE KEY `idx_carrier_name` (`carrier_name`);
