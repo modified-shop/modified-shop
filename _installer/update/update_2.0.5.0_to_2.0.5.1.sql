faffb8e77199869e1345b7f2db3b170b75e0d02a
GTB <GTB@modified-shop.org>
2020-03-05 14:33:45 +0000

    use backticks for sql updates

--- _installer/update/update_2.0.5.0_to_2.0.5.1.sql
+++ _installer/update/update_2.0.5.0_to_2.0.5.1.sql
@@ -11,6 +11,6 @@
 INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.5.1');
 
 #GTB - 2019-11-18 - Force Cookie Usage, see: https://trac.modified-shop.org/changeset/12419
-UPDATE configuration SET configuration_value = 'True' WHERE configuration_key = 'SESSION_FORCE_COOKIE_USE';
+UPDATE `configuration` SET `configuration_value` = 'True' WHERE `configuration_key` = 'SESSION_FORCE_COOKIE_USE';
 
 # Keep an empty line at the end of this file for the db_updater to work properly
