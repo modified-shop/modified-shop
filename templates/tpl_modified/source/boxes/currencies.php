7f663c93183a7dd8b7ae04b703e393ae536b9dad
GTB <GTB@modified-shop.org>
2020-03-12 09:20:04 +0000

    fix #1777 - fix currency change on active cache

--- templates/tpl_modified/source/boxes/currencies.php
+++ templates/tpl_modified/source/boxes/currencies.php
@@ -20,7 +20,7 @@
 include(DIR_FS_BOXES_INC . 'smarty_default.php');
 
 // set cache id
-$cache_id = md5($_SESSION['currency'].$_SESSION['language'].xtc_get_all_get_params('currency', 'language'));
+$cache_id = md5(basename($PHP_SELF).$_SESSION['currency'].$_SESSION['language'].xtc_get_all_get_params(array('currency', 'language')));
 
 if (!$box_smarty->is_cached(CURRENT_TEMPLATE.'/boxes/box_currencies.html', $cache_id) || !$cache) {
 
@@ -38,7 +38,7 @@ if (!$box_smarty->is_cached(CURRENT_TEMPLATE.'/boxes/box_currencies.html', $cach
                    . xtc_draw_pull_down_menu('currency', $currencies_array, $_SESSION['currency'], 'onchange="this.form.submit();"')
                    . xtc_hide_session_id();
     
-    parse_str(xtc_get_all_get_params('currency', 'language'), $params_array);
+    parse_str(xtc_get_all_get_params(array('currency', 'language')), $params_array);
     if (is_array($params_array) && count($params_array) > 0) {
       foreach ($params_array as $k => $v) {
         $box_content .= xtc_draw_hidden_field($k, $v);
