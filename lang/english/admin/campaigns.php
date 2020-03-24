b4a57652fa5d9e40497e62317c2f40c45f8fa1b2
Tomcraft1980 <hallo@modified-shop.org>
2020-03-24 11:00:54 +0000

    update campaigns
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12670 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/admin/campaigns.php
+++ lang/english/admin/campaigns.php
@@ -38,7 +38,7 @@ define('TEXT_CAMPAIGNS_REFID', 'Campaigns refID:');
 define('TEXT_DISPLAY_NUMBER_OF_CAMPAIGNS', 'Tracked campaigns:');
 
 define('TEXT_DELETE_INTRO', 'Are you sure you want to delete the campaign?');
-define('TEXT_DELETE_REFFERERS', 'delete Conversions');
+define('TEXT_DELETE_REFFERERS', 'Delete Conversions');
 define('TEXT_DELETE_WARNING_REFFERERS', 'Delete all conversions for this campaign?');
 
 define('TEXT_CAMPAIGNS_ERROR_REFID', 'refID is invalid');
