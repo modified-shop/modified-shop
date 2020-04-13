c9e75e3a276af5ddf7dda0db09a1f8ddf768cc16
GTB <GTB@modified-shop.org>
2020-04-13 10:08:26 +0000

    fix #1799 - fix captcha on login_shop.php

--- includes/login_shop.php
+++ includes/login_shop.php
@@ -20,43 +20,47 @@ include ('includes/application_top.php');
 define('LOGIN_NUM', 2);
 defined('MODULE_CAPTCHA_CODE_LENGTH') or define('MODULE_CAPTCHA_CODE_LENGTH', 6);
 
+// include needed classes
+require_once (DIR_WS_CLASSES.'modified_captcha.php');
+
+$mod_captcha = $_mod_captcha_class::getInstance();
+
+if (!isset($_SESSION['customers_login_tries'])) {
+  $_SESSION['customers_login_tries'] = 0;
+}
+
 if (is_file(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/module/offline/login_shop.html')) {
 
-    // create smarty elements
-    $smarty = new Smarty;
+  // create smarty elements
+  $smarty = new Smarty;
 
-    if (!isset($_SESSION['customers_login_tries'])) {
-      $_SESSION['customers_login_tries'] = 0;
-    }
+  if (isset($_GET['info_message']) && xtc_not_null($_GET['info_message'])) {
+    $messageStack->add('login', get_message('info_message'));
+  }
 
-    if (isset($_GET['info_message']) && xtc_not_null($_GET['info_message'])) {
-      $messageStack->add('login', get_message('info_message'));
-    }
+  if ($messageStack->size('login') > 0) {
+    $smarty->assign('info_message', $messageStack->output('login'));
+  }
 
-    if ($messageStack->size('login') > 0) {
-      $smarty->assign('info_message', $messageStack->output('login'));
-    }
+  $smarty->assign('FORM_ACTION', xtc_draw_form('login', xtc_href_link(FILENAME_LOGIN, xtc_get_all_get_params().'action=process', 'SSL')));
+  $smarty->assign('INPUT_MAIL', xtc_draw_input_field('email_address'));
+  $smarty->assign('INPUT_PASSWORD', xtc_draw_password_field('password'));
+  $smarty->assign('LINK_LOST_PASSWORD', xtc_href_link(FILENAME_PASSWORD_DOUBLE_OPT, '', 'SSL'));
+  $smarty->assign('FORM_END', '</form>');
 
-    $smarty->assign('FORM_ACTION', xtc_draw_form('login', xtc_href_link(FILENAME_LOGIN, xtc_get_all_get_params().'action=process', 'SSL')));
-    $smarty->assign('INPUT_MAIL', xtc_draw_input_field('email_address'));
-    $smarty->assign('INPUT_PASSWORD', xtc_draw_password_field('password'));
-    $smarty->assign('LINK_LOST_PASSWORD', xtc_href_link(FILENAME_PASSWORD_DOUBLE_OPT, '', 'SSL'));
-    $smarty->assign('FORM_END', '</form>');
+  // captcha
+  if ($_SESSION['customers_login_tries'] >= LOGIN_NUM) {
+    $smarty->assign('VVIMG', $mod_captcha->get_image_code());
+    $smarty->assign('INPUT_VVCODE', $mod_captcha->get_input_code());
+  }
 
-    // captcha
-    if ($_SESSION['customers_login_tries'] >= LOGIN_NUM) {
-      $smarty->assign('VVIMG', '<img src="'.xtc_href_link(FILENAME_DISPLAY_VVCODES, '', 'SSL').'" alt="Captcha" />');
-      $smarty->assign('INPUT_CODE', xtc_draw_input_field('vvcode', '', 'size="'.MODULE_CAPTCHA_CODE_LENGTH.'" maxlength="'.MODULE_CAPTCHA_CODE_LENGTH.'"', 'text', false));
-    }
+  $smarty->assign('charset', $_SESSION['language_charset']);
+  $smarty->assign('language', $_SESSION['language']);
+  $smarty->caching = 0;
 
-    $smarty->assign('charset', $_SESSION['language_charset']);
-    $smarty->assign('language', $_SESSION['language']);
-    $smarty->caching = 0;
-
-
-    $smarty->display(CURRENT_TEMPLATE.'/module/offline/login_shop.html');
-    exit();
-} 
+  $smarty->display(CURRENT_TEMPLATE.'/module/offline/login_shop.html');
+  exit();
+}
 
 //Fallback for missing template file
 ?>
@@ -221,10 +225,10 @@ table td {
         if ($_SESSION['customers_login_tries'] >= LOGIN_NUM) {
           ?>
           <tr>
-            <td><span class="fieldtext">Sicherheitscode</span><?php echo '<img src="'.xtc_href_link(FILENAME_DISPLAY_VVCODES, '', 'SSL').'" alt="Captcha" />'; ?></td>
+            <td><span class="fieldtext">Sicherheitscode</span><?php echo $mod_captcha->get_image_code(); ?></td>
           </tr>
           <tr>
-            <td><span class="fieldtext">Sicherheitscode</span><?php echo xtc_draw_input_field('vvcode', '', 'size="'.MODULE_CAPTCHA_CODE_LENGTH.'" maxlength="'.MODULE_CAPTCHA_CODE_LENGTH.'"', 'text', false); ?></td>
+            <td><span class="fieldtext">Sicherheitscode</span><?php echo $mod_captcha->get_input_code(); ?></td>
           </tr>
         <?php
         }
