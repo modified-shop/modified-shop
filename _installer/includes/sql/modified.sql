b038260af6a54637d4fdb483a71d33f397d9c356
Tomcraft <Tomcraft@modified-shop.org>
2020-04-22 20:20:13 +0000

    Changed final revision number for 2.0.5.1

--- _installer/includes/sql/modified.sql
+++ _installer/includes/sql/modified.sql
@@ -2170,7 +2170,7 @@ INSERT INTO currencies VALUES (3,'Schweizer Franken','CHF', 'CHF', '', '.', '', 
 INSERT INTO currencies VALUES (4,'Great Britain Pound','GBP', '', '£', '.', ',', '2','0.8094',NOW(),'0');
 
 # database Version
-INSERT INTO database_version(version) VALUES ('MOD_2.0.5.0');
+INSERT INTO database_version(version) VALUES ('MOD_2.0.5.1');
 
 # languages
 INSERT INTO languages VALUES (1,'English','en','icon.gif','english',2,'iso-8859-15',1,1);
