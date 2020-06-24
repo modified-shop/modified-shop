5ab85f6d32ef6822692547014e2862aa9b38ad72
Markus <Markus@modified-shop.org>
2020-06-24 17:34:43 +0000

    fix #1545 - new cookie consent (remove cc-link)

--- templates/tpl_modified_responsive/javascript/extra/colorbox.js.php
+++ templates/tpl_modified_responsive/javascript/extra/colorbox.js.php
@@ -13,7 +13,7 @@
 <script>
   $(document).ready(function(){
     $(".cbimages").colorbox({rel:'cbimages', scalePhotos:true, maxWidth: "90%", maxHeight: "90%", fixed: true, close: '<i class="fas fa-times"></i>', next: '<i class="fas fa-chevron-right"></i>', previous: '<i class="fas fa-chevron-left"></i>'});
-    $(".iframe, .cc-link").colorbox({iframe:true, width:"780", height:"560", maxWidth: "90%", maxHeight: "90%", fixed: true, close: '<i class="fas fa-times"></i>'});
+    $(".iframe").colorbox({iframe:true, width:"780", height:"560", maxWidth: "90%", maxHeight: "90%", fixed: true, close: '<i class="fas fa-times"></i>'});
     $("#print_order_layer").on('submit', function(event) {
       $.colorbox({iframe:true, width:"780", height:"560", maxWidth: "90%", maxHeight: "90%", close: '<i class="fas fa-times"></i>', href:$(this).attr("action") + '&' + $(this).serialize()});
       return false;
