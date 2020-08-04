ae48c5454c964a2cea3fd161f766676d011ee74b
GTB <GTB@modified-shop.org>
2020-08-04 15:08:03 +0000

    fix #1859 - fix notice for MAX_DISPLAY_PAGE_LINKS in split_page_results.php

--- admin/includes/configuration_limits.php
+++ admin/includes/configuration_limits.php
@@ -23,6 +23,7 @@ $value_limits['MAX_DISPLAY_SPECIAL_PRODUCTS'] = array('min' => 1);
 $value_limits['MAX_DISPLAY_SEARCH_RESULTS'] = array('min' => 1);
 $value_limits['MAX_DISPLAY_ADVANCED_SEARCH_RESULTS'] = array('min' => 1);
 $value_limits['MAX_DISPLAY_PRODUCTS_NEW'] = array('min' => 1);
+$value_limits['MAX_DISPLAY_PAGE_LINKS'] = array('min' => 1);
 
 $value_limits['STORE_DB_SLOW_QUERY_TIME'] = array('min' => 0);
 $value_limits['STORE_PAGE_PARSE_TIME_THRESHOLD'] = array('min' => 0);
