aa1aad124230a941a73ce09e606c7b054973d79d
GTB <GTB@modified-shop.org>
2020-12-14 15:16:05 +0000

    update export module

--- admin/includes/modules/export/products_export.php
+++ admin/includes/modules/export/products_export.php
@@ -12,29 +12,6 @@
 
   defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
 
-  define('MODULE_PRODUCTS_EXPORT_TEXT_DESCRIPTION', 'Artikel Export');
-  define('MODULE_PRODUCTS_EXPORT_TEXT_TITLE', 'Artikel Export - CSV');
-
-  define('MODULE_PRODUCTS_EXPORT_STATUS_DESC','Modulstatus');
-  define('MODULE_PRODUCTS_EXPORT_STATUS_TITLE','Status');
-  define('MODULE_PRODUCTS_EXPORT_FILE_TITLE' , 'Dateiname');
-  define('MODULE_PRODUCTS_EXPORT_FILE_DESC' , 'Geben Sie einen Dateinamen ein.<br />(Verzeichnis export/)');
-
-  define('MODULE_PRODUCTS_EXPORT_CUSTOMERS_STATUS_TITLE','<br><strong>Kundengruppe:</strong>');
-  define('MODULE_PRODUCTS_EXPORT_CUSTOMERS_STATUS_DESC','Bitte w&auml;hlen Sie die Kundengruppe, die Basis f&uuml;r den Exportierten Preis bildet. (Falls Sie keine Kundengruppenpreise haben, w&auml;hlen Sie <i>Gast</i>):</strong>');
-  define('MODULE_PRODUCTS_EXPORT_LANGUAGE_TITLE','<br><strong>Sprache:</strong>');
-  define('MODULE_PRODUCTS_EXPORT_LANGUAGE_DESC','Sprache in der Exportdatei');
-  define('MODULE_PRODUCTS_EXPORT_CURRENCY_TITLE','<br><strong>W&auml;hrung:</strong>');
-  define('MODULE_PRODUCTS_EXPORT_CURRENCY_DESC','W&auml;hrung in der Exportdatei');
-  define('MODULE_PRODUCTS_EXPORT_CAMPAIGNS_TITLE','<br><strong>Kampagnen:</strong>');
-  define('MODULE_PRODUCTS_EXPORT_CAMPAIGNS_DESC','Mit Kampagne zur Nachverfolgung verbinden.');
-  define('MODULE_PRODUCTS_EXPORT_EXPORT_TITLE','<br><strong>Speicherart:</strong>');
-  define('MODULE_PRODUCTS_EXPORT_EXPORT_DESC','Bitte den Sicherungsprozess AUF KEINEN FALL unterbrechen. Dieser kann einige Minuten in Anspruch nehmen.');
-
-  define('TEXT_EXPORT_YES','Nur Herunterladen');
-  define('TEXT_EXPORT_NO','Am Server Speichern');
-
-
   class products_export {
     var $code, $title, $description, $enabled;
 
