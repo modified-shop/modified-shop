43151f7d2459ea854757fc75ff3768f188a4c46e
modGTB <gerhard@modified-shop.org>
2021-03-05 14:04:41 +0000

    fix #1991 - fix filemanager for php8
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13449 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/filemanager/include/php_image_magician.php
+++ admin/includes/modules/filemanager/include/php_image_magician.php
@@ -2788,7 +2788,8 @@ class imageLib {
 	{
 
 		// *** Perform a check or two.
-		if ( ! is_resource($this->imageResized))
+		//if ( ! is_resource($this->imageResized))
+		if (! is_resource($this->imageResized) && ! $this->imageResized instanceof \GdImage)
 		{
 			if ($this->debug)
 			{
@@ -2899,7 +2900,8 @@ class imageLib {
 		#
 	{
 
-		if ( ! is_resource($this->imageResized))
+		//if ( ! is_resource($this->imageResized))
+		if (! is_resource($this->imageResized) && ! $this->imageResized instanceof \GdImage)
 		{
 			if ($this->debug)
 			{
@@ -3469,7 +3471,8 @@ class imageLib {
 		# Notes:
 		#
 	{
-		if ( ! is_resource($img))
+		//if ( ! is_resource($img))
+		if (! is_resource($img) && ! $img instanceof \GdImage)
 		{
 			return false;
 		}
@@ -3718,7 +3721,8 @@ class imageLib {
 
 	public function __destruct()
 	{
-		if (is_resource($this->imageResized))
+		//if (is_resource($this->imageResized))
+		if (is_resource($this->imageResized) || $this->imageResized instanceof \GdImage)
 		{
 			imagedestroy($this->imageResized);
 		}
