bea8c2e01f0a747a2298dd92f90946c54fead489
GTB <GTB@modified-shop.org>
2021-07-27 12:42:17 +0000

    fix #1582 - move customs tariff to system module

---
+++ lang/english/modules/system/products_tariff.php
@@ -0,0 +1,17 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_PRODUCTS_TARIFF_TEXT_TITLE', 'Customs');
+  define('MODULE_PRODUCTS_TARIFF_TEXT_DESCRIPTION', 'Extension for Country of Origin, Tariff Code and Tariff Description.');
+
+  define('MODULE_PRODUCTS_TARIFF_STATUS_TITLE', 'Status');
+  define('MODULE_PRODUCTS_TARIFF_STATUS_DESC', 'Module enabled');
