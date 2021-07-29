ae1cc93571483174c95c14563ef5105f524e0c82
GTB <GTB@modified-shop.org>
2021-07-29 06:54:44 +0000

    update DHL

--- includes/external/dhl/DHLBusinessShipment.php
+++ includes/external/dhl/DHLBusinessShipment.php
@@ -484,7 +484,7 @@
         }
         $ExportDocument->ExportDocPosition[$i]->description = ((isset($this->order->products[$i]['tariff_title']) && $this->order->products[$i]['tariff_title'] != '') ? $this->order->products[$i]['tariff_title'] : $this->order->products[$i]['name']);
         $ExportDocument->ExportDocPosition[$i]->countryCodeOrigin = ((isset($this->order->products[$i]['origin']) && $this->order->products[$i]['origin'] != '') ? $this->order->products[$i]['origin'] : $this->info['country_iso_2']);
-        $ExportDocument->ExportDocPosition[$i]->customsTariffNumber = ((isset($this->order->products[$i]['tariff']) && $this->order->products[$i]['tariff'] != '') ? $this->order->products[$i]['tariff'] : $this->order->products[$i]['model']);
+        $ExportDocument->ExportDocPosition[$i]->customsTariffNumber = ((isset($this->order->products[$i]['tariff']) && $this->order->products[$i]['tariff'] != '') ? $this->order->products[$i]['tariff'] : '');
         $ExportDocument->ExportDocPosition[$i]->amount = $this->order->products[$i]['quantity'];
         $ExportDocument->ExportDocPosition[$i]->netWeightInKG = $weight + (($weight == 0) ? 0.1 : 0);
         $ExportDocument->ExportDocPosition[$i]->customsValue = $this->order->products[$i]['price'];
