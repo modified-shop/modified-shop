0c5fe49349658ea90b59f727e716f2682a215ce5
modGTB <gerhard@modified-shop.org>
2021-08-12 09:01:37 +0000

    make banner slider dynamic
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13679 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/extra/header/header_body/banners.php
+++ includes/extra/header/header_body/banners.php
@@ -33,32 +33,33 @@
     $banners_group_condition = ((isset($banners_group_condition)) ? $banners_group_condition : '');
     
     $groups_query = xtc_db_query("SELECT DISTINCT banners_group 
-                                             FROM " . TABLE_BANNERS . " 
-                                            WHERE banners_group != 'slider'
-                                                  ".$banners_group_condition."
-                                         ORDER BY banners_group");
+                                             FROM ".TABLE_BANNERS." 
+                                            WHERE status = '1' 
+                                                  ".$banners_group_condition);
     while ($groups = xtc_db_fetch_array($groups_query)) {
-      if ($banner = xtc_banner_exists('dynamic', $groups['banners_group'])) {
-        $banner_array = xtc_display_banner('static', $banner);
-
-        if (is_file(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/module/banners.html')) {
-          $banner_smarty->assign('banner_data', $banner_array);
-          $banner_smarty->caching = 0;
-          $banners = $banner_smarty->fetch(CURRENT_TEMPLATE.'/module/banners.html');
-        } else {
-          if (xtc_not_null($banner_array['TEXT'])) {
-            $banners = $banner_array['TEXT'];
-          } elseif (xtc_not_null($banner_array['LINK'])) {      
-            $banners = '<a title="'.$banner_array['TITLE'].'" href="'.$banner_array['LINK'].'"'.$banner_array['TARGET'].'>'.$banner_array['IMAGE_IMG'].'</a>';
-          } else {
-            $banners = $banner_array['IMAGE_IMG'];
-          }
+      if (stripos($groups['banners_group'], 'slider') !== false) {
+        if ($banner = xtc_banner_exists('slider', $groups['banners_group'])) {
+          $smarty->assign(strtoupper($groups['banners_group']), xtc_display_banner('slider', $banner));
         }
-        $smarty->assign(strtoupper($groups['banners_group']), $banners);
-      }
-    }
+      } else {
+        if ($banner = xtc_banner_exists('dynamic', $groups['banners_group'])) {
+          $banner_array = xtc_display_banner('static', $banner);
 
-    if ($banner = xtc_banner_exists('slider', 'slider')) {
-      $smarty->assign('SLIDER', xtc_display_banner('slider', $banner));
+          if (is_file(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/module/banners.html')) {
+            $banner_smarty->assign('banner_data', $banner_array);
+            $banner_smarty->caching = 0;
+            $banners = $banner_smarty->fetch(CURRENT_TEMPLATE.'/module/banners.html');
+          } else {
+            if (xtc_not_null($banner_array['TEXT'])) {
+              $banners = $banner_array['TEXT'];
+            } elseif (xtc_not_null($banner_array['LINK'])) {      
+              $banners = '<a title="'.$banner_array['TITLE'].'" href="'.$banner_array['LINK'].'"'.$banner_array['TARGET'].'>'.$banner_array['IMAGE_IMG'].'</a>';
+            } else {
+              $banners = $banner_array['IMAGE_IMG'];
+            }
+          }
+          $smarty->assign(strtoupper($groups['banners_group']), $banners);
+        }
+      }
     }
   }
