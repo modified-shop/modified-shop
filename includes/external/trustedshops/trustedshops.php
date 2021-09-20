90798178e4965d67b307e3594e3722424da1748e
GTB <GTB@modified-shop.org>
2021-09-20 10:18:59 +0000

    remove not needed code

--- includes/external/trustedshops/trustedshops.php
+++ includes/external/trustedshops/trustedshops.php
@@ -19,32 +19,6 @@
   $default_trustbadge_code = "
 <script type=\"text/javascript\">
   (function () { 
-    var _tsid = '%s'; /* do not change this */
-    _tsConfig = { 
-      'yOffset': '0', /* offset from page bottom */
-      'variant': 'reviews', /* text, default, small, reviews, custom, custom_reviews */
-      'customElementId': '', /* required for variants custom and custom_reviews */
-      'trustcardDirection': '', /* for custom variants: topRight, topLeft, bottomRight, bottomLeft */
-      'customBadgeWidth': '', /* for custom variants: 40 - 90 (in pixels) */
-      'customBadgeHeight': '', /* for custom variants: 40 - 90 (in pixels) */
-      'disableResponsive': 'false', /* deactivate responsive behaviour */
-      'disableTrustbadge': 'false', /* deactivate trustbadge */
-      'trustCardTrigger': 'mouseenter' /* set to 'click' if you want the trustcard to be opened on click instead */
-    };
-    var _ts = document.createElement('script');
-    _ts.type = 'text/javascript'; 
-    _ts.charset = 'utf-8'; 
-    _ts.async = true; 
-    _ts.src = '//widgets.trustedshops.com/js/' + _tsid + '.js'; 
-    var __ts = document.getElementsByTagName('script')[0];
-    __ts.parentNode.insertBefore(_ts, __ts);
-  })();
-</script>
-";
-
-  $default_trustbadge_code = "
-<script type=\"text/javascript\">
-  (function () { 
     var _tsid = '%s'; 
     _tsConfig = { 
       'yOffset': '%s', /* offset from page bottom */
