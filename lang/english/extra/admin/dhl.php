61aa2cb467d6da8db9bb8a9f5e2f4e2727daeb92
modGTB <gerhard@modified-shop.org>
2021-10-29 15:54:25 +0000

    update dhl for preselecting visual age check and ident check
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13782 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/extra/admin/dhl.php
+++ lang/english/extra/admin/dhl.php
@@ -15,10 +15,11 @@
   define('TEXT_DHL_NO', 'No');
   define('TEXT_DHL_NO_STATUS_CHANGE', 'no status change');
   define('TEXT_DHL_ORDER_COMMENT', 'DHL Tracking Information %s');
+  define('TEXT_DHL_AVS_YEAR', '%s years');  
+
   define('TEXT_DHL_WEIGHT', 'Package weight (kg):');
   define('TEXT_DHL_INSURANCE', 'Insurance:');
   define('TEXT_DHL_AVS', 'Visual check of age:');
-  define('TEXT_DHL_AVS_YEAR', '%s years');  
   define('TEXT_DHL_TYPE', 'Product:');
   define('TEXT_DHL_RETOURE', 'Return label:');
   define('TEXT_DHL_BULKY', 'Bulky goods:');
@@ -26,11 +27,12 @@
   define('TEXT_DHL_CODEABLE', 'Codeable:');
   define('TEXT_DHL_IDENT', 'Identcheck:');
   define('TEXT_DHL_DOB', 'Date of Birth:');
+  define('TEXT_DHL_STATUS_UPDATE', 'Notification:');
+  define('TEXT_DHL_NO_NEIGHBOUR', 'No Neighbour Delivery:');
+  define('TEXT_DHL_PARCEL_OUTLET', 'Parcel Outlet Routing:');
+
   define('TEXT_DHL_SHOW_MORE', '[show more]');
   define('TEXT_DHL_SHOW_LESS', '[show less]');
-  define('TEXT_DHL_STATUS_UPDATE', 'Notification:');
-  define('TEXT_DHL_NO_NEIGHBOUR', 'No Neighbour Delivery');
-  define('TEXT_DHL_PARCEL_OUTLET', 'Parcel Outlet Routing');
 
   define('TEXT_DHL_BUTTON_CREATE', 'Create DHL Label');
   define('TEXT_DHL_CREATE_SUCCESS', 'DHL Label was created');
