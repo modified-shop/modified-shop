4aff0d0f4694fc4fd57b1bcba74346bea6d07cb2
GTB <GTB@modified-shop.org>
2021-11-09 11:19:06 +0000

    also check for unreadable ip address for blacklist

--- inc/ip_clearing.inc.php
+++ inc/ip_clearing.inc.php
@@ -10,18 +10,22 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-function ip_clearing($ip_address) {
-  if (SAVE_IP_LOG == 'xxx') {
-    if (strpos($ip_address, '.') !== false) {
-      $ip_address = preg_replace('/(?!\d{1,3}\.)\d/', '', $ip_address);
-      $ip_address .= 'xxx';
-    } else {
-      $ip_address = preg_replace('/(?!\w{1,4}\:)\w/', '', $ip_address);    
-      $ip_address .= 'xxxx';
+  function ip_clearing($ip_address, $type = '') {
+    if ($type == '' && defined('SAVE_IP_LOG')) { 
+      $type = SAVE_IP_LOG;
     }
-  } elseif (SAVE_IP_LOG == 'false') {
-    $ip_address = '';
-  }
   
-  return $ip_address;
-}
+    if ($type == 'xxx') {
+      if (strpos($ip_address, '.') !== false) {
+        $ip_address = preg_replace('/(?!\d{1,3}\.)\d/', '', $ip_address);
+        $ip_address .= 'xxx';
+      } else {
+        $ip_address = preg_replace('/(?!\w{1,4}\:)\w/', '', $ip_address);    
+        $ip_address .= 'xxxx';
+      }
+    } elseif ($type == '') {
+      $ip_address = '';
+    }
+  
+    return $ip_address;
+  }
