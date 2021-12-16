8c8a3fafc51878183fa5efe31e4c546d0a88522a
modGTB <gerhard@modified-shop.org>
2021-12-16 10:48:28 +0000

    add Zettle by PayPal
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13892 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/german/extra/admin/zettle.php
@@ -0,0 +1,22 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+  define('TEXT_MODULE_ZETTLE_HEADING', 'Zettle');
+  define('TEXT_MODULE_ZETTLE_STATUS', 'Artikelstatus:');
+  define('TEXT_MODULE_ZETTLE_STOCK', 'Lagerbestand &uuml;bertragen:');
+  
+  define('TEXT_MODULE_ZETTLE_BULK_HEADING', 'Zettle Bulkimport');
+  define('TEXT_MODULE_ZETTLE_BULK', 'Artikeländerungen vornehmen?');
+  define('TEXT_MODULE_ZETTLE_BULK_STATUS', 'Artikelstatus:');
+  define('TEXT_MODULE_ZETTLE_BULK_STOCK', 'Lagerbestand &uuml;bertragen:');
+  define('TEXT_MODULE_ZETTLE_BULK_ACTIVE', 'nur aktive Artikel?');
+  define('TEXT_MODULE_ZETTLE_BULK_SUB', 'Artikel aus Unterkategorien?');
