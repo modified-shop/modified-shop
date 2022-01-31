81f5c34c53114a520ede213806eb67d425166ae7
GTB <GTB@modified-shop.org>
2022-01-31 09:09:44 +0000

    fix php8.1

--- includes/extra/default/center_modules/customer_greeting.php
+++ includes/extra/default/center_modules/customer_greeting.php
@@ -11,6 +11,8 @@
    ---------------------------------------------------------------------------------------*/
 
   require_once (DIR_FS_INC.'xtc_customer_greeting.inc.php');
-
-  $shop_content_data['content_text'] = str_replace('{$greeting}', xtc_customer_greeting(), $shop_content_data['content_text']);
+  
+  if (!empty($shop_content_data['content_text'])) {
+    $shop_content_data['content_text'] = str_replace('{$greeting}', xtc_customer_greeting(), $shop_content_data['content_text']);
+  }
   $default_smarty->assign('text', $shop_content_data['content_text']);
