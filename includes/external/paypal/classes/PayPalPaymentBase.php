1921cc51f928ce951871fb67221d3aaddb493fa1
Tomcraft <Tomcraft@modified-shop.org>
2022-02-11 21:01:51 +0000

    update paypal - raise module version from 1.70 to 1.71 due to r11044, r14034, r14037, r14045, r14053 & r14056

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.70';
+    $this->paypal_version = '1.71';
 
     $this->admin_access_array = array(
       'paypal_info',
