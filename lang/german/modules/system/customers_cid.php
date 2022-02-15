048d5ed8d30fda66a3861ff1d766e74a8c643856
Tomcraft <Tomcraft@modified-shop.org>
2022-02-15 22:43:52 +0000

    fix #2016 - fix typos

--- lang/german/modules/system/customers_cid.php
+++ lang/german/modules/system/customers_cid.php
@@ -14,7 +14,7 @@ define('MODULE_CUSTOMERS_CID_STATUS_TITLE','Status');
 define('MODULE_CUSTOMERS_CID_STATUS_DESC','Modulstatus');
 define('MODULE_CUSTOMERS_CID_STATUS_INFO','Aktiv');
 define('MODULE_CUSTOMERS_CID_TEXT_TITLE', 'Automatische Kundennummer');
-define('MODULE_CUSTOMERS_CID_TEXT_DESCRIPTION', 'Automtische Kundennummer bei Registrierung eines Kundenkontos');    
+define('MODULE_CUSTOMERS_CID_TEXT_DESCRIPTION', 'Automatische Kundennummer bei Registrierung eines Kundenkontos');    
 define('MODULE_CUSTOMERS_CID_NEXT_TITLE', 'N&auml;chste Kundennummer');
 define('MODULE_CUSTOMERS_CID_NEXT_DESC', 'Bei einer Registrierung wird diese Nummer als n&auml;chstes vergeben.');
 define('MODULE_CUSTOMERS_CID_FORMAT_TITLE', 'Kundennummer Format');
