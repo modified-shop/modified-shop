7ac6d0e0f6ab1e9428c6dabe6b633d2113342b7e
modGTB <gerhard@modified-shop.org>
2022-02-17 10:20:47 +0000

    update payone to replace strftime
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14108 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/payone_logs.php
+++ admin/payone_logs.php
@@ -13,7 +13,7 @@
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(ot_cod_fee.php,v 1.02 2003/02/24); www.oscommerce.com
    (C) 2001 - 2003 TheMedia, Dipl.-Ing Thomas Plänkers ; http://www.themedia.at & http://www.oscommerce.at
-   (c) 2003 XT-Commerce - community made shopping http://www.xt-commerce.com ($Id: ot_cod_fee.php 1003 2005-07-10 18:58:52Z mz $)
+   (c) 2003 XT-Commerce - community made shopping http://www.xt-commerce.com ($Id$)
 
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
@@ -111,7 +111,7 @@ for ($i = 1; $i < 32; $i++) {
 
 $month_array = array();
 for ($i = 1; $i < 13; $i++) {
-  $month_array[] = array('id' => $i, 'text' => decode_utf8(strftime("%B", mktime(0, 0, 0, $i, 1))));
+  $month_array[] = array('id' => $i, 'text' => decode_utf8(date("F", mktime(0, 0, 0, $i, 1))));
 }
 
 $year_array = array();
