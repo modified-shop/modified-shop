35f5056e7f27e4049f7c49bffd633fe68c1ba7e3
GTB <GTB@modified-shop.org>
2022-03-17 14:50:05 +0000

    change minimum php version to 7.4.0 and maximum php version to 8.1.99

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 /* --------------------------------------------------------------
    $Id$
 
@@ -55,8 +55,8 @@ if (file_exists('../includes/local/configure.php')) {
 }
 
 // minimum requirement
-if (version_compare(PHP_VERSION, '5.6', '<')) {
-  die('<h1>Minimum requirement PHP Version 5.6</h1>');
+if (version_compare(PHP_VERSION, '7.4', '<')) {
+  die('<h1>Minimum requirement PHP Version 7.4</h1>');
 }
 
 // default time zone
