d36f75bf22cd73d4e3fb8658cb68159f4abc5e3c
modGTB <gerhard@modified-shop.org>
2022-03-23 09:23:10 +0000

    update klarna - reduce sql
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14169 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/klarna/classes/KlarnaPaymentBase.php
+++ includes/external/klarna/classes/KlarnaPaymentBase.php
@@ -369,10 +369,14 @@ class KlarnaPaymentBase extends KlarnaAutoload {
 
   function check() {
     if (!isset ($this->_check)) {
-      $check_query = xtc_db_query("SELECT configuration_value 
-                                     FROM ".TABLE_CONFIGURATION." 
-                                    WHERE configuration_key = 'MODULE_PAYMENT_".strtoupper($this->code)."_STATUS'");
-      $this->_check = xtc_db_num_rows($check_query);
+      if (defined('MODULE_PAYMENT_'.strtoupper($this->code).'_STATUS')) {
+        $this->_check = true;
+      } else {
+        $check_query = xtc_db_query("SELECT configuration_value 
+                                       FROM ".TABLE_CONFIGURATION." 
+                                      WHERE configuration_key = 'MODULE_PAYMENT_".strtoupper($this->code)."_STATUS'");
+        $this->_check = xtc_db_num_rows($check_query);
+      }
     }
     return $this->_check;
   }
