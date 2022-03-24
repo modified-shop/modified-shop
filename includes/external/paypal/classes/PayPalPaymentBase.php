f37cddc39214afc23f8780ae46bcf9b4ac05a0cf
GTB <GTB@modified-shop.org>
2022-03-24 18:12:23 +0000

    fix #2181 - allowed shipping modules

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -167,7 +167,7 @@ class PayPalPaymentBase extends PayPalCommon {
         $_SESSION['delivery_zone'] = $order->delivery['country']['iso_code_2'];
       }
 
-      if (isset($this->delivery['delivery_zone']) && $this->delivery['delivery_zone'] != '') {
+      if (isset($order->delivery['delivery_zone']) && $order->delivery['delivery_zone'] != '') {
         $_SESSION['delivery_zone'] = $order->delivery['delivery_zone'];
       }
 
@@ -198,7 +198,7 @@ class PayPalPaymentBase extends PayPalCommon {
       $_SESSION['delivery_zone'] = $order->delivery['country']['iso_code_2'];
     }
 
-    if (isset($this->delivery['delivery_zone']) && $this->delivery['delivery_zone'] != '') {
+    if (isset($order->delivery['delivery_zone']) && $order->delivery['delivery_zone'] != '') {
       $_SESSION['delivery_zone'] = $order->delivery['delivery_zone'];
     }
 
