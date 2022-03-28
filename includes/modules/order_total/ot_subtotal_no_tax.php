37e7daa6aaeb26ac8cc130aac508b398ff19dddc
GTB <GTB@modified-shop.org>
2022-03-28 15:08:36 +0000

    update order total modules - reduce sql

--- includes/modules/order_total/ot_subtotal_no_tax.php
+++ includes/modules/order_total/ot_subtotal_no_tax.php
@@ -65,10 +65,13 @@
 
     function check() {
       if (!isset($this->_check)) {
-        $check_query = xtc_db_query("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS'");
-        $this->_check = xtc_db_num_rows($check_query);
+        if (defined('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS')) {
+          $this->_check = true;
+        } else {
+          $check_query = xtc_db_query("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS'");
+          $this->_check = xtc_db_num_rows($check_query);
+        }
       }
-
       return $this->_check;
     }
 
--- includes/modules/order_total/ot_tax.php
+++ includes/modules/order_total/ot_tax.php
@@ -66,10 +66,13 @@
 
     function check() {
       if (!isset($this->_check)) {
-        $check_query = xtc_db_query("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_ORDER_TOTAL_TAX_STATUS'");
-        $this->_check = xtc_db_num_rows($check_query);
+        if (defined('MODULE_ORDER_TOTAL_TAX_STATUS')) {
+          $this->_check = true;
+        } else {
+          $check_query = xtc_db_query("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_ORDER_TOTAL_TAX_STATUS'");
+          $this->_check = xtc_db_num_rows($check_query);
+        }
       }
-
       return $this->_check;
     }
 
