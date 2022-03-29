149f69b09e23faae1bb123684d462da6dae5d423
GTB <GTB@modified-shop.org>
2022-03-29 17:47:15 +0000

    add caching for create account

--- create_account.php
+++ create_account.php
@@ -606,15 +606,16 @@ if (ACCOUNT_STATE == 'true') { //important no $required_zones because of ajax lo
   if ($process == true) {
     if ($entry_state_has_zones == true) {
       $zones_array = array ();
-      $zones_query = xtc_db_query("SELECT zone_id, 
-                                          zone_name 
-                                     FROM ".TABLE_ZONES." 
-                                    WHERE zone_country_id = '".(int)$country."' 
-                                 ORDER BY zone_name");
-      while ($zones_values = xtc_db_fetch_array($zones_query)) {
-        $zones_array[] = array ('id' => $zones_values['zone_id'],
-                                'text' => $zones_values['zone_name']
-                                );
+      $zones_query = xtDBquery("SELECT zone_id, 
+                                       zone_name 
+                                  FROM ".TABLE_ZONES." 
+                                 WHERE zone_country_id = '".(int)$country."' 
+                              ORDER BY zone_name");
+      while ($zones_values = xtc_db_fetch_array($zones_query, true)) {
+        $zones_array[] = array (
+          'id' => $zones_values['zone_id'],
+          'text' => $zones_values['zone_name']
+        );
       }
       $state_input = xtc_draw_pull_down_menuNote(array ('name' => 'state', 'text' => '&nbsp;'. (xtc_not_null(ENTRY_STATE_TEXT) ? '<span class="inputRequirement">'.ENTRY_STATE_TEXT.'</span>' : '')), $zones_array, $zone_id);
     } else {
