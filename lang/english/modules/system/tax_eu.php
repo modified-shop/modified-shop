2f5662a32ce81e18ac4013e674cfa6bafc97c5b9
Tomcraft <Tomcraft@modified-shop.org>
2022-05-02 13:56:20 +0000

    revised r14322 (add EU One-Stop-Shop (OSS) tax rates)

--- lang/english/modules/system/tax_eu.php
+++ lang/english/modules/system/tax_eu.php
@@ -10,8 +10,8 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-define('MODULE_TAX_EU_TEXT_TITLE', 'Tax rates for EU');
-define('MODULE_TAX_EU_TEXT_DESCRIPTION', 'All tax rates of the EU Member States are inserted.');
+define('MODULE_TAX_EU_TEXT_TITLE', 'EU One-Stop-Shop (OSS) tax rates');
+define('MODULE_TAX_EU_TEXT_DESCRIPTION', 'Install tax rates and tax zones for EU One-Stop-Shop (OSS)');
 define('MODULE_TAX_EU_TEXT_DESCRIPTION_PROCESSED', 'All tax rates of the EU Member States have already been inserted.<br/><br/>');
 
 ?>
