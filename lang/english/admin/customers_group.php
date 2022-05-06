aefc09ee88682bb07abeb2d176a2412549e65963
GTB <GTB@modified-shop.org>
2022-05-06 19:21:54 +0000

    update group check

--- lang/english/admin/customers_group.php
+++ lang/english/admin/customers_group.php
@@ -10,8 +10,10 @@ define('HEADING_TITLE', 'Set customer group authorizations');
 
 define('TEXT_CATEGORIES', 'Categories');
 define('TEXT_PRODUCTS', 'Products');
-define('TEXT_CONTENT', 'Site content (Content Manager)');
-define('TEXT_PRODUCTS_CONTENT', 'Products content (Content Manager)');
+define('TEXT_CONTENT_MANAGER', 'Content sites');
+define('TEXT_CONTENT_MANAGER_CONTENT', 'Content attachments');
+define('TEXT_PRODUCTS_CONTENT', 'Products attachments');
+define('TEXT_EMAIL_CONTENT', 'E-Mail attachments');
 define('TEXT_PERMISSION', '<div class="error_message">ATTENTION: All permissions will be overwritten!</div><br/>Authorization');
 define('TEXT_SET', 'give');
 define('TEXT_UNSET', 'revoke');
@@ -23,8 +25,12 @@ define('TEXT_CATEGORIES_SUCCESSFULLY_SET', 'Categorie authorizations successfull
 define('TEXT_CATEGORIES_SUCCESSFULLY_UNSET', 'Categorie authorizations successfully removed.');
 define('TEXT_PRODUCTS_SUCCESSFULLY_SET', 'Product authorizations successfully set.');
 define('TEXT_PRODUCTS_SUCCESSFULLY_UNSET', 'Product authorizations successfully removed.');
-define('TEXT_CONTENT_SUCCESSFULLY_SET', 'Site content authorizations successfully set.');
-define('TEXT_CONTENT_SUCCESSFULLY_UNSET', 'Site content authorizations successfully removed.');
+define('TEXT_CONTENT_MANAGER_SUCCESSFULLY_SET', 'Content sites authorizations successfully set.');
+define('TEXT_CONTENT_MANAGER_SUCCESSFULLY_UNSET', 'Content sites authorizations successfully removed.');
+define('TEXT_CONTENT_MANAGER_CONTENT_SUCCESSFULLY_SET', 'Content attachments authorizations successfully set.');
+define('TEXT_CONTENT_MANAGER_CONTENT_SUCCESSFULLY_UNSET', 'Content attachments authorizations successfully removed.');
 define('TEXT_PRODUCTS_CONTENT_SUCCESSFULLY_SET', 'Product content authorizations successfully set.');
 define('TEXT_PRODUCTS_CONTENT_SUCCESSFULLY_UNSET', 'Product content authorizations successfully removed.');
+define('TEXT_EMAIL_CONTENT_SUCCESSFULLY_SET', 'E-Mail content authorizations successfully set.');
+define('TEXT_EMAIL_CONTENT_SUCCESSFULLY_UNSET', 'E-Mail content authorizations successfully removed.');
 ?>
