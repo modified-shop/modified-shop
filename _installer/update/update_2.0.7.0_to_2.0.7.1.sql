ab9781bf952cfc30efa7c40ae88423df18330fe8
GTB <GTB@modified-shop.org>
2022-06-14 07:51:19 +0000

    Add update_2.0.7.0_to_2.0.7.1.sql

---
+++ _installer/update/update_2.0.7.0_to_2.0.7.1.sql
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------------
+#  $Id: update_2.0.7.0_to_2.0.7.1.sql 14470 2022-05-17 10:47:29Z Tomcraft $
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#GTB - 2022-06-14 - changed database_version
+INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.7.1');
+
+# Keep an empty line at the end of this file for the db_updater to work properly
