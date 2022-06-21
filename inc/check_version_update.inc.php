ab623785d21a1009d241b5655c376403ba2ef892
modGTB <gerhard@modified-shop.org>
2022-06-21 09:52:15 +0000

    update version check
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14555 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/check_version_update.inc.php
+++ inc/check_version_update.inc.php
@@ -10,6 +10,10 @@
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
+  // include needed functions
+  require_once(DIR_FS_INC.'get_database_version.inc.php');
+  
+  // include needed classes
   require_once (DIR_FS_CATALOG.'includes/classes/modified_api.php');
 
   function check_version_update($cache = true) {
@@ -25,26 +29,83 @@
         || $cache === false
         )
     {
+      $contents = array();
+      $contents['total'] = 0;
+      $contents['version_installed'] = $version;
+      
       modified_api::reset();
       $response = modified_api::request('modified/version');
       
-      if (is_array($response) && isset($response['stable'])) {
-        $check_version = $response['stable'];
-        file_put_contents($filename, $check_version);
+      $contents['version'] = $response['stable'];
+      $contents['update'] = version_compare($contents['version'], $contents['version'], '>');
+
+      $dbversion = get_database_version();
+      $response = modified_api::request('modified/version/install/');
+
+      $details = array(
+        'Shop' => array(
+          'shop' => array(
+            'title' => 'Shopversion',
+            'version' => $response['version'],
+            'shop' => PROJECT_VERSION_NO,
+            'link' => version_compare($response['version'], PROJECT_VERSION_NO, '>') ? xtc_href_link(basename($PHP_SELF), 'action=autoupdate') : '',
+            'update' => version_compare($response['version'], PROJECT_VERSION_NO, '>')
+          ),
+          'db' => array(
+            'title' => 'Database',
+            'version' => $response['version'],
+            'shop' => $dbversion['plain'],
+            'link' => '',
+            'update' => version_compare($response['version'], $dbversion['plain'], '>')
+          ),
+        ),
+      );  
+      
+      defined('_VALID_XTC') OR define('_VALID_XTC', true);
+      
+      modified_api::reset();
+      $modules_data = modified_api::request('modified/version/modules/');
+      
+      foreach ($modules_data as $heading => $modules) {
+        foreach ($modules as $module => $data) {
+          $details[$heading][$module] = $data;
+          $data['path'] = str_replace('DIR_ADMIN/', DIR_ADMIN, $data['path']);
+          $data['lang'] = str_replace('DIR_LANG', $_SESSION['language'], $data['lang']);
+  
+          if ($data['lang'] != '' && is_file(DIR_FS_CATALOG.$data['lang'])) {
+            require_once(DIR_FS_CATALOG.$data['lang']);
+          }
+  
+          require_once(DIR_FS_CATALOG.$data['path']);
+          ${$module} = new $data['class']($data['module']);
+  
+          if (array_key_exists($data['variable'], get_object_vars(${$module}))) {
+            $details[$heading][$module]['shop'] = ${$module}->{$data['variable']};
+          }
+  
+          if ($data['regex'] != '') {
+            $details[$heading][$module]['shop'] = preg_replace($data['regex'], '', $details[$heading][$module]['shop']);
+          }
+    
+          $details[$heading][$module]['update'] = version_compare($data['version'], $details[$heading][$module]['shop'], '>');
+        }
       }
+
+      foreach ($details as $heading => $modules) {
+        foreach ($modules as $module => $data) {
+          if ($data['update'] == true) {
+            $contents['total'] ++;
+          }
+        }
+      }
+  
+      $contents['details'] = $details;
+      
+      file_put_contents($filename, json_encode($contents));
+    } else {
+      $contents = json_decode(file_get_contents($filename), true);
     }
-  
-    $check_version = file_get_contents($filename);
-  
-    $update_recomended = false;
-    if (version_compare($check_version, $version, '>')) {
-      $update_recomended = true;
-    }
-  
-    return array(
-      'update' => $update_recomended,
-      'version' => $check_version,
-      'version_installed' => $version,
-    );
+    
+    return $contents;
   }
 ?>
--- lang/english/admin/check_update.php
+++ lang/english/admin/check_update.php
@@ -10,7 +10,7 @@
    based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(customers.php,v 1.13 2002/06/15); www.oscommerce.com
-   (c) 2003	 nextcommerce (customers.php,v 1.8 2003/08/15); www.nextcommerce.org
+   (c) 2003 nextcommerce (customers.php,v 1.8 2003/08/15); www.nextcommerce.org
    (c) 2006 xt:Commerce; www.xt-commerce.com
 
    Released under the GNU General Public License
@@ -19,20 +19,17 @@
 define('HEADING_TITLE', 'Software Update');
 define('HEADING_SUBTITLE', 'Update Check');
 
-define('TEXT_DB_VERSION','Database version:');
-define('TEXT_INFO_UPDATE_RECOMENDED', '<div class="error_message">A new Version is available. You can download from here: <a rel="nofollow noopener" href="https://www.modified-shop.org/download" target="_blank">https://www.modified-shop.org/download</a></div>');
-define('TEXT_INFO_UPDATE_NOT_POSSIBLE', '<div class="error_message">Sorry, no check was possible. Please visit our <a rel="nofollow noopener" target="_blank" href="https://www.modified-shop.org"><b>Website</b></a>.</div>');
-define('TEXT_INFO_UPDATE', '<div class="success_message">Your Version is up to date.</div>');
+define('TEXT_HEADING_STATUS', 'Status');
+define('TEXT_HEADING_INSTALLED', 'Installed');
+define('TEXT_HEADING_AVAILABLE', 'Available');
+define('TEXT_HEADING_ACTION', 'Action');
 
-define('TEXT_HEADING_DEVELOPERS', 'Developers of the modified eCommerce Shopsoftware:');
-define('TEXT_HEADING_FORMER_DEVELOPERS', 'Former Developers of the modified eCommerce Shopsoftware:');
-define('TEXT_HEADING_SUPPORT', 'Please support further development:');
-define('TEXT_HEADING_DONATIONS', 'Donations:');
-define('TEXT_HEADING_BASED_ON', 'The shop software is based on:');
+define('IMAGE_ICON_STATUS_OK','up to date');
+define('IMAGE_ICON_STATUS_UPDATE','update necessary');
 
-define('TEXT_INFO_THANKS', 'We wish to thank all coders and developers involved in this project. In case we forgot to mention anyone in the listing below, please post a message to the <a rel="nofollow noopener" style="font-size: 12px; text-decoration: underline;" href="https://www.modified-shop.org/forum/" target="_blank">forum</a> or to one of the listed developers.');
-define('TEXT_INFO_DISCLAIMER', 'This program is distributed in the hope that it will be useful. Nevertheless, we do not give any warranty that it is without bugs.');
-define('TEXT_INFO_DONATIONS', 'The modified eCommerce Shopsoftware is an open source project, yet a lot of work and spare time go into this project. Therefore we would be grateful if you show your appreciation by donating to the project.');
-define('TEXT_INFO_DONATIONS_IMG_ALT', 'Please support this project with your donation.');
-define('BUTTON_DONATE', '<a rel="nofollow noopener" href="https://www.modified-shop.org/spenden"><img src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" alt="' . TEXT_INFO_DONATIONS_IMG_ALT . '" border="0"></a>');
-?>
+define('BUTTON_MODULE_DOWNLOAD','Module Download');
+define('BUTTON_OFFER','Request update offer');
+define('BUTTON_AUTOUPDATER','Autoupdater');
+
+define('TEXT_INFO_UPDATE_OK','<div class="success_message">Your version is up to date, no update is required.</div>');
+define('TEXT_INFO_UPDATE_NEEDED','<div class="error_message">Your version is no longer up to date, an update is required.</div>');
