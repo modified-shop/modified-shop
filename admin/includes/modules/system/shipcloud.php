6e0dc0d6dd3f6ee09f4a4009b9875bd9671a3742
modGTB <gerhard@modified-shop.org>
2022-06-21 09:56:27 +0000

    update version check - versioning modules (cookie consent, easycredit, haendlerbund, internetmarke, janolaw, protectedshops, semknox, shipcloud, shopvote, sofort & trustedshops)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14556 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/system/shipcloud.php
+++ admin/includes/modules/system/shipcloud.php
@@ -18,6 +18,7 @@ class shipcloud {
   function __construct() {
     global $order;
 
+    $this->version = '1.20';
     $this->code = 'shipcloud';
     $this->title = MODULE_SHIPCLOUD_TEXT_TITLE;
     $this->description = MODULE_SHIPCLOUD_TEXT_DESCRIPTION;
