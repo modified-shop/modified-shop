c68a9fbc9a9898fe59892149ab4a00854192e62b
GTB <GTB@modified-shop.org>
2022-07-10 09:17:14 +0000

    fix #2266 - modify zone_id to not be null

--- _installer/update/update_2.0.7.2_to_2.0.8.0.sql
+++ _installer/update/update_2.0.7.2_to_2.0.8.0.sql
@@ -13,4 +13,7 @@ INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.8.0');
 #GTB - 2022-07-10 - fix wrong function
 UPDATE configuration SET set_function = 'xtc_cfg_select_content(\'REVIEWS_PURCHASED_INFOS\',' WHERE configuration_key = 'REVIEWS_PURCHASED_INFOS';
 
+#GTB - 2022-07-10 - fix #2266 - fix NULL for zone_id
+ALTER TABLE `zones_to_geo_zones` MODIFY `zone_id` INT(11) NOT NULL;
+
 # Keep an empty line at the end of this file for the db_updater to work properly
