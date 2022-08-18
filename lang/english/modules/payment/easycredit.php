f7e7309ed9b1537819dd633845ede5a56d43598f
Tomcraft <Tomcraft@modified-shop.org>
2022-08-18 12:05:05 +0000

    rename module „ratenkauf by easyCredit“ to „easyCredit-Ratenkauf“

--- lang/english/modules/payment/easycredit.php
+++ lang/english/modules/payment/easycredit.php
@@ -12,13 +12,13 @@
 
 
 $lang_array = array(
-  'MODULE_PAYMENT_EASYCREDIT_TEXT_TITLE' => 'ratenkauf by easyCredit',
+  'MODULE_PAYMENT_EASYCREDIT_TEXT_TITLE' => 'easyCredit-Ratenkauf',
   'MODULE_PAYMENT_EASYCREDIT_TEXT_INFO' => '',
   'MODULE_PAYMENT_EASYCREDIT_TEXT_DESCRIPTION' => '',
   'MODULE_PAYMENT_EASYCREDIT_ALLOWED_TITLE' => 'Allowed zones',
   'MODULE_PAYMENT_EASYCREDIT_ALLOWED_DESC' => 'Please enter the zones <b>separately</b> which should be allowed to use this module (e.g. AT,DE (leave empty if you want to allow all zones))',
   'MODULE_PAYMENT_EASYCREDIT_STATUS_TITLE' => 'Enable Module',
-  'MODULE_PAYMENT_EASYCREDIT_STATUS_DESC' => 'Do you want to accept payments with ratenkauf by easyCredit?',
+  'MODULE_PAYMENT_EASYCREDIT_STATUS_DESC' => 'Do you want to accept payments with easyCredit-Ratenkauf?',
   'MODULE_PAYMENT_EASYCREDIT_SORT_ORDER_TITLE' => 'Sort order',
   'MODULE_PAYMENT_EASYCREDIT_SORT_ORDER_DESC' => 'Sort order of display. Lowest is displayed first.',
   'MODULE_PAYMENT_EASYCREDIT_ZONE_TITLE' => 'Payment zone',
@@ -35,12 +35,12 @@ $lang_array = array(
   'MODULE_PAYMENT_EASYCREDIT_LOG_LEVEL_DESC' => 'Specify the log level. Default: "error"',
 
   'MODULE_PAYMENT_EASYCREDIT_TEXT_ERROR_HEADING' => 'Note',
-  'MODULE_PAYMENT_EASYCREDIT_TEXT_ERROR_MESSAGE' => 'The payment with installment plan by easyCredit was cancelled',
-  'MODULE_PAYMENT_EASYCREDIT_TEXT_ERROR_CHECKBOX' => 'Please accept the additional necessary agreements for installment plan by easyCredit',
+  'MODULE_PAYMENT_EASYCREDIT_TEXT_ERROR_MESSAGE' => 'The payment with easyCredit-Ratenkauf was cancelled',
+  'MODULE_PAYMENT_EASYCREDIT_TEXT_ERROR_CHECKBOX' => 'Please accept the additional necessary agreements for easyCredit-Ratenkauf',
   'MODULE_PAYMENT_EASYCREDIT_TEXT_LEGAL' => 'Get pre-contractual information on installment purchase here',
 
   'TEXT_EASYCREDIT_TBAID' => 'Activity identification',
-  'TEXT_EASYCREDIT_RATING_PLAN' => 'Financing from %s in %s installments with installment plan by easyCredit',
+  'TEXT_EASYCREDIT_RATING_PLAN' => 'Financing from %s in %s installments with easyCredit-Ratenkauf',
   'TEXT_EASYCREDIT_RATING_PLAN_SHORT' => 'Financing from %s per month',
   'TEXT_EASYCREDIT_RATING_PLAN_CALC' => 'more information about installment purchase',
   'TEXT_EASYCREDIT_LEGAL' => 'Representative example according to &sect; 6a PAngV',
