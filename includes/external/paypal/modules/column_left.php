7ef19380c17b0256a96c0eb71416192374e154b3
modGTB <gerhard@modified-shop.org>
2023-01-25 16:33:31 +0000

    fix #2256 - update paypal - show menu only if access granted
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14899 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/modules/column_left.php
+++ includes/external/paypal/modules/column_left.php
@@ -12,20 +12,29 @@
 
   defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
   
+  if (!isset($menu_access)) {
+    $menu_access = array();
+    $menu_aoutput = true;
+  }
+
   if ((isset($admin_access['paypal_config']) && $admin_access['paypal_config'] == '1')
       || (isset($admin_access['paypal_profile']) && $admin_access['paypal_profile'] == '1')
       || (isset($admin_access['paypal_webhook']) && $admin_access['paypal_webhook'] == '1')
       || (isset($admin_access['paypal_module']) && $admin_access['paypal_module'] == '1')
       )
   {      
-    echo '<li><a href="javascript:void(0)" class="menuBoxContentLinkSub"> -PayPal</a><ul>';
-    if (isset($admin_access['paypal_info']) && $admin_access['paypal_info'] == '1') echo '<li><a href="' . xtc_href_link('paypal_info.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_INFO . '</a></li>';
-    if (isset($admin_access['paypal_module']) && $admin_access['paypal_module'] == '1') echo '<li><a href="' . xtc_href_link('paypal_module.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_MODULE . '</a></li>';
-    if (isset($admin_access['paypal_config']) && $admin_access['paypal_config'] == '1') echo '<li><a href="' . xtc_href_link('paypal_config.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_CONFIG . '</a></li>';
-    if (isset($admin_access['paypal_profile']) && $admin_access['paypal_profile'] == '1') echo '<li><a href="' . xtc_href_link('paypal_profile.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_PROFILE . '</a></li>';
-    if (isset($admin_access['paypal_webhook']) && $admin_access['paypal_webhook'] == '1') echo '<li><a href="' . xtc_href_link('paypal_webhook.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_WEBHOOK . '</a></li>';
-    echo '  </ul></li>';
+    $menu_access[] = '<li><a href="javascript:void(0)" class="menuBoxContentLinkSub"> -PayPal</a><ul>';
+    if (isset($admin_access['paypal_info']) && $admin_access['paypal_info'] == '1') $menu_access[] = '<li><a href="' . xtc_href_link('paypal_info.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_INFO . '</a></li>';
+    if (isset($admin_access['paypal_module']) && $admin_access['paypal_module'] == '1') $menu_access[] = '<li><a href="' . xtc_href_link('paypal_module.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_MODULE . '</a></li>';
+    if (isset($admin_access['paypal_config']) && $admin_access['paypal_config'] == '1') $menu_access[] = '<li><a href="' . xtc_href_link('paypal_config.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_CONFIG . '</a></li>';
+    if (isset($admin_access['paypal_profile']) && $admin_access['paypal_profile'] == '1') $menu_access[] = '<li><a href="' . xtc_href_link('paypal_profile.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_PROFILE . '</a></li>';
+    if (isset($admin_access['paypal_webhook']) && $admin_access['paypal_webhook'] == '1') $menu_access[] = '<li><a href="' . xtc_href_link('paypal_webhook.php', '') . '" class="menuBoxContentLink"> -' . TEXT_PAYPAL_TAB_WEBHOOK . '</a></li>';
+    $menu_access[] = '</ul></li>';
   } elseif (isset($admin_access['paypal_info']) && $admin_access['paypal_info'] == '1') {
-    echo '<li><a href="' . xtc_href_link('paypal_info.php', '') . '" class="menuBoxContentLink"> -PayPal</a></li>';
+    $menu_access[] = '<li><a href="' . xtc_href_link('paypal_info.php', '') . '" class="menuBoxContentLink"> -PayPal</a></li>';
   }
-?>
+
+  if ($menu_aoutput === true) {
+    echo implode(PHP_EOL, $menu_access);
+    unset($menu_access);
+  }
