62f1e6404bc6306f195fc25108e013fe00acfc7d
modGTB <gerhard@modified-shop.org>
2023-02-02 18:41:40 +0000

    add avalex legals
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14948 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/english/modules/system/avalex.php
@@ -0,0 +1,33 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_AVALEX_TEXT_TITLE', 'Avalex legal texts');
+  define('MODULE_AVALEX_TEXT_DESCRIPTION', 'Legal texts from Avalex');
+  define('MODULE_AVALEX_ACTION_TITLE', 'Import Avalex legal texts');
+  define('MODULE_AVALEX_ACTION_DESC', 'Should the legal texts from Avalex be imported now?');
+  
+  define('MODULE_AVALEX_STATUS_TITLE', 'Status');
+  define('MODULE_AVALEX_STATUS_DESC', 'Module status');
+  define('MODULE_AVALEX_API_TITLE', 'API Key');
+  define('MODULE_AVALEX_API_DESC', 'Avalex API Key');
+  define('MODULE_AVALEX_DOMAIN_TITLE', 'Domain');
+  define('MODULE_AVALEX_DOMAIN_DESC', 'Avalex Domain');
+  define('MODULE_AVALEX_TYPE_TITLE', 'Typ');
+  define('MODULE_AVALEX_TYPE_DESC', 'Choose how the legal texts should be saved.');
+  define('MODULE_AVALEX_TYPE_AGB_TITLE', '<hr noshade>Legal text Terms');
+  define('MODULE_AVALEX_TYPE_AGB_DESC', 'Please specify in which page this legal text is to be inserted automatically.');
+  define('MODULE_AVALEX_TYPE_DSE_TITLE', 'Legal Text Privacy');
+  define('MODULE_AVALEX_TYPE_DSE_DESC', 'Please specify in which page this legal text is to be inserted automatically.');
+  define('MODULE_AVALEX_TYPE_WRB_TITLE', 'Legal text Revocation');
+  define('MODULE_AVALEX_TYPE_WRB_DESC', 'Please specify in which page this legal text is to be inserted automatically.');
+  define('MODULE_AVALEX_TYPE_IMP_TITLE', 'Legal text Legal Notice');
+  define('MODULE_AVALEX_TYPE_IMP_DESC', 'Please specify in which page this legal text is to be inserted automatically.');
