66e909ee59f72a9d4ef3af38f0ae0bda0a127286
modGTB <gerhard@modified-shop.org>
2023-02-14 12:38:11 +0000

    fix #2207 - add sort order for content attachments
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14999 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_products_content.php
+++ admin/includes/modules/new_products_content.php
@@ -33,22 +33,22 @@
       <div class="main" style="margin:15px 5px 2px 5px"><b><?php echo $lng_image . '&nbsp;' . HEADING_PRODUCTS_CONTENT; ?></b></div>
         <table width="100%" cellspacing="0" cellpadding="5" border="0" style="padding: 0 2px 0 4px;">
           <tr class="dataTableHeadingRow">
-            <td class="dataTableHeadingContent" nowrap width="2%" ><?php echo TABLE_HEADING_PRODUCTS_CONTENT_ID; ?></td>
-            <td class="dataTableHeadingContent" nowrap width="2%" >&nbsp;</td>
-            <td class="dataTableHeadingContent" nowrap width="20%" ><?php echo TABLE_HEADING_CONTENT_NAME; ?></td>
-            <td class="dataTableHeadingContent" nowrap width="28%" ><?php echo TABLE_HEADING_CONTENT_FILE; ?></td>
-            <td class="dataTableHeadingContent" nowrap width="1%" ><?php echo TABLE_HEADING_CONTENT_FILESIZE; ?></td>
-            <td class="dataTableHeadingContent" nowrap align="middle" width="15%" ><?php echo TABLE_HEADING_CONTENT_LINK; ?></td>
-            <td class="dataTableHeadingContent" nowrap width="5%" ><?php echo TABLE_HEADING_CONTENT_HITS; ?></td>
-            <td class="dataTableHeadingContent" nowrap width="5%" ><?php echo TABLE_HEADING_CONTENT_SORT; ?></td>
-            <td class="dataTableHeadingContent" nowrap width="22%" ><?php echo TABLE_HEADING_CONTENT_ACTION; ?></td>
+            <td class="dataTableHeadingContent txta-c" nowrap width="2%" ><?php echo TABLE_HEADING_PRODUCTS_CONTENT_ID; ?></td>
+            <td class="dataTableHeadingContent txta-c" nowrap width="2%" >&nbsp;</td>
+            <td class="dataTableHeadingContent txta-c" nowrap width="20%" ><?php echo TABLE_HEADING_CONTENT_NAME; ?></td>
+            <td class="dataTableHeadingContent txta-c" nowrap width="28%" ><?php echo TABLE_HEADING_CONTENT_FILE; ?></td>
+            <td class="dataTableHeadingContent txta-c" nowrap width="1%" ><?php echo TABLE_HEADING_CONTENT_FILESIZE; ?></td>
+            <td class="dataTableHeadingContent txta-c" nowrap align="middle" width="15%" ><?php echo TABLE_HEADING_CONTENT_LINK; ?></td>
+            <td class="dataTableHeadingContent txta-c" nowrap width="5%" ><?php echo TABLE_HEADING_CONTENT_HITS; ?></td>
+            <td class="dataTableHeadingContent txta-r" nowrap width="5%" ><?php echo TABLE_HEADING_CONTENT_SORT; ?></td>
+            <td class="dataTableHeadingContent txta-c" nowrap width="22%" ><?php echo TABLE_HEADING_CONTENT_ACTION; ?></td>
           </tr>
           <?php
           while ($content_array = xtc_db_fetch_array($content_query)) {
             echo '<tr class="dataTableRow" onmouseover="this.className=\'dataTableRowOver\'" onmouseout="this.className=\'dataTableRow\'">' . "\n";
             ?>
-              <td class="dataTableContent" align="left"><?php echo  $content_array['content_id']; ?> </td>
-              <td class="dataTableContent" align="left">
+              <td class="dataTableContent txta-c"><?php echo  $content_array['content_id']; ?> </td>
+              <td class="dataTableContent txta-c">
                 <?php
                   if ($content_array['content_file'] != '') {
                     $filename = DIR_FS_CATALOG . 'media/products/' . $content_array['content_file'];
@@ -59,10 +59,10 @@
                   }
                 ?>
               </td>
-              <td class="dataTableContent" align="left"><?php echo $content_array['content_name']; ?></td>
-              <td class="dataTableContent" align="left"><?php echo $content_array['content_file'] . '&nbsp;'; ?></td>
-              <td class="dataTableContent" align="left"><?php echo xtc_filesize($content_array['content_file']); ?></td>
-              <td class="dataTableContent" align="left" align="middle">
+              <td class="dataTableContent"><?php echo $content_array['content_name']; ?></td>
+              <td class="dataTableContent"><?php echo $content_array['content_file'] . '&nbsp;'; ?></td>
+              <td class="dataTableContent txta-c"><?php echo xtc_filesize($content_array['content_file']); ?></td>
+              <td class="dataTableContent txta-c">
                 <?php
                   if ($content_array['content_link']!='') {
                     echo '<a href="../'.$content_array['content_link'].'" target="new">'.$content_array['content_link'].'</a>';
@@ -70,9 +70,9 @@
                 ?>
                 &nbsp;
               </td>
-              <td class="dataTableContent" align="left"><?php echo $content_array['content_read']; ?></td>
-              <td class="dataTableContent" align="left"><?php echo $content_array['sort_order']; ?></td>
-              <td class="dataTableContent" align="left">
+              <td class="dataTableContent txta-c"><?php echo $content_array['content_read']; ?></td>
+              <td class="dataTableContent txta-r"><?php echo $content_array['sort_order']; ?></td>
+              <td class="dataTableContent txta-l">
                 <a href="<?php echo xtc_href_link(FILENAME_CONTENT_MANAGER, xtc_get_all_get_params(array('action')) . 'last_action='.$_GET['action'].'&special=delete_product&coID='.$content_array['content_id'].'&set=product'); ?>" onclick="return confirmLink('<?php echo CONFIRM_DELETE.'<br/>'.CONTINUE_WITHOUT_SAVE; ?>', '', this)">
                 <?php
                   echo xtc_image(DIR_WS_ICONS.'delete.gif', ICON_DELETE,'','','style="cursor:pointer"').'  '.TEXT_DELETE.'</a>&nbsp;&nbsp;';
