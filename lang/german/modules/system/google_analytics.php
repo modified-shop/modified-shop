80b9e37cfe0851564ae295618d3070363ae5588c
GTB <GTB@modified-shop.org>
2023-04-17 10:42:54 +0000

    update Google Analytics 4 and remove old modules

--- lang/german/modules/system/google_analytics.php
+++ lang/german/modules/system/google_analytics.php
@@ -19,14 +19,14 @@
   define('MODULE_GOOGLE_ANALYTICS_TAG_ID_TITLE', 'Google Analytics TAG ID');
   define('MODULE_GOOGLE_ANALYTICS_TAG_ID_DESC', 'Tragen Sie hier die Google Analytics TAG ID im Format "G-XXXXXXXXXX" ein, die Sie nach einer erfolgreichen Kontoerstellen bekommen haben.');
 
-  define('TRACKING_GOOGLE_ECOMMERCE_TITLE' , 'E-Commerce Tracking');
-  define('TRACKING_GOOGLE_ECOMMERCE_DESC' , 'Setzen Sie E-Commerce Tracking ein, um herauszufinden, was Besucher &uuml;ber Ihre Website oder App kaufen. Zudem erhalten Sie folgende Informationen:<br><br><strong>Produkte:</strong> Gekaufte Produkte, sowie die Mengen und die mit diesen Produkten erzielten Ums&auml;tze<br><strong>Transaktionen:</strong> Informationen zu Umsatz, Steuern, Versandkosten und Mengen f&uuml;r jede Transaktion<br><strong>Zeit bis zum Kauf:</strong> Anzahl von Tagen und Besuchen, beginnend von der aktuellen Kampagne bis zum Abschluss der Transaktion.');
+  define('MODULE_GOOGLE_ANALYTICS_ECOMMERCE_TITLE' , 'E-Commerce Tracking');
+  define('MODULE_GOOGLE_ANALYTICS_ECOMMERCE_DESC' , 'Setzen Sie E-Commerce Tracking ein, um herauszufinden, was Besucher &uuml;ber Ihre Website oder App kaufen. Zudem erhalten Sie folgende Informationen:<br><br><strong>Produkte:</strong> Gekaufte Produkte, sowie die Mengen und die mit diesen Produkten erzielten Ums&auml;tze<br><strong>Transaktionen:</strong> Informationen zu Umsatz, Steuern, Versandkosten und Mengen f&uuml;r jede Transaktion<br><strong>Zeit bis zum Kauf:</strong> Anzahl von Tagen und Besuchen, beginnend von der aktuellen Kampagne bis zum Abschluss der Transaktion.');
 
   define('MODULE_GOOGLE_ANALYTICS_COUNT_ADMIN_TITLE' , 'Seitenaufrufe des Shopbetreibers mitz&auml;hlen');
   define('MODULE_GOOGLE_ANALYTICS_COUNT_ADMIN_DESC' , 'Wird diese Option aktiviert, so werden auch alle Zugriffe des Administrators mitgez&auml;hlt, die (durch die h&auml;ufigeren Zugriffe auf den Shop) die Besucherstatistik verf&auml;lschen k&ouml;nnen.');
 
-  define('TRACKING_GOOGLE_LINKID_TITLE', 'Google Analytics LinkID');
-  define('TRACKING_GOOGLE_LINKID_DESC', 'Sie k&ouml;nnen separate Informationen zu mehreren Links auf einer Seite sehen, die alle dasselbe Ziel haben. Wenn es zum Beispiel zwei Links auf derselben Seite gibt, die beide auf die Seite Kontakt f&uuml;hren, sehen Sie separate Klickinformationen f&uuml;r jeden Link.');
+  define('MODULE_GOOGLE_ANALYTICS_LINKID_TITLE', 'Google Analytics LinkID');
+  define('MODULE_GOOGLE_ANALYTICS_LINKID_DESC', 'Sie k&ouml;nnen separate Informationen zu mehreren Links auf einer Seite sehen, die alle dasselbe Ziel haben. Wenn es zum Beispiel zwei Links auf derselben Seite gibt, die beide auf die Seite Kontakt f&uuml;hren, sehen Sie separate Klickinformationen f&uuml;r jeden Link.');
   
-  define('TRACKING_GOOGLE_DISPLAY_TITLE', 'Google Analytics Displayfeature');
-  define('TRACKING_GOOGLE_DISPLAY_DESC', 'Die Bereiche zu demografischen Merkmalen und zum Interesse enthalten eine &Uuml;bersicht sowie neue Berichte zur Leistung nach Alter, Geschlecht und Interessenkategorien.');
+  define('MODULE_GOOGLE_ANALYTICS_DISPLAY_TITLE', 'Google Analytics Displayfeature');
+  define('MODULE_GOOGLE_ANALYTICS_DISPLAY_DESC', 'Die Bereiche zu demografischen Merkmalen und zum Interesse enthalten eine &Uuml;bersicht sowie neue Berichte zur Leistung nach Alter, Geschlecht und Interessenkategorien.');
