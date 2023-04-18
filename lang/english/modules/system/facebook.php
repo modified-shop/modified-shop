4661ebb6b711e8c9489f9007bf283ae3c239c24d
modGTB <gerhard@modified-shop.org>
2023-04-18 13:13:56 +0000

    revised r15092 (move facebook to system module)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15093 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/modules/system/facebook.php
+++ lang/english/modules/system/facebook.php
@@ -10,14 +10,14 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-  define('MODULE_MATOMO_TEXT_TITLE', 'Facebook Conversion Tracking');
-  define('MODULE_MATOMO_TEXT_DESCRIPTION', 'By activating this option, all purchases will be submitted to Facebook for later evaluation. Before using this option, you need to register at <a href="https://www.facebook.com" target="_blank"><b>Facebook</b></a> and create a new account.');    
+  define('MODULE_FACEBOOK_TEXT_TITLE', 'Facebook Conversion Tracking');
+  define('MODULE_FACEBOOK_TEXT_DESCRIPTION', 'By activating this option, all purchases will be submitted to Facebook for later evaluation. Before using this option, you need to register at <a href="https://www.facebook.com" target="_blank"><b>Facebook</b></a> and create a new account.');    
 
-  define('MODULE_MATOMO_STATUS_TITLE', 'Status');
-  define('MODULE_MATOMO_STATUS_DESC', 'Module status');
+  define('MODULE_FACEBOOK_STATUS_TITLE', 'Status');
+  define('MODULE_FACEBOOK_STATUS_DESC', 'Module status');
 
-  define('MODULE_MATOMO_ID_TITLE' , 'Facebook Conversion ID');
-  define('MODULE_MATOMO_ID_DESC' , 'Your Facebook conversion ID');
+  define('MODULE_FACEBOOK_ID_TITLE' , 'Facebook Conversion ID');
+  define('MODULE_FACEBOOK_ID_DESC' , 'Your Facebook conversion ID');
 
-  define('MODULE_MATOMO_COUNT_ADMIN_TITLE' , 'Count page views of the shop owner');
-  define('MODULE_MATOMO_COUNT_ADMIN_DESC' , 'By activating this option, all page views of the administration usersof the shop owner will be counted as well. This will falsify the visitor stats.');
+  define('MODULE_FACEBOOK_COUNT_ADMIN_TITLE' , 'Count page views of the shop owner');
+  define('MODULE_FACEBOOK_COUNT_ADMIN_DESC' , 'By activating this option, all page views of the administration usersof the shop owner will be counted as well. This will falsify the visitor stats.');
