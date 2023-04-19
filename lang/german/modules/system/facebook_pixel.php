de5cc2c56fd4840d941967c3c177502e20a6873b
modGTB <gerhard@modified-shop.org>
2023-04-19 12:26:17 +0000

    rename module facebook
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15099 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/german/modules/system/facebook_pixel.php
@@ -0,0 +1,23 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_FACEBOOK_PIXEL_TEXT_TITLE', 'Facebook Conversion Tracking');
+  define('MODULE_FACEBOOK_PIXEL_TEXT_DESCRIPTION', 'Wird diese Option aktiviert, so werden alle K&auml;ufe an Facebook &uuml;bermittelt und k&ouml;nnen sp&auml;ter ausgewertet werden. Dazu ist vorher die Anlage eines Kontos bei <a href="https://www.facebook.com" target="_blank"><b>Facebook</b></a> erforderlich.');    
+
+  define('MODULE_FACEBOOK_PIXEL_STATUS_TITLE', 'Status');
+  define('MODULE_FACEBOOK_PIXEL_STATUS_DESC', 'Modulstatus');
+  
+  define('MODULE_FACEBOOK_PIXEL_ID_TITLE' , 'Facebook Conversion ID');
+  define('MODULE_FACEBOOK_PIXEL_ID_DESC' , 'Ihre Facebook Conversion ID');
+
+  define('MODULE_FACEBOOK_PIXEL_COUNT_ADMIN_TITLE' , 'Seitenaufrufe des Shopbetreibers mitz&auml;hlen');
+  define('MODULE_FACEBOOK_PIXEL_COUNT_ADMIN_DESC' , 'Wird diese Option aktiviert, so werden auch alle Zugriffe des Administrators mitgez&auml;hlt, die (durch die h&auml;ufigeren Zugriffe auf den Shop) die Besucherstatistik verf&auml;lschen k&ouml;nnen.');
