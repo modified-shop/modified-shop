9a461008404a1d08a82b9fb7b46e5abfc4369ff4
GTB <GTB@modified-shop.org>
2023-05-11 06:47:55 +0000

    fix #2510 - add description to scheduled tasks

--- lang/german/extra/admin/paypal_tracking.php
+++ lang/german/extra/admin/paypal_tracking.php
@@ -10,4 +10,5 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
-  define('TEXT_INFO_TASKS_PAYPAL_TRACKING', 'PayPal Tracking');
+  define('TEXT_HEADING_TASKS_PAYPAL_TRACKING', 'PayPal Tracking');
+  define('TEXT_INFO_TASKS_PAYPAL_TRACKING', 'Tracking Informationen werden an PayPal &uuml;bertragen');
