517d8568aad6080693390c749576ae107697fe02
GTB <GTB@modified-shop.org>
2023-06-14 10:45:49 +0000

    new error handling

--- lang/english/english.php
+++ lang/english/english.php
@@ -658,11 +658,12 @@ define('PRODUCT_REVIEWS_SUCCESS_WAITING', 'Thank you for your review. This will 
 define('TITLE_PRODUCTS_NEW', 'New products');
 define('TITLE_SPECIALS', 'Special offers');
 
-define('SITEMAP_ERROR_400', 'Error 400: Bad Request.');
-define('SITEMAP_ERROR_401', 'Error 401: Unauthorized.');
-define('SITEMAP_ERROR_403', 'Error 403: Forbidden.');
-define('SITEMAP_ERROR_404', 'Error 404: Not Found!');
-define('SITEMAP_ERROR_500', 'Error 500: Internal Server Error.');
+define('NAVBAR_TITLE_ERROR', 'Error');
+
+define('TEXT_ERROR_HANDLER_400', 'Error 400: Bad Request.');
+define('TEXT_ERROR_HANDLER_401', 'Error 401: Unauthorized.');
+define('TEXT_ERROR_HANDLER_403', 'Error 403: Forbidden.');
+define('TEXT_ERROR_HANDLER_404', 'Error 404: Not Found!');
+define('TEXT_ERROR_HANDLER_500', 'Error 500: Internal Server Error.');
 
 define('ERROR_PRODUCTS_MISSING_CONFIGURATION_NOT_ADDED', 'Some items from your order are no longer available in the desired configuration and have not been placed in the shopping cart.');
-?>
