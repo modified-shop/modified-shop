225237b4a4a1770b670679553808940554ff17b9
GTB <GTB@modified-shop.org>
2023-07-07 05:18:11 +0000

    update boxes for caching in tpl_modified_nova

--- templates/tpl_modified_nova/source/boxes/greeting.php
+++ templates/tpl_modified_nova/source/boxes/greeting.php
@@ -14,18 +14,20 @@
   include(DIR_FS_BOXES_INC . 'smarty_default.php');
 
   // set cache id
-  $cache_id = '';
+  $cache_id = md5('lID:'.$_SESSION['language'].'|csID:'.$_SESSION['customers_status']['customers_status_id']);
 
-  $shop_content_data = $main->getContentData(5, '', '', false, ADD_SELECT_CONTENT);
-  if (!empty($shop_content_data['content_heading'])) {
-    $box_smarty->assign('TITLE', $shop_content_data['content_heading']);
+  if (!$box_smarty->is_cached(CURRENT_TEMPLATE.'/boxes/box_greeting.html', $cache_id) || !$cache) {
+    $shop_content_data = $main->getContentData(5, '', '', false, ADD_SELECT_CONTENT);
+    if (!empty($shop_content_data['content_heading'])) {
+      $box_smarty->assign('TITLE', $shop_content_data['content_heading']);
+    }
+
+    if (!empty($shop_content_data['content_text'])) {
+      $shop_content_data['content_text'] = str_replace('{$greeting}', xtc_customer_greeting(), $shop_content_data['content_text']);
+      $box_smarty->assign('BOX_CONTENT', $shop_content_data['content_text']);
+    }
   }
-
-  if (!empty($shop_content_data['content_text'])) {
-    $shop_content_data['content_text'] = str_replace('{$greeting}', xtc_customer_greeting(), $shop_content_data['content_text']);
-    $box_smarty->assign('BOX_CONTENT', $shop_content_data['content_text']);
-  }
-
+  
   $box_greeting = $box_smarty->fetch(CURRENT_TEMPLATE.'/boxes/box_greeting.html', $cache_id);
 
   $smarty->assign('box_GREETING', $box_greeting);
