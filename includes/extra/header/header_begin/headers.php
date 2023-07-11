41ea401d83553b82002e3a15f4ff5165b8d8cdc5
GTB <GTB@modified-shop.org>
2023-07-11 11:21:20 +0000

    auto_include for security headers

--- includes/extra/header/header_begin/headers.php
+++ includes/extra/header/header_begin/headers.php
@@ -18,3 +18,21 @@
   if (HTTP_SERVER == HTTPS_SERVER && $request_type == 'SSL') {
     header("strict-transport-security: max-age=3600");
   }
+
+  $csp_src_array = array();
+  $csp_src_array['script-src'][] = "'self'";
+  $csp_src_array['script-src'][] = "'unsafe-inline'";
+  $csp_src_array['script-src'][] = '"unsafe-eval"';
+  $csp_src_array['img-src'][] = "'self'";
+  $csp_src_array['style-src'][] = "'self'";
+  $csp_src_array['style-src'][] = "'unsafe-inline'";
+  $csp_src_array['connect-src'][] = "'self'";
+  
+  foreach(auto_include(DIR_FS_CATALOG.'includes/extra/header/header_csp/','php') as $file) require_once ($file);
+  
+  $csp_header = '';
+  foreach ($csp_src_array as $csp_k => $csp) {
+    $csp_header .= $csp_k.' '.implode(' ', $csp).'; ';
+  }  
+   
+  header("Content-Security-Policy: default-src 'self'; " . $csp_header);
