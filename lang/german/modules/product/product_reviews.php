618d13f6f3d35e892d4a516ee2608bdcf4ba3ecb
GTB <GTB@modified-shop.org>
2023-11-09 11:29:08 +0000

    add class extension for reviews in listing

---
+++ lang/german/modules/product/product_reviews.php
@@ -0,0 +1,14 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_PRODUCT_PRODUCT_REVIEWS_TITLE', 'Bewertungen im Listing');
+  define('MODULE_PRODUCT_PRODUCT_REVIEWS_DESCRIPTION', 'Anezige der Anzahl und Durchschnitt der Bewertungen');
