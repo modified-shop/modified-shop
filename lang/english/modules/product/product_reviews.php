8b987c4a1b85db2577a6e0cc174800ffa4b5f3b1
modGTB <gerhard@modified-shop.org>
2023-11-09 11:29:08 +0000

    add class extension for reviews in listing
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15553 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/english/modules/product/product_reviews.php
@@ -0,0 +1,14 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_PRODUCT_PRODUCT_REVIEWS_TITLE', 'Reviews in the listing');
+  define('MODULE_PRODUCT_PRODUCT_REVIEWS_DESCRIPTION', 'Display the number and average of reviews');
