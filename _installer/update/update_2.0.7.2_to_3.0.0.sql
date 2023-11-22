2dba64651cfd9c7cdea6805aec5e7ab73e8e1144
Tomcraft1980 <hallo@modified-shop.org>
2023-11-22 09:16:06 +0000

    Rename update_2.0.7.2_to_2.0.8.0.sql to update_2.0.7.2_to_3.0.0.sql
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15586 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.7.2_to_3.0.0.sql
+++ _installer/update/update_2.0.7.2_to_3.0.0.sql
@@ -8,7 +8,7 @@
 #  -----------------------------------------------------------------------------------------
 
 #GTB - 2022-07-10 - changed database_version
-INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.8.0');
+INSERT INTO `database_version` (`version`) VALUES ('MOD_3.0.0');
 
 #GTB - 2022-07-10 - fix wrong function
 UPDATE configuration SET set_function = 'xtc_cfg_select_content(\'REVIEWS_PURCHASED_INFOS\',' WHERE configuration_key = 'REVIEWS_PURCHASED_INFOS';
