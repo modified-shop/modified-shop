652508d992acf4bbb1b59ed07864247e94795754
modGTB <gerhard@modified-shop.org>
2024-01-08 11:59:17 +0000

    revised r15666 (fix #2663 - add include for magnaCallback.php)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15667 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/extra/application_top/application_top_end/magnalister.php
+++ admin/includes/extra/application_top/application_top_end/magnalister.php
@@ -14,23 +14,24 @@
 
   if (defined('MODULE_MAGNALISTER_STATUS') 
       && MODULE_MAGNALISTER_STATUS == 'True'
+      && !defined('MAGNALISTER_PLUGIN') 
+      && file_exists(DIR_FS_DOCUMENT_ROOT.'magnaCallback.php')
       )
-  {
-    if (!defined('MAGNALISTER_PLUGIN') 
-        && file_exists(DIR_FS_DOCUMENT_ROOT.'magnaCallback.php')
-        )
-    {
-      ob_start();
-      require_once (DIR_FS_DOCUMENT_ROOT.'magnaCallback.php');
-      ob_end_clean();
-    }
-    
+  {    
     switch (basename($PHP_SELF)) {
       case FILENAME_CATEGORIES:
+        ob_start();
+        require_once (DIR_FS_DOCUMENT_ROOT.'magnaCallback.php');
+        ob_end_clean();
+        
         if (function_exists('magnaExecute')) magnaExecute('magnaInventoryUpdate', array('action' => 'inventoryUpdate'), array('inventoryUpdate.php'));
         break;
 
       case FILENAME_ORDERS:
+        ob_start();
+        require_once (DIR_FS_DOCUMENT_ROOT.'magnaCallback.php');
+        ob_end_clean();
+        
         if (function_exists('magnaExecute')) magnaExecute('magnaSubmitOrderStatus', array(), array('order_details.php'));
         break;
     }
