6d36933de851b97d6a6cf6948f36bf55ec3feabb
Tomcraft1980 <hallo@modified-shop.org>
2024-01-16 11:57:13 +0000

    Changed final revision number for 3.0.1
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15695 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -9,13 +9,13 @@
    --------------------------------------------------------------*/
 
 // DB version, used for updates (_installer)
-define('DB_VERSION', 'MOD_3.0.0'); // ToDo before release!
+define('DB_VERSION', 'MOD_3.0.1'); // ToDo before release!
 
 define('PROJECT_MAJOR_VERSION', '3');
-define('PROJECT_MINOR_VERSION', '0.0');
-define('PROJECT_REVISION', '15588'); // ToDo before release!
+define('PROJECT_MINOR_VERSION', '0.1');
+define('PROJECT_REVISION', '15696'); // ToDo before release!
 define('PROJECT_SERVICEPACK_VERSION', ''); // currently not in use since new version numbers
-define('PROJECT_RELEASE_DATE', '2023-11-22'); // ToDo before release!
+define('PROJECT_RELEASE_DATE', '2024-01-16'); // ToDo before release!
 define('MINIMUM_DB_VERSION', '200'); // currently not in use
 
 // Define the project version
