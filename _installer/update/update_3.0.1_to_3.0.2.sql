b4629f33d3f368b7691834bf674fc00e31920de1
Tomcraft1980 <hallo@modified-shop.org>
2024-01-17 11:49:46 +0000

    Changed final revision number for 3.0.2
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15700 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ _installer/update/update_3.0.1_to_3.0.2.sql
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#Tomcraft - 2024-01-17 - changed database_version
+INSERT INTO `database_version` (`version`) VALUES ('MOD_3.0.2');
+
+# Keep an empty line at the end of this file for the db_updater to work properly
