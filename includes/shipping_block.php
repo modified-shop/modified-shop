edfce52aa64beb3f1ef3009795a7c1e8b975fc1a
modGTB <gerhard@modified-shop.org>
2024-02-23 09:57:44 +0000

    revised r11272 (fix #1448) - fix #2683 - check cheapest shipping module
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15744 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/shipping_block.php
+++ includes/shipping_block.php
@@ -46,7 +46,7 @@
               }
               $quotes[$i]['methods'][$j]['price'] = $xtPrice->xtcFormat($xtPrice->xtcAddTax($quotes[$i]['methods'][$j]['cost'], isset($quotes[$i]['tax']) ? $quotes[$i]['tax'] : 0, false), true, 0, true).xtc_draw_hidden_field('shipping', $quotes[$i]['id'].'_'.$quotes[$i]['methods'][$j]['id']);
               if (CHECK_CHEAPEST_SHIPPING_MODUL == 'true') {
-                $quotes[$i]['methods'][$j]['radio_field'] = xtc_draw_radio_field('shipping', $quotes[$i]['id'].'_'.$quotes[$i]['methods'][$j]['id'], $checked, 'id="rd-'.$radio_buttons.'"');
+                $quotes[$i]['methods'][$j]['radio_field'] = xtc_draw_radio_field('shipping', $quotes[$i]['id'].'_'.$quotes[$i]['methods'][$j]['id'], true, 'id="rd-'.$radio_buttons.'"');
               }
             }
             $radio_buttons ++;
