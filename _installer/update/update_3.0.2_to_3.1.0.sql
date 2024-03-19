5009b7d9aefc738c4ed5628117f663c38fb37afe
Tomcraft <Tomcraft@modified-shop.org>
2024-03-19 09:21:04 +0000

    revised r15780 (update paypal - remove paypalsofort)

---
+++ _installer/update/update_3.0.2_to_3.1.0.sql
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#Tomcraft - 2024-03-19 - changed database_version
+INSERT INTO `database_version` (`version`) VALUES ('MOD_3.1.0');
+
+#GTB - 2024-03-19 - remove paypal sofort
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_PAYMENT_PAYPALSOFORT_STATUS';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_PAYMENT_PAYPALSOFORT_ALLOWED';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_PAYMENT_PAYPALSOFORT_ZONE';
+DELETE FROM `configuration` WHERE `configuration_key` = 'MODULE_PAYMENT_PAYPALSOFORT_SORT_ORDER';
+
+# Keep an empty line at the end of this file for the db_updater to work properly
