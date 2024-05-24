14ed81c817c0f5eb4a994f46c15df8e325c09b98
GTB <GTB@modified-shop.org>
2024-05-24 15:15:21 +0000

    fix warning

--- includes/extra/modules/products_attributes_data/10_products_attributes_vpe.php
+++ includes/extra/modules/products_attributes_data/10_products_attributes_vpe.php
@@ -1,27 +1,38 @@
 <?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
 
-//VPE Attribut
-$vpe_price = $full;
-$attr_vpe_name = $attr_vpe_price = '';
-$attr_vpe_value = $attr_vpe_price_plain = 0;
-$vpe_value = $product->data['products_vpe_value'] + $products_options['attributes_vpe_value'];
-switch ($products_options['weight_prefix']) {
-  case '-':
-    $vpe_value = $product->data['products_vpe_value'] - $products_options['attributes_vpe_value'];
-    break;
-  case '=':
-    $vpe_value = $products_options['attributes_vpe_value'];
-    break;
-}
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-if ($products_options['attributes_vpe_value'] != 0.0 && $vpe_price > 0) {
-  $attr_vpe_name = xtc_get_vpe_name($products_options['attributes_vpe_id']);
-  $attr_vpe_value = $vpe_value;
-  $attr_vpe_price_plain = $xtPrice->xtcFormat($vpe_price * (1 / $vpe_value), false);
-  $attr_vpe_price = $xtPrice->xtcFormatCurrency($attr_vpe_price_plain, 0, false);
-}
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
 
-$products_options_data[$row]['DATA'][$col]['VPE_NAME'] = $attr_vpe_name;
-$products_options_data[$row]['DATA'][$col]['VPE_VALUE'] = $attr_vpe_value;
-$products_options_data[$row]['DATA'][$col]['VPE_PRICE'] = $attr_vpe_price;
-$products_options_data[$row]['DATA'][$col]['VPE_PRICE_PLAIN'] = $attr_vpe_price_plain;
+  if ($_SESSION['customers_status']['customers_status_show_price'] == '1') {
+    $vpe_price = $full;
+    $attr_vpe_name = $attr_vpe_price = '';
+    $attr_vpe_value = $attr_vpe_price_plain = 0;
+    $vpe_value = $product->data['products_vpe_value'] + $products_options['attributes_vpe_value'];
+    switch ($products_options['weight_prefix']) {
+      case '-':
+        $vpe_value = $product->data['products_vpe_value'] - $products_options['attributes_vpe_value'];
+        break;
+      case '=':
+        $vpe_value = $products_options['attributes_vpe_value'];
+        break;
+    }
+    
+    if ($products_options['attributes_vpe_value'] != 0.0 && $vpe_price > 0) {
+      $attr_vpe_name = xtc_get_vpe_name($products_options['attributes_vpe_id']);
+      $attr_vpe_value = $vpe_value;
+      $attr_vpe_price_plain = $xtPrice->xtcFormat($vpe_price * (1 / $vpe_value), false);
+      $attr_vpe_price = $xtPrice->xtcFormatCurrency($attr_vpe_price_plain, 0, false);
+    }
+    
+    $products_options_data[$row]['DATA'][$col]['VPE_NAME'] = $attr_vpe_name;
+    $products_options_data[$row]['DATA'][$col]['VPE_VALUE'] = $attr_vpe_value;
+    $products_options_data[$row]['DATA'][$col]['VPE_PRICE'] = $attr_vpe_price;
+    $products_options_data[$row]['DATA'][$col]['VPE_PRICE_PLAIN'] = $attr_vpe_price_plain;
+  }
