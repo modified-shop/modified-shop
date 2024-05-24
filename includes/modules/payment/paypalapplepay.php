aaae360c6a5b852ebab9d30fdf0e097129c9ccd8
GTB <GTB@modified-shop.org>
2024-05-24 09:08:40 +0000

    partly revoke r15888 (update paypal - update applepay to get order via ajax and get association file via api)

--- includes/modules/payment/paypalapplepay.php
+++ includes/modules/payment/paypalapplepay.php
@@ -58,6 +58,22 @@ class paypalapplepay extends PayPalPaymentV2 {
     
     $smarty->clear_assign('CHECKOUT_BUTTON');
     
+    if (!isset($_SESSION['paypal'])
+        || $_SESSION['paypal']['cartID'] != $_SESSION['cart']->cartID
+        || $_SESSION['paypal']['OrderID'] == ''
+        )
+    {
+      $_SESSION['paypal'] = array(
+        'cartID' => $_SESSION['cart']->cartID,
+        'OrderID' => $this->CreateOrder()
+      );
+    }
+    
+    $error_url = xtc_href_link(FILENAME_CHECKOUT_PAYMENT, 'payment_error='.$this->code, 'SSL');
+    if ($_SESSION['paypal']['OrderID'] == '') {
+	    xtc_redirect($error_url);
+    }
+
     $paypal_smarty = new Smarty();
     $paypal_smarty->assign('language', $_SESSION['language']);
     $paypal_smarty->assign('checkout', true);
@@ -70,9 +86,6 @@ class paypalapplepay extends PayPalPaymentV2 {
       $tpl_file = DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/module/paypal/apms.html';
     }
     $process_button = $paypal_smarty->fetch($tpl_file);
-
-    $order_url = DIR_WS_BASE.'ajax.php?ext=create_paypal_order';
-    $error_url = xtc_href_link(FILENAME_CHECKOUT_PAYMENT, 'payment_error='.$this->code, 'SSL');
     
     $paypalscript = '
     if ($("#apms_button3").length) {    
@@ -92,13 +105,7 @@ class paypalapplepay extends PayPalPaymentV2 {
     $process_button .= '
     <script>
       function getAppleOrderID() {
-        var formdata = $("#checkout_confirmation").serializeArray(); 
-        return $.ajax({
-          type: "POST",
-          url: "'.$order_url.'",
-          data: formdata,
-          dataType: "json"
-        });
+        return "'.$_SESSION['paypal']['OrderID'].'";
       }
       
       function getAppleTransactionInfo() {
@@ -117,7 +124,7 @@ class paypalapplepay extends PayPalPaymentV2 {
       }
       
       function redirectAppleError() {
-        window.location.href = "'.$error_url.'";
+        window.location.href = "'.xtc_href_link(FILENAME_CHECKOUT_PAYMENT, 'payment_error='.$this->code, 'SSL').'";
       }
     </script>
     ';
