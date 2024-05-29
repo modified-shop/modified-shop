c54784ed9b65f7b6a2266f9e919398aa0d63a3f5
Tomcraft1980 <hallo@modified-shop.org>
2024-05-29 08:50:26 +0000

    update dhl module version to 1.30 due to r15906
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15913 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/system/dhl_business.php
+++ admin/includes/modules/system/dhl_business.php
@@ -25,7 +25,7 @@
     function __construct() {
       global $order;
       
-      $this->version = '1.29';
+      $this->version = '1.30';
       $this->code = 'dhl_business';
       $this->title = MODULE_DHL_BUSINESS_TEXT_TITLE;
       $this->description = MODULE_DHL_BUSINESS_TEXT_DESCRIPTION.'<br><br><br><b>Version</b><br>'.$this->version;
