ec06b25c6e23160286d10da60e8f83040ef45e0c
GTB <GTB@modified-shop.org>
2024-06-12 10:29:09 +0000

    add compressed stylesheet to popups

--- print_product_info.php
+++ print_product_info.php
@@ -25,9 +25,10 @@ require_once (DIR_FS_INC.'xtc_get_products_mo_images.inc.php');
 // create smarty elements
 $info_smarty = new Smarty();
 $info_smarty->assign('language', $_SESSION['language']);
-$info_smarty->assign('charset', $_SESSION['language_charset'] ); 
 $info_smarty->assign('tpl_path', DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/');
-
+$info_smarty->assign('html_params', ((TEMPLATE_HTML_ENGINE == 'xhtml') ? ' '.HTML_PARAMS : ' lang="'.$_SESSION['language_code'].'"'));
+$info_smarty->assign('doctype', ((TEMPLATE_HTML_ENGINE == 'xhtml') ? ' PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"' : ''));
+$info_smarty->assign('charset', $_SESSION['language_charset']);
 if (DIR_WS_BASE == '') {
   $info_smarty->assign('base_href', (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG);
 }
