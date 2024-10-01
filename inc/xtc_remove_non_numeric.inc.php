85d6b64546f8fa015533affa6d5f51c14b3c4b34
modGTB <gerhard@modified-shop.org>
2024-10-01 17:40:46 +0000

    fix #2843 - change variable type
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16143 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/xtc_remove_non_numeric.inc.php
+++ inc/xtc_remove_non_numeric.inc.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: xtc_remove_non_numeric.inc.php 829 2005-03-12 21:34:16Z mz $   
+   $Id$   
 
    XT-Commerce - community made shopping
    http://www.xt-commerce.com
@@ -14,10 +14,8 @@
 
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
-//BOF - Hetfield - 2009.08.18 - Update function for PHP 5.3
-function xtc_remove_non_numeric($var) {	  
-	  $var=preg_replace('![^0-9]!', '', $var);
-	  return $var;
-}
-//EOF - Hetfield - 2009.08.18 - Update function for PHP 5.3
-?>
+
+  function xtc_remove_non_numeric($var) {	  
+	  $var = preg_replace('![^0-9]!', '', $var);
+	  return (int)$var;
+  }
