c7a66bbdb5be23bca6da3ef4652e60a5b139cffa
GTB <GTB@modified-shop.org>
2025-01-14 17:19:23 +0000

    revised r16219 (fix #2903 - add index to banner history and do not count redirect banner)

--- _installer/update/update_3.1.2_to_3.1.3.sql
+++ _installer/update/update_3.1.2_to_3.1.3.sql
@@ -11,6 +11,7 @@
 INSERT INTO `database_version` (`version`, `date_added`) VALUES ('MOD_3.1.3', NOW());
 
 #GTB - 2024-12-03 - add index 
-ALTER TABLE `banners_history` ADD INDEX `idx_banners_history_date` (`banners_history_date`); 
+ALTER TABLE `banners_history` DROP INDEX `idx_banners_id`, ADD INDEX `idx_banners_id` (`banners_id`, `banners_history_date`);
+UPDATE `banners_history` SET `banners_history_date` = date_format(banners_history_date, '%Y-%m-%d 00:00:00');
 
 # Keep an empty line at the end of this file for the db_updater to work properly
