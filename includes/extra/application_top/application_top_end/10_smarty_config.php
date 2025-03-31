0ec4492c87524285bb821f55dc596b6675f1c417
modGTB <gerhard@modified-shop.org>
2025-03-31 15:46:34 +0000

    fix scheduled tasks ajax call
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16383 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/extra/application_top/application_top_end/10_smarty_config.php
+++ includes/extra/application_top/application_top_end/10_smarty_config.php
@@ -1,11 +1,24 @@
 <?php
-// listing
-if (isset($_GET['show'])) {
-  $_SESSION['listbox'] = (($_GET['show'] == 'box') ? 'true' : 'false');
-}
+/* -----------------------------------------------------------------------------------------
+   $Id$
 
-// load Template config
-if (file_exists(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/config/config.php')) {
-  require_once(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/config/config.php');
-}
-?>
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  // listing
+  if (isset($_GET['show'])) {
+    $_SESSION['listbox'] = (($_GET['show'] == 'box') ? 'true' : 'false');
+  }
+  
+  // load Template config
+  if (defined('CURRENT_TEMPLATE') 
+      && file_exists(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/config/config.php')
+      )
+  {
+    require_once(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/config/config.php');
+  }
