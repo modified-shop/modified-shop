24a7fb0b202e6a9a426bbc8175742c03f2468c7c
GTB <GTB@modified-shop.org>
2025-03-31 15:46:34 +0000

    fix scheduled tasks ajax call

--- includes/extra/application_top/application_top_end/20_smarty.php
+++ includes/extra/application_top/application_top_end/20_smarty.php
@@ -1,7 +1,17 @@
 <?php
-// Smarty Template Engine 
-if (!defined('TEMPLATE_ENGINE')) {
-  define('TEMPLATE_ENGINE','smarty_2');
-}
-require (DIR_FS_EXTERNAL.'smarty/'.TEMPLATE_ENGINE.'/Smarty.class.php');
-?>
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  // Smarty Template Engine 
+  if (!defined('TEMPLATE_ENGINE')) {
+    define('TEMPLATE_ENGINE','smarty_2');
+  }
+  require (DIR_FS_EXTERNAL.'smarty/'.TEMPLATE_ENGINE.'/Smarty.class.php');
