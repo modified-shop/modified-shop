b49a584c04d6513a3398d9e3a9723e743beed6e5
GTB <GTB@modified-shop.org>
2025-05-06 11:30:22 +0000

    update paypal - use separate PAYPAL_COMPANY_LABEL instead of STORE_NAME

--- includes/modules/payment/paypalapplepay.php
+++ includes/modules/payment/paypalapplepay.php
@@ -136,7 +136,7 @@ class paypalapplepay extends PayPalPaymentV2 {
           currencyIsoCode: "'.$order->info['currency'].'",
           totalPrice: "'.sprintf($this->numberFormat, round($total, 2)).'",
           totalPriceStatus: "final",
-          totalLabel: "'.$this->encode_utf8($paypal->get_config('PAYPAL_COMPANY_LABEL')).'",
+          totalLabel: "'.$this->encode_utf8($this->get_config('PAYPAL_COMPANY_LABEL')).'",
         };
       }
       
