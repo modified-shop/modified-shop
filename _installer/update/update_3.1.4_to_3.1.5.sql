d13797a462021bd7fcdee43884172ba15b894878
Tomcraft1980 <hallo@modified-shop.org>
2025-05-28 14:51:28 +0000

    replace mail placeholder
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16470 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_3.1.4_to_3.1.5.sql
+++ _installer/update/update_3.1.4_to_3.1.5.sql
@@ -10,4 +10,7 @@
 #Tomcraft - 2025-05-28 - changed database_version
 INSERT INTO `database_version` (`version`, `date_added`) VALUES ('MOD_3.1.5', NOW());
 
+#Tomcraft - 2025-05-28 - replace mail placeholder
+UPDATE content_manager SET content_text = REPLACE(content_text, '@muster.de', '@example.com');
+
 # Keep an empty line at the end of this file for the db_updater to work properly
