0592ee9f6586a7a0c25234d71ded4ace53e68fb8
Tomcraft <Tomcraft@modified-shop.org>
2025-05-28 07:21:16 +0000

    Add update_3.1.4_to_3.1.5.sql

---
+++ _installer/update/update_3.1.4_to_3.1.5.sql
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#Tomcraft - 2025-05-28 - changed database_version
+INSERT INTO `database_version` (`version`, `date_added`) VALUES ('MOD_3.1.5', NOW());
+
+# Keep an empty line at the end of this file for the db_updater to work properly
