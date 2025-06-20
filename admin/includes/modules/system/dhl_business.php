ab02cef6bd2232e3777e259c7eb469298efac798
GTB <GTB@modified-shop.org>
2025-06-20 09:33:11 +0000

    update dhl - check for date_added column for shop version 2.0.0.0

--- admin/includes/modules/system/dhl_business.php
+++ admin/includes/modules/system/dhl_business.php
@@ -169,6 +169,8 @@
       xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('MODULE_DHL_BUSINESS_BIC', '',  '6', '1', '', now())");
 
       $table_array = array(
+        array('table' => TABLE_ORDERS_TRACKING, 'column' => 'date_added', 'default' => 'DATETIME NOT NULL DEFAULT \'0000-00-00 00:00:00\' AFTER parcel_id'), //check for date added for previous versions
+
         array('table' => TABLE_ORDERS_TRACKING, 'column' => 'external', 'default' => 'INT(1) NOT NULL'),
         array('table' => TABLE_ORDERS_TRACKING, 'column' => 'dhl_label_url', 'default' => 'VARCHAR(512) NOT NULL'),
         array('table' => TABLE_ORDERS_TRACKING, 'column' => 'dhl_export_url', 'default' => 'VARCHAR(512) NOT NULL'),
