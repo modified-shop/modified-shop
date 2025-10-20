5e26d11b3d7fedca6a192d99c4f62fe377858f43
GTB <GTB@modified-shop.org>
2025-10-20 09:44:03 +0000

    fix #2482 - update Guzzle to version 7.10.0

--- includes/external/GuzzleHttp/Promise/functions_include.php
+++ includes/external/GuzzleHttp/Promise/functions_include.php
@@ -1,6 +1,4 @@
 <?php
-
-// Don't redefine the functions if included multiple times.
-if (!function_exists('GuzzleHttp\Promise\promise_for')) {
-    require __DIR__ . '/functions.php';
-}
+/*
+ * empty file for fallback
+ */
