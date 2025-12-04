591f01cbcca2ccb4a0e18c01cb2622e5945e0905
modGTB <gerhard@modified-shop.org>
2025-12-04 08:09:37 +0000

    fix #3053 - new system module for deleting old images
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16657 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/english/modules/system/image_delete.php
@@ -0,0 +1,60 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_IMAGE_DELETE_TEXT_TITLE', 'Delete old images');
+  define('MODULE_IMAGE_DELETE_TEXT_DESCRIPTION', 'All not used Images in these directories<br /><br />
+  /images/product_images/popup_images/<br />
+  /images/product_images/info_images/<br />
+  /images/product_images/midi_images/<br />
+  /images/product_images/thumbnail_images/<br />
+  /images/product_images/mini_images/ <br /> 
+  /images/categories/ <br /> 
+  /images/manufacturers/ <br /> 
+  /images/banner/ <br /> 
+  <br /> getting deleted.<br /> <br />
+  For this purpose, the script uses only a limited number of %s images and calls himself afterwards again.<br /> <br />');
+  
+  define('MODULE_IMAGE_DELETE_STATUS_DESC','Module status');
+  define('MODULE_IMAGE_DELETE_STATUS_TITLE','Status');
+  
+  define('MODULE_IMAGE_DELETE_EXPORT_TEXT','Press Start to start the processing. This process may take some time - do not interrupt in any case!');
+  define('MODULE_IMAGE_DELETE_EXPORT_TEXT_TYPE','<hr noshade><strong>Batch Processing:</strong>');
+  
+  define('MODULE_IMAGE_DELETE_IMAGE_STEP_INFO','%s Images checked: ');
+  define('MODULE_IMAGE_DELETE_IMAGE_STEP_INFO_READY',' - Finished!');
+  define('MODULE_IMAGE_DELETE_TEXT_MAX_IMAGES','<b>Max. Images for each reload:</b>');
+  define('MODULE_IMAGE_DELETE_TEXT_PROCESS_TYPE', '<b>Delete Images:</b>');
+  define('MODULE_IMAGE_DELETE_TEXT_READY', '<div class="ready_info">%s</div>');
+  define('MODULE_IMAGE_DELETE_TEXT_READY_BACK', MODULE_IMAGE_DELETE_TEXT_READY);
+  
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS','Products');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_MINI_IMAGES','Mini Images');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_THUMBNAIL_IMAGES','Thumbnail Images');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_MIDI_IMAGES','Midi Images');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_INFO_IMAGES','Info Images');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_POPUP_IMAGES','Popup Images');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_ORIGINAL_IMAGES','Original Images');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_CATEGORIES','Categories');
+  define('MODULE_IMAGE_DELETE_TEXT_CATEGORIES_IMAGES','Categories Images');
+  define('MODULE_IMAGE_DELETE_TEXT_CATEGORIES_ORIGINAL_IMAGES','Categories Original Images');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_MANUFACTURERS','Manufacturers');
+  define('MODULE_IMAGE_DELETE_TEXT_MANUFACTURERS_IMAGES','Manufacturers Images');
+  define('MODULE_IMAGE_DELETE_TEXT_MANUFACTURERS_ORIGINAL_IMAGES','Manufacturers Original Images');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_BANNER','Banner');
+  define('MODULE_IMAGE_DELETE_TEXT_BANNER_IMAGES','Banner Images');
+  define('MODULE_IMAGE_DELETE_TEXT_BANNER_ORIGINAL_IMAGES','Banner Original Images');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_LOGGING', '<b>Log:</b>');
+  define('MODULE_IMAGE_DELETE_TEXT_LOGFILE','The log file is saved in the folder /log in the root directory and includes the deleted images.');
---
+++ lang/german/modules/system/image_delete.php
@@ -0,0 +1,60 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_IMAGE_DELETE_TEXT_TITLE', 'Alte Bilder l&ouml;schen');
+  define('MODULE_IMAGE_DELETE_TEXT_DESCRIPTION', 'Es werden alle nicht verwendeten Bilder in den Verzeichnissen<br /><br />
+  /images/product_images/popup_images/<br />
+  /images/product_images/info_images/<br />
+  /images/product_images/midi_images/<br />
+  /images/product_images/thumbnail_images/<br />
+  /images/product_images/mini_images/ <br /> 
+  /images/categories/ <br /> 
+  /images/manufacturers/ <br /> 
+  /images/banner/ <br /> 
+  <br /> gel&ouml;scht.<br /> <br />
+  Hierzu verarbeitet das Script nur eine begrenzte Anzahl von %s Bildern und ruft sich danach selbst wieder auf.<br /> <br />');
+  
+  define('MODULE_IMAGE_DELETE_STATUS_DESC','Modulstatus');
+  define('MODULE_IMAGE_DELETE_STATUS_TITLE','Status');
+  
+  define('MODULE_IMAGE_DELETE_EXPORT_TEXT','Dr&uuml;cken Sie Start um die Stapelverarbeitung zu starten. Dieser Vorgang kann einige Zeit dauern - auf keinen Fall unterbrechen!');
+  define('MODULE_IMAGE_DELETE_EXPORT_TEXT_TYPE','<hr noshade><strong>Stapelverarbeitung:</strong>');
+  
+  define('MODULE_IMAGE_DELETE_IMAGE_STEP_INFO','%s Bilder &uuml;berpr&uuml;ft: ');
+  define('MODULE_IMAGE_DELETE_IMAGE_STEP_INFO_READY',' - Fertig!');
+  define('MODULE_IMAGE_DELETE_TEXT_MAX_IMAGES','<b>Bilder pro Seitenreload:</b>');
+  define('MODULE_IMAGE_DELETE_TEXT_PROCESS_TYPE', '<b>Bilder L&ouml;schen:</b>');
+  define('MODULE_IMAGE_DELETE_TEXT_READY', '<div class="ready_info">%s</div>');
+  define('MODULE_IMAGE_DELETE_TEXT_READY_BACK', MODULE_IMAGE_DELETE_TEXT_READY);
+  
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS','Artikel');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_MINI_IMAGES','Mini Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_THUMBNAIL_IMAGES','Thumbnail Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_MIDI_IMAGES','Midi Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_INFO_IMAGES','Info Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_POPUP_IMAGES','Popup Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_PRODUCTS_ORIGINAL_IMAGES','Original Bilder');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_CATEGORIES','Kategorien');
+  define('MODULE_IMAGE_DELETE_TEXT_CATEGORIES_IMAGES','Kategorie Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_CATEGORIES_ORIGINAL_IMAGES','Kategorie Original Bilder');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_MANUFACTURERS','Hersteller');
+  define('MODULE_IMAGE_DELETE_TEXT_MANUFACTURERS_IMAGES','Hersteller Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_MANUFACTURERS_ORIGINAL_IMAGES','Hersteller Original Bilder');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_BANNER','Banner');
+  define('MODULE_IMAGE_DELETE_TEXT_BANNER_IMAGES','Banner Bilder');
+  define('MODULE_IMAGE_DELETE_TEXT_BANNER_ORIGINAL_IMAGES','Banner Original Bilder');
+  
+  define('MODULE_IMAGE_DELETE_TEXT_LOGGING', '<b>Log:</b>');
+  define('MODULE_IMAGE_DELETE_TEXT_LOGFILE','Die Logdatei wird im Ordner /log im Hauptverzeichnis gespeichert und beinhaltet die gel&ouml;schten Bilder.');
