f7a3a82ab857dda54c9bfd41ffa1eadb9e6bb752
GTB <GTB@modified-shop.org>
2025-12-09 10:54:32 +0000

    fix #2221 - check for MAX_DISPLAY_NEW_PRODUCTS

--- includes/modules/new_products.php
+++ includes/modules/new_products.php
@@ -42,48 +42,50 @@ if (!CacheCheck()) {
 }
 
 if (!$module_smarty->is_cached(CURRENT_TEMPLATE.'/module/new_products.html', $cache_id) || !$cache) {
-  $days = '';
-  if (MAX_DISPLAY_NEW_PRODUCTS_DAYS != '0') {
-    $date_new_products = date("Y-m-d", mktime(1, 1, 1, date("m"), date("d") - MAX_DISPLAY_NEW_PRODUCTS_DAYS, date("Y")));
-    $days = " AND p.products_date_added > '".$date_new_products."' ";
-  }
-  $new_products_query = "SELECT ".$product->default_select.",
-                                m.manufacturers_name
-                           FROM ".TABLE_PRODUCTS." p
-                           JOIN ".TABLE_PRODUCTS_DESCRIPTION." pd
-                                ON p.products_id = pd.products_id
-                                   AND pd.products_name <> ''
-                                   AND pd.language_id = '".(int) $_SESSION['languages_id']."'
-                           JOIN ".TABLE_PRODUCTS_TO_CATEGORIES." p2c
-                                ON p.products_id = p2c.products_id
-                           JOIN ".TABLE_CATEGORIES." c
-                                ON p2c.categories_id = c.categories_id
-                                   AND c.categories_status = 1
-                                   AND c.parent_id = '".(int)$new_products_category_id."'
-                                       ".CATEGORIES_CONDITIONS_C."
-                      LEFT JOIN ".TABLE_MANUFACTURERS." m
-                                ON p.manufacturers_id = m.manufacturers_id
-                          WHERE p.products_status = 1
-                                ".PRODUCTS_CONDITIONS_P."
-                                ".$days."
-                       GROUP BY p.products_id
-                       ORDER BY p.products_date_added DESC
-                          LIMIT ".MAX_DISPLAY_NEW_PRODUCTS;
-
-  $module_content = array();
-  $new_products_query = xtDBquery($new_products_query);
-  while ($new_products = xtc_db_fetch_array($new_products_query, true)) {
-    $module_content[] = $product->buildDataArray($new_products);
-  }
-
-  if (count($module_content) > 0) {
-    $module_smarty->assign('module_content', $module_content);
-
-    if (defined('PICTURESET_BOX')) {
-      $module_smarty->assign('pictureset_box', get_pictureset_data(PICTURESET_BOX));
+  if (MAX_DISPLAY_NEW_PRODUCTS != '0') {
+    $days = '';
+    if (MAX_DISPLAY_NEW_PRODUCTS_DAYS != '0') {
+      $date_new_products = date("Y-m-d", mktime(1, 1, 1, date("m"), date("d") - MAX_DISPLAY_NEW_PRODUCTS_DAYS, date("Y")));
+      $days = " AND p.products_date_added > '".$date_new_products."' ";
     }
-    if (defined('PICTURESET_ROW')) {
-      $module_smarty->assign('pictureset_row', get_pictureset_data(PICTURESET_ROW));
+    $new_products_query = "SELECT ".$product->default_select.",
+                                  m.manufacturers_name
+                             FROM ".TABLE_PRODUCTS." p
+                             JOIN ".TABLE_PRODUCTS_DESCRIPTION." pd
+                                  ON p.products_id = pd.products_id
+                                     AND pd.products_name <> ''
+                                     AND pd.language_id = '".(int) $_SESSION['languages_id']."'
+                             JOIN ".TABLE_PRODUCTS_TO_CATEGORIES." p2c
+                                  ON p.products_id = p2c.products_id
+                             JOIN ".TABLE_CATEGORIES." c
+                                  ON p2c.categories_id = c.categories_id
+                                     AND c.categories_status = 1
+                                     AND c.parent_id = '".(int)$new_products_category_id."'
+                                         ".CATEGORIES_CONDITIONS_C."
+                        LEFT JOIN ".TABLE_MANUFACTURERS." m
+                                  ON p.manufacturers_id = m.manufacturers_id
+                            WHERE p.products_status = 1
+                                  ".PRODUCTS_CONDITIONS_P."
+                                  ".$days."
+                         GROUP BY p.products_id
+                         ORDER BY p.products_date_added DESC
+                            LIMIT ".MAX_DISPLAY_NEW_PRODUCTS;
+  
+    $module_content = array();
+    $new_products_query = xtDBquery($new_products_query);
+    while ($new_products = xtc_db_fetch_array($new_products_query, true)) {
+      $module_content[] = $product->buildDataArray($new_products);
+    }
+  
+    if (count($module_content) > 0) {
+      $module_smarty->assign('module_content', $module_content);
+  
+      if (defined('PICTURESET_BOX')) {
+        $module_smarty->assign('pictureset_box', get_pictureset_data(PICTURESET_BOX));
+      }
+      if (defined('PICTURESET_ROW')) {
+        $module_smarty->assign('pictureset_row', get_pictureset_data(PICTURESET_ROW));
+      }
     }
   }
 }
