5d8e95765d10487dce7477318fc7cb5e41de5644
Tomcraft1980 <hallo@modified-shop.org>
2025-12-18 19:37:54 +0000

    Add update_3.2.0_to_3.2.1.sql
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16727 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ _installer/update/update_3.2.0_to_3.2.1.sql
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#Tomcraft - 2025-12-18 - changed database_version
+INSERT INTO `database_version` (`version`, `date_added`) VALUES ('MOD_3.2.1', NOW());
+
+# Keep an empty line at the end of this file for the db_updater to work properly
