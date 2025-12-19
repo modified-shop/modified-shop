e1436d2c5f89f576f622c9df844da28d8d7643d2
Tomcraft <Tomcraft@modified-shop.org>
2025-12-19 21:01:53 +0000

    revised r16731 (update installer to show notes after autoupdate) - add syntax highlighting

--- _installer/includes/header.php
+++ _installer/includes/header.php
@@ -19,8 +19,15 @@
   <title>Installer</title>
   <link rel="stylesheet" type="text/css" href="templates/stylesheet.css?v=<?php echo filemtime(DIR_FS_INSTALLER.'templates/stylesheet.css'); ?>" />
   <link rel="stylesheet" type="text/css" href="templates/css/font-awesome.css?v=<?php echo filemtime(DIR_FS_INSTALLER.'templates/css/font-awesome.css'); ?>">
+  <link rel="stylesheet" type="text/css" href="templates/css/prism-coy.min.css?v=<?php echo filemtime(DIR_FS_INSTALLER.'templates/css/prism-coy.min.css'); ?>" />
   <script src="templates/javascript/jquery.min.js" type="text/javascript"></script>
   <base href="<?php echo xtc_href_link(DIR_WS_INSTALLER); ?>" />
   <link rel="icon" type="image/png" href="<?php echo xtc_href_link(DIR_WS_INSTALLER.'images/favicon.ico', '', 'SSL'); ?>">
+  <script src="templates/javascript/prism.min.js"></script>
+  <script src="templates/javascript/prism-clike.min.js"></script>
+  <script src="templates/javascript/prism-markup-templating.min.js"></script>
+  <script src="templates/javascript/prism-php.min.js"></script>
+  <script src="templates/javascript/prism-css.min.js"></script>
+  <script src="templates/javascript/prism-javascript.min.js"></script>
 </head>
 <body>
