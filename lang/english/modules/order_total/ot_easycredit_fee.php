7a10d8316afc1a52f17bb1f37256c9ae42b61ad4
modGTB <gerhard@modified-shop.org>
2026-01-13 08:21:14 +0000

    update easycredit language
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16765 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/modules/order_total/ot_easycredit_fee.php
+++ lang/english/modules/order_total/ot_easycredit_fee.php
@@ -9,11 +9,11 @@
    -----------------------------------------------------------------------------------------
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-
-  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_TITLE', 'Zinsen');
-  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_TOTAL_TITLE', 'Summe inkl. Zinsen');
-  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_DESCRIPTION', 'Finanzierungskosten f&uuml;r easyCredit-Ratenkauf');
-  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_STATUS_TITLE', 'Finanzierungskosten');
-  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_STATUS_DESC', 'Anzeige der Finanzierungskosten?');
-  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_SORT_ORDER_TITLE', 'Sortierreihenfolge');
-  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_SORT_ORDER_DESC','Anzeigereihenfolge');
+  
+  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_TITLE', 'Interest charges');
+  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_TOTAL_TITLE', 'Total including interest');
+  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_DESCRIPTION', 'Financing costs for easyCredit installment purchase');
+  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_STATUS_TITLE', 'Financing costs');
+  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_STATUS_DESC', 'Display of financing costs?');
+  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_SORT_ORDER_TITLE', 'Sorting order');
+  define('MODULE_ORDER_TOTAL_EASYCREDIT_FEE_SORT_ORDER_DESC','Display order');
