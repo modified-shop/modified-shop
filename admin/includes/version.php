0d87eac30ebd8da54af60437018d8ef00298cb2c
Tomcraft <Tomcraft@modified-shop.org>
2026-01-29 17:21:00 +0000

    Changed final revision number for 3.3.0

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -9,13 +9,13 @@
    --------------------------------------------------------------*/
 
 // DB version, used for updates (_installer)
-define('DB_VERSION', 'MOD_3.2.1'); // ToDo before release!
+define('DB_VERSION', 'MOD_3.3.0'); // ToDo before release!
 
 define('PROJECT_MAJOR_VERSION', '3');
-define('PROJECT_MINOR_VERSION', '2.1');
-define('PROJECT_REVISION', '16738'); // ToDo before release!
+define('PROJECT_MINOR_VERSION', '3.0');
+define('PROJECT_REVISION', '16825'); // ToDo before release!
 define('PROJECT_SERVICEPACK_VERSION', ''); // currently not in use since new version numbers
-define('PROJECT_RELEASE_DATE', '2025-12-19'); // ToDo before release!
+define('PROJECT_RELEASE_DATE', '2026-01-29'); // ToDo before release!
 define('MINIMUM_DB_VERSION', '200'); // currently not in use
 
 // Define the project version
