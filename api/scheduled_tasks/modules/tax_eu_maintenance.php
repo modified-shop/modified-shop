4f05e97b64ae87d1d11608315c59baed3b6434bf
noRiddle <79751926+noRiddle@users.noreply.github.com>
2026-07-10 18:48:39 +0200

    Add scheduled task for tax_eu maintenance
    This file contains a scheduled task for the tax_eu system module, including a function to update tax_eu if the module is enabled.

---
+++ api/scheduled_tasks/modules/tax_eu_maintenance.php
@@ -0,0 +1,32 @@
+<?php
+/***************************************************************
+* file: tax_eu_maintenance.php
+* path: /api/scheduled_tasks/modules/
+* use: scheduled task for system module tax_eu
+*
+* © copyright noRiddle, 07-2026
+             ____
+            |    |       |     | |
+  __   ___  |____/ °  ___|  ___| |  ___
+|/  | |   | |   \  | |   | |   | | |___|
+|   n |___o |    R i |___d |__ d l |__e
+
+***************************************************************/
+
+function cron_tax_eu_maintenance() {
+  if(defined('MODULE_TAX_EU_STATUS') && MODULE_TAX_EU_STATUS == 'true') {
+    if(class_exists('tax_eu') === false) {
+      require_once(DIR_FS_CATALOG.DIR_ADMIN.'includes/modules/system/tax_eu.php');
+    }
+
+    //BOC avoid undefined constants in tax_eu module
+    defined('FILENAME_MODULE_EXPORT') OR define('FILENAME_MODULE_EXPORT', '');
+    defined('BUTTON_UPDATE') OR define('BUTTON_UPDATE', '');
+    //EOC avoid undefined constants in tax_eu module
+
+    $st_tax_eu = new tax_eu();
+    $st_tax_eu->update();
+  }
+
+  return true;
+}
