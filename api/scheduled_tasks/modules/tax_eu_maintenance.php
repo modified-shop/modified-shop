97b98402dfe336b65bc71739877d136e4d58c6ad
noRiddle <79751926+noRiddle@users.noreply.github.com>
2026-07-10 19:21:24 +0200

    added missing closing brackets in tax_eu_maintenance.php

--- api/scheduled_tasks/modules/tax_eu_maintenance.php
+++ api/scheduled_tasks/modules/tax_eu_maintenance.php
@@ -16,11 +16,11 @@ function cron_tax_eu_maintenance() {
     //BOC avoid undefined constants in tax_eu module
     defined('FILENAME_MODULE_EXPORT') OR define('FILENAME_MODULE_EXPORT', '');
     defined('BUTTON_UPDATE') OR define('BUTTON_UPDATE', '');
-    defined('TABLE_COUNTRIES' OR define('TABLE_COUNTRIES', 'countries');
-    defined('TABLE_GEO_ZONES' OR define('TABLE_GEO_ZONES', 'geo_zones');
-    defined('TABLE_ZONES_TO_GEO_ZONES' OR define('TABLE_ZONES_TO_GEO_ZONES', 'zones_to_geo_zones');
-    defined('TABLE_TAX_RATES' OR define('TABLE_TAX_RATES', 'tax_rates');
-    defined('TABLE_CONFIGURATION' OR define('TABLE_CONFIGURATION', 'configuration');
+    defined('TABLE_COUNTRIES') OR define('TABLE_COUNTRIES', 'countries');
+    defined('TABLE_GEO_ZONES') OR define('TABLE_GEO_ZONES', 'geo_zones');
+    defined('TABLE_ZONES_TO_GEO_ZONES') OR define('TABLE_ZONES_TO_GEO_ZONES', 'zones_to_geo_zones');
+    defined('TABLE_TAX_RATES') OR define('TABLE_TAX_RATES', 'tax_rates');
+    defined('TABLE_CONFIGURATION') OR define('TABLE_CONFIGURATION', 'configuration');
     //EOC avoid undefined constants in tax_eu module
 
     $st_tax_eu = new tax_eu();
