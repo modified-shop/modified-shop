c9d3a6c5fe38dcaded08138189349762852bcfef
GTB <gerhard@modified-shop.org>
2026-07-29 16:50:21 +0200

    Align unit price rounding across displays

--- includes/classes/main.php
+++ includes/classes/main.php
@@ -331,9 +331,8 @@ class main {
       require_once (DIR_FS_INC.'xtc_get_vpe_name.inc.php');
       
       $this->vpe_name = xtc_get_vpe_name($products['products_vpe']);
-      $decimal_places = $xtPrice->currencies[$xtPrice->actualCurr]['decimal_places'];
-      $rounded_price = round((float)$price, $decimal_places);
-      $vpeText = $xtPrice->xtcFormatCurrency(($rounded_price * (1 / $products['products_vpe_value'])), 0, true).TXT_PER.$this->vpe_name;
+      $price = round((float)$price, $xtPrice->get_decimal_places());
+      $vpeText = $xtPrice->xtcFormatCurrency(($price * (1 / $products['products_vpe_value'])), 0, true).TXT_PER.$this->vpe_name;
     }
     
     //new module support
