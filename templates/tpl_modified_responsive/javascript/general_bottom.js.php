c221bb31ca2f06bbeb25dbf79cbe453cbbab8be2
Robin Wieschendorf <mail@robinwieschendorf.de>
2026-07-31 22:43:16 +0200

    refactor: update script_min path for consistency across templates

--- templates/tpl_modified_responsive/javascript/general_bottom.js.php
+++ templates/tpl_modified_responsive/javascript/general_bottom.js.php
@@ -29,7 +29,7 @@ $script_array = array(
   'templates/' . Template::resolve('javascript/jquery.sumoselect.min.js'),
   'templates/' . Template::resolve('javascript/jquery.sidebar.min.js'),
 );
-$script_min = DIR_TMPL_JS.'tpl_plugins.min.js';
+$script_min = 'templates/'.CURRENT_TEMPLATE.'/javascript/tpl_plugins.min.js';
   
 $this_f_time = filemtime(Template::path('javascript/general_bottom.js.php'));
   
