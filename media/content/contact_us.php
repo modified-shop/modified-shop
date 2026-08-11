25133020e84b04f8d184d27d5c55727724ebc08e
Robin Wieschendorf <mail@robinwieschendorf.de>
2026-08-11 09:55:46 +0200

    fix: correct logo_path assignment

--- media/content/contact_us.php
+++ media/content/contact_us.php
@@ -121,7 +121,7 @@
         // Legacy compatibility for custom templates.
         // New Smarty templates must use {template_asset ...} for concrete template assets. Not tpl_path or logo_path.
         $smarty->assign('tpl_path', Template::absoluteUrl(''));
-        $smarty->assign('logo_path', Template::absoluteUrl('') . '/img/');
+        $smarty->assign('logo_path', Template::absoluteUrl('') . 'img/');
 
         $smarty->assign('NAME', $name);
         $smarty->assign('EMAIL', $email);
