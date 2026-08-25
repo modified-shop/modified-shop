0ac0687b2e661c0d1138784e4c51b2b59722c856
Robin Wieschendorf <mail@robinwieschendorf.de>
2026-08-25 08:48:16 +0200

    feat: add module template resolver and LogicalTemplatePath

---
+++ includes/classes/storefront/src/Ui/Module/Template/Exception/CurrentModuleTemplateException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modified\Storefront\Ui\Module\Template\Exception;
+
+use RuntimeException;
+
+final class CurrentModuleTemplateException extends RuntimeException
+{
+}
---
+++ includes/classes/storefront/src/Ui/Module/Template/Exception/ModuleTemplateNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modified\Storefront\Ui\Module\Template\Exception;
+
+use RuntimeException;
+
+final class ModuleTemplateNotFoundException extends RuntimeException
+{
+}
