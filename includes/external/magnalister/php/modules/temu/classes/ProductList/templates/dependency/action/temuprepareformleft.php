be67401ae520cebe346d78d0cd64032d678c301e
Tomcraft1980 <hallo@modified-shop.org>
2026-09-08 13:15:56 +0200

    Updated magnalister build version 9000 to latest modified-shop compatible build version 9006

--- includes/external/magnalister/php/modules/temu/classes/ProductList/templates/dependency/action/temuprepareformleft.php
+++ includes/external/magnalister/php/modules/temu/classes/ProductList/templates/dependency/action/temuprepareformleft.php
@@ -2,8 +2,11 @@
 /**
  * Temu prepare form left - Unprepare button
  */
+/* @var $this MLProductList */
+/* @var $oObject MLProductListDependency */
+class_exists('MLProductList') or die();
 ?>
 <form action="<?php echo $this->getUrl(false, false, false); ?>" method="post">
     <input type="submit" class="ml-button" value="<?php echo ML_EBAY_BUTTON_UNPREPARE; ?>"
-           id="unprepare" name="unprepare"/>
+           id="unprepare" name="action[<?php echo $oObject->getIdent(); ?>][unprepare]"/>
 </form>
