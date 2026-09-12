a2250715c56af3bbf8c58a8f2c9543c10e8bd70c
Tomcraft1980 <hallo@modified-shop.org>
2026-09-12 22:34:57 +0200

    Bump GARAN module and renderer versions to 1.02

--- admin/includes/modules/system/guarantee_labels.php
+++ admin/includes/modules/system/guarantee_labels.php
@@ -33,7 +33,7 @@
     var $properties;
 
     function __construct() {
-      $this->version = '1.01';
+      $this->version = '1.02';
       $this->code = 'guarantee_labels';
       $this->title = MODULE_GUARANTEE_LABELS_TEXT_TITLE;
       $this->description = MODULE_GUARANTEE_LABELS_TEXT_DESCRIPTION;
